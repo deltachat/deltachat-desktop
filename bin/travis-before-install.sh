bbdec844ff9a92d243e33bbd7704616621ee26c8
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-09-18 12:32:46 +0200

    Finalize mac on travis (#47)
    * Upgrade deltachat-node to ^0.13.1
    
    * npm fixed indentation of package.json
    
    * Update travis-before-install.sh to cover build dependencies for mac case

--- bin/travis-before-install.sh
+++ bin/travis-before-install.sh
@@ -2,20 +2,25 @@
 
 set -e
 
-# Set python 3.5 as preferred python3 interpreter (needed for pip3)
-sudo ln -sf /usr/bin/python3.5 /usr/bin/python3
-python3 --version
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+   # Set python 3.5 as preferred python3 interpreter (needed for pip3)
+   sudo ln -sf /usr/bin/python3.5 /usr/bin/python3
+   python3 --version
 
-# Install meson
-sudo pip3 install meson
+   # Install meson
+   sudo pip3 install meson
 
-# Compile and install libsasl2-dev
-wget http://http.debian.net/debian/pool/main/c/cyrus-sasl2/cyrus-sasl2_2.1.27~101-g0780600+dfsg.orig.tar.xz
-tar xfv cyrus-sasl2_2.1.27~101-g0780600+dfsg.orig.tar.xz
-pushd cyrus-sasl2-2.1.27~101-g0780600+dfsg.orig && ./autogen.sh && make && sudo make install && popd
-rm -rf cyrus-sasl2-2.1.27~101-g0780600+dfsg.orig
+   # Install ninja-build
+   wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip
+   unzip ninja-linux.zip
+   sudo cp ninja /usr/bin
+elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    # Upgrade pip
+    sudo -H pip3 install --upgrade pip
 
-# Install ninja-build
-wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip
-unzip ninja-linux.zip
-sudo cp ninja /usr/bin
+    # Install meson
+    sudo -H pip3 install meson
+
+    # Install ninja
+    brew install ninja
+fi
