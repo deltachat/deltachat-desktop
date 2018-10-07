e26a27634dd42b624ed614bb412fc626f788c5b7
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-10-07 16:15:56 +0200

    Cleanup config and startup (#130)
    * Remove APP_TEAM
    
    * Remove DEFAULT_DOWNLOAD_PATH
    
    * Remove APP_FILE_ICON
    
    * Remove DELAYED_INIT
    
    * Remove ROOT_PATH
    
    * Remove OS_SYSARCH and arch dependency
    
    * Remove WINDOW_ABOUT
    
    * Remove getPath() (not used anymore)
    
    * Remove electron variable (not used anymore)
    
    * Remove squirrel-win32 usage in windows case, local file not even there
    
    * Change getConfigPath()
    
    * Scrap IS_PORTABLE and should quit code
    
    * shouldQuit always false -> init function not needed

--- src/config.js
+++ src/config.js
@@ -1,35 +1,22 @@
 const appConfig = require('application-config')('DeltaChat')
 const path = require('path')
-const electron = require('electron')
-const arch = require('arch')
 
 const APP_NAME = 'DeltaChat'
-const APP_TEAM = 'DeltaChat'
 const APP_VERSION = require('../package.json').version
 
 const IS_TEST = isTest()
-const PORTABLE_PATH = IS_TEST
-  ? path.join(process.platform === 'win32' ? 'C:\\Windows\\Temp' : '/tmp', 'DeltaChatTest')
-  : path.join(path.dirname(process.execPath), 'Portable Settings')
 const IS_PRODUCTION = isProduction()
-const IS_PORTABLE = isPortable()
 
 const UI_HEADER_HEIGHT = 38
 const UI_MESSAGE_HEIGHT = 100
 
 module.exports = {
-  APP_FILE_ICON: path.join(__dirname, '..', 'static', 'DeltaChatFile'),
   APP_ICON: path.join(__dirname, '..', 'static', 'DeltaChat'),
   APP_NAME: APP_NAME,
-  APP_TEAM: APP_TEAM,
   APP_VERSION: APP_VERSION,
   APP_WINDOW_TITLE: APP_NAME + ' (BETA)',
 
   CONFIG_PATH: getConfigPath(),
-
-  DELAYED_INIT: 3000 /* 3 seconds */,
-
-  DEFAULT_DOWNLOAD_PATH: getDefaultDownloadPath(),
 
   GITHUB_URL: 'https://github.com/deltachat/deltachat-desktop',
   GITHUB_URL_ISSUES: 'https://github.com/deltachat/deltachat-desktop/issues',
@@ -37,16 +24,11 @@ module.exports = {
 
   HOME_PAGE_URL: 'https://delta.chat',
 
-  IS_PORTABLE: IS_PORTABLE,
   IS_PRODUCTION: IS_PRODUCTION,
   IS_TEST: IS_TEST,
 
-  OS_SYSARCH: arch() === 'x64' ? 'x64' : 'ia32',
-
-  ROOT_PATH: path.join(__dirname, '..'),
   STATIC_PATH: path.join(__dirname, '..', 'static'),
 
-  WINDOW_ABOUT: 'file://' + path.join(__dirname, '..', 'static', 'about.html'),
   WINDOW_MAIN: 'file://' + path.join(__dirname, '..', 'static', 'main.html'),
 
   WINDOW_INITIAL_BOUNDS: {
@@ -60,58 +42,15 @@ module.exports = {
 }
 
 function getConfigPath () {
-  if (IS_PORTABLE) {
-    return PORTABLE_PATH
+  if (IS_TEST) {
+    return path.join(process.platform === 'win32' ? 'C:\\Windows\\Temp' : '/tmp', 'DeltaChatTest')
   } else {
     return path.dirname(appConfig.filePath)
   }
 }
 
-function getDefaultDownloadPath () {
-  if (IS_PORTABLE) {
-    return path.join(getConfigPath(), 'Downloads')
-  } else {
-    return getPath('downloads')
-  }
-}
-
-function getPath (key) {
-  if (!process.versions.electron) {
-    // Node.js process
-    return ''
-  } else if (process.type === 'renderer') {
-    // Electron renderer process
-    return electron.remote.app.getPath(key)
-  } else {
-    // Electron main process
-    return electron.app.getPath(key)
-  }
-}
-
 function isTest () {
   return process.env.NODE_ENV === 'test'
-}
-
-function isPortable () {
-  if (IS_TEST) {
-    return true
-  }
-
-  if (process.platform !== 'win32' || !IS_PRODUCTION) {
-    // Fast path: Non-Windows platforms should not check for path on disk
-    return false
-  }
-
-  const fs = require('fs')
-
-  try {
-    // This line throws if the "Portable Settings" folder does not exist, and does
-    // nothing otherwise.
-    fs.accessSync(PORTABLE_PATH, fs.constants.R_OK | fs.constants.W_OK)
-    return true
-  } catch (err) {
-    return false
-  }
 }
 
 function isProduction () {
--- src/main/index.js
+++ src/main/index.js
@@ -14,7 +14,6 @@ const menu = require('./menu')
 const State = require('../renderer/lib/state')
 const windows = require('./windows')
 
-let shouldQuit = false
 let argv = sliceArgv(process.argv)
 
 if (config.IS_PRODUCTION) {
@@ -27,113 +26,72 @@ if (config.IS_PRODUCTION) {
 const hidden = argv.includes('--hidden') ||
   (process.platform === 'darwin' && app.getLoginItemSettings().wasOpenedAsHidden)
 
-if (process.platform === 'win32') {
-  const squirrelWin32 = require('./squirrel-win32')
-  shouldQuit = squirrelWin32.handleEvent(argv[0])
+const ipcMain = electron.ipcMain
+
+app.ipcReady = false // main window has finished loading and IPC is ready
+app.isQuitting = false
+
+parallel({
+  logins: (cb) => logins(config.CONFIG_PATH, cb),
+  appReady: (cb) => app.on('ready', () => cb(null)),
+  state: (cb) => State.load(cb)
+}, onReady)
+
+function onReady (err, results) {
+  if (err) throw err
+
+  const state = results.state
+  app.logins = results.logins
+
+  localize.setup(app, state.saved.locale || app.getLocale())
+  windows.main.init(state, { hidden })
+  menu.init()
+  if (argv.indexOf('--debug') > -1) windows.main.toggleDevTools()
+
+  // Report uncaught exceptions
+  process.on('uncaughtException', (err) => {
+    console.error(err)
+    const error = { message: err.message, stack: err.stack }
+    windows.main.dispatch('uncaughtError', 'main', error)
+  })
 }
 
-if (!shouldQuit && !config.IS_PORTABLE) {
-  // Prevent multiple instances of app from running at same time. New instances
-  // signal this instance and quit. Note: This feature creates a lock file in
-  // %APPDATA%\Roaming\DeltaChat so we do not do it for the Portable App since
-  // we want to be "silent" as well as "portable".
-  shouldQuit = app.makeSingleInstance(onAppOpen)
-  if (shouldQuit) {
+ipc.init()
+
+app.once('ipcReady', function () {
+  log('Command line args:', argv)
+  console.timeEnd('init')
+
+  var win = windows.main.win
+  win.on('close', e => {
+    if (!app.isQuitting) {
+      e.preventDefault()
+      windows.main.hide()
+      quit(e)
+    }
+  })
+})
+
+function quit (e) {
+  if (app.isQuitting) return
+
+  app.isQuitting = true
+  e.preventDefault()
+  windows.main.dispatch('stateSaveImmediate') // try to save state on exit
+  ipcMain.once('stateSaved', () => app.quit())
+  setTimeout(() => {
+    console.error('Saving state took too long. Quitting.')
     app.quit()
-  }
+  }, 4000) // quit after 4 secs, at most
 }
 
-if (!shouldQuit) {
-  init()
-}
+app.on('before-quit', function (e) {
+  quit(e)
+})
 
-function init () {
-  if (config.IS_PORTABLE) {
-    const path = require('path')
-    // Put all user data into the "Portable Settings" folder
-    app.setPath('userData', config.CONFIG_PATH)
-    // Put Electron crash files, etc. into the "Portable Settings\Temp" folder
-    app.setPath('temp', path.join(config.CONFIG_PATH, 'Temp'))
-  }
-
-  const ipcMain = electron.ipcMain
-
-  app.ipcReady = false // main window has finished loading and IPC is ready
-  app.isQuitting = false
-
-  parallel({
-    logins: (cb) => logins(config.CONFIG_PATH, cb),
-    appReady: (cb) => app.on('ready', () => cb(null)),
-    state: (cb) => State.load(cb)
-  }, onReady)
-
-  function onReady (err, results) {
-    if (err) throw err
-
-    const state = results.state
-    app.logins = results.logins
-
-    localize.setup(app, state.saved.locale || app.getLocale())
-    windows.main.init(state, { hidden })
-    menu.init()
-    if (argv.indexOf('--debug') > -1) windows.main.toggleDevTools()
-
-    // Report uncaught exceptions
-    process.on('uncaughtException', (err) => {
-      console.error(err)
-      const error = { message: err.message, stack: err.stack }
-      windows.main.dispatch('uncaughtError', 'main', error)
-    })
-  }
-
-  ipc.init()
-
-  app.once('ipcReady', function () {
-    log('Command line args:', argv)
-    console.timeEnd('init')
-
-    var win = windows.main.win
-    win.on('close', e => {
-      if (!app.isQuitting) {
-        e.preventDefault()
-        windows.main.hide()
-        quit(e)
-      }
-    })
-  })
-
-  function quit (e) {
-    if (app.isQuitting) return
-
-    app.isQuitting = true
-    e.preventDefault()
-    windows.main.dispatch('stateSaveImmediate') // try to save state on exit
-    ipcMain.once('stateSaved', () => app.quit())
-    setTimeout(() => {
-      console.error('Saving state took too long. Quitting.')
-      app.quit()
-    }, 4000) // quit after 4 secs, at most
-  }
-
-  app.on('before-quit', function (e) {
-    quit(e)
-  })
-
-  app.on('window-all-closed', function (e) {
-    quit(e)
-  })
-}
-
-function onAppOpen (newArgv) {
-  newArgv = sliceArgv(newArgv)
-
-  if (app.ipcReady) {
-    log('Second app instance opened, but was prevented:', newArgv)
-    windows.main.show()
-  } else {
-    argv.push(...newArgv)
-  }
-}
+app.on('window-all-closed', function (e) {
+  quit(e)
+})
 
 // Remove leading args.
 // Production: 1 arg, eg: /Applications/DeltaChat.app/Contents/MacOS/DeltaChat
