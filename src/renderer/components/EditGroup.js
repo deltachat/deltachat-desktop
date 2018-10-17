0574d2642521371d064df82226ee976014abdcd0
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-10-17 22:39:38 +0200

    Refactor CreateGroup and EditGroup with GroupBase component

--- src/renderer/components/EditGroup.js
+++ src/renderer/components/EditGroup.js
@@ -1,69 +1,26 @@
-const React = require('react')
 const { ipcRenderer } = require('electron')
+const GroupBase = require('./GroupBase')
 
-const ContactListItem = require('./ContactListItem')
-
-const {
-  Alignment,
-  Classes,
-  InputGroup,
-  ControlGroup,
-  Navbar,
-  NavbarGroup,
-  NavbarHeading,
-  Button
-} = require('@blueprintjs/core')
-
-class EditGroup extends React.Component {
+class EditGroup extends GroupBase {
   constructor (props) {
-    super(props)
-    const { chatId, chatName } = this.props.screenProps
+    const { chatId, chatName } = props.screenProps
     const group = {}
     const readOnly = ipcRenderer.sendSync(
       'dispatchSync',
       'getChatContacts',
       chatId
     ).filter(id => id !== 1)
-    readOnly.forEach(id => group[id] = true)
-    this.state = {
+    readOnly.forEach(id => { group[id] = true })
+    super(props, {
+      buttonLabel: 'save',
       group: group,
       name: chatName,
+      heading: 'editGroup',
       readOnly
-    }
+    })
   }
 
-  addToGroup (contactId) {
-    const group = this.state.group
-    group[contactId] = true
-    this.setState({ group })
-  }
-
-  removeFromGroup (contactId) {
-    const group = this.state.group
-    delete group[contactId]
-    this.setState({ group })
-  }
-
-  shouldComponentUpdate (nextProps, nextState) {
-    // we don't care about the props for now, really.
-    return (this.state !== nextState)
-  }
-
-  contactInGroup (contactId) {
-    return !!this.state.group[contactId]
-  }
-
-  toggleContact (contactId) {
-    if (this.state.readOnly.includes(contactId)) return
-
-    if (this.contactInGroup(contactId)) {
-      this.removeFromGroup(contactId)
-    } else {
-      this.addToGroup(contactId)
-    }
-  }
-
-  save () {
+  onSubmit () {
     const contactIds = Object.keys(this.state.group).filter(id => {
       return !this.state.readOnly.includes(Number(id))
     })
@@ -77,56 +34,6 @@ class EditGroup extends React.Component {
     )
     this.props.changeScreen('ChatList')
   }
-
-  handleNameChange (e) {
-    this.setState({ name: e.target.value })
-  }
-
-  isButtonDisabled () {
-    if (!this.state.name.length) return true
-    if (!Object.keys(this.state.group).length) return true
-    return false
-  }
-
-  render () {
-    const { deltachat } = this.props
-    const tx = window.translate
-
-    return (
-      <div>
-        <Navbar fixedToTop>
-          <NavbarGroup align={Alignment.LEFT}>
-            <Button className={Classes.MINIMAL} icon='undo' onClick={this.props.changeScreen} />
-            <NavbarHeading>{tx('editGroup')}</NavbarHeading>
-          </NavbarGroup>
-        </Navbar>
-        <div className='window'>
-          <div>
-            <ControlGroup fill vertical={false}>
-              <InputGroup
-                type='text'
-                id='name'
-                value={this.state.name}
-                onChange={this.handleNameChange.bind(this)}
-                placeholder={tx('groupName')} />
-              <Button
-                disabled={this.isButtonDisabled()}
-                onClick={this.save.bind(this)}
-                text={tx('save')} />
-            </ControlGroup>
-          </div>
-          {deltachat.contacts.map((contact) => {
-            return (
-              <ContactListItem
-                color={this.contactInGroup(contact.id) ? 'green' : ''}
-                onClick={this.toggleContact.bind(this, contact.id)}
-                contact={contact}
-              />
-            )
-          })}
-        </div>
-      </div>
-    )
-  }
 }
+
 module.exports = EditGroup
---
+++ src/renderer/components/GroupBase.js
@@ -0,0 +1,106 @@
+const React = require('react')
+const ContactListItem = require('./ContactListItem')
+const {
+  Alignment,
+  Classes,
+  InputGroup,
+  ControlGroup,
+  Navbar,
+  NavbarGroup,
+  NavbarHeading,
+  Button
+} = require('@blueprintjs/core')
+
+class GroupBase extends React.Component {
+  constructor (props, state) {
+    super(props)
+    this.state = state
+    this.state.group = this.state.group || {}
+    this.state.name = this.state.name || ''
+    this.state.readOnly = this.state.readOnly || []
+  }
+
+  addToGroup (contactId) {
+    const group = this.state.group
+    group[contactId] = true
+    this.setState({ group })
+  }
+
+  removeFromGroup (contactId) {
+    const group = this.state.group
+    delete group[contactId]
+    this.setState({ group })
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+    // we don't care about the props for now, really.
+    return (this.state !== nextState)
+  }
+
+  contactInGroup (contactId) {
+    return !!this.state.group[contactId]
+  }
+
+  toggleContact (contactId) {
+    if (this.state.readOnly.includes(contactId)) return
+
+    if (this.contactInGroup(contactId)) {
+      this.removeFromGroup(contactId)
+    } else {
+      this.addToGroup(contactId)
+    }
+  }
+
+  handleNameChange (e) {
+    this.setState({ name: e.target.value })
+  }
+
+  isButtonDisabled () {
+    if (!this.state.name.length) return true
+    if (!Object.keys(this.state.group).length) return true
+    return false
+  }
+
+  render () {
+    const { deltachat } = this.props
+    const tx = window.translate
+
+    return (
+      <div>
+        <Navbar fixedToTop>
+          <NavbarGroup align={Alignment.LEFT}>
+            <Button className={Classes.MINIMAL} icon='undo' onClick={this.props.changeScreen} />
+            <NavbarHeading>{tx(this.state.heading)}</NavbarHeading>
+          </NavbarGroup>
+        </Navbar>
+        <div className='window'>
+          <div>
+            <ControlGroup fill vertical={false}>
+              <InputGroup
+                type='text'
+                id='name'
+                value={this.state.name}
+                onChange={this.handleNameChange.bind(this)}
+                placeholder={tx('groupName')} />
+              <Button
+                disabled={this.isButtonDisabled()}
+                onClick={this.onSubmit.bind(this)}
+                text={tx(this.state.buttonLabel)} />
+            </ControlGroup>
+          </div>
+          {deltachat.contacts.map((contact) => {
+            return (
+              <ContactListItem
+                color={this.contactInGroup(contact.id) ? 'green' : ''}
+                onClick={this.toggleContact.bind(this, contact.id)}
+                contact={contact}
+              />
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
+}
+
+module.exports = GroupBase
