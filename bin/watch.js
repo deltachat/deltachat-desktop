44e9c0329baa3ba707140490bbf632d5c4e00f12
Karissa McKelvey <krmckelv@gmail.com>
2018-10-29 18:21:23 +0200

    make conversations build only when necessary to improve build time

--- bin/watch.js
+++ bin/watch.js
@@ -2,12 +2,23 @@ var watch = require('glob-watcher')
 var child = require('child_process')
 
 watch([
-  './src',
+  './src/',
   './_locales/*.json',
   './static',
+  './conversations/build',
   '!./es5'
 ], function (done) {
-  var p = child.spawn('npm', ['run', 'build'])
+  var p = child.spawn('npm', ['run', 'build-es5'])
+  p.stdout.pipe(process.stdout)
+  p.stderr.pipe(process.stderr)
+  p.on('close', done)
+})
+
+watch([
+  './conversations',
+  '!./conversations/build'
+], function (done) {
+  var p = child.spawn('npm', ['run', 'build-conversations'])
   p.stdout.pipe(process.stdout)
   p.stderr.pipe(process.stderr)
   p.on('close', done)
