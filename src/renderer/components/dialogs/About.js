33904e61ce2b68a093752f1460e33fa47d3c17fb
jikstra <jikstra@disroot.org>
2018-10-31 00:20:34 +0100

    Updated about dialog, different text (not translated yet) and hiding advanced info in a collapse

--- src/renderer/components/dialogs/About.js
+++ src/renderer/components/dialogs/About.js
@@ -1,7 +1,22 @@
 const React = require('react')
-const { Classes, Dialog } = require('@blueprintjs/core')
+const { Classes, Dialog, Collapse, Button } = require('@blueprintjs/core')
+const { version } = require('../../../../package.json')
 
 class AboutDialog extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      showAdvanced: false
+    }
+
+    this.handleShowAdvanced = this.handleShowAdvanced.bind(this)
+  }
+
+  handleShowAdvanced () {
+    this.setState({ showAdvanced: !this.state.showAdvanced })
+  }
+
   render () {
     const { isOpen, onClose, info } = this.props
     const tx = window.translate
@@ -18,9 +33,19 @@ class AboutDialog extends React.Component {
         onClose={onClose}
         canOutsideClickClose={false}>
         <div className={Classes.DIALOG_BODY}>
-          {rows.map(row => {
-            return <div><div>{row.key}</div><div>{row.value}</div></div>
-          })}
+          <p style={{color: 'grey'}}>{'version ' + version}</p>
+          <p>Official Delta.Chat Desktop app.</p>
+          <p>This software is licensed under <a href="https://github.com/deltachat/deltachat-desktop/blob/master/LICENSE">GNU GPL version 3</a>.</p>
+          <p>Source code is available on <a href="https://github.com/deltachat/deltachat-desktop">GitHub</a>.</p>
+          <Button onClick={this.handleShowAdvanced}>{(this.state.showAdvanced ? '-' : '+') + ' ' + tx('login.advanced') }</Button>
+          <Collapse isOpen={this.state.showAdvanced}>
+            <ul>
+              {rows.map(row => {
+                return <li>{row.key + ': ' + row.value}</li>
+              })}
+            </ul>
+
+          </Collapse>
         </div>
       </Dialog>
     )
