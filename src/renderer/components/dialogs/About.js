db4675ebcc43af4d4b4eecd61cfc2005aaf702e2
jikstra <jikstra@disroot.org>
2018-10-31 01:06:24 +0100

    Fix standard

--- src/renderer/components/dialogs/About.js
+++ src/renderer/components/dialogs/About.js
@@ -3,7 +3,7 @@ const { Classes, Dialog, Collapse, Button } = require('@blueprintjs/core')
 const { version } = require('../../../../package.json')
 
 class AboutDialog extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
 
     this.state = {
@@ -33,10 +33,10 @@ class AboutDialog extends React.Component {
         onClose={onClose}
         canOutsideClickClose={false}>
         <div className={Classes.DIALOG_BODY}>
-          <p style={{color: 'grey'}}>{'version ' + version}</p>
+          <p style={{ color: 'grey' }}>{'version ' + version}</p>
           <p>Official Delta.Chat Desktop app.</p>
-          <p>This software is licensed under <a href="https://github.com/deltachat/deltachat-desktop/blob/master/LICENSE">GNU GPL version 3</a>.</p>
-          <p>Source code is available on <a href="https://github.com/deltachat/deltachat-desktop">GitHub</a>.</p>
+          <p>This software is licensed under <a href='https://github.com/deltachat/deltachat-desktop/blob/master/LICENSE'>GNU GPL version 3</a>.</p>
+          <p>Source code is available on <a href='https://github.com/deltachat/deltachat-desktop'>GitHub</a>.</p>
           <Button onClick={this.handleShowAdvanced}>{(this.state.showAdvanced ? '-' : '+') + ' ' + tx('login.advanced') }</Button>
           <Collapse isOpen={this.state.showAdvanced}>
             <ul>
