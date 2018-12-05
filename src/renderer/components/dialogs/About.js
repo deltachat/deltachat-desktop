cd58d5cc3c02497dcff3f414a48384d22669085b
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-12-05 22:27:22 +0100

    about: make upper <p> also unselectable

--- src/renderer/components/dialogs/About.js
+++ src/renderer/components/dialogs/About.js
@@ -30,8 +30,8 @@ class AboutDialog extends React.Component {
         onClose={onClose}
         canOutsideClickClose={false}>
         <div className={Classes.DIALOG_BODY}>
-          <p style={{ color: 'grey' }}>{`Version ${APP_VERSION}`}</p>
           <Unselectable>
+            <p style={{ color: 'grey' }}>{`Version ${APP_VERSION}`}</p>
             <p>Official Delta Chat Desktop app.</p>
             <p>This software is licensed under <ClickableLink href='https://github.com/deltachat/deltachat-desktop/blob/master/LICENSE' text='GNU GPL version 3' />.</p>
             <p>Source code is available on <ClickableLink href='https://github.com/deltachat/deltachat-desktop' text='GitHub' />.</p>
