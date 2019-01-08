563b64a0fd5eada45d0132cab951c07d4e34932d
jikstra <jikstra@disroot.org>
2019-01-08 19:04:51 +0100

    Remove weird proprietary -webkit-search-cancel-button use complete width for search input

--- src/renderer/components/SearchInput.js
+++ src/renderer/components/SearchInput.js
@@ -1,13 +1,37 @@
 const React = require('react')
 const { InputGroup } = require('@blueprintjs/core')
+const styled = require('styled-components').default
 
-module.exports = function (props) {
-  const tx = window.translate
-  return <InputGroup
-    type='search'
-    large
-    placeholder={tx('search')}
-    leftIcon='search'
-    {...props}
-  />
+
+
+const SearchInputWrapper = styled.div`
+  width: 100%
+  .bp3-input-group {
+    width: 100%
+  }
+
+  input[type="search"]::-webkit-search-cancel-button {
+    /* Remove default */
+    -webkit-appearance: none;
+  }
+`
+
+class SearchInput extends React.Component {
+  render() {
+
+    const tx = window.translate
+    return (
+      <SearchInputWrapper>
+        <InputGroup
+          type='search'   
+          large
+          placeholder={tx('search')}
+          leftIcon='search'
+          {...this.props}
+        />
+      </SearchInputWrapper>
+    )
+  }
 }
+
+module.exports = SearchInput
