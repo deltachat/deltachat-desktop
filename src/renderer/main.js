143dc9b84e219af1acc4a3a2de2a57301aad1690
jikstra <jikstra@disroot.org>
2019-01-09 18:41:53 +0100

    Build conversations.css to /static. Get rid of the ipc hack to include the css file.

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -6,8 +6,6 @@ window.eval = global.eval = function () {
 const React = require('react')
 const ReactDOM = require('react-dom')
 
-const insertCss = require('insert-css')
-
 const { ipcRenderer } = require('electron')
 const State = require('./lib/state')
 const localize = require('../localize')
@@ -26,9 +24,6 @@ function onState (err, _state) {
   state = window.state = _state
 
   setupLocaleData(state.saved.locale)
-
-  const conversationsCss = ipcRenderer.sendSync('get-css')
-  insertCss(conversationsCss, { prepend: true })
 
   app = ReactDOM.render(<App state={state} />, document.querySelector('#root'))
 
