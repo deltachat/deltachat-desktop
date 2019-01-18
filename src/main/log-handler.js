66c0cc92eebd471079b8a6001fa08bc62956fd2e
Lars-Magnus Skog <ralphtheninja@riseup.net>
2019-01-18 23:54:45 +0100

    Use variadic arguments in log handler

--- src/main/log-handler.js
+++ src/main/log-handler.js
@@ -9,39 +9,20 @@ module.exports = () => {
 
   const handler = {
     /**
-     * Internal logger - please don't call directly unless you know what your doing
-     * @param {string} channel The part/module where the message was logged. Like 'Tanslations'
-     * @param {Number} level The level of importance. 0 for debug, 1 for info, 2 for warning, 3 for error, 4 for critical error
-     * @param {string} message the message (human readable)
-     * @param {string} errorCode (optional) machine readable error code (string in snake_case)
-     * @param {any|string} payload (optional) JSON payload or any other object that can be converted to a JSON string
-     * @param {StackFrame} stacktrace (optional) for errors -> gets converted to json
+     * Internal log handler. Do not call directly!
      */
-    log: (channel, level, message, errorCode, payload, stacktrace) => {
+    log: (channel, level, ...args) => {
       const timestamp = new Date().toISOString()
-      if (payload) {
-        var payloadForLogfile = JSON.stringify(payload)
-        if (payloadForLogfile.charAt(0) !== '"') {
-          payloadForLogfile = JSON.stringify(payloadForLogfile)
-        }
-      }
-
-      const logString = `${timestamp}|=|=|"${channel}"|=|=|${level}|=|=|"${message}"|=|=|"${errorCode || ''}"|=|=|${payloadForLogfile || ''}|=|=|${JSON.stringify(stacktrace || '')}`
-        .replace(/\t/g, '  ').replace(/\n|(:?\r\n)/g, '') // Cleanup
-        .replace(/\|=\|=\|/g, String.fromCharCode(9))
-
-      stream.write(`${logString}\n`)
+      const out = [
+        timestamp,
+        channel,
+        level
+      ].concat(args.map(arg => JSON.stringify(arg)))
+      stream.write(`${out.join(' ')}\n`)
     },
     end: () => stream.end()
   }
 
-  handler.log(
-    'logger',
-    1,
-    `Logfile: ${file}`,
-    'log_file_init',
-    file
-  )
   console.log(`Logfile: ${file}`)
 
   return handler
