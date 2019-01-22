0e0b248bfa2ee22295ed68b6f3cbc87c038998d6
jikstra <jikstra@disroot.org>
2019-01-22 05:13:19 +0100

    Exclude deltachat-node dependency for production builds on backend

--- webpack.backend.prod.config.js
+++ webpack.backend.prod.config.js
@@ -2,5 +2,7 @@ const merge = require('lodash.merge')
 
 module.exports = merge(require('./webpack.backend.dev.config.js'), {
   mode: 'production',
-  devtool: 'source-map'
+  devtool: 'source-map',
 })
+
+module.exports.externals = ['deltachat-node']
