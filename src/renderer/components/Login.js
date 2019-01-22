5505ced6e43a6b2ba308a868ed75d1d7f7a9cdb5
Jikstra <34889164+Jikstra@users.noreply.github.com>
2019-01-22 14:35:01 +0100

    Implement login instruction (#607)
    * Implement a way to load experimental english strings from _locales/_experimental-en.json. Refactored localize.js a bit.
    
    Add section for experimental language strings to README
    
    * Implement instruction text to login form. We use experimental language strings, yeyy
    
    fix standard
    
    Apply requested changes
    
    * Fix merge conflicts

--- src/renderer/components/Login.js
+++ src/renderer/components/Login.js
@@ -3,6 +3,7 @@ const { ipcRenderer } = require('electron')
 
 const {
   Button,
+  Callout,
   InputGroup,
   FormGroup,
   Collapse,
@@ -111,143 +112,147 @@ class Login extends React.Component {
     const tx = window.translate
 
     return (
-      <form onSubmit={this.handleSubmit}>
-        <FormGroup label={tx('email_address')} placeholder={tx('email_address')} labelFor='email'>
-          <InputGroup
-            id='addr'
-            disabled={addrDisabled}
-            type='text'
-            value={addr}
-            leftIcon='envelope'
-            onChange={this.handleCredentialsChange}
-          />
-        </FormGroup>
-        <FormGroup label={tx('password')} placeholder={tx('password')} labelFor='mailPw'>
-          {this.renderPasswordInput('showPasswordMail', 'mailPw')}
-        </FormGroup>
-        <Button onClick={this.handleUISwitchStateProperty.bind(this, 'showAdvanced')}>
-          {(showAdvanced ? '-' : '+') + ' ' + tx('menu_advanced') }
-        </Button>
-        <Collapse isOpen={showAdvanced}>
-          <h2>{tx('login_inbox')}</h2>
-          <p>{tx('login_subheader')}</p>
-          <FormGroup
-            label={tx('login_imap_login')}
-            placeholder={tx('login_imap_login')}
-            labelFor='mailUser'>
+      <React.Fragment>
+        <Callout>{tx('login_instruction_desktop')}</Callout>
+        <br />
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup label={tx('email_address')} placeholder={tx('email_address')} labelFor='email'>
             <InputGroup
-              id='mailUser'
+              id='addr'
+              disabled={addrDisabled}
               type='text'
-              value={mailUser}
+              value={addr}
               leftIcon='envelope'
               onChange={this.handleCredentialsChange}
             />
           </FormGroup>
-          <FormGroup
-            label={tx('login_imap_server')}
-            placeholder={tx('login_imap_server')}
-            labelFor='mailServer'>
-            <InputGroup
-              id='mailServer'
-              type='text'
-              value={mailServer}
-              leftIcon='envelope'
-              onChange={this.handleCredentialsChange}
-            />
+          <FormGroup label={tx('password')} placeholder={tx('password')} labelFor='mailPw'>
+            {this.renderPasswordInput('showPasswordMail', 'mailPw')}
           </FormGroup>
-          <FormGroup
-            label={tx('login_imap_port')}
-            placeholder={tx('login_imap_port')}
-            labelFor='mailPort'>
-            <InputGroup
-              id='mailPort'
-              type='number'
-              min='0'
-              max='65535'
-              value={mailPort}
-              leftIcon='envelope'
-              onChange={this.handleCredentialsChange}
-            />
-          </FormGroup>
-          <FormGroup label={tx('login_imap_security')} placeholder={tx('login_imap_security')}
-            labelFor='mailSecurity'>
-            <div className='bp3-select .modifier'>
-              <select id='mailSecurity' value={mailSecurity} onChange={this.handleCredentialsChange}>
-                <option value='ssl'>SSL/TLS</option>
-                <option value='starttls'>STARTTLS</option>
-              </select>
-            </div>
-          </FormGroup>
-          <h2>{tx('login_outbox')}</h2>
-          <FormGroup
-            label={tx('login_smtp_login')}
-            placeholder={tx('login_smtp_login')}
-            labelFor='sendUser'>
-            <InputGroup
-              id='sendUser'
-              type='text'
-              value={sendUser}
-              leftIcon='envelope'
-              onChange={this.handleCredentialsChange}
-            />
-          </FormGroup>
-          <FormGroup
-            label={tx('login_smtp_password')}
-            placeholder={tx('login_smtp_password')}
-            labelFor='sendPw'>
-            {this.renderPasswordInput('showPasswordSend', 'sendPw')}
-          </FormGroup>
-          <FormGroup
-            label={tx('login_smtp_server')}
-            placeholder={tx('login_smtp_server')}
-            labelFor='sendServer'>
-            <InputGroup
-              id='sendServer'
-              type='text'
-              value={sendServer}
-              leftIcon='envelope'
-              onChange={this.handleCredentialsChange}
-            />
-          </FormGroup>
-          <FormGroup
-            label={tx('login_smtp_port')}
-            placeholder={tx('login_smtp_port')}
-            labelFor='sendPort'>
-            <InputGroup
-              id='sendPort'
-              type='number'
-              min='0'
-              max='65535'
-              value={sendPort}
-              leftIcon='envelope'
-              onChange={this.handleCredentialsChange}
-            />
-          </FormGroup>
-          <FormGroup
-            label={tx('login_smtp_security')}
-            placeholder={tx('login_smtp_security')}
-            labelFor='sendSecurity'>
-            <div className='bp3-select .modifier'>
-              <select id='sendSecurity' value={sendSecurity} onChange={this.handleCredentialsChange}>
-                <option value='ssl'>SSL/TLS</option>
-                <option value='starttls'>STARTTLS</option>
-              </select>
-            </div>
-          </FormGroup>
-        </Collapse>
-        {React.Children.map(this.props.children, (child) => {
-          var props = {}
-          if (child.props.type === 'submit') {
-            props.disabled = loading || (!addr || !mailPw)
-          }
-          if (child.props.type === 'cancel') {
-            props.onClick = this.cancelClick.bind(this)
-            if (!loading) return
-          }
-          return React.cloneElement(child, props)
-        })}
+          <Button onClick={this.handleUISwitchStateProperty.bind(this, 'showAdvanced')}>
+            {(showAdvanced ? '-' : '+') + ' ' + tx('menu_advanced') }
+          </Button>
+          <Collapse isOpen={showAdvanced}>
+            <h2>{tx('login_inbox')}</h2>
+            <p>{tx('login_subheader')}</p>
+            <FormGroup
+              label={tx('login_imap_login')}
+              placeholder={tx('login_imap_login')}
+              labelFor='mailUser'>
+              <InputGroup
+                id='mailUser'
+                type='text'
+                value={mailUser}
+                leftIcon='envelope'
+                onChange={this.handleCredentialsChange}
+              />
+            </FormGroup>
+            <FormGroup
+              label={tx('login_imap_server')}
+              placeholder={tx('login_imap_server')}
+              labelFor='mailServer'>
+              <InputGroup
+                id='mailServer'
+                type='text'
+                value={mailServer}
+                leftIcon='envelope'
+                onChange={this.handleCredentialsChange}
+              />
+            </FormGroup>
+            <FormGroup
+              label={tx('login_imap_port')}
+              placeholder={tx('login_imap_port')}
+              labelFor='mailPort'>
+              <InputGroup
+                id='mailPort'
+                type='number'
+                min='0'
+                max='65535'
+                value={mailPort}
+                leftIcon='envelope'
+                onChange={this.handleCredentialsChange}
+              />
+            </FormGroup>
+            <FormGroup label={tx('login_imap_security')} placeholder={tx('login_imap_security')}
+              labelFor='mailSecurity'>
+              <div className='bp3-select .modifier'>
+                <select id='mailSecurity' value={mailSecurity} onChange={this.handleCredentialsChange}>
+                  <option value='ssl'>SSL/TLS</option>
+                  <option value='starttls'>STARTTLS</option>
+                </select>
+              </div>
+            </FormGroup>
+            <h2>{tx('login_outbox')}</h2>
+            <FormGroup
+              label={tx('login_smtp_login')}
+              placeholder={tx('login_smtp_login')}
+              labelFor='sendUser'>
+              <InputGroup
+                id='sendUser'
+                type='text'
+                value={sendUser}
+                leftIcon='envelope'
+                onChange={this.handleCredentialsChange}
+              />
+            </FormGroup>
+            <FormGroup
+              label={tx('login_smtp_password')}
+              placeholder={tx('login_smtp_password')}
+              labelFor='sendPw'>
+              {this.renderPasswordInput('showPasswordSend', 'sendPw')}
+            </FormGroup>
+            <FormGroup
+              label={tx('login_smtp_server')}
+              placeholder={tx('login_smtp_server')}
+              labelFor='sendServer'>
+              <InputGroup
+                id='sendServer'
+                type='text'
+                value={sendServer}
+                leftIcon='envelope'
+                onChange={this.handleCredentialsChange}
+              />
+            </FormGroup>
+            <FormGroup
+              label={tx('login_smtp_port')}
+              placeholder={tx('login_smtp_port')}
+              labelFor='sendPort'>
+              <InputGroup
+                id='sendPort'
+                type='number'
+                min='0'
+                max='65535'
+                value={sendPort}
+                leftIcon='envelope'
+                onChange={this.handleCredentialsChange}
+              />
+            </FormGroup>
+            <FormGroup
+              label={tx('login_smtp_security')}
+              placeholder={tx('login_smtp_security')}
+              labelFor='sendSecurity'>
+              <div className='bp3-select .modifier'>
+                <select id='sendSecurity' value={sendSecurity} onChange={this.handleCredentialsChange}>
+                  <option value='ssl'>SSL/TLS</option>
+                  <option value='starttls'>STARTTLS</option>
+                </select>
+              </div>
+            </FormGroup>
+          </Collapse>
+          {React.Children.map(this.props.children, (child) => {
+            var props = {}
+            if (child.props.type === 'submit') {
+              props.disabled = loading || (!addr || !mailPw)
+            }
+            if (child.props.type === 'cancel') {
+              props.onClick = this.cancelClick.bind(this)
+              if (!loading) return
+            }
+            return React.cloneElement(child, props)
+          })}
 
-      </form>
+        </form>
+      </React.Fragment>
     )
   }
 }
