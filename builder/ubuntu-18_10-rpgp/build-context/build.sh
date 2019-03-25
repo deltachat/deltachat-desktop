4c418f782b898dc21ba4a28738bb7b5686ae17a9
jikstra <jikstra@disroot.org>
2019-03-25 20:46:35 +0100

    Implemented fixing & repacking .deb

--- builder/ubuntu-18_10-rpgp/build-context/build.sh
+++ builder/ubuntu-18_10-rpgp/build-context/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+BUILD_NAME="ubuntu-18_10-rpgp"
 
 # Compile deltachat-core with rpgp
 cd /
@@ -12,9 +13,31 @@ ninja install
 # Compile deltachat-node with our self compiled deltachat-core
 git clone https://github.com/deltachat/deltachat-node.git
 cd /deltachat-node
-cp -r /deltachat-core .
 npm install
+rm -rf deltachat-core
+cp -r /deltachat-core .
+cp /build-context/binding.gyp . # replace build script with fixed for rpgp one
 
-#npm install --build-from-source
-#npm run build
-#npx electron-builder -c /build-context/electron-builder-ubuntu.json
+# Build deltachat-desktop
+cd /build
+npm install --build-from-source
+npm run build
+npx electron-builder -c /build-context/electron-builder-ubuntu.json
+
+# Extract already built .deb
+cd "dist/$BUILD_NAME"
+NAME_DEB=`basename $(find ./ -maxdepth 1 -name *.deb -print -quit)`
+mkdir -p extract/DEBIAN
+dpkg-deb -x $NAME_DEB extract
+dpkg-deb -e $NAME_DEB extract/DEBIAN
+
+# Fix extracted contents
+cp -r /opt/DeltaChat/rpgp extract/opt/DeltaChat
+cp -r /opt/DeltaChat/libdeltachat extract/opt/DeltaChat
+mv extract/opt/DeltaChat/deltachat-desktop extract/opt/DeltaChat/deltachat-desktop-electron
+cp /build/deltachat-desktop extract/opt/DeltaChat
+
+# Repack into .deb
+rm $NAME_DEB
+dpkg-deb -b extract $NAME_DEB
+
