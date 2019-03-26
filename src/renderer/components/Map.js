b2f05e5fd0d50b621a946d27f2e66ccc8a076b75
Nico de Haen <mail@ndh-websolutions.de>
2019-03-26 23:00:28 +0100

    Refactoring & cleanup

--- src/renderer/components/Map.js
+++ src/renderer/components/Map.js
@@ -6,45 +6,44 @@ const moment = require('moment')
 const MapLayerFactory = require('./helpers/MapLayerFactory')
 
 class Map extends React.Component {
-
   constructor (props) {
     super(props)
     this.state = {
       points: []
     }
-    this.renderMap = this.renderMap.bind(this)
+    this.renderLayer = this.renderLayer.bind(this)
+    this.onMapClick = this.onMapClick.bind(this)
   }
 
   componentDidMount () {
     this.componentDidMount = Date.now()
-    console.log('componentDidMount', Date.now())
+    console.log('componentDidMount', this.componentDidMount)
     mapboxgl.accessToken = MapLayerFactory.getAccessToken()
     this.map = new mapboxgl.Map(
       {
-        container: 'map', // container id
-        style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-        zoom: 12, // starting zoom
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        zoom: 12,
         attributionControl: false
       })
 
-    this.map.on('load', this.renderMap)
+    this.map.on('load', this.renderLayer)
+    this.map.on('click', this.onMapClick)
   }
 
-  renderMap () {
+  renderLayer () {
     const { selectedChat } = this.props
     const contacts = selectedChat.contacts
+    let allPoints = []
 
     if (this.map.loaded()) {
       console.log('map loaded', this.componentDidMount - Date.now())
-      let allPoints = []
-      let center = null
       contacts.map(contact => {
         let locationsForContact = ipcRenderer.sendSync('getLocations', selectedChat.id, contact.id)
         if (locationsForContact && locationsForContact.length) {
-          let pointsForLayer = []
-          locationsForContact.map(point => pointsForLayer.push([point.lon, point.lat]))
-          let layer = MapLayerFactory.getGeoJSONLineLayer(pointsForLayer, contact)
-          this.map.addLayer(layer)
+          let pointsForLayer = locationsForContact.map(point => [point.lon, point.lat])
+
+          this.map.addLayer(MapLayerFactory.getGeoJSONLineLayer(pointsForLayer, contact))
           this.map.addLayer(MapLayerFactory.getGeoJSONPointsLayer(locationsForContact, contact))
 
           let lastPoint = locationsForContact[locationsForContact.length - 1]
@@ -55,26 +54,25 @@ class Map extends React.Component {
             .setPopup(popup)
             .addTo(this.map)
           allPoints = allPoints.concat(pointsForLayer)
-          center = [lastPoint.lon, lastPoint.lat]
         }
       })
-      this.map.setCenter(center)
+      if (allPoints.length > 0) {
+        this.map.fitBounds(geojsonExtent({ type: 'Point', coordinates: allPoints }), { padding: 100 })
+      }
 
-      this.map.fitBounds(geojsonExtent({ type: 'Point', coordinates: allPoints }), { padding: 100 })
-
-      this.map.on('click', e => {
-        let features = this.map.queryRenderedFeatures(e.point)
-        const contactFeature = features.find(f => f.properties.contact !== undefined)
-        console.log(contactFeature)
-        if (contactFeature) {
-          new mapboxgl.Popup({ offset: [0, -15] })
-            .setLngLat(contactFeature.geometry.coordinates)
-            .setHTML(contactFeature.properties.contact + '<br />' + contactFeature.properties.reported)
-            .setLngLat(contactFeature.geometry.coordinates)
-            .addTo(this.map)
-        }
-      })
       console.log('map rendered', this.componentDidMount - Date.now())
+    }
+  }
+
+  onMapClick (event) {
+    let features = this.map.queryRenderedFeatures(event.point)
+    const contactFeature = features.find(f => f.properties.contact !== undefined)
+    if (contactFeature) {
+      new mapboxgl.Popup({ offset: [0, -15] })
+        .setLngLat(contactFeature.geometry.coordinates)
+        .setHTML(contactFeature.properties.contact + '<br />' + contactFeature.properties.reported)
+        .setLngLat(contactFeature.geometry.coordinates)
+        .addTo(this.map)
     }
   }
 
