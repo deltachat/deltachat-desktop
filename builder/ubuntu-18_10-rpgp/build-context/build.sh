68bebe78bfe96d325dc6a4241260ada83b8b018f
jikstra <jikstra@disroot.org>
2019-03-26 14:45:40 +0100

    Fixed ubuntu-18_18-rpgp

--- builder/ubuntu-18_10-rpgp/build-context/build.sh
+++ builder/ubuntu-18_10-rpgp/build-context/build.sh
@@ -1,31 +1,44 @@
 #!/bin/bash
 BUILD_NAME="ubuntu-18_10-rpgp"
 
-# Compile deltachat-core with rpgp
+# COMPILE DELTACHAT-CORE with RPGP
 cd /
 git clone https://github.com/deltachat/deltachat-core.git
 cd deltachat-core/
-git checkout fix_meson_rpgp_pkg-config
 PKG_CONFIG_PATH=/opt/DeltaChat/rpgp/lib/pkgconfig/ meson -Dpgp=true --prefix /opt/DeltaChat/libdeltachat builddir
 cd builddir/
 ninja install
 
-# Compile deltachat-node with our self compiled deltachat-core
+# COMPILE DELTACHAT-NODE WITH OUR SELF COMPILED DELTACHAT-CORE
+cd /
 git clone https://github.com/deltachat/deltachat-node.git
 cd /deltachat-node
 npm install
 rm -rf deltachat-core
 cp -r /deltachat-core .
-cp /build-context/binding.gyp . # replace build script with fixed for rpgp one
 
-# Build deltachat-desktop
+# COPY OVER MODIFIED BINDING.GYP
+cp /build-context/binding.gyp . # replace build script with fixed for rpgp one
+# rebuild bindings, for whatever reason we need to install node-gyp to make it
+# work
+npm install node-gyp
+npx node-gyp rebuild
+
+# BUILD DELTACHAT-DESKTOP
 cd /build
-npm install --build-from-source
+rm -rf node_modules
+npm install
+
+# Replace deltachat-node with our manually compiled
+rm -rf node_modules/deltachat-node
+cp -r /deltachat-node node_modules
+
+# Rebuild desktop
 npm run build
 npx electron-builder -c /build-context/electron-builder-ubuntu.json
 
-# Extract already built .deb
-cd "dist/$BUILD_NAME"
+# EXTRACT ALREADY BUILT .DEB
+cd "/build/dist/$BUILD_NAME"
 NAME_DEB=`basename $(find ./ -maxdepth 1 -name *.deb -print -quit)`
 mkdir -p extract/DEBIAN
 dpkg-deb -x $NAME_DEB extract
@@ -35,7 +48,7 @@ dpkg-deb -e $NAME_DEB extract/DEBIAN
 cp -r /opt/DeltaChat/rpgp extract/opt/DeltaChat
 cp -r /opt/DeltaChat/libdeltachat extract/opt/DeltaChat
 mv extract/opt/DeltaChat/deltachat-desktop extract/opt/DeltaChat/deltachat-desktop-electron
-cp /build/deltachat-desktop extract/opt/DeltaChat
+cp /build-context/deltachat-desktop extract/opt/DeltaChat
 
 # Repack into .deb
 rm $NAME_DEB
--- builder/ubuntu-18_10-rpgp/build-package.sh
+++ builder/ubuntu-18_10-rpgp/build-package.sh
@@ -2,6 +2,6 @@
 SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 
-docker run --rm -it -v "$SCRIPTPATH/../..":/build  -w /build-context builder-deltachat-desktop-ubuntu-18_10-rpgp bash -i -c 'tail -f /dev/null'
+docker run --rm -it -v "$SCRIPTPATH/../..":/build  -w /build-context builder-deltachat-desktop-ubuntu-18_10-rpgp bash -i -c './build.sh; tail -f /dev/null'
 #docker run --rm -it -v "$SCRIPTPATH/../..":/build  -w /build-context builder-deltachat-desktop-ubuntu-18_10-rpgp tail -f /dev/null
 
