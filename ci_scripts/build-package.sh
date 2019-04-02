0f2d4aa9626549bf0df2fe1f1e25992670c57c5d
holger krekel <holger@merlinux.eu>
2019-04-02 21:18:16 +0200

    fix package run

--- ci_scripts/build-package.sh
+++ ci_scripts/build-package.sh
@@ -4,4 +4,4 @@ DIR=${1:?specify directory of ubuntu docker dir}
 export BUILDER_NAME=$(basename $DIR)
 export DOCKERTAG=deltachat/desktop-$BUILDER_NAME 
 
-docker run -e BUILDER_NAME --rm -it -v $(PWD):/build  -w /build-context $DOCKERTAG bash -i -c './build.sh'
+docker run -e BUILDER_NAME --rm -it -v "${PWD}:/build"  -w /build-context $DOCKERTAG bash -i -c './build.sh'
