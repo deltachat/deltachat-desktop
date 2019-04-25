7e2cf7400b2cb4a4df39d705465ef45cd50d8a18
Nico de Haen <mail@ndh-websolutions.de>
2019-04-25 17:52:46 +0200

    Skip node-externals

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,8 @@
-const nodeExternals = require('webpack-node-externals')
 const path = require('path')
 
 module.exports = (_, argv) => ({
   entry: path.normalize(`${__dirname}/src/renderer/main.js`),
   target: 'electron-main',
-  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
   output: {
     path: path.normalize(`${__dirname}/static`),
     filename: 'bundle.js',
