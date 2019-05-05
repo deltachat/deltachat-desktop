1c83c152da912b12a2af439d1139eb068405526c
Nico de Haen <mail@ndh-websolutions.de>
2019-05-05 17:36:11 +0200

    Add location store
    Handle events from DC_CORE and dispatch events
    from UI
    
    The event DC_EVENT_LOCATION_CHANGED is only handled
    by location store:
    if it is not related to current chat just ignore it
    otherwise trigger a EVENT_DC_FUNCTION_CALL event for
    dc.getLocations
    
    In MapComponent we subscribe to this store to update
    when new locations are available

---
+++ src/renderer/stores/locations.js
@@ -0,0 +1,78 @@
+const { ipcRenderer } = require('electron')
+const { Store } = require('./store')
+
+const defaultState = {
+  selectedChat: null,
+  mapSettings: {
+    timestampFrom: 0,
+    timestampTo: 0
+  },
+  locations: []
+}
+const locationStore = new Store(defaultState)
+
+const getLocations = (chatId, mapSettings) => {
+  const { timestampFrom, timestampTo } = mapSettings
+  ipcRenderer.send(
+    'EVENT_DC_FUNCTION_CALL',
+    'getLocations',
+    chatId,
+    0,
+    timestampFrom,
+    timestampTo
+  )
+}
+
+ipcRenderer.on('DC_EVENT_LOCATION_CHANGED', (evt, payload) => {
+  const { selectedChat, mapSettings } = locationStore.getState()
+  const { contactId } = payload
+  if (contactId === 0) {
+    // this means all locations were deleted
+    getLocations(selectedChat, mapSettings)
+  }
+  if (selectedChat && selectedChat.contacts) {
+    const isMemberOfSelectedChat = selectedChat.contacts.find(contact => contact.id === contactId)
+    if (isMemberOfSelectedChat) {
+      getLocations(selectedChat, mapSettings)
+    }
+  }
+})
+
+// sometimes a MSGS_CHANGED is sent instead of locations changed
+ipcRenderer.on('DC_EVENT_MSGS_CHANGED', (evt, payload) => {
+  const { selectedChat, mapSettings } = locationStore.getState()
+  const { chatId } = payload
+  if (selectedChat && chatId === selectedChat.id) {
+    getLocations(chatId, mapSettings)
+  }
+})
+
+ipcRenderer.on('DC_EVENT_INCOMING_MSG', (evt, payload) => {
+  const { selectedChat, mapSettings } = locationStore.getState()
+  const { chatId } = payload
+  if (selectedChat && chatId === selectedChat.id) {
+    getLocations(chatId, mapSettings)
+  }
+})
+
+ipcRenderer.on('DD_EVENT_LOCATIONS_UPDATED', (evt, payload) => {
+  const { locations } = payload
+  locationStore.setState({ ...locationStore.getState(), locations: locations })
+})
+
+locationStore.reducers.push((action, state) => {
+  if (action.type === 'DC_GET_LOCATIONS') {
+    const { timestampFrom, timestampTo } = action.payload
+    state = Object.assign({}, state, { mapSettings: { timestampFrom: timestampFrom, timestampTo: timestampTo } })
+    return state
+  }
+})
+
+locationStore.effects.push((action, state) => {
+  if (action.type === 'DC_GET_LOCATIONS') {
+    const { chatId } = action.payload
+    getLocations(chatId, state.mapSettings)
+  }
+})
+
+module.exports = locationStore
---
+++ src/renderer/stores/store.js
@@ -0,0 +1,45 @@
+
+class Store {
+  constructor (state) {
+    this.state = state
+    this.listeners = []
+    this.reducers = []
+    this.effects = []
+  }
+
+  getState () {
+    return this.state
+  }
+
+  dispatch (action) {
+    let state = Object.assign({}, this.state)
+    this.reducers.forEach(reducer => {
+      state = reducer(action, state)
+    })
+    this.effects.forEach(effect => {
+      effect(action, state)
+    })
+    this.state = state
+    this.listeners.forEach(listener => listener(this.state))
+  }
+
+  subscribe (listener) {
+    this.listeners.push(listener)
+    return () => {
+      const index = this.listeners.indexOf(listener)
+      this.listeners.splice(index, 1)
+    }
+  }
+
+  unsubscribe (listener) {
+    const index = this.listeners.indexOf(listener)
+    this.listeners.splice(index, 1)
+  }
+
+  setState (state) {
+    this.state = state
+    this.listeners.forEach(listener => listener(this.state))
+  }
+}
+
+export { Store }
