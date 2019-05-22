41c3d9805a6e2be004e645007f42136f7488e3ad
jikstra <jikstra@disroot.org>
2019-05-22 23:13:41 +0200

    Refactored build script, reduced redundancy and improved start scripts. Fixed the build scripts to work with new deltachat-node and implemented build files for ubuntu 19.04 & 19.10.

--- ci_scripts/build-package.sh
+++ ci_scripts/build-package.sh
@@ -4,4 +4,20 @@ DIR=${1:?specify directory of ubuntu docker dir}
 export BUILDER_NAME=$(basename $DIR)
 export DOCKERTAG=deltachat/desktop-$BUILDER_NAME 
 
-docker run -e BUILDER_NAME --rm -it -v "${PWD}:/build"  -w /build-context $DOCKERTAG bash -i -c './build.sh'
+COMMAND="./build.sh"
+
+if [ $2 = '--debug' ]; then
+  COMMAND="$COMMAND && tail -f /dev/null"
+fi
+
+if [ $2 = '--shell' ]; then
+  COMMAND="tail -f /dev/null"
+fi
+
+docker run -e BUILDER_NAME --rm -it \
+  -v "${PWD}:/build" \
+  -v "${PWD}/ci_scripts/shared-build-context:/shared-build-context" \
+  -v "${PWD}/ci_scripts/${BUILDER_NAME}:/build-context" \
+  -w /build-context \
+  $DOCKERTAG \
+  bash -i -c $COMMAND
