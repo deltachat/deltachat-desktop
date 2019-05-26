e991cf94ad7b8ec36d9ac9856c35f55e6d7434dc
Simon Laux <mobile.info@simonlaux.de>
2019-05-26 20:39:57 +0200

    refactor contactlist to allow for other types of searchable lists

--- src/renderer/components/ContactList.js
+++ src/renderer/components/ContactList.js
@@ -2,15 +2,11 @@ const React = require('react')
 const { ipcRenderer } = require('electron')
 const C = require('deltachat-node/constants')
 const styled = require('styled-components').default
+const SearchableList = require('./SearchableList')
 
 const { RenderContact } = require('./Contact')
-const SearchInput = require('./SearchInput.js')
 
 const ContactListDiv = styled.div`
-  max-height: 400px;
-  overflow: scroll;
-  margin-top: 10px;
-  border: 1px solid darkgrey;
   .module-contact-list-item--with-click-handler {
     padding: 10px;
   }
@@ -19,30 +15,18 @@ const ContactListDiv = styled.div`
   }
 `
 
-class ContactList extends React.Component {
+class ContactList extends SearchableList {
   constructor (props) {
     super(props)
-    this.state = {
-      queryStr: '',
-      showVerifiedContacts: false
-    }
+    this.state.showVerifiedContacts = false
     this.handleSearch = this.handleSearch.bind(this)
     this.search = this.search.bind(this)
   }
 
-  handleSearch (event) {
-    this.search(event.target.value)
-  }
-
-  search (queryStr) {
-    this.setState({ queryStr })
-  }
-
-  componentDidMount () {
-    this.search('')
-  }
-
-  _getContacts () {
+  _getData () {
+    if (this.props.contacts) {
+      return this.props.contacts
+    }
     const listFlags = this.props.showVerifiedContacts ? C.DC_GCL_VERIFIED_ONLY : 0
     return ipcRenderer.sendSync(
       'getContacts',
@@ -52,29 +36,18 @@ class ContactList extends React.Component {
   }
 
   render () {
-    const { childProps, onContactClick, filter } = this.props
-    let contacts = this.props.contacts
-    if (!contacts) contacts = this._getContacts()
-    if (filter) contacts = contacts.filter(filter)
-    return <div>
-      <SearchInput
-        onChange={this.handleSearch}
-        value={this.state.queryStr}
-      />
-      <ContactListDiv>
-        {contacts.map((contact) => {
-          var props = childProps ? childProps(contact) : {}
-          return (
-            <RenderContact
-              key={contact.id}
-              onClick={() => onContactClick(contact)}
-              contact={contact}
-              {...props}
-            />
-          )
-        })}
-      </ContactListDiv>
-    </div>
+    return <ContactListDiv>{super.render()}</ContactListDiv>
+  }
+
+  renderItem (contact) {
+    const { childProps, onContactClick } = this.props
+    const props = childProps ? childProps(contact) : {}
+    return <RenderContact
+      key={contact.id}
+      onClick={() => onContactClick(contact)}
+      contact={contact}
+      {...props}
+    />
   }
 }
 module.exports = ContactList
