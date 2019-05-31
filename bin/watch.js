f00e1f0219951be805e9fcd94ae1597314b60e68
jikstra <jikstra@disroot.org>
2019-05-31 17:33:48 +0200

    Implement incremental builds on watching
    - We now use webpacks watch mode, this speeds up builds in watch mode quite a lot

--- bin/watch.js
+++ bin/watch.js
@@ -1,13 +1,6 @@
 var watch = require('glob-watcher')
 var child = require('child_process')
 
-watch([ './src/' ], function (done) {
-  var p = child.spawn('npm', ['run', 'build-js'])
-  p.stdout.pipe(process.stdout)
-  p.stderr.pipe(process.stderr)
-  p.on('close', done)
-})
-
 watch([
   './conversations/stylesheets'
 ], function (done) {
