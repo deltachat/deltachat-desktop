f6cda51e722835c200a151b27e4dcf256bdec144
Simon Laux <mobile.info@simonlaux.de>
2019-06-20 17:21:21 +0200

    match links with < > - don't take in the last > as part of the link

--- src/renderer/components/MessageMarkdown.js
+++ src/renderer/components/MessageMarkdown.js
@@ -47,7 +47,7 @@ const rules = Object.assign({
   }), // uses style of codeBlock
   link: {
     order: 18,
-    match: anyScopeRegex(/^(https?:\/\/[^\s<]+[^<.,:;"')\]\s])/),
+    match: anyScopeRegex(/^(https?:\/\/[^\s<]+[^<>.,:;"')\]\s])/),
     parse: function (capture, recurseParse, state) {
       return { content: capture[1] }
     },
