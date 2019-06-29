baac90815882be1a0849375e66e62e8f04bef0da
Simon Laux <mobile.info@simonlaux.de>
2019-06-29 16:51:11 +0200

    make version selectable #878

--- src/renderer/components/dialogs/About.js
+++ src/renderer/components/dialogs/About.js
@@ -75,7 +75,7 @@ class About extends React.Component {
         onClose={onClose}
         canOutsideClickClose={false}>
         <div className={Classes.DIALOG_BODY}>
-          <p style={{ color: 'grey' }}>{`Version ${appVersion()}`}</p>
+          <p style={{ color: 'grey', userSelect: 'all' }}>{`Version ${appVersion()}`}</p>
           <p>Official Delta Chat Desktop app.</p>
           <p>This software is licensed under <ClickableLink href={gitHubLicenseUrl()} text='GNU GPL version 3' />.</p>
           <p>Source code is available on <ClickableLink href={gitHubUrl()} text='GitHub' />.</p>
