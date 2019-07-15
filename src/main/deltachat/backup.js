b9772376ea48e7fb614930f114aa1bfbcb4fd93d
jikstra <jikstra@disroot.org>
2019-07-16 00:10:27 +0200

    Fixing standard

--- src/main/deltachat/backup.js
+++ src/main/deltachat/backup.js
@@ -4,7 +4,7 @@ const binding = require('deltachat-node/binding')
 const events = require('deltachat-node/events')
 const tempy = require('tempy')
 const fs = require('fs-extra')
-const {promisify} = require('util');
+const { promisify } = require('util')
 const windows = require('../windows')
 const { ipcMain } = require('electron')
 const path = require('path')
@@ -16,19 +16,17 @@ function backupExport (dir) {
 }
 
 function backupImport (file) {
-
   let self = this
 
-  async function moveImportedConfigFolder(addr, newPath, overwrite=false) {
-    if(overwrite === true) {
+  async function moveImportedConfigFolder (addr, newPath, overwrite = false) {
+    if (overwrite === true) {
       await fs.remove(newPath)
     }
     await fs.move(tmpConfigPath, newPath)
     console.log(`backupImport: ${tmpConfigPath} successfully copied to ${newPath}`)
   }
 
-
-  function onErrorImport(err) {
+  function onErrorImport (err) {
     self.sendToRenderer('DD_EVENT_BACKUP_IMPORT_ERROR', err)
   }
 
@@ -42,51 +40,51 @@ function backupImport (file) {
       log.debug(`closed context for getConfig ${dir}`)
     })
     cb(err, result)
-    
   }
 
-  const dcn_event = new EventEmitter();
+  const dcnEvent = new EventEmitter()
   binding.dcn_set_event_handler(dcn_context, (event, data1, data2) => {
-    eventStr = events[event]
+    const eventStr = events[event]
     log.debug('backup event:', eventStr, data1, data2)
-    dcn_event.emit(eventStr, data1, data2)
+    dcnEvent.emit(eventStr, data1, data2)
   })
-  
-  dcn_event.on('DC_EVENT_IMEX_PROGRESS', progress => {
+
+  dcnEvent.on('DC_EVENT_IMEX_PROGRESS', progress => {
     this.sendToRenderer('DD_EVENT_IMPORT_PROGRESS', progress / 2)
-    if(progress == 0) {
+    if (progress === 0) {
       onErrorImport('UNKNOWN_ERROR')
     }
-    if(progress == 1000) {
+    if (progress === 1000) {
       onSuccessfulImport()
     }
   })
 
-  function onSuccessfulMove(addr) {
+  function onSuccessfulMove (addr) {
     self.sendToRenderer('DD_EVENT_BACKUP_IMPORTED', addr)
   }
 
-  function onSuccessfulImport() {
+  function onSuccessfulImport () {
     const addr = binding.dcn_get_config(dcn_context, 'addr')
 
     log.debug(`backupImport: Closing dc instance...`)
     binding.dcn_close(dcn_context, async (err) => {
+      if (err) throw err
 
-      self.sendToRenderer('DD_EVENT_IMPORT_PROGRESS', 600) 
+      self.sendToRenderer('DD_EVENT_IMPORT_PROGRESS', 600)
 
       let newPath = self.getPath(addr)
       let configFolderExists = await fs.pathExists(newPath)
 
-      if(configFolderExists) {
+      if (configFolderExists) {
         log.debug(`backupImport: ${newPath} already exists`)
         self.sendToRenderer('DD_EVENT_BACKUP_IMPORT_EXISTS', true)
         self.sendToRenderer('DD_EVENT_IMPORT_PROGRESS', 700)
-    
+
         ipcMain.once('DU_EVENT_BACKUP_IMPORT_OVERWRITE', async () => {
           log.debug('DU_EVENT_OVERWRITE_IMPORT')
           await moveImportedConfigFolder(addr, newPath, true)
           onSuccessfulMove(addr)
-        })        
+        })
       } else {
         log.debug(`backupImport: ${newPath} does not exist, moving...`)
         self.sendToRenderer('DD_EVENT_IMPORT_PROGRESS', 700)
@@ -94,18 +92,16 @@ function backupImport (file) {
         await moveImportedConfigFolder(addr, newPath, false)
         onSuccessfulMove(addr)
       }
-
-    })    
+    })
   }
   binding.dcn_open(dcn_context, db, '', err => {
     if (err) return done(err)
     log.debug(`openend context`)
     log.debug(`Starting backup import of ${file}`)
-    
+
     binding.dcn_start_threads(dcn_context)
     binding.dcn_imex(dcn_context, C.DC_IMEX_IMPORT_BACKUP, file, '')
   })
-
 }
 
 module.exports = function () {
--- src/renderer/components/LoginScreen.js
+++ src/renderer/components/LoginScreen.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect, Fragment} from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import { remote } from 'electron'
-import { sendToBackend, ipcBackend}  from '../ipc'
+import { sendToBackend, ipcBackend } from '../ipc'
 import NavbarWrapper from './NavbarWrapper'
 import confirmation from './dialogs/confirmationDialog'
 import styled from 'styled-components'
@@ -10,7 +10,6 @@ import {
   Classes,
   Elevation,
   Intent,
-  H5,
   Card,
   Alignment,
   Navbar,
@@ -23,7 +22,6 @@ import { DeltaHeadline, DeltaBlueButton, DeltaProgressBar } from './Login-Styles
 import logger from '../../logger'
 
 const log = logger.getLogger('renderer/components/LoginScreen')
-
 
 const LoginWrapper = styled.div`
   align-items: center;
@@ -69,7 +67,7 @@ const LoginItem = styled.li`
     width: 90%;
   }
 `
-const ImportDialogContent = React.memo(function ImportDialogContent(props) {
+const ImportDialogContent = React.memo(function ImportDialogContent (props) {
   const tx = window.translate
   const [importProgress, setImportProgress] = useState(0)
   const [error, setError] = useState(null)
@@ -79,10 +77,15 @@ const ImportDialogContent = React.memo(function ImportDialogContent(props) {
 
   useEffect(() => {
     log.debug('useEffect', ipcBackend)
+    ipcBackend.on('ALL', (eventName, data1, data2) => log.debug('ALL core events: ', eventName, data1, data2))
     ipcBackend.on('DD_EVENT_CHATLIST_UPDATED', () => log.debug('test'))
     ipcBackend.on('DD_EVENT_IMPORT_PROGRESS', (evt, progress) => {
       log.debug('DC_EVENT_IMEX_PROGRESS', progress)
       setImportProgress(progress)
+    })
+
+    ipcBackend.on('DC_EVENT_ERROR', (data1, data2) => {
+      setError('DC_EVENT_ERROR: ' + data2)
     })
 
     ipcBackend.on('DD_EVENT_BACKUP_IMPORTED', (evt, a) => {
@@ -97,27 +100,27 @@ const ImportDialogContent = React.memo(function ImportDialogContent(props) {
     })
   }, [])
 
-  function overwriteBackup() {
+  function overwriteBackup () {
     sendToBackend('DU_EVENT_BACKUP_IMPORT_OVERWRITE')
   }
 
   return (
     <div className={Classes.DIALOG_BODY}>
       { error && <p>Error: {error}</p>}
-      { importState[0] == 'INIT' && <p></p> }
-      { importState[0] == 'IMPORT_EXISTS' && 
+      { importState[0] === 'INIT' && <p /> }
+      { importState[0] === 'IMPORT_EXISTS' &&
         <Card elevation={Elevation.ONE}>
           <Fragment>
             {`Seems like there's already an existing Account with the ${addr} address.
           To import this backup you need to overwrite the existing account. Do you want to?`}
           </Fragment>
-          <br/>
+          <br />
           <Button onClick={overwriteBackup} type='submit' text='Yes!' />
           <Button onClick={props.onClose} type='cancel' text={tx('cancel')} />
         </Card>
       }
-      { importState[0] == 'INIT' && <p></p> }
-      { importState[0] == 'IMPORT_COMPLETE' && 
+      { importState[0] === 'INIT' && <p /> }
+      { importState[0] === 'IMPORT_COMPLETE' &&
         <Card elevation={Elevation.ONE}>
           Successfully imported backup
         </Card>
@@ -131,11 +134,11 @@ const ImportDialogContent = React.memo(function ImportDialogContent(props) {
   )
 })
 
-const ImportButton = React.memo(function ImportButton(props) {
+const ImportButton = React.memo(function ImportButton (props) {
   const tx = window.translate
   const [showDialog, setShowDialog] = useState(false)
 
-  function onClickImportBackup() {
+  function onClickImportBackup () {
     const opts = {
       title: tx('import_backup_title'),
       properties: ['openFile'],
@@ -153,20 +156,20 @@ const ImportButton = React.memo(function ImportButton(props) {
     sendToBackend('updateLogins')
   }
 
-  return(
+  return (
     <Fragment>
       <DeltaBlueButton onClick={onClickImportBackup} >
         <p>{tx('import_backup_title') }</p>
       </DeltaBlueButton>
       {showDialog &&
         <Dialog
-          icon="info-sign"
+          icon='info-sign'
           onClose={onHandleClose}
-          title={tx('import_backup_title')} 
-          canOutsideClickClose={true}
+          title={tx('import_backup_title')}
+          canOutsideClickClose
           isOpen={showDialog}
         >
-          <ImportDialogContent onClose={onHandleClose} />  
+          <ImportDialogContent onClose={onHandleClose} />
         </Dialog> }
     </Fragment>
   )
@@ -184,7 +187,6 @@ export default function LoginScreen (props) {
       if (yes) sendToBackend('forgetLogin', login)
     })
   }
-
 
   return (
     <LoginWrapper>
@@ -216,7 +218,7 @@ export default function LoginScreen (props) {
             <Button type='submit' text={tx('login_title')} />
             <Button type='cancel' text={tx('cancel')} />
           </Login>
-          <ImportButton/>
+          <ImportButton />
         </Card>
       </div>
     </LoginWrapper>
