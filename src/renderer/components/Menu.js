0ba3bea93b91d7d6b9c5878f33759d7641016f22
jikstra <jikstra@disroot.org>
2019-07-31 19:17:25 +0200

    - Refactor Menu component   - Convert Menu component to hook function   - use import syntax in Menu   - use ScreenContext instead of passing down screen methods - use new ConfirmationDialog in Menu

--- src/renderer/components/Menu.js
+++ src/renderer/components/Menu.js
@@ -1,160 +1,135 @@
-const React = require('react')
-const { ipcRenderer } = require('electron')
-const autobind = require('class-autobind').default
-const confirmation = require('./dialogs/confirmationDialog').default
+import React, { useContext } from 'react'
+import { ipcRenderer } from 'electron'
+import ScreenContext from '../contexts/ScreenContext'
 
-const {
+import {
   Menu,
   MenuItem
-} = require('@blueprintjs/core')
+} from '@blueprintjs/core'
 
-class Controller {
-  constructor (props) {
-    this.props = props
-    autobind(this)
-  }
+export default function DeltaMenu (props) {
+  const {
+    selectedChat,
+    showArchivedChats
+  } = props
 
-  onCreateChat () {
-    this.props.changeScreen('CreateChat')
-  }
+  const tx = window.translate
 
-  onEditGroup () {
-    this.props.changeScreen('EditGroup', { chat: this.props.selectedChat })
-  }
+  const isGroup = selectedChat && selectedChat.isGroup
+  const screenContext = useContext(ScreenContext)
 
-  onLeaveGroup () {
-    const selectedChat = this.props.selectedChat
+  let chatMenu = <div />
+
+  const onCreateChat = () => screenContext.changeScreen('CreateChat')
+
+  const onEditGroup = () => screenContext.changeScreen('EditGroup', { chat: selectedChat })
+
+  const onLeaveGroup = () => {
     const tx = window.translate
-    confirmation(tx('ask_leave_group'), yes => {
-      if (yes) {
-        ipcRenderer.send('EVENT_DC_FUNCTION_CALL', 'leaveGroup', selectedChat.id)
-      }
-    })
+    screenContext.openDialog('ConfirmationDialog', {
+      message: tx('ask_leave_group'),
+      cb: yes => {
+        if (yes) {
+          ipcRenderer.send('EVENT_DC_FUNCTION_CALL', 'leaveGroup', selectedChat.id)
+        }
+      } })
   }
 
-  onArchiveChat (archive) {
-    const selectedChat = this.props.selectedChat
-    ipcRenderer.send('EVENT_DC_FUNCTION_CALL', 'archiveChat', selectedChat.id, archive)
-  }
+  const onArchiveChat = archive => ipcRenderer.send('EVENT_DC_FUNCTION_CALL', 'archiveChat', selectedChat.id, archive)
 
-  onBlockContact () {
-    const selectedChat = this.props.selectedChat
+  const onBlockContact = () => {
     const tx = window.translate
     if (selectedChat && selectedChat.contacts.length) {
       var contact = selectedChat.contacts[0]
-      confirmation(tx('ask_block_contact'), yes => {
+      screenContext.openDialog('ConfirmationDialog', {
+        message: tx('ask_block_contact'),
+        cb: yes => {
+          if (yes) {
+            ipcRenderer.send('blockContact', contact.id)
+          }
+        } })
+    }
+  }
+
+  const onDeleteChat = () => {
+    const tx = window.translate
+    screenContext.openDialog('ConfirmationDialog', {
+      message: tx('ask_delete_chat_desktop'),
+      cb: yes => {
         if (yes) {
-          ipcRenderer.send('blockContact', contact.id)
+          ipcRenderer.send('EVENT_DC_FUNCTION_CALL', 'deleteChat', selectedChat.id)
         }
-      })
-    }
+      } })
   }
 
-  onDeleteChat () {
-    const selectedChat = this.props.selectedChat
-    const tx = window.translate
-    confirmation(tx('ask_delete_chat_desktop'), yes => {
-      if (yes) {
-        ipcRenderer.send('EVENT_DC_FUNCTION_CALL', 'deleteChat', selectedChat.id)
+  const onUnblockContacts = () => screenContext.changeScreen('UnblockContacts')
+
+  const onContactRequests = () => ipcRenderer.send('EVENT_DC_FUNCTION_CALL', 'contactRequests')
+
+  const logout = () => ipcRenderer.send('logout')
+
+  const onEncrInfo = () => screenContext.openDialog('EncrInfo', { chat: selectedChat })
+
+  if (selectedChat && !selectedChat.isDeaddrop) {
+    chatMenu = <div>
+      <Menu.Divider />
+      {showArchivedChats
+        ? <MenuItem icon='export' text={tx('menu_unarchive_chat')}
+          onClick={() => onArchiveChat(false)} />
+        : <MenuItem icon='import' text={tx('menu_archive_chat')}
+          onClick={() => onArchiveChat(true)} />
       }
-    })
+      <MenuItem
+        icon='delete'
+        text={tx('menu_delete_chat')}
+        onClick={onDeleteChat} />
+      <MenuItem
+        icon='lock'
+        text={tx('encryption_info_desktop')}
+        onClick={onEncrInfo} />
+      {isGroup
+        ? (
+          <div>
+            <MenuItem
+              icon='edit'
+              text={tx('menu_edit_group')}
+              onClick={onEditGroup}
+            />
+            <MenuItem
+              icon='log-out' text={tx('menu_leave_group')}
+              onClick={onLeaveGroup}
+            />
+          </div>
+        ) : <MenuItem
+          icon='blocked-person'
+          text={tx('menu_block_contact')}
+          onClick={onBlockContact}
+        />
+      }
+      <Menu.Divider />
+    </div>
+  } else {
+    chatMenu = <Menu.Divider />
   }
 
-  onUnblockContacts () {
-    this.props.changeScreen('UnblockContacts')
-  }
-
-  onContactRequests () {
-    ipcRenderer.send('EVENT_DC_FUNCTION_CALL', 'contactRequests')
-  }
-
-  logout () {
-    ipcRenderer.send('logout')
-  }
-
-  onEncrInfo () {
-    this.props.openDialog('EncrInfo', { chat: this.props.selectedChat })
-  }
+  return (<Menu>
+    <MenuItem icon='plus' text={tx('menu_new_chat')} onClick={() => onCreateChat(screenContext)} />
+    {chatMenu}
+    <MenuItem
+      icon='settings'
+      text={tx('menu_settings')}
+      onClick={() => screenContext.openDialog('Settings')}
+    />
+    <MenuItem
+      icon='person'
+      text={tx('menu_deaddrop')}
+      onClick={onContactRequests}
+    />
+    <MenuItem
+      icon='blocked-person'
+      text={tx('unblock_contacts_desktop')}
+      onClick={onUnblockContacts}
+    />
+    <MenuItem icon='log-out' text={tx('logout_desktop')} onClick={logout} />
+  </Menu>)
 }
-
-class DeltaMenu extends React.Component {
-  render () {
-    const {
-      openDialog,
-      selectedChat,
-      showArchivedChats
-    } = this.props
-
-    const tx = window.translate
-
-    const isGroup = selectedChat && selectedChat.isGroup
-    const controller = new Controller(this.props)
-
-    let chatMenu = <div />
-
-    if (selectedChat && !selectedChat.isDeaddrop) {
-      chatMenu = <div>
-        <Menu.Divider />
-        {showArchivedChats
-          ? <MenuItem icon='export' text={tx('menu_unarchive_chat')}
-            onClick={() => controller.onArchiveChat(false)} />
-          : <MenuItem icon='import' text={tx('menu_archive_chat')}
-            onClick={() => controller.onArchiveChat(true)} />
-        }
-        <MenuItem
-          icon='delete'
-          text={tx('menu_delete_chat')}
-          onClick={controller.onDeleteChat} />
-        <MenuItem
-          icon='lock'
-          text={tx('encryption_info_desktop')}
-          onClick={controller.onEncrInfo} />
-        {isGroup
-          ? (
-            <div>
-              <MenuItem
-                icon='edit'
-                text={tx('menu_edit_group')}
-                onClick={controller.onEditGroup}
-              />
-              <MenuItem
-                icon='log-out' text={tx('menu_leave_group')}
-                onClick={controller.onLeaveGroup}
-              />
-            </div>
-          ) : <MenuItem
-            icon='blocked-person'
-            text={tx('menu_block_contact')}
-            onClick={controller.onBlockContact}
-          />
-        }
-        <Menu.Divider />
-      </div>
-    } else {
-      chatMenu = <Menu.Divider />
-    }
-
-    return (<Menu>
-      <MenuItem icon='plus' text={tx('menu_new_chat')} onClick={controller.onCreateChat} />
-      {chatMenu}
-      <MenuItem
-        icon='settings'
-        text={tx('menu_settings')}
-        onClick={() => openDialog('Settings')}
-      />
-      <MenuItem
-        icon='person'
-        text={tx('menu_deaddrop')}
-        onClick={controller.onContactRequests}
-      />
-      <MenuItem
-        icon='blocked-person'
-        text={tx('unblock_contacts_desktop')}
-        onClick={controller.onUnblockContacts}
-      />
-      <MenuItem icon='log-out' text={tx('logout_desktop')} onClick={controller.logout} />
-    </Menu>)
-  }
-}
-
-module.exports = DeltaMenu
