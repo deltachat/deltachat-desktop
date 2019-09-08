7506574134c730cc1eb13c80d83c64b9d67d6534
Nico de Haen <mail@ndh-websolutions.de>
2019-09-08 16:44:36 +0200

    Use callback for getLocations

--- src/main/deltachat/locations.js
+++ src/main/deltachat/locations.js
@@ -3,8 +3,7 @@ function setLocation (latitude, longitude, accuracy) {
 }
 
 function getLocations (chatId, contactId, timestampFrom, timestampTo) {
-  const locations = this._dc.getLocations(chatId, contactId, timestampFrom, timestampTo)
-  this.sendToRenderer('DD_EVENT_LOCATIONS_UPDATED', { locations, chatId, contactId, timestampFrom, timestampTo })
+  return this._dc.getLocations(chatId, contactId, timestampFrom, timestampTo)
 }
 
 module.exports = function () {
