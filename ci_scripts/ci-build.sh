e224a47eb5cee9b6bea88da2f29baf021be43798
jikstra <jikstra@disroot.org>
2019-09-16 14:31:56 +0200

    Add version logging

--- ci_scripts/ci-build.sh
+++ ci_scripts/ci-build.sh
@@ -23,5 +23,10 @@ if [ -z "$TRAVIS_OS_NAME" ]; then
 fi
 
 export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig
+
+node --version
+npm --version
+cargo --version
+
 npm install;
 npm run build;
