374ff24e0ea256d02decb2a6f779fda1351fe940
jikstra <jikstra@disroot.org>
2019-09-30 16:32:49 +0200

    Reimplement backed stuff for stickers

--- src/main/deltachat/index.js
+++ src/main/deltachat/index.js
@@ -15,6 +15,7 @@ class DeltaChatController extends EventEmitter {
   constructor (cwd, saved) {
     super()
     this.cwd = cwd
+    this.fullCwd = false
     this._resetState()
     if (!saved) throw new Error('Saved settings are a required argument to DeltaChatController')
     this.loadSplitOuts()
@@ -29,6 +30,7 @@ class DeltaChatController extends EventEmitter {
     require('./login').bind(this)()
     require('./messagelist').bind(this)()
     require('./settings').bind(this)()
+    require('./stickers').bind(this)()
   }
 
   logCoreEvent (event, data1, data2) {
@@ -41,14 +43,20 @@ class DeltaChatController extends EventEmitter {
     log.debug('Core Event', event, data1, data2)
   }
 
-  callMethod (evt, methodName, args) {
+  async callMethod (evt, methodName, args) {
     if (typeof this[methodName] !== 'function') {
       const message = 'Method is not of type function: ' + methodName
       log.error(message)
       throw new Error(message)
     }
 
-    return this[methodName](...args)
+    let returnValue
+    try {
+      returnValue = await this[methodName](...args)
+    } catch (err) {
+      log.error(`Error calling ${methodName}(${args.join(', ')}):\n ${err.stack}`)
+    }
+    return returnValue
   }
 
   /**
