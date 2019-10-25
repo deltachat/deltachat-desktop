8cc2d4ad2b62d7f5df8e9bd729abb8a749da60fc
Nico de Haen <mail@ndh-websolutions.de>
2019-10-25 18:09:35 +0200

    Unselect chat
    resolves #1059

--- src/renderer/components/helpers/ChatMethods.js
+++ src/renderer/components/helpers/ChatMethods.js
@@ -1,7 +1,12 @@
 import { callDcMethod } from '../../ipc'
+import chatStore from '../../stores/chat'
+
+const unselectChat = () => {
+  chatStore.dispatch({ type: 'UI_UNSELECT_CHAT' })
+}
 
 export function archiveChat (chatId, archive) {
-  callDcMethod('chat.archive', [chatId, archive])
+  callDcMethod('chat.archive', [chatId, archive], unselectChat)
 }
 
 export function openLeaveChatDialog (screenContext, chatId) {
@@ -16,7 +21,7 @@ export function openDeleteChatDialog (screenContext, chat) {
   const tx = window.translate
   screenContext.openDialog('ConfirmationDialog', {
     message: tx('ask_delete_chat_desktop', chat.name),
-    cb: yes => yes && callDcMethod('chat.delete', chat.id)
+    cb: yes => yes && callDcMethod('chat.delete', chat.id, unselectChat)
   })
 }
 
@@ -26,7 +31,7 @@ export function openBlockContactDialog (screenContext, selectedChat) {
     const contact = selectedChat.contacts[0]
     screenContext.openDialog('ConfirmationDialog', {
       message: tx('ask_block_contact'),
-      cb: yes => yes && callDcMethod('contacts.blockContact', contact.id)
+      cb: yes => yes && callDcMethod('contacts.blockContact', contact.id, unselectChat)
     })
   }
 }
