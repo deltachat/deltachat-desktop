963a322ffa83c70ebea104ed51a644eb28d9d53e
Alexander Krotov <ilabdsf@gmail.com>
2019-10-26 02:46:03 +0300

    Consistently capitalize "STARTTLS"
    It is "STARTTLS" in all places but this one.

--- src/renderer/components/Login.js
+++ src/renderer/components/Login.js
@@ -203,7 +203,7 @@ export default class Login extends React.Component {
             >
               <option value='automatic'>Automatic</option>
               <option value='ssl'>SSL/TLS</option>
-              <option value='starttls'>StartTLS</option>
+              <option value='starttls'>STARTTLS</option>
               <option value='plain'>{tx('off')}</option>
             </DeltaSelect>
 
