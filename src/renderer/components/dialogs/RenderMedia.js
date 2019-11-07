5e312ffe30761c74ea37f89ae43ba6527b8cf802
Simon Laux <mobile.info@simonlaux.de>
2019-11-07 02:13:30 +0100

    move attachmentview SC styles to scss

--- src/renderer/components/dialogs/RenderMedia.js
+++ src/renderer/components/dialogs/RenderMedia.js
@@ -1,49 +1,8 @@
 const React = require('react')
-const styled = require('styled-components').default
 const createGlobalStyle = require('styled-components').createGlobalStyle
 const { Icon, Overlay } = require('@blueprintjs/core')
 
 const OverlayGlobal = createGlobalStyle`
-`
-
-const Container = styled.div`
-    height: 100%;
-    padding: 0;
-    margin: 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: white;
-    
-    img, video {
-      width: 100vw;
-      max-height: 100vh;
-      object-fit: contain;
-    }
-    video {
-      width: 95vw;
-    }
-`
-
-const RenderMediaWrapper = styled.div`
-  width: 100vw;
-  height: 100vh;
-`
-
-const CloseButtonWrapper = styled.div`
-  float: right;
-  position: absolute;
-  right: 5px;
-  cursor: pointer;
-  z-index: 10;
-`
-
-const DownloadButtonWrapper = styled.div`
-  float: right;
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  z-index: 10;
 `
 
 class RenderMedia extends React.Component {
@@ -76,22 +35,22 @@ class RenderMedia extends React.Component {
         className='attachment-overlay'
         onClose={onClose}>
         <OverlayGlobal />
-        <RenderMediaWrapper>
+        <div className='render-media-wrapper'>
           {elm &&
-            <CloseButtonWrapper>
+            <div className='btn-wrapper' style={{ right: '5px' }}>
               <Icon onClick={onClose} icon='cross' iconSize={32} color={'grey'} aria-label={tx('close')} />
-            </CloseButtonWrapper>
+            </div>
           }
-          <Container>
+          <div className='attachment-view'>
             {elm}
-          </Container>
-          <DownloadButtonWrapper>
+          </div>
+          <div className='btn-wrapper' style={{ right: 0, bottom: 0 }}>
             <div role='button'
               onClick={message.onDownload}
-              className='msg-button download icon-medium'
+              className='download-btn'
               aria-label={tx('save')} />
-          </DownloadButtonWrapper>
-        </RenderMediaWrapper>
+          </div>
+        </div>
       </Overlay>
     )
   }
