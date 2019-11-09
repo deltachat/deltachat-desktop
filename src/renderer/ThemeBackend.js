4bdb0b7ad3ea69e08558cf0ec3a35463dab1e128
maris <maris@cccfr.de>
2019-11-09 22:14:11 +0100

    add theming support to mediaview

--- src/renderer/ThemeBackend.js
+++ src/renderer/ThemeBackend.js
@@ -181,7 +181,7 @@ export function ThemeDataBuilder (theme) {
       theme.bgSecondary, c => blendColor(c, invertColor(c), 0.2)
     ),
     // Misc
-    galleryBg: theme.bgSecondary,
+    galleryBg: theme.bgPrimary,
     avatarLabelColor: '#ffffff', // Only changable with theme.raw
     brokenMediaText: '#070c14',
     brokenMediaBg: '#ffffff',
