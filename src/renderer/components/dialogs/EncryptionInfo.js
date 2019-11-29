3607611eb3e3af9dac29261d0c47490ce9c2a69e
jikstra <jikstra@disroot.org>
2019-11-30 00:32:11 +0100

    Fix styling

--- src/renderer/components/dialogs/EncryptionInfo.js
+++ src/renderer/components/dialogs/EncryptionInfo.js
@@ -8,7 +8,7 @@ export default function EncryptionInfo (props) {
   useEffect(() => {
     const { chat } = props
     if (!chat) return
-    callDcMethod('chat.getEncryptionInfo', chat.contacts[0].id, setEncryptionInfo)
+    callDcMethod('chat.getEncryptionInfo', chat.contactIds[0], setEncryptionInfo)
   })
 
   const { isOpen, onClose } = props
