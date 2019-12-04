4e8af455be951441cc7f230ebf07c2dcf0c9fc0f
jikstra <jikstra@disroot.org>
2019-12-04 08:17:28 +0100

    Fix accepting contact request

--- src/renderer/components/dialogs/DeadDrop.js
+++ src/renderer/components/dialogs/DeadDrop.js
@@ -1,65 +1,54 @@
 import React from 'react'
-import { callDcMethod } from '../../ipc'
+import { callDcMethod, callDcMethodAsync } from '../../ipc'
 import { Classes } from '@blueprintjs/core'
 import SmallDialog, { DeltaButton, DeltaButtonPrimary, DeltaButtonDanger } from './SmallDialog'
+import { useChatStore } from '../../stores/chat'
 
-export default class DeadDrop extends React.Component {
-  constructor (props) {
-    super(props)
-    this.yes = this.yes.bind(this)
-    this.never = this.never.bind(this)
-    this.close = this.close.bind(this)
+export default function DeadDrop (props) {
+  const { deaddrop, onClose } = props
+  const chatStoreDispatch = useChatStore()[1]
+  const yes = async () => {
+    const chatId = await callDcMethodAsync('contacts.acceptContactRequest', [deaddrop])
+    chatStoreDispatch({ type: 'SELECT_CHAT', payload: chatId })
+    onClose()
   }
 
-  yes () {
-    callDcMethod('contacts.acceptContactRequest', [this.props.deaddrop])
-    this.close()
+  const never = () => {
+    callDcMethod('contacts.blockContact', [deaddrop.contact.id])
+    onClose()
   }
 
-  close () {
-    this.props.onClose()
-  }
+  const isOpen = !!deaddrop
+  const nameAndAddr = deaddrop && deaddrop.contact && deaddrop.contact.nameAndAddr
 
-  never () {
-    callDcMethod('contacts.blockContact', [this.props.deaddrop.contact.id])
-    this.close()
-  }
+  const tx = window.translate
+  const body = tx('ask_start_chat_with', nameAndAddr)
 
-  render () {
-    const { deaddrop } = this.props
-
-    const isOpen = !!deaddrop
-    const nameAndAddr = deaddrop && deaddrop.contact && deaddrop.contact.nameAndAddr
-
-    const tx = window.translate
-    const body = tx('ask_start_chat_with', nameAndAddr)
-
-    return (
-      <SmallDialog
-        isOpen={isOpen}
-        onClose={this.close}
-      >
-        <div className='bp3-dialog-body-with-padding'>
-          <p>{body}</p>
-          <div className={Classes.DIALOG_FOOTER}>
-            <div
-              className={Classes.DIALOG_FOOTER_ACTIONS}
-              style={{ justifyContent: 'space-between', marginTop: '7px' }}
+  return (
+    <SmallDialog
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <div className='bp3-dialog-body-with-padding'>
+        <p>{body}</p>
+        <div className={Classes.DIALOG_FOOTER}>
+          <div
+            className={Classes.DIALOG_FOOTER_ACTIONS}
+            style={{ justifyContent: 'space-between', marginTop: '7px' }}
+          >
+            <DeltaButtonDanger bold={false} onClick={never}>
+              {tx('never').toUpperCase()}
+            </DeltaButtonDanger>
+            <DeltaButton
+              bold={false}
+              onClick={onClose}
             >
-              <DeltaButtonDanger bold={false} onClick={this.never}>
-                {tx('never').toUpperCase()}
-              </DeltaButtonDanger>
-              <DeltaButton
-                bold={false}
-                onClick={this.close}
-              >
-                {tx('not_now').toUpperCase()}
-              </DeltaButton>
-              <DeltaButtonPrimary bold={false} onClick={this.yes}>{tx('yes').toUpperCase()}</DeltaButtonPrimary>
-            </div>
+              {tx('not_now').toUpperCase()}
+            </DeltaButton>
+            <DeltaButtonPrimary bold={false} onClick={yes}>{tx('yes').toUpperCase()}</DeltaButtonPrimary>
           </div>
         </div>
-      </SmallDialog>
-    )
-  }
+      </div>
+    </SmallDialog>
+  )
 }
