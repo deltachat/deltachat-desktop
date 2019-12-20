39922bcd49cc4f5e383bbe44d646104bb11b3050
jikstra <jikstra@disroot.org>
2019-12-20 12:41:32 +0100

    Implement useChatStore2 method to easily get the chatStore and/or chatStoreDispatch. Start filling OneOnOneChatInfo dialog with content

--- src/renderer/components/dialogs/OneOnOneChatInfo.js
+++ src/renderer/components/dialogs/OneOnOneChatInfo.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react'
 import { Card, Classes } from '@blueprintjs/core'
-import { DeltaDialogBase, DeltaDialogHeader, DeltaDialogFooter } from './DeltaDialog'
+import { DeltaDialogBase, DeltaDialogHeader, DeltaDialogFooter, DeltaDialogBody } from './DeltaDialog'
 import { DeltaButtonPrimary } from './SmallDialog'
+import { useChatStore2 } from '../../stores/chat'
 
 export default function OneOnOneChatInfo (props) {
   const { isOpen, onClose } = props
   const [viewMode, setViewMode] = useState('main')
+  const { selectedChat } = useChatStore2()
+  console.log(selectedChat)
+
+  const tx = window.translate
 
   return (
     <DeltaDialogBase
@@ -13,8 +18,19 @@ export default function OneOnOneChatInfo (props) {
       onClose={onClose}
       fixed
     >
-      <p>Test</p>
+      <DeltaDialogHeader
+        title={tx('menu_view_profile')}
+        onClose={onClose}
+        borderBottom
+      />
+      <DeltaDialogBody>
+        <Card>
+        <p>{selectedChat.name}</p>
+        <p>{selectedChat.subtitle}</p>
+        </Card>
+      </DeltaDialogBody>
+      <DeltaDialogFooter>
+      </DeltaDialogFooter>
     </DeltaDialogBase>
   )
 }
-
