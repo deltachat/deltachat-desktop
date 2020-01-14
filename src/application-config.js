1bf33e55e15de925d17f7b951676d3bb969c63c6
developer <developer@mail.com>
2020-01-14 04:25:48 +0100

    Use PORTABLE_EXECUTABLE_DIR to determine if we're in portable mode -const fileContent = fs.readFileSync(appConfig, 'utf-8') -  .replace( -    /const portable = (?:false|true)/, -    `const portable = ${JSON.stringify(action === 'do')}` -  ) - -fs.writeFileSync(appConfig, fileContent) diff --git a/package.json b/package.json index 81f7a532..38a5f8be 100644 --- a/package.json +++ b/package.json @@ -18,7 +18,7 @@      "pack": "npm run build && electron-builder --dir",      "dist-ci": "version-unchanged || npm run dist",      "dist": "electron-builder --publish=onTagOrDraft", -    "dist-portable": "node bin/prepare-build-portable && electron-builder --win portable --linux AppImage && node bin/prepare-build-portable undo", +    "dist-portable": "electron-builder --win portable --linux AppImage",      "build": "npm run build-css && npm run build-js && npm run build-translations",      "build-css": "node-sass scss/manifest.scss static/main.css",      "build-js": "webpack --mode=development --progress --profile --colors", diff --git a/src/application-config.js b/src/application-config.js index be2a08f2..725068f1 100644 --- a/src/application-config.js +++ b/src/application-config.js @@ -1,14 +1,12 @@  const appConfig = require('application-config')('DeltaChat')  const path = require('path')
    -const portable = false
    -
     if (process.env.TEST_DIR) {
       appConfig.filePath = path.join(process.env.TEST_DIR, 'config.json')
    -} else if (portable) {
    +} else if (process.env.PORTABLE_EXECUTABLE_DIR) {
       /* ignore-console-log */
    -  console.log('Running in Portable Mode', process.cwd())
    -  appConfig.filePath = path.join(process.cwd(), 'DeltaChatData', 'config.json')
    +  console.log('Running in Portable Mode', process.env.PORTABLE_EXECUTABLE_DIR)
    +  appConfig.filePath = path.join(process.env.PORTABLE_EXECUTABLE_DIR, 'DeltaChatData', 'config.json')
     }
    
     module.exports = Object.freeze(appConfig)

--- src/application-config.js
+++ src/application-config.js
@@ -1,14 +1,12 @@
 const appConfig = require('application-config')('DeltaChat')
 const path = require('path')
 
-const portable = false
-
 if (process.env.TEST_DIR) {
   appConfig.filePath = path.join(process.env.TEST_DIR, 'config.json')
-} else if (portable) {
+} else if (process.env.PORTABLE_EXECUTABLE_DIR) {
   /* ignore-console-log */
-  console.log('Running in Portable Mode', process.cwd())
-  appConfig.filePath = path.join(process.cwd(), 'DeltaChatData', 'config.json')
+  console.log('Running in Portable Mode', process.env.PORTABLE_EXECUTABLE_DIR)
+  appConfig.filePath = path.join(process.env.PORTABLE_EXECUTABLE_DIR, 'DeltaChatData', 'config.json')
 }
 
 module.exports = Object.freeze(appConfig)
