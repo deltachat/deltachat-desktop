bd340e74446860b43ce6df68290805e61fb701fd
Nico de Haen <mail@ndh-websolutions.de>
2020-01-16 19:31:32 +0100

    Remove unused variable

--- test/integration/index.js
+++ test/integration/index.js
@@ -10,10 +10,10 @@ process.env.NODE_ENV = 'test'
 chai.should()
 chai.use(chaiAsPromised)
 const app = setup.createApp()
-const assert = chai.assert
+// const assert = chai.assert
 chai.config.includeStack = true
 
-const testMessage1 = 'Test message 1'
+// const testMessage1 = 'Test message 1'
 const conf = {
   account1: null,
   account2: null
