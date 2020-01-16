ea2f463cbdf9d942518137032cf3474261e2f6f5
Nico de Haen <mail@ndh-websolutions.de>
2020-01-16 09:07:01 +0100

    Debug 2nd logn failure

--- test/integration/index.js
+++ test/integration/index.js
@@ -108,7 +108,8 @@ describe('Login with other valid credentials works', function () {
     app.client
       .setValue('#addr', conf.account2.email)
       .setValue('#mail_pw', conf.account2.password)
-      .click('button[type=\'submit\']')
+    assert.equal(await app.client.getValue('#addr'), conf.account2.email)
+    app.client.click('button[type=\'submit\']')
 
     await app.client.waitUntilTextExists('h2', welcomeMessage, 20e3)
     app.client.getText('h2').should.eventually.equal(welcomeMessage)
@@ -116,64 +117,64 @@ describe('Login with other valid credentials works', function () {
   })
 })
 
-describe('Create chat and send message works', function () {
-  before(function () {
-    chaiAsPromised.transferPromiseness = app.transferPromiseness
-    return app.start()
-  })
-  after(function () {
-    if (app && app.isRunning()) {
-      return app.stop()
-    }
-  })
-  it('create chat', async () => {
-    domHelper.init(app)
-    await domHelper.login(conf.account2.email)
-    await app.client.waitUntilTextExists('h2', welcomeMessage, 20e3)
-    await domHelper.openMainMenuItem('New chat')
-    await app.client.waitUntilTextExists('p', 'New group', 20e3)
-    assert.isOk(await app.client.$('.FixedDeltaDialog'), 'Dialog is shown')
-    app.client.$('.FixedDeltaDialog input').setValue(conf.account1.email)
-    await app.client.waitUntilTextExists('p', 'New contact', 20e3)
-    assert.isOk(await app.client.$('p=New contact'), 'New contact button is visible')
-    await app.client.$('p=New contact').click()
-    domHelper.clickChatByName(conf.account1.email)
-  })
-  it('send message', async () => {
-    await app.client.waitForExist('#composer-textarea', 3000)
-    await app.client.setValue('#composer-textarea', testMessage1)
-    await app.client.click('button[aria-label=\'Send\']')
-    await domHelper.logout()
-  })
-})
+// describe('Create chat and send message works', function () {
+//   before(function () {
+//     chaiAsPromised.transferPromiseness = app.transferPromiseness
+//     return app.start()
+//   })
+//   after(function () {
+//     if (app && app.isRunning()) {
+//       return app.stop()
+//     }
+//   })
+//   it('create chat', async () => {
+//     domHelper.init(app)
+//     await domHelper.login(conf.account2.email)
+//     await app.client.waitUntilTextExists('h2', welcomeMessage, 20e3)
+//     await domHelper.openMainMenuItem('New chat')
+//     await app.client.waitUntilTextExists('p', 'New group', 20e3)
+//     assert.isOk(await app.client.$('.FixedDeltaDialog'), 'Dialog is shown')
+//     app.client.$('.FixedDeltaDialog input').setValue(conf.account1.email)
+//     await app.client.waitUntilTextExists('p', 'New contact', 20e3)
+//     assert.isOk(await app.client.$('p=New contact'), 'New contact button is visible')
+//     await app.client.$('p=New contact').click()
+//     domHelper.clickChatByName(conf.account1.email)
+//   })
+//   it('send message', async () => {
+//     await app.client.waitForExist('#composer-textarea', 3000)
+//     await app.client.setValue('#composer-textarea', testMessage1)
+//     await app.client.click('button[aria-label=\'Send\']')
+//     await domHelper.logout()
+//   })
+// })
 
-describe('Contact request and receive message works', function () {
-  before(function async () {
-    chaiAsPromised.transferPromiseness = app.transferPromiseness
-    return app.start()
-  })
-  after(function () {
-    if (app && app.isRunning()) {
-      return app.stop()
-    }
-  })
-  it('contact request is displayed', async () => {
-    domHelper.init(app)
-    await app.client.waitUntilTextExists('.bp3-button-text', conf.account1.email, 20e3)
-    await domHelper.login(conf.account1.email)
-    await app.client.waitUntilTextExists('h2', welcomeMessage, 20e3)
-    await app.client.waitUntilTextExists('.chat-list-item__name', 'Contact request', 20e3)
-    await domHelper.clickChatByName('Contact request')
-  })
-  it('contact request and message are displayed', async () => {
-    await app.client.waitUntilTextExists('p', 'YES', 20e3)
-    assert.isOk(await app.client.$('p=YES'), 'Contact request is visible')
-    await setup.wait(200)
-    await app.client.click('p=YES')
-    const messages = await app.client.$$('#message-list li')
-    assert.equal(messages.length, 1, 'Message is displayed')
-    const messageText = await app.client.$('.text').getText()
-    assert.equal(testMessage1, messageText)
-    await domHelper.logout()
-  })
-})
+// describe('Contact request and receive message works', function () {
+//   before(function async () {
+//     chaiAsPromised.transferPromiseness = app.transferPromiseness
+//     return app.start()
+//   })
+//   after(function () {
+//     if (app && app.isRunning()) {
+//       return app.stop()
+//     }
+//   })
+//   it('contact request is displayed', async () => {
+//     domHelper.init(app)
+//     await app.client.waitUntilTextExists('.bp3-button-text', conf.account1.email, 20e3)
+//     await domHelper.login(conf.account1.email)
+//     await app.client.waitUntilTextExists('h2', welcomeMessage, 20e3)
+//     await app.client.waitUntilTextExists('.chat-list-item__name', 'Contact request', 20e3)
+//     await domHelper.clickChatByName('Contact request')
+//   })
+//   it('contact request and message are displayed', async () => {
+//     await app.client.waitUntilTextExists('p', 'YES', 20e3)
+//     assert.isOk(await app.client.$('p=YES'), 'Contact request is visible')
+//     await setup.wait(200)
+//     await app.client.click('p=YES')
+//     const messages = await app.client.$$('#message-list li')
+//     assert.equal(messages.length, 1, 'Message is displayed')
+//     const messageText = await app.client.$('.text').getText()
+//     assert.equal(testMessage1, messageText)
+//     await domHelper.logout()
+//   })
+// })
