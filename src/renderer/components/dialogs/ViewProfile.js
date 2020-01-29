0ea03350af45742d32a2a0c6ca4542816676ba6a
Simon Laux <Simon-Laux@users.noreply.github.com>
2020-01-29 05:47:36 +0100

    add button for opening DM to profile view (#1385)
    * add button for opening DM to profile view
    fixes #1357
    
    * move the actions buttons in the profile details
    this affects only open chat ATM,
    but we could add a call button there, too
    Though we might need to switch to icon buttons then to save space.
    (in that case don't forget the accesibility strings!)

--- src/renderer/components/dialogs/ViewProfile.js
+++ src/renderer/components/dialogs/ViewProfile.js
@@ -6,19 +6,40 @@ import styled from 'styled-components'
 import ChatListItem from '../chat/ChatListItem'
 import { useChatListIds, useLazyChatListItems } from '../chat/ChatListHelpers'
 import { selectChat } from '../../stores/chat'
+import { callDcMethodAsync } from '../../ipc'
 
 export const ProfileInfoContainer = styled.div`
   margin-left: 10px;
-  display: inline-grid;
-  grid-template-columns: auto auto;
+  display: flex;
+  width: calc(100% - 20px);
   .profile-info-name-container {
     margin: auto 10px;
+    flex-grow: 1;
     .name {
       font-size: medium;
       font-weight: bold;
     }
     .address {
       color: #565656;
+    }
+  }
+  .actions {
+    display: flex;
+    align-items: center;
+
+    .open-dm-chat {
+      font-size: 16px;
+      color: #404092;
+      border: 1px solid #545eaf;
+      padding: 4px;
+      border-radius: 4px;
+      user-select: none;
+      cursor: pointer;
+      &:hover {
+        color: var(--chatListItemSelectedText);
+        background-color: var(--chatListItemSelectedBg);
+        font-weight: bold;
+      }
     }
   }
 `
@@ -75,6 +96,13 @@ export default function ViewProfile (props) {
           <ProfileInfoContainer>
             <ProfileInfoAvatar contact={contact} />
             <ProfileInfoName name={contact.displayName} address={contact.address} />
+            <div className='actions'>
+              <div className='open-dm-chat' role='button' onClick={() => {
+                callDcMethodAsync('contacts.getDMChatId', [contact.id]).then((chatId) => onChatClick(chatId))
+              }}>
+                {tx('profile_action_direct_message')}
+              </div>
+            </div>
           </ProfileInfoContainer>
           <DeltaDialogContentTextSeperator text={tx('profile_shared_chats')} />
           <div className='mutual-chats' ref={scrollRef} onScroll={onChatListScroll}>
