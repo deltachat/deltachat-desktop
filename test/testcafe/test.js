7f4ac53e24d0386516da1a3540ac82c129b1dfe1
Pablo <57864086+pabzm@users.noreply.github.com>
2020-02-02 15:45:10 +0100

    Test help page (#1409)
    * Adapt tests for help-page to testcafe.
    
    * Use test-helpers.
    
    * Include help-page.js into `test-e2e`-script.

--- test/testcafe/test.js
+++ test/testcafe/test.js
@@ -1,6 +1,7 @@
 import { Selector } from 'testcafe'
 import { waitForReact } from 'testcafe-react-selectors'
 const { createTmpUser } = require('../integration/fixtures/config')
+import { loginWithTmpUser, logout, clickAppMenuItem } from './helpers'
 
 /* global fixture, test */
 
@@ -11,16 +12,6 @@ const testMessage = 'Test message'
 let accountButton1 = null
 let accountButton2 = null
 
-async function openMainMenuItem (t, label) {
-  await t.click('#main-menu-button').wait(1000)
-  if (label) {
-    await t.expect(Selector('a.bp3-menu-item').withText(label).exists).ok()
-    await t.click(Selector('a.bp3-menu-item').withText(label))
-  }
-  return t
-}
-
-const logout = async (t) => openMainMenuItem(t, 'Switch account')
 
 const clickChatByName = async (t, name) => {
   return t.click(Selector('.chat-list-item__name').withText(name))
@@ -39,13 +30,8 @@ test('shows correct headline', async t => {
 })
 
 test('login works', async t => {
-  conf.account1 = await createTmpUser()
-  await t.expect(Selector('.bp3-navbar-heading').innerText).eql('Welcome to Delta Chat')
-    .typeText('#addr', conf.account1.email)
-    .typeText('#mail_pw', conf.account1.password)
-    .click('button[type=\'submit\']')
-    .expect(Selector('h2', { timeout: waitForLogin }).innerText).eql(welcomeMessage)
-  await logout(t)
+  conf.account1 = await loginWithTmpUser()
+  await logout()
 })
 
 test('login button is shown', async t => {
@@ -54,13 +40,8 @@ test('login button is shown', async t => {
 })
 
 test('second login works', async t => {
-  conf.account2 = await createTmpUser()
-  await t.expect(Selector('.bp3-navbar-heading').innerText).eql('Welcome to Delta Chat')
-    .typeText('#addr', conf.account2.email)
-    .typeText('#mail_pw', conf.account2.password)
-    .click('button[type=\'submit\']')
-    .expect(Selector('h2', { timeout: waitForLogin }).innerText).eql(welcomeMessage)
-  await logout(t)
+  conf.account2 = await loginWithTmpUser()
+  await logout()
 })
 
 test('both login buttons are shown', async t => {
@@ -72,7 +53,7 @@ test('both login buttons are shown', async t => {
 test('create chat', async t => {
   await t.click(accountButton1)
     .expect(Selector('h2', { timeout: waitForLogin }).innerText).eql(welcomeMessage)
-  await openMainMenuItem(t, 'New chat')
+  await clickAppMenuItem('New chat')
   await t.expect(Selector('.FixedDeltaDialog').exists).ok()
   await t.typeText('.FixedDeltaDialog input', conf.account2.email)
     .click(Selector('p').withText('New contact'))
@@ -83,7 +64,7 @@ test('write message', async t => {
   await t.typeText('#composer-textarea', testMessage)
     .click('button[aria-label=\'Send\']')
     .expect(Selector('#message-list li').count).eql(1)
-  await logout(t)
+  await logout()
 })
 
 test('Contact request and receive message works', async t => {
