4a6cdbf3b666fb9c4023c6bd671399dbfb023263
Simon Laux <mobile.info@simonlaux.de>
2020-02-11 13:14:30 +0100

    fix watch script

--- bin/watch.js
+++ bin/watch.js
@@ -7,3 +7,11 @@ watch(['./_locales/*.xml'], function (done) {
   p.stderr.pipe(process.stderr)
   p.on('close', done)
 })
+
+var tsc = child.spawn('npx', 'tsc -b src/renderer -w --pretty'.split(' '))
+tsc.stdout.pipe(process.stdout)
+tsc.stderr.pipe(process.stderr)
+
+var parcel = child.spawn('npx', 'parcel src/renderer/*.html --out-dir html-dist --public-url ./ --target electron'.split(' '))
+parcel.stdout.pipe(process.stdout)
+parcel.stderr.pipe(process.stderr)
