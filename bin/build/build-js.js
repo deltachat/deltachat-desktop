93a5e93bd5a3aae83558c4cbbd36fe96748927ef
Simon Laux <mobile.info@simonlaux.de>
2020-02-18 22:23:01 +0100

    fix standard

--- bin/build/build-js.js
+++ bin/build/build-js.js
@@ -1,56 +1,55 @@
 const child = require('child_process')
-const { join } = require('path')
 const fs = require('fs-extra')
 /**
- * 
- * @param {boolean} watch 
- * @param {boolean} sourcemap 
+ *
+ * @param {boolean} watch
+ * @param {boolean} sourcemap
  */
-async function jsBuilder(watch, sourcemap) {
-    if (watch) {
-        console.error("jsBuilder: Watch is not implemented yet")
-        return
-    }
+async function jsBuilder (watch, sourcemap) {
+  if (watch) {
+    console.error('jsBuilder: Watch is not implemented yet')
+    return
+  }
 
-    // note we can't turn of sourcemaps for tsc -b (without modifing the tsconfig file)
-    await run('npx', `tsc -b src/renderer --pretty`.split(' '))
-    console.log('TS compile completed')
+  // note we can't turn of sourcemaps for tsc -b (without modifing the tsconfig file)
+  await run('npx', `tsc -b src/renderer --pretty`.split(' '))
+  console.log('TS compile completed')
 
-    const parcelArgs = [
-        'parcel', 'build', 'tsc-dist/renderer/main.js',
-        '--out-dir', 'html-dist',
-        '--out-file', 'bundle.js',
-        '--public-url', './',
-        '--target', 'electron'
-    ]
-    if(!sourcemap) parcelArgs.push('--no-source-maps')
-    await run('npx', parcelArgs)
-    console.log('Parcel (bundle + minification) completed')
+  const parcelArgs = [
+    'parcel', 'build', 'tsc-dist/renderer/main.js',
+    '--out-dir', 'html-dist',
+    '--out-file', 'bundle.js',
+    '--public-url', './',
+    '--target', 'electron'
+  ]
+  if (!sourcemap) parcelArgs.push('--no-source-maps')
+  await run('npx', parcelArgs)
+  console.log('Parcel (bundle + minification) completed')
 
-    if(sourcemap){
-        // fix source maps
-        const source_map = await fs.readJSON('./html-dist/bundle.js.map')
-        source_map.sources = source_map.sources.map((source) => {
-            return source.replace(/\.\.\//g, '')
-        })
-        source_map.sourceRoot = '../'
-    
-        await fs.writeJSON('./html-dist/bundle.js.map', source_map)
-    }
+  if (sourcemap) {
+    // fix source maps
+    const sourceMap = await fs.readJSON('./html-dist/bundle.js.map')
+    sourceMap.sources = sourceMap.sources.map((source) => {
+      return source.replace(/\.\.\//g, '')
+    })
+    sourceMap.sourceRoot = '../'
+
+    await fs.writeJSON('./html-dist/bundle.js.map', sourceMap)
+  }
 }
 
 /**
- * 
+ *
  * @param {string[]} args arguments for the command
  */
-async function run(command, args) {
-    return new Promise((resolve, reject) => {
-        const p = child.spawn(command, args)
-        p.stdout.pipe(process.stdout)
-        p.stderr.pipe(process.stderr)
-        p.on('close', resolve)
-        p.on('error', reject)
-    })
+async function run (command, args) {
+  return new Promise((resolve, reject) => {
+    const p = child.spawn(command, args)
+    p.stdout.pipe(process.stdout)
+    p.stderr.pipe(process.stderr)
+    p.on('close', resolve)
+    p.on('error', reject)
+  })
 }
 
 module.exports = { jsBuilder }
--- bin/build/builder.js
+++ bin/build/builder.js
@@ -17,24 +17,25 @@ Only run specific stuff (default is everything):
 --translations  build only translations
 --static        copy only static files
 
-// --special      release stuff like generating thumbnails for bg images 
+// --special      release stuff like generating thumbnails for bg images
 
 */
 const child = require('child_process')
 const fs = require('fs-extra')
 const globWatch = require('glob-watcher')
-const rc = require('rc')("Deltachat-Desktop-Builder", {
-    // only some things are run:
-    styles: false,
-    js: false,
-    translations: false,
-    static: false,
-    special: false,
-    // general options
-    S: false, // no source maps
-    watch: false, w: false, //watch
+const rc = require('rc')('Deltachat-Desktop-Builder', {
+  // only some things are run:
+  styles: false,
+  js: false,
+  translations: false,
+  static: false,
+  special: false,
+  // general options
+  S: false, // no source maps
+  watch: false,
+  w: false // watch
 })
-const {jsBuilder} = require('./build-js')
+const { jsBuilder } = require('./build-js')
 
 const buildEverything = !(rc.styles || rc.js || rc.translations || rc.static || rc.special)
 
@@ -48,68 +49,66 @@ fs.ensureDirSync('./html-dist')
 
 // SCSS
 if (buildEverything || rc.styles) {
-    const command = [
-        'node-sass', 'scss/manifest.scss', 'html-dist/main.css',
-        '--output-style', 'compressed'
-    ]
+  const command = [
+    'node-sass', 'scss/manifest.scss', 'html-dist/main.css',
+    '--output-style', 'compressed'
+  ]
 
-    if (watch) command.push('--watch')
-    if (sourceMaps) command.push('--source-map', 'true')
+  if (watch) command.push('--watch')
+  if (sourceMaps) command.push('--source-map', 'true')
 
-    const { stdout, stderr } = child.spawn('npx', command)
-    stdout.pipe(process.stdout)
-    stderr.pipe(process.stderr)
+  const { stdout, stderr } = child.spawn('npx', command)
+  stdout.pipe(process.stdout)
+  stderr.pipe(process.stderr)
 }
 
-//JS
+// JS
 
 if (buildEverything || rc.js) {
-    jsBuilder(watch, sourceMaps)
+  jsBuilder(watch, sourceMaps)
 }
 
 // TRANSLATIONS
 
 if (buildEverything || rc.translations) {
-    const buildTranslations = (done) => {
-        const { stdout, stderr, on } = child.spawn('node', ['bin/convert-translations-from-xml-to-json.js'])
-        stdout.pipe(process.stdout)
-        stderr.pipe(process.stderr)
-        if (done) on('close', done)
-    }
-    if (watch) {
-        // (whats the usecase of this? live update after pulling in translations??)
-        globWatch(['./_locales/*.xml'], buildTranslations)
-    } {
-        buildTranslations()
-    }
+  const buildTranslations = (done) => {
+    const { stdout, stderr, on } = child.spawn('node', ['bin/convert-translations-from-xml-to-json.js'])
+    stdout.pipe(process.stdout)
+    stderr.pipe(process.stderr)
+    if (done) on('close', done)
+  }
+  if (watch) {
+    // (whats the usecase of this? live update after pulling in translations??)
+    globWatch(['./_locales/*.xml'], buildTranslations)
+  } else {
+    buildTranslations()
+  }
 }
 
 // STATIC
 
 if (buildEverything || rc.static) {
-    const copyAction = async () => {
-        // docs of fs-extra.copy https://github.com/jprichardson/node-fs-extra/blob/master/docs/copy.md
-        await fs.copy('./static/', './html-dist/')
-    }
-    if (watch) {
-        // (whats the usecase of this? live update after pulling in translations??)
-        globWatch(['./static/*'], (done) => {
-            copyAction.then(done)
-        })
-    } {
-        copyAction().then(_ => {
-            console.log('DONE: copy files from static folder')
-        })
-    }
+  const copyAction = async () => {
+    // docs of fs-extra.copy https://github.com/jprichardson/node-fs-extra/blob/master/docs/copy.md
+    await fs.copy('./static/', './html-dist/')
+  }
+  if (watch) {
+    // (whats the usecase of this? live update after pulling in translations??)
+    globWatch(['./static/*'], (done) => {
+      copyAction.then(done)
+    })
+  } {
+    copyAction().then(_ => {
+      console.log('DONE: copy files from static folder')
+    })
+  }
 }
-
 
 // Special (misc tasks like generating thumbnails for the background images)
 
 // TODO "special", make sure that that code runs in another process
 // and that it checks the hashes of the files first
 // in order to don't do work when not nessesary
-// (compare with cache file, that has the previous hashes) 
-
+// (compare with cache file, that has the previous hashes)
 
 // TODO -> log all verbose output to seperate temp log files
