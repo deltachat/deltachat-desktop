c59c5c3573e7f814f7f6d74f2967cd26396ecdd4
Simon Laux <mobile.info@simonlaux.de>
2020-02-19 01:58:59 +0100

    remove native 'crypto' dependency from renderer

--- src/renderer/components/dialogs/Settings.js
+++ src/renderer/components/dialogs/Settings.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import crypto from 'crypto'
 const { remote } = window.electron_functions
 const { ipcRenderer } = window.electron_functions
 import { callDcMethodAsync } from '../../ipc'
@@ -20,7 +19,7 @@ import { DeltaDialogBase, DeltaDialogBody, DeltaDialogHeader } from './DeltaDial
 import Login from '../Login'
 import { confirmationDialogLegacy as confirmationDialog } from './ConfirmationDialog'
 const { SettingsContext } = require('../../contexts')
-const MAGIC_PW = crypto.randomBytes(8).toString('hex')
+const MAGIC_PW = '9bbdc87b50bbc684'
 
 function flipDeltaBoolean (value) {
   return value === '1' ? '0' : '1'
