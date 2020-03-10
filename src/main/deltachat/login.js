5d539d1f9da3f42aeb6ab62d02b96415f07c7d48
Simon Laux <mobile.info@simonlaux.de>
2020-03-10 16:33:35 +0100

    change spacing, remove some annoying white spaces

--- src/main/deltachat/login.js
+++ src/main/deltachat/login.js
@@ -114,7 +114,7 @@ We are happy to announce version 1.0.0 release of DeltaChat Desktop! 🎉This re
 - restyle of various menus
 - many bug fixes
 - new users will get informed if their provider is known to need special care to run seamless with DeltaChat
-- new users will use Ed25519 keys (shorter & better cryptographic keys)            
+- new users will use Ed25519 keys (shorter & better cryptographic keys)
 
 Full changelog: https://github.com/deltachat/deltachat-desktop/blob/master/CHANGELOG.md#10000---2020-02-22
     `
@@ -125,7 +125,8 @@ Full changelog: https://github.com/deltachat/deltachat-desktop/blob/master/CHANG
 
 - Add an zoom🔍 option in order to adjust interface and font size. (It's found under View -> Zoom Factor)
 - some interface bug fixes
-- fixed encrypting to Ed25519 keys (there was a bug that broke encrypting to people with Ed25519 keys)            
+- fixed encrypting to Ed25519 keys (there was a bug that broke encrypting to people with Ed25519 keys)
+
 Full changelog: https://github.com/deltachat/deltachat-desktop/blob/master/CHANGELOG.md#10100---2020-03-10
     `
     )
