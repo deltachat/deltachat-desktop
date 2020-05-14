56dbbb7099ecc976628607ea32f4463db629ad58
jikstra <jikstra@disroot.org>
2020-05-14 15:41:40 +0200

    Don't rename error events in backend, just pass them through how they are and handle them accordingly in the frontend

--- src/main/deltachat/controller.ts
+++ src/main/deltachat/controller.ts
@@ -164,8 +164,7 @@ export default class DeltaChatController extends EventEmitter {
         logCoreEvent.warn(event, data1, data2)
       } else if (event === 'DC_EVENT_INFO') {
         logCoreEvent.info(event, data1, data2)
-      } else if (event && event.startsWith('DC_EVENT_ERROR')) {
-        this.emit('error', event, data1, data2)
+      } else if (event.startsWith('DC_EVENT_ERROR')) {
         logCoreEvent.error(event, data1, data2)
       } else if (app.rc['log-debug']) {
         // in debug mode log all core events
