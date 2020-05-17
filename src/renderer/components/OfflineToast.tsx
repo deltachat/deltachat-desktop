54a6a9789ca95da9f39d0bf9a7aaeffb56a42b12
jikstra <jikstra@disroot.org>
2020-05-17 21:32:58 +0200

    Refactor OfflineToast into functional component, listen to browser onOnline/Offline

--- src/renderer/components/OfflineToast.tsx
+++ src/renderer/components/OfflineToast.tsx
@@ -1,67 +1,57 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import { DeltaBackend } from '../delta-remote'
 import { ipcBackend } from '../ipc'
 
-export default class OfflineToast extends Component<
-  {},
-  { online: boolean; tryConnectCooldown: boolean }
-> {
-  onOnline: any
-  onOffline: any
-  constructor(props: any) {
-    super(props)
-    this.state = {
-      online: true,
-      tryConnectCooldown: true,
-    }
 
-    this.onOnline = this.onNetworkChange.bind(this, true)
-    this.onOffline = this.onNetworkChange.bind(this, false)
-  }
+export default function OfflineToast() {
+  const [online, setOnline] = useState(true)
+  const [tryConnectCooldown, setTryConnectCooldown] = useState(true)
 
-  componentDidMount() {
-    ipcBackend.on('DC_EVENT_ERROR_NETWORK', this.onOffline)
+  const onOffline = () => setOnline(false)
+  const onOnline = () => setOnline(true)
+  const onMaybeOnline = () => DeltaBackend.call('context.maybeNetwork')
+  useEffect(() => {
+    DeltaBackend.call('context.maybeNetwork')
+    
+    window.addEventListener('online',  onMaybeOnline);
+    window.addEventListener('offline', onOffline);
+    ipcBackend.on('DC_EVENT_ERROR_NETWORK', onOffline)
     // ugly hack to find out when the user goes online again:
-    ipcBackend.on('DC_EVENT_SMTP_CONNECTED', this.onOnline)
-    ipcBackend.on('DC_EVENT_IMAP_CONNECTED', this.onOnline)
-    ipcBackend.on('DC_EVENT_INCOMING_MSG', this.onOnline)
-    ipcBackend.on('DC_EVENT_MSG_DELIVERED', this.onOnline)
-    ipcBackend.on('DC_EVENT_IMAP_MESSAGE_MOVED', this.onOnline)
+    ipcBackend.on('DC_EVENT_SMTP_CONNECTED', onOnline)
+    ipcBackend.on('DC_EVENT_IMAP_CONNECTED', onOnline)
+    ipcBackend.on('DC_EVENT_INCOMING_MSG', onOnline)
+    ipcBackend.on('DC_EVENT_MSG_DELIVERED', onOnline)
+    ipcBackend.on('DC_EVENT_IMAP_MESSAGE_MOVED', onOnline)
+
+    return () => {
+      window.removeEventListener('online',  onMaybeOnline);
+      window.removeEventListener('offline', onOffline);
+      ipcBackend.removeListener('DC_EVENT_ERROR_NETWORK', onOffline)
+      ipcBackend.removeListener('DC_EVENT_SMTP_CONNECTED', onOnline)
+      ipcBackend.removeListener('DC_EVENT_IMAP_CONNECTED', onOnline)
+      ipcBackend.removeListener('DC_EVENT_INCOMING_MSG', onOnline)
+      ipcBackend.removeListener('DC_EVENT_MSG_DELIVERED', onOnline)
+      ipcBackend.removeListener('DC_EVENT_IMAP_MESSAGE_MOVED', onOnline)
+    }
+  }, [])
+
+  const onTryReconnectClick = () => {
+    setTryConnectCooldown(false)
+    setTimeout(() => setTryConnectCooldown(true), 15000)
+    setTimeout(() => DeltaBackend.call('context.maybeNetwork'), 100)
   }
 
-  componentWillUnmount() {
-    ipcBackend.removeListener('DC_EVENT_ERROR_NETWORK', this.onOffline)
-    ipcBackend.removeListener('DC_EVENT_SMTP_CONNECTED', this.onOnline)
-    ipcBackend.removeListener('DC_EVENT_IMAP_CONNECTED', this.onOnline)
-    ipcBackend.removeListener('DC_EVENT_INCOMING_MSG', this.onOnline)
-    ipcBackend.removeListener('DC_EVENT_MSG_DELIVERED', this.onOnline)
-    ipcBackend.removeListener('DC_EVENT_IMAP_MESSAGE_MOVED', this.onOnline)
-  }
-
-  async onNetworkChange(online: boolean, _event?: any) {
-    this.setState({ online })
-  }
-
-  render() {
-    return (
-      !this.state.online && (
-        <div className='no-network-toast'>
-          <b>Offline</b>
-          <div
-            onClick={() => {
-              this.setState({ tryConnectCooldown: false })
-              setTimeout(
-                () => this.setState({ tryConnectCooldown: true }),
-                15000
-              )
-              setTimeout(() => DeltaBackend.call('context.maybeNetwork'), 100)
-            }}
-            className={this.state.tryConnectCooldown ? '' : 'disabled'}
-          >
-            Try to connect now
-          </div>
+  return (
+    !online && (
+      <div className='no-network-toast'>
+        <b>Offline</b>
+        <div
+          className={tryConnectCooldown ? '' : 'disabled'}
+          onClick={onTryReconnectClick}
+        >
+          Try to connect now
         </div>
-      )
+      </div>
     )
-  }
+  )
 }
