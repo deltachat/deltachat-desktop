2a828844b41401696d4724e9fe5639e921a99a34
Simon Laux <mobile.info@simonlaux.de>
2020-05-18 23:53:49 +0200

    replace colors dependency

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,7 +1,15 @@
 import { parse } from 'error-stack-parser'
-import { blue, yellow, red, grey } from 'colors/safe'
 import { RC_Config } from './shared-types'
 const startTime = Date.now()
+
+export const colorize = (light: number, code: number) => (str: string) =>
+  '\x1B[' + light + ';' + code + 'm' + str + '\x1b[0m'
+export const blue = colorize(1, 34)
+export const red = colorize(1, 31)
+export const yellow = colorize(1, 33)
+export const grey = colorize(0, 37)
+export const green = colorize(1, 37)
+export const cyan = colorize(1, 36)
 
 const emojiFontCss =
   'font-family: Roboto, "Apple Color Emoji", NotoEmoji, "Helvetica Neue", Arial, Helvetica, NotoMono, sans-serif !important;'
