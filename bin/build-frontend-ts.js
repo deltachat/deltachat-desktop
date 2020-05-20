5df7735d597a85b4d1aa63a24560ee7630780435
jikstra <jikstra@disroot.org>
2020-05-20 19:43:30 +0200

    Move building logic into own scripts runnable via npm run

---
+++ bin/build-frontend-ts.js
@@ -0,0 +1,65 @@
+const child = require('child_process')
+const fs = require('fs-extra')
+
+/**
+ *
+ * @param {string[]} args arguments for the command
+ * @param {import('child_process').SpawnOptionsWithoutStdio} options
+ */
+async function run (command, args, options) {
+  return new Promise((resolve, reject) => {
+    const p = child.spawn(command, args, options)
+    p.stdout.pipe(process.stdout)
+    p.stderr.pipe(process.stderr)
+    p.on('close', resolve)
+    p.on('error', reject)
+    p.on('exit', (code)=>{
+      if (code != 0) {
+        console.log(command + 'exited with ERR CODE '+ code)
+        reject('ERR CODE '+ code)
+      }
+    })
+  })
+}
+
+async function main(sourcemap=true, watch=false) {
+  console.log('- Compiling TypeScript...')
+  await run('npx', `tsc -b src/renderer --pretty`.split(' '))
+  console.log('- Finished compiling TypeScript...')
+
+  const parcelArgs = [
+    'parcel', 'build', 'tsc-dist/renderer/main.js',
+    '--out-dir', 'html-dist',
+    '--out-file', 'bundle.js',
+    '--public-url', './',
+    '--target', 'browser'
+  ]
+  const parcelENV = Object.assign({}, process.env)
+
+  if (!sourcemap) parcelArgs.push('--no-source-maps')
+  if (process.env['NODE_ENV'] !== 'production') {
+    parcelArgs.push('--no-minify')
+    parcelENV['NODE_ENV'] = 'development'
+  } else {
+    parcelENV['NODE_ENV'] = 'production'
+  }
+
+  console.log('- Bundling with parcel...')
+  await run('npx', parcelArgs, {env:parcelENV})
+  console.log('- Parcel (bundle + minification) completed')
+
+  if (sourcemap) {
+    // fix source maps
+    const sourceMap = await fs.readJSON('./html-dist/bundle.js.map')
+    sourceMap.sources = sourceMap.sources.map((source) => 
+      // fix path depth & move all non renderer souces to .ignore folder
+-     (source.indexOf('src/renderer') > -1) ? source.replace(/\.\.\//g, '') : source.replace(/\.\.\//g, '.ignore/')
+    )
+    sourceMap.sourceRoot = '../'
+
+    await fs.writeJSON('./html-dist/bundle.js.map', sourceMap)
+  }
+}
+
+main()
+
