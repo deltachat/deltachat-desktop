4e873cc86f8c6955d253e50af24032d1f76ec5d3
Simon Laux <mobile.info@simonlaux.de>
2020-06-04 01:29:18 +0200

    fix formatting

--- src/main/deltachat/chatlist.ts
+++ src/main/deltachat/chatlist.ts
@@ -122,7 +122,7 @@ export default class DCChatList extends SplitOut {
     const summary = await this.getChatListSummary(list, i)
     const lastUpdated = summary.timestamp ? summary.timestamp * 1000 : null
 
-    const name = chat.name|| summary.text1
+    const name = chat.name || summary.text1
     const isGroup = isGroupChat(chat)
     const contactIds = await this._getChatContactIds(chatId)
     // This is NOT the Chat Oject, it's a smaller version for use as ChatListItem in the ChatList
