154b4134c16c6c56266fae8cf9335f55bbc6e3ea
jikstra <jikstra@disroot.org>
2020-06-05 19:32:27 +0200

    Fix backup import

--- src/main/deltachat/backup.ts
+++ src/main/deltachat/backup.ts
@@ -36,20 +36,26 @@ export default class DCBackup extends SplitOut {
       )
     }
 
-    const dcnContext = binding.dcn_context_new()
 
     const tmpConfigPath = tempy.directory()
     log.debug(`Creating dummy dc config for importing at ${tmpConfigPath}`)
     const db = path.join(tmpConfigPath, 'db.sqlite')
+
+    const dcnContext = binding.dcn_context_new(db)
+
+    const shutdown = () => {
+      binding.dcn_stop_event_handler(dcnContext)
+      binding.dcn_context_unref(dcnContext)
+      log.debug(`closed context for backupImport ${file}`)
+    }
+
     const onError = (err: any) => {
       sendToRenderer('DD_EVENT_BACKUP_IMPORT_ERROR', err)
-      binding.dcn_close(dcnContext, () => {
-        log.debug(`closed context for backupImport ${file}`)
-      })
+      shutdown()
     }
 
     const dcnEvent = new EventEmitter()
-    binding.dcn_set_event_handler(
+    binding.dcn_start_event_handler(
       dcnContext,
       (event: any, data1: any, data2: any) => {
         const eventStr = EventId2EventName[event]
@@ -69,55 +75,51 @@ export default class DCBackup extends SplitOut {
 
     function onSuccessfulMove(addr: string) {
       sendToRenderer('DD_EVENT_BACKUP_IMPORTED', addr)
+      shutdown()
     }
 
-    function onSuccessfulImport() {
+    async function onSuccessfulImport() {
       const addr = binding.dcn_get_config(dcnContext, 'addr')
 
       log.debug(`backupImport: Closing dc instance...`)
-      binding.dcn_close(dcnContext, async (err: any) => {
-        if (err) throw err
+      binding.dcn_context_unref(dcnContext)
 
-        sendToRenderer('DD_EVENT_IMPORT_PROGRESS', 600)
+      sendToRenderer('DD_EVENT_IMPORT_PROGRESS', 600)
 
-        const newPath = getNewAccountPath()
-        const configFolderExists = await fs.pathExists(newPath)
+      const newPath = getNewAccountPath(addr)
+      const configFolderExists = await fs.pathExists(newPath)
 
-        if (configFolderExists) {
-          log.debug(`backupImport: ${newPath} already exists`)
-          sendToRenderer('DD_EVENT_BACKUP_IMPORT_EXISTS', true)
-          sendToRenderer('DD_EVENT_IMPORT_PROGRESS', 700)
+      if (configFolderExists) {
+        log.debug(`backupImport: ${newPath} already exists`)
+        sendToRenderer('DD_EVENT_BACKUP_IMPORT_EXISTS', true)
+        sendToRenderer('DD_EVENT_IMPORT_PROGRESS', 700)
 
-          ipcMain.once('DU_EVENT_BACKUP_IMPORT_OVERWRITE', async () => {
-            log.debug('DU_EVENT_OVERWRITE_IMPORT')
-            await moveImportedConfigFolder(addr, newPath, true)
-            onSuccessfulMove(addr)
-          })
-        } else {
-          log.debug(`backupImport: ${newPath} does not exist, moving...`)
-          sendToRenderer('DD_EVENT_IMPORT_PROGRESS', 700)
-          sendToRenderer('DD_EVENT_BACKUP_IMPORT_EXISTS', false)
-          try {
-            // future compatibiliy: remove a symlink if it exists
-            if ((await fs.lstat(newPath)).isSymbolicLink()) {
-              await fs
-                .remove(newPath)
-                .catch(log.error.bind(null, 'symlink removing failed'))
-            }
-          } catch (error) {
-            /* but we don't care about the error of a not found symlink */
-          }
-          await moveImportedConfigFolder(addr, newPath, false)
+        ipcMain.once('DU_EVENT_BACKUP_IMPORT_OVERWRITE', async () => {
+          log.debug('DU_EVENT_OVERWRITE_IMPORT')
+          await moveImportedConfigFolder(addr, newPath, true)
           onSuccessfulMove(addr)
+        })
+      } else {
+        log.debug(`backupImport: ${newPath} does not exist, moving...`)
+        sendToRenderer('DD_EVENT_IMPORT_PROGRESS', 700)
+        sendToRenderer('DD_EVENT_BACKUP_IMPORT_EXISTS', false)
+        try {
+          // future compatibiliy: remove a symlink if it exists
+          if ((await fs.lstat(newPath)).isSymbolicLink()) {
+            await fs
+              .remove(newPath)
+              .catch(log.error.bind(null, 'symlink removing failed'))
+          }
+        } catch (error) {
+          /* but we don't care about the error of a not found symlink */
         }
-      })
+        await moveImportedConfigFolder(addr, newPath, false)
+      }
     }
-    binding.dcn_open(dcnContext, db)
 
     log.debug(`openend context`)
     log.debug(`Starting backup import of ${file}`)
 
     binding.dcn_imex(dcnContext, C.DC_IMEX_IMPORT_BACKUP, file, '')
-    binding.dcn_perform_imap_jobs(dcnContext)
   }
 }
