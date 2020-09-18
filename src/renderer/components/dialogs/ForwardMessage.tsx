60071bf192c88b8fd57e44f7c5fb5eb1a9ffb73a
Simon Laux <mobile.info@simonlaux.de>
2020-09-18 03:58:59 +0200

    use virtual list also for forward message + remove a unused and a duplicated css class

--- src/renderer/components/dialogs/ForwardMessage.tsx
+++ src/renderer/components/dialogs/ForwardMessage.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Card, Classes } from '@blueprintjs/core'
 import { DeltaDialogBase, DeltaDialogHeader } from './DeltaDialog'
-import { useChatListIds, useLazyChatListItems } from '../chat/ChatListHelpers'
 import ChatListItem from '../chat/ChatListItem'
 import { PseudoListItemNoSearchResults } from '../helpers/PseudoListItem'
 import classNames from 'classnames'
@@ -10,6 +9,9 @@ import { DialogProps } from './DialogController'
 import { MessageType } from '../../../shared/shared-types'
 
 import { C } from 'deltachat-node/dist/constants'
+import { ChatListPart, useLogicVirtualChatList } from '../chat/ChatList'
+import { AutoSizer } from 'react-virtualized'
+import { useChatList } from '../chat/ChatListHelpers'
 
 export default function ForwardMessage(props: {
   message: MessageType
@@ -17,10 +19,10 @@ export default function ForwardMessage(props: {
 }) {
   const tx = window.static_translate
   const { message, onClose } = props
-  const { chatListIds, queryStr, setQueryStr } = useChatListIds(
+  const { chatListIds, queryStr, setQueryStr } = useChatList(
     C.DC_GCL_FOR_FORWARDING | C.DC_GCL_NO_SPECIALS
   )
-  const { chatItems, onChatListScroll, scrollRef } = useLazyChatListItems(
+  const { isChatLoaded, loadChats, chatCache } = useLogicVirtualChatList(
     chatListIds
   )
 
@@ -32,6 +34,7 @@ export default function ForwardMessage(props: {
     setQueryStr(e.target.value)
 
   var isOpen = !!message
+  let noResults = chatListIds.length === 0 && queryStr !== ''
   return (
     <DeltaDialogBase isOpen={isOpen} onClose={onClose} fixed>
       <DeltaDialogHeader onClose={onClose}>
@@ -44,25 +47,39 @@ export default function ForwardMessage(props: {
         />
       </DeltaDialogHeader>
       <div
-        ref={scrollRef}
         className={classNames(
           Classes.DIALOG_BODY,
           '.bp3-dialog-body-no-footer'
         )}
-        onScroll={onChatListScroll}
       >
         <Card style={{ padding: '0px' }}>
           <div className='forward-message-list-chat-list'>
-            {chatListIds.map(chatId => (
-              <ChatListItem
-                key={chatId}
-                chatListItem={chatItems[chatId]}
-                onClick={onChatClick.bind(null, chatId)}
-              />
-            ))}
-            {chatListIds.length === 0 &&
-              queryStr !== '' &&
-              PseudoListItemNoSearchResults({ queryStr })}
+            {noResults && PseudoListItemNoSearchResults({ queryStr })}
+            <div style={{ height: noResults ? '0px' : '100%' }}>
+              <AutoSizer>
+                {({ width, height }) => (
+                  <ChatListPart
+                    isRowLoaded={isChatLoaded}
+                    loadMoreRows={loadChats}
+                    rowCount={chatListIds.length}
+                    width={width}
+                    height={height}
+                  >
+                    {({ index, key, style }) => {
+                      const [chatId] = chatListIds[index]
+                      return (
+                        <div style={style} key={key}>
+                          <ChatListItem
+                            chatListItem={chatCache[chatId] || undefined}
+                            onClick={onChatClick.bind(null, chatId)}
+                          />
+                        </div>
+                      )
+                    }}
+                  </ChatListPart>
+                )}
+              </AutoSizer>
+            </div>
           </div>
         </Card>
       </div>
