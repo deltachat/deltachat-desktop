27a73bad44ee02e3db50b549a2c5b1b1e1414687
jikstra <jikstra@disroot.org>
2020-10-02 13:49:20 +0200

    Refactor try code, remove indentation levels if possible

--- src/main/tray.ts
+++ src/main/tray.ts
@@ -17,70 +17,71 @@ ipcMain.on('updateTrayIcon', updateTrayIcon)
 export function updateTrayIcon() {
   const tx = app.translate
 
-  const IsMac = process.platform === 'darwin'
-  if (!IsMac) {
-    if (app.state.saved.minimizeToTray !== true) {
-      // User doesn't want tray icon => destroy it
-      if (tray != null) {
-        log.info('destroy icon tray')
-        tray.destroy()
-      }
-    } else {
-      // Add tray icon
-      log.info('add icon tray')
-      tray = new Tray(appIcon())
-      const win = mainWindow.window
-      contextMenu = Menu.buildFromTemplate([
-        {
-          id: 'open_windows',
-          label: tx('global_menu_file_open_desktop'),
-          type: 'normal',
-          click() {
-            win.show()
-          },
-        },
-        {
-          id: 'reduce_window',
-          label: tx('global_menu_file_reduce_desktop'),
-          type: 'normal',
-          accelerator: 'Escape',
-          click() {
-            win.close()
-          },
-        },
-        {
-          id: 'quit_app',
-          label: tx('global_menu_file_quit_desktop'),
-          type: 'normal',
-          click() {
-            globalShortcut.unregisterAll()
-            if (process.platform !== 'darwin') {
-              app.quit()
-            }
-          },
-        },
-      ])
-      tray.setToolTip('Delta Chat')
-      tray.setContextMenu(contextMenu)
-      tray.on('double-click', (event, bounds) => {
-        mainWindow.show()
-      })
-      tray.on('click', (event, bounds) => {
-        mainWindow.show()
-      })
-    }
+  // Don't show a tray icon for mac
+  if (process.platform === 'darwin') return
 
+  // User doesn't want tray icon => destroy it
+  if (app.state.saved.minimizeToTray !== true) {
+    if (tray != null) {
+      log.info('destroy icon tray')
+      tray.destroy()
+    }
     updateTrayMenu()
+    return
   }
+  
+  // Add tray icon
+  log.info('add icon tray')
+  tray = new Tray(appIcon())
+  const win = mainWindow.window
+  contextMenu = Menu.buildFromTemplate([
+    {
+      id: 'open_windows',
+      label: tx('global_menu_file_open_desktop'),
+      type: 'normal',
+      click() {
+        win.show()
+      },
+    },
+    {
+      id: 'reduce_window',
+      label: tx('global_menu_file_reduce_desktop'),
+      type: 'normal',
+      accelerator: 'Escape',
+      click() {
+        win.close()
+      },
+    },
+    {
+      id: 'quit_app',
+      label: tx('global_menu_file_quit_desktop'),
+      type: 'normal',
+      click() {
+        globalShortcut.unregisterAll()
+        if (process.platform !== 'darwin') {
+          app.quit()
+        }
+      },
+    },
+  ])
+  tray.setToolTip('Delta Chat')
+  tray.setContextMenu(contextMenu)
+  tray.on('double-click', (event, bounds) => {
+    mainWindow.show()
+  })
+  tray.on('click', (event, bounds) => {
+    mainWindow.show()
+  })
+
+  updateTrayMenu()
 }
 
 export function updateTrayMenu() {
-  if (contextMenu !== null) {
-    contextMenu.getMenuItemById(
-      'reduce_window'
-    ).enabled = mainWindow.window.isVisible()
+  if (contextMenu === null) return
+  contextMenu.getMenuItemById(
+    'reduce_window'
+  ).enabled = mainWindow.window.isVisible()
 
-    // Called to update menu on Linux
-    tray.setContextMenu(contextMenu)
-  }
+  // Called to update menu on Linux
+  tray.setContextMenu(contextMenu)
 }
