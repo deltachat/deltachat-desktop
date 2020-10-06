fbd432ff2b61226907aadb2adceabccaa6fbe72d
Simon Laux <mobile.info@simonlaux.de>
2020-10-06 21:09:01 +0200

    add keyboard navigation (arrow keys, tab, enter) to account selection on account selection screen. add new theme variables for contactListItemHover and -Focus remove some unused theme vars

--- src/renderer/components/LoginScreen.tsx
+++ src/renderer/components/LoginScreen.tsx
@@ -15,6 +15,7 @@ import {
   Navbar,
   NavbarGroup,
   NavbarHeading,
+  Icon,
 } from '@blueprintjs/core'
 import { DeltaProgressBar } from './Login-Styles'
 import { getLogger } from '../../shared/logger'
@@ -35,11 +36,7 @@ import { DeltaBackend } from '../delta-remote'
 import { Screens } from '../ScreenController'
 import { IpcRendererEvent } from 'electron'
 import { Avatar } from './Avatar'
-import {
-  PseudoListItemAddContact,
-  PseudoListItem,
-} from './helpers/PseudoListItem'
-import { ContactListItem } from './contact/ContactListItem'
+import { PseudoContact } from './contact/Contact'
 
 const log = getLogger('renderer/components/LoginScreen')
 
@@ -186,21 +183,6 @@ export default function LoginScreen({
     openDialog(ConfigureProgressDialog, { credentials, onSuccess })
   }
 
-  const removeAccount = (login: DeltaChatAccount) => {
-    const message = tx('forget_login_confirmation_desktop')
-    openDialog('ConfirmationDialog', {
-      message,
-      confirmLabel: tx('delete_account'),
-      isConfirmDanger: true,
-      cb: async (yes: boolean) => {
-        if (yes) {
-          await DeltaBackend.call('login.forgetAccount', login)
-          refreshAccounts()
-        }
-      },
-    })
-  }
-
   if (logins === null) return null
 
   return (
@@ -271,26 +253,9 @@ export default function LoginScreen({
                     />
                     <DeltaDialogBody>
                       <DeltaDialogContent noPadding={true}>
-                        <div className='accounts'>
-                          <ul>
-                            <PseudoListItem
-                              id='action-go-to-login'
-                              cutoff='+'
-                              text={tx('add_account')}
-                              onClick={() => setView('login')}
-                            />
-                            {logins.map(
-                              (login: DeltaChatAccount, index: Number) => (
-                                <AccountItem
-                                  key={`login-${index}`}
-                                  login={login}
-                                  loadAccount={loadAccount}
-                                  removeAccount={removeAccount}
-                                />
-                              )
-                            )}
-                          </ul>
-                        </div>
+                        <AccountSelection
+                          {...{ refreshAccounts, setView, logins, loadAccount }}
+                        />
                       </DeltaDialogContent>
                     </DeltaDialogBody>
                   </>
@@ -312,7 +277,87 @@ export default function LoginScreen({
   )
 }
 
-export function AccountItem({
+function AccountSelection({
+  refreshAccounts,
+  setView,
+  logins,
+  loadAccount,
+}: {
+  refreshAccounts: () => Promise<void>
+  setView: React.Dispatch<React.SetStateAction<string>>
+  logins: any
+  loadAccount: (account: DeltaChatAccount) => {}
+}) {
+  const tx = useTranslationFunction()
+  const { openDialog } = useContext(ScreenContext)
+
+  const removeAccount = (login: DeltaChatAccount) => {
+    const message = tx('forget_login_confirmation_desktop')
+    openDialog('ConfirmationDialog', {
+      message,
+      confirmLabel: tx('delete_account'),
+      isConfirmDanger: true,
+      cb: async (yes: boolean) => {
+        if (yes) {
+          await DeltaBackend.call('login.forgetAccount', login)
+          refreshAccounts()
+        }
+      },
+    })
+  }
+
+  useEffect(() => {
+    const onKeyDown = (ev: KeyboardEvent) => {
+      const parent = document.querySelector<HTMLDivElement>('#accounts')
+      const current = parent?.querySelector(':focus')
+
+      if (ev.key == 'ArrowDown') {
+        if (current && current.nextElementSibling) {
+          ;(current.nextElementSibling as HTMLDivElement)?.focus()
+        } else {
+          ;(parent?.firstElementChild as HTMLDivElement).focus()
+        }
+      } else if (ev.key == 'ArrowUp') {
+        if (current && current.previousElementSibling) {
+          ;(current.previousElementSibling as HTMLDivElement)?.focus()
+        } else {
+          ;(parent?.lastElementChild as HTMLDivElement).focus()
+        }
+      } else if (ev.key == 'Enter') {
+        if (current) {
+          ;(current as HTMLDivElement)?.click()
+        }
+      }
+    }
+
+    document.addEventListener('keydown', onKeyDown)
+    return () => document.removeEventListener('keydown', onKeyDown)
+  })
+
+  return (
+    <div className='accounts' id='accounts' role='menu'>
+      <div
+        role='menu-item'
+        id='action-go-to-login'
+        className='contact-list-item'
+        onClick={() => setView('login')}
+        tabIndex={0}
+      >
+        <PseudoContact cutoff='+' text={tx('add_account')}></PseudoContact>
+      </div>
+      {logins.map((login: DeltaChatAccount, index: Number) => (
+        <AccountItem
+          key={`login-${index}`}
+          login={login}
+          loadAccount={loadAccount}
+          removeAccount={removeAccount}
+        />
+      ))}
+    </div>
+  )
+}
+
+function AccountItem({
   login,
   loadAccount,
   removeAccount,
@@ -321,28 +366,54 @@ export function AccountItem({
   loadAccount: todo
   removeAccount: todo
 }) {
-  const contact = {
-    address: login.addr,
-    color: login.color,
-    displayName: login.displayname || login.addr,
-    firstName: '',
-    id: 0,
-    name: login.displayname,
-    profileImage: login.profileImage,
-    nameAndAddr: login.displayname,
-    isBlocked: false,
-    isVerified: false,
+  const removeAction = (ev: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    ev?.stopPropagation()
+    removeAccount(login)
   }
 
   return (
-    <ContactListItem
-      key={login.addr}
-      contact={contact}
+    <div
+      role='menu-item'
+      className='contact-list-item'
+      title={
+        filesizeConverter(login.size) +
+        '\n' +
+        ('accounts' + login.path.split('accounts')[1])
+      }
       onClick={() => loadAccount(login)}
-      showCheckbox={false}
-      checked={false}
-      showRemove={true}
-      onRemoveClick={() => removeAccount(login)}
-    />
+      tabIndex={0}
+    >
+      <div style={{ width: '100%' }}>
+        <div className='contact'>
+          <Avatar
+            {...{
+              avatarPath: login.profileImage,
+              color: login.color,
+              displayName: login.displayname,
+            }}
+          />
+          <div className='contact-name'>
+            <div className='display-name'>
+              {login.displayname || login.addr}
+            </div>
+            <div className='email'>{login.addr}</div>
+          </div>
+        </div>
+      </div>
+
+      <div className='remove-icon' onClick={removeAction}>
+        <Icon icon='cross' />
+      </div>
+    </div>
   )
 }
+
+// TODO
+
+// [] - hover effect for remove icon
+// [] - show properties somewhere (size, path) -> find a good way, I'm not satisfied with the title-hover-popover
+// [X] - keyboard navigation (arrow keys, tab, enter)
+// [] - keyboard navigation for dialog buttons
+// [] - remove not needed imports
+
+// [] - show account name in remove dialog?
