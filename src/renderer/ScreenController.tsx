e79cfe49e7ca4231c8fff1edca763c3e9fde7e16
Simon Laux <mobile.info@simonlaux.de>
2020-11-16 01:40:06 +0100

    remove workaround fix #1888

--- src/renderer/ScreenController.tsx
+++ src/renderer/ScreenController.tsx
@@ -111,9 +111,6 @@ export default class ScreenController extends Component {
   onError(_event: any, [data1, data2]: [string | number, string]) {
     if (this.state.screen === Screens.Login) return
     if (data1 === 0) data1 = ''
-    // Can get removed as soon as we use a rust core including this fix: https://github.com/deltachat/deltachat-core-rust/pull/1911
-    if (data2 === 'dc_continue_key_transfer: "invalid symmetric key algorithm"')
-      return
     const text = data1 + data2
     this.userFeedback({ type: 'error', text })
   }
