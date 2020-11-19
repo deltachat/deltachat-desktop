6ae2ca93b2e788ce8bfb2c880542b44aa68e464e
Simon Laux <mobile.info@simonlaux.de>
2020-11-19 08:36:24 +0100

    add some unicode chars to make sure download links stand out more

--- bin/github-actions/postLinksToDetails.js
+++ bin/github-actions/postLinksToDetails.js
@@ -14,13 +14,13 @@ const GITHUB_TOKEN = process.env['GITHUB_TOKEN']
 let platform_status = {}
 
 if (process.platform === 'darwin') {
-  platform_status['context'] = 'MacOS Preview Build'
+  platform_status['context'] = '📦 MacOS Preview Build'
   platform_status['target_url'] = base_url + prId + '.dmg'
 } else if (process.platform === 'win32') {
-  platform_status['context'] = 'Windows Preview Build (portable)'
+  platform_status['context'] = '📦 Windows Preview Build (portable)'
   platform_status['target_url'] = base_url + prId + '.portable.exe'
 } else if (process.platform === 'linux') {
-  platform_status['context'] = 'Linux Preview Build'
+  platform_status['context'] = '📦 Linux Preview Build'
   platform_status['target_url'] = base_url + prId + '.AppImage'
 } else {
   throw new Error('Unsuported platform: ' + process.platform)
@@ -28,7 +28,7 @@ if (process.platform === 'darwin') {
 
 const STATUS_DATA = {
   state: 'success',
-  description: 'Click on "Details" to download',
+  description: 'Click on "Details" to download →',
   context: platform_status.context,
   target_url: platform_status.target_url,
 }
