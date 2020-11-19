a175c59f4a57f81efd265f98ad7c52286806ff9b
Simon Laux <mobile.info@simonlaux.de>
2020-11-19 08:32:25 +0100

    format postLinksToDetails.js script

--- bin/github-actions/postLinksToDetails.js
+++ bin/github-actions/postLinksToDetails.js
@@ -1,6 +1,6 @@
-const {readJsonSync} = require('fs-extra')
+const { readJsonSync } = require('fs-extra')
 
-const sha = readJsonSync(process.env["GITHUB_EVENT_PATH"]).pull_request.head.sha
+const sha = readJsonSync(process.env['GITHUB_EVENT_PATH']).pull_request.head.sha
 
 const base_url =
   'https://download.delta.chat/desktop/preview/deltachat-desktop-'
@@ -8,23 +8,22 @@ const base_url =
 const GITHUB_API_URL =
   'https://api.github.com/repos/deltachat/deltachat-desktop/statuses/' + sha
 
-const prId = process.env["PR_ID"]
-const GITHUB_TOKEN = process.env["GITHUB_TOKEN"]
-
+const prId = process.env['PR_ID']
+const GITHUB_TOKEN = process.env['GITHUB_TOKEN']
 
 let platform_status = {}
 
 if (process.platform === 'darwin') {
-    platform_status["context"] = 'MacOS Preview Build'
-    platform_status["target_url"] = base_url + prId + '.dmg'
+  platform_status['context'] = 'MacOS Preview Build'
+  platform_status['target_url'] = base_url + prId + '.dmg'
 } else if (process.platform === 'win32') {
-    platform_status["context"] = 'Windows Preview Build (portable)'
-    platform_status["target_url"] = base_url + prId + '.portable.exe'
+  platform_status['context'] = 'Windows Preview Build (portable)'
+  platform_status['target_url'] = base_url + prId + '.portable.exe'
 } else if (process.platform === 'linux') {
-    platform_status["context"] = 'Linux Preview Build'
-    platform_status["target_url"] = base_url + prId + '.AppImage'
+  platform_status['context'] = 'Linux Preview Build'
+  platform_status['target_url'] = base_url + prId + '.AppImage'
 } else {
-    throw new Error("Unsuported platform: " + process.platform)
+  throw new Error('Unsuported platform: ' + process.platform)
 }
 
 const STATUS_DATA = {
@@ -34,29 +33,27 @@ const STATUS_DATA = {
   target_url: platform_status.target_url,
 }
 
-var http = require("https");
+const http = require('https')
 
-var options = {
-  "method": "POST",
-  "headers": {
-    "Content-Type": "application/json",
-    "User-Agent": "github-action ci for deltachat deskop",
-    "authorization": "Bearer " + GITHUB_TOKEN
-  }
-};
+const options = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'User-Agent': 'github-action ci for deltachat deskop',
+    authorization: 'Bearer ' + GITHUB_TOKEN,
+  },
+}
 
-var req = http.request(GITHUB_API_URL, options, function (res) {
-  var chunks = [];
+const req = http.request(GITHUB_API_URL, options, function(res) {
+  var chunks = []
+  res.on('data', function(chunk) {
+    chunks.push(chunk)
+  })
+  res.on('end', function() {
+    var body = Buffer.concat(chunks)
+    console.log(body.toString())
+  })
+})
 
-  res.on("data", function (chunk) {
-    chunks.push(chunk);
-  });
-
-  res.on("end", function () {
-    var body = Buffer.concat(chunks);
-    console.log(body.toString());
-  });
-});
-
-req.write(JSON.stringify(STATUS_DATA));
-req.end();
+req.write(JSON.stringify(STATUS_DATA))
+req.end()
