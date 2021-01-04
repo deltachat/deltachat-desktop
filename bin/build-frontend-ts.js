bd43bbd6b2912aaf5311851665364e5ae754b97e
Simon Laux <mobile.info@simonlaux.de>
2021-01-04 13:04:05 +0100

    refactor buildscript and add watching also fix scss watching

--- bin/build-frontend-ts.js
+++ bin/build-frontend-ts.js
@@ -1,16 +1,23 @@
+//@ts-check
 const child = require('child_process')
-const fs = require('fs-extra')
 
 /**
  *
  * @param {string[]} args arguments for the command
- * @param {import('child_process').SpawnOptionsWithoutStdio} options
+ * @param {import('child_process').SpawnOptionsWithoutStdio} [options]
  */
-async function run(command, args, options) {
+async function run(command, args, options, listener = undefined) {
   return new Promise((resolve, reject) => {
     console.log(`- Executing "${command} ${args.join(' ')}"`)
-    const p = child.spawn(command, args, { shell: true, ...options })
+    const p = child.spawn(command, args, {
+      shell: true,
+      cwd: process.cwd(),
+      ...options,
+    })
     p.stdout.pipe(process.stdout)
+    if (listener) {
+      p.stdout.on('data', listener)
+    }
     p.stderr.pipe(process.stderr)
     p.on('close', resolve)
     p.on('error', reject)
@@ -23,53 +30,19 @@ async function run(command, args, options) {
   })
 }
 
-async function main({ watch, sourcemap }) {
-  if (watch === false) {
-    console.log('- Compiling TypeScript...')
-    await run('npx', 'tsc -b src/renderer --pretty'.split(' '))
-    console.log('- Finished compiling TypeScript...')
-  } else {
-    console.log('- Watch is enabled')
-    run(
-      'npx',
-      'tsc -b src/renderer --pretty -w --preserveWatchOutput'.split(' ')
-    )
-  }
-
+async function bundle(production) {
   const bundleArgs = [
     'esbuild',
     'tsc-dist/renderer/main.js',
     '--bundle',
     '--outfile=html-dist/bundle.js',
+    production
+      ? '--define:process.env.NODE_ENV=\\"production\\"'
+      : '--define:process.env.NODE_ENV=\\"development\\"',
   ]
-  let minify = false
 
-  if (sourcemap) bundleArgs.push('--sourcemap')
-  if (process.env['NODE_ENV'] !== 'production' && !watch) {
-    bundleArgs.push('--define:process.env.NODE_ENV=\\"development\\"')
-  } else {
-    minify = true
-    bundleArgs.push('--define:process.env.NODE_ENV=\\"production\\"')
-  }
-
-  console.log('- Bundling with esbuild...')
   await run('npx', bundleArgs)
-
-  if (minify) {
-    console.log('- Minify with terser...')
-    await run('npx', [
-      'terser',
-      '--compress',
-      '--mangle',
-      '--source-map',
-      '"content=\'html-dist/bundle.js.map\'"',
-      'html-dist/bundle.js',
-      '--output',
-      'html-dist/bundle.js',
-    ])
-  }
-
-  console.log('- build completed')
+  // todo fix sourcemap
 
   // if (sourcemap) {
   //   // fix source maps
@@ -86,9 +59,81 @@ async function main({ watch, sourcemap }) {
   // }
 }
 
+async function main(watch_ = false, production_, minify_ = false) {
+  let watchTscProcess = Promise.resolve()
+
+  const watch = watch_
+  const production = !watch && production_
+  const minify = (!watch && minify_) || production
+
+  watch && console.log('- First Compile...')
+  !watch && console.log('- Compile ts ...')
+  await run('npx', 'tsc -b src/renderer --pretty'.split(' '))
+  !watch && console.log('- Compile esbuild ...')
+  await bundle(production)
+
+  if (minify) {
+    console.log('- Minify with terser...')
+    await run('npx', [
+      'terser',
+      '--compress',
+      '--mangle',
+      '--source-map',
+      '"content=\'html-dist/bundle.js.map\'"',
+      'html-dist/bundle.js',
+      '--output',
+      'html-dist/bundle.js',
+    ])
+  }
+
+  if (watch) {
+    let isBundling = false
+    let isScheduled = false
+
+    const BundleIfNeeded = () => {
+      if (isBundling) {
+        isScheduled = true
+      } else {
+        isBundling = true
+        bundle(false)
+          .then(() => {
+            console.log('- bundling done')
+            isBundling = false
+            if (isScheduled) {
+              isScheduled = false
+              BundleIfNeeded()
+            }
+          })
+          .catch(console.log.bind(null, 'bundling failed'))
+      }
+    }
+
+    /** @type {(chunk:Buffer)=>void} chunk */
+    const TSCoutput = chunk => {
+      if (
+        chunk
+          .toString()
+          .indexOf('Found 0 errors. Watching for file changes.') !== -1
+      ) {
+        BundleIfNeeded()
+      }
+    }
+
+    watchTscProcess = run(
+      'npx',
+      'tsc -b src/renderer --pretty -w --preserveWatchOutput'.split(' '),
+      {},
+      TSCoutput
+    )
+  } else {
+    console.log('- build completed')
+  }
+
+  await Promise.all([watchTscProcess])
+}
+
 // console.log(process.argv.indexOf('-w'))
 const watch = process.argv.indexOf('-w') !== -1
+const minify = process.argv.indexOf('-m') !== -1
 
-main({ watch, sourcemap: true }).catch(error => {
-  if (!watch) process.exit(1)
-})
+main(watch, process.env['NODE_ENV'] !== 'production', minify)
