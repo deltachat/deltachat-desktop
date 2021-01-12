69cd999959bacecaa0238f74df12db7e913ca2a7
jikstra <jikstra@disroot.org>
2021-01-12 01:20:59 +0100

    Include branch into git version

--- bin/build-shared-version-info.js
+++ bin/build-shared-version-info.js
@@ -2,14 +2,25 @@ const { spawnSync } = require('child_process')
 const fs = require('fs-extra')
 const { join } = require('path')
 
+function gatherProcessStdout(cmd, args) {
+  const { status, stdout, stderr } = spawnSync(cmd, args)
+  if (status !== 0) throw new Err(stderr)
+  return stdout.toString().replace(/\n/g, '')
+}
+
 async function gatherBuildInfo() {
-  const { status, stdout, stderr } = spawnSync('git', ['describe'])
-  let git_ref = "None"
-  if (status !== 0) {
-    console.log(stderr)
-  } else {
-    git_ref = stdout.toString().replace(/\n/g, '')
+  let git_describe, git_branch;
+  try {
+    git_describe = gatherProcessStdout('git', ['describe'])
+    git_branch = gatherProcessStdout('git', ['symbolic-ref', 'HEAD']).split('/').pop()
+    
+  } catch (err) {
+    console.log(err)
+    process.exit(1)
   }
+
+  const git_ref = git_describe + (git_branch === 'master' ? '' : '-' + git_branch)
+
   const package = await fs.readJSON(join(__dirname, '../package.json'))
   return {
     VERSION: package.version,
