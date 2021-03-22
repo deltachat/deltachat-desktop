14f6e006cae7f98d62d86ec3f4dc855201d54772
Simon Laux <mobile.info@simonlaux.de>
2021-03-22 22:11:59 +0100

    make notifications pretty, show chat icon or image if available show chat name

--- src/main/notifications.ts
+++ src/main/notifications.ts
@@ -1,9 +1,11 @@
 import * as mainWindow from './windows/main'
-import { app, Notification } from 'electron'
+import { app, Notification, NativeImage, nativeImage } from 'electron'
 import { appName } from '../shared/constants'
 import { appIcon } from './application-constants'
 import DeltaChatController from './deltachat/controller'
 import { ExtendedAppMainProcess } from './types'
+import { FullChat, MessageType } from '../shared/shared-types'
+import { C } from 'deltachat-node/dist/constants'
 
 export default function (dc: DeltaChatController, settings: any) {
   if (!Notification.isSupported()) return
@@ -12,14 +14,87 @@ export default function (dc: DeltaChatController, settings: any) {
     return await dc.callMethod(null, 'chatList.isChatMuted', [chatId])
   }
 
-  async function getMsgBody(msgId: number) {
+  async function createNotification(
+    chatId: number,
+    msgId: number
+  ): Promise<Notification> {
     const tx = (app as ExtendedAppMainProcess).translate
-    if (!settings.showNotificationContent) return tx('notify_new_message')
-    const json = await dc.callMethod(null, 'messageList.messageIdToJson', [
-      msgId,
-    ])
-    const summary = json.msg.summary
-    return `${summary.text1 || json.contact.displayName}: ${summary.text2}`
+
+    if (!settings.showNotificationContent) {
+      // Notification which does not show content
+      return new Notification({
+        title: appName,
+        body: tx('notify_new_message'),
+        icon: appIcon(),
+      })
+    } else {
+      const [chatInfo, message_json]: [
+        FullChat,
+        { msg: null } | MessageType
+      ] = await Promise.all([
+        dc.callMethod(null, 'chatList.getFullChatById', [chatId]),
+        dc.callMethod(null, 'messageList.messageIdToJson', [msgId]),
+      ])
+
+      const summary = message_json.msg.summary
+
+      let icon: NativeImage
+      if (message_json.msg?.viewType === C.DC_MSG_IMAGE) {
+        // if is image
+        icon = nativeImage.createFromPath(message_json.msg.file)
+        // idea - maybe needs resize
+        // issue/idea - does not work with all imagetypes - idea: have something reliable for thumbnail generation
+      } else if (chatInfo.profileImage) {
+        // if has chat an avatar picture
+        icon = nativeImage.createFromPath(chatInfo.profileImage)
+      }
+
+      if (!icon || icon.isEmpty()) {
+        icon = nativeImage.createFromPath(appIcon())
+        console.log(icon.isEmpty())
+      }
+
+      return new Notification({
+        title: `${chatInfo.name} | ${appName}`,
+        body: summary.text1
+          ? `${summary.text1}: ${summary.text2}`
+          : summary.text2,
+        icon,
+        timeoutType: 'never',
+      })
+    }
+  }
+
+  const notifications: { [key: number]: Notification[] } = {}
+
+  function clearNotificationsForChat(chatId: number) {
+    if (notifications[chatId]) {
+      for (const notify of notifications[chatId]) {
+        notify.close()
+      }
+      notifications[chatId] = null
+    }
+  }
+
+  function addNotificationForChat(chatId: number, notify: Notification) {
+    if (notifications[chatId]) {
+      notifications[chatId].push(notify)
+    } else {
+      notifications[chatId] = [notify]
+    }
+  }
+
+  function onClickNotification(
+    chatId: number,
+    msgId: number,
+    _ev: Electron.Event
+  ) {
+    console.log('onClickNotification', chatId, msgId)
+
+    dc.sendToRenderer('ClickOnNotification', { chatId, msgId })
+    clearNotificationsForChat(chatId)
+    mainWindow.show()
+    app.focus()
   }
 
   dc._dc.on('DC_EVENT_INCOMING_MSG', async (chatId: number, msgId: number) => {
@@ -30,17 +105,10 @@ export default function (dc: DeltaChatController, settings: any) {
       if (await isMuted(chatId)) {
         return
       }
-      const notify = new Notification({
-        title: appName,
-        body: await getMsgBody(msgId),
-        icon: appIcon(),
-      })
-      notify.on('click', () => {
-        dc.sendToRenderer('ClickOnNotification', { chatId, msgId })
-        mainWindow.show()
-        app.focus()
-      })
-      notify.on('close', () => {})
+      const notify = await createNotification(chatId, msgId)
+      notify.on('click', onClickNotification.bind(null, chatId, msgId))
+      // notify.on('close', () => {})
+      addNotificationForChat(chatId, notify)
       notify.show()
     }
   })
