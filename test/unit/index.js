0405520c7445b0488e158693ca61d255fcab292e
Simon Laux <mobile.info@simonlaux.de>
2021-05-12 17:07:58 +0200

    add unit tests for mailto parsing

--- test/unit/index.js
+++ test/unit/index.js
@@ -7,9 +7,12 @@ test('that translation files are valid json', t => {
   fs.readdir(dir, (err, files) => {
     t.error(err, 'no error reading folder')
     const xmlFiles = files.filter(f => path.extname(f) === '.xml')
-    const jsonFiles = files.filter(f => path.extname(f) === '.json' &&
-                                        path.basename(f) !== '_languages.json' &&
-                                        path.basename(f) !== '_untranslated_en.json')
+    const jsonFiles = files.filter(
+      f =>
+        path.extname(f) === '.json' &&
+        path.basename(f) !== '_languages.json' &&
+        path.basename(f) !== '_untranslated_en.json'
+    )
     const name = f => f.split('.')[0]
     t.deepEqual(
       xmlFiles.map(name),
@@ -35,11 +38,17 @@ test('that translation files are valid json', t => {
             return false
           }
 
-          function testString (str) {
+          function testString(str) {
             const regex = new RegExp(str, 'g')
             const match = regex.exec(v2)
             if (match) {
-              console.error(`> ${JSON.stringify(v2)} contains ${str} (${file} -> ${k1} -> ${k2}) (index: ${match.index})`)
+              console.error(
+                `> ${JSON.stringify(
+                  v2
+                )} contains ${str} (${file} -> ${k1} -> ${k2}) (index: ${
+                  match.index
+                })`
+              )
               return false
             }
             return true
@@ -67,15 +76,15 @@ test('test translation method', t => {
   const l = require('../../tsc-dist/shared/localize')
   const translate = l.translate({
     test_a: {
-      message: 'foo %1$s %2$s blubb'
+      message: 'foo %1$s %2$s blubb',
     },
     test_b: {
-      message: 'fo2o %s %d blu2bb'
+      message: 'fo2o %s %d blu2bb',
     },
     test_c: {
       other: '%n foo',
-      one: '1 foo'
-    }
+      one: '1 foo',
+    },
   })
 
   t.equal(translate('test_a', ['asd', 'dsa']), 'foo asd dsa blubb')
@@ -83,6 +92,47 @@ test('test translation method', t => {
   t.equal(translate('test_b'), 'fo2o %s %d blu2bb')
   t.equal(translate('test_c', null, 'one'), '1 foo')
   t.equal(translate('test_c', 10, 'other'), '10 foo')
+
+  t.end()
+})
+
+test('mailto-parsing', t => {
+  const { parseMailto } = require('../../tsc-dist/shared/parse_mailto')
+
+  t.deepEqual(parseMailto('mailto:ex@example.com?body=hello'), {
+    to: 'ex@example.com',
+    subject: undefined,
+    body: 'hello',
+  })
+  t.deepEqual(parseMailto('mailto:ex@example.com?body=hello&subject=hi'), {
+    to: 'ex@example.com',
+    subject: 'hi',
+    body: 'hello',
+  })
+  t.deepEqual(parseMailto('mailto:?body=hello&subject=hi'), {
+    to: null,
+    subject: 'hi',
+    body: 'hello',
+  })
+  t.deepEqual(parseMailto('mailto:?body=hello'), {
+    to: null,
+    subject: undefined,
+    body: 'hello',
+  })
+  t.deepEqual(parseMailto('mailto:?body=hello%20world'), {
+    to: null,
+    subject: undefined,
+    body: 'hello world',
+  })
+  // unescaped
+  t.deepEqual(
+    parseMailto('mailto:deltabot@example.com?body=/web+https://github.com/'),
+    {
+      to: 'deltabot@example.com',
+      subject: undefined,
+      body: '/web https://github.com/',
+    }
+  )
 
   t.end()
 })
