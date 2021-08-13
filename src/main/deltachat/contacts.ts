2eded463f147b8079a6db44e56b0282646221870
jikstra <jikstra@disroot.org>
2021-08-13 03:08:39 +0200

    Fix contacts

--- src/main/deltachat/contacts.ts
+++ src/main/deltachat/contacts.ts
@@ -8,23 +8,23 @@ const log = getLogger('main/deltachat/contacts')
 
 export default class DCContacts extends SplitOut {
   unblockContact(contactId: number) {
-    const contact = this._dc.getContact(contactId)
-    this._dc.blockContact(contactId, false)
+    const contact = this.selectedAccountContext.getContact(contactId)
+    this.selectedAccountContext.blockContact(contactId, false)
     const name = contact.getNameAndAddress()
     log.info(`Unblocked contact ${name} (id = ${contactId})`)
   }
 
   blockContact(contactId: number) {
-    const contact = this._dc.getContact(contactId)
-    this._dc.blockContact(contactId, true)
+    const contact = this.selectedAccountContext.getContact(contactId)
+    this.selectedAccountContext.blockContact(contactId, true)
     const name = contact.getNameAndAddress()
     log.debug(`Blocked contact ${name} (id = ${contactId})`)
   }
 
   changeNickname(contactId: number, name: string) {
-    const contact = this._dc.getContact(contactId)
+    const contact = this.selectedAccountContext.getContact(contactId)
     const address = contact.getAddress()
-    const result = this._dc.createContact(name, address)
+    const result = this.selectedAccountContext.createContact(name, address)
 
     // trigger interface updates
     const chatId = this.getChatIdByContactId(contactId)
@@ -38,21 +38,21 @@ export default class DCContacts extends SplitOut {
     if (!DeltaChat.maybeValidAddr(email)) {
       throw new Error(this._controller.translate('bad_email_address'))
     }
-    return this._dc.createContact(name || '', email)
+    return this.selectedAccountContext.createContact(name || '', email)
   }
 
   createChatByContactId(contactId: number) {
-    const contact = this._dc.getContact(contactId)
+    const contact = this.selectedAccountContext.getContact(contactId)
     if (!contact) {
       log.warn(`no contact could be found with id ${contactId}`)
       return 0
     }
-    const chatId = this._dc.createChatByContactId(contactId)
+    const chatId = this.selectedAccountContext.createChatByContactId(contactId)
     log.debug(`created chat ${chatId} with contact' ${contactId}`)
-    const chat = this._dc.getChat(chatId)
+    const chat = this.selectedAccountContext.getChat(chatId)
     if (chat && chat.getVisibility() === C.DC_CHAT_VISIBILITY_ARCHIVED) {
       log.debug('chat was archived, unarchiving it')
-      this._dc.setChatVisibility(chatId, C.DC_CHAT_VISIBILITY_NORMAL)
+      this.selectedAccountContext.setChatVisibility(chatId, C.DC_CHAT_VISIBILITY_NORMAL)
     }
     this._controller.chatList.updateChatList()
     this._controller.chatList.selectChat(chatId)
@@ -60,20 +60,20 @@ export default class DCContacts extends SplitOut {
   }
 
   getContact(contactId: number) {
-    return this._dc.getContact(contactId).toJson()
+    return this.selectedAccountContext.getContact(contactId).toJson()
   }
 
   getChatIdByContactId(contactId: number) {
-    return this._dc.getChatIdByContactId(contactId)
+    return this.selectedAccountContext.getChatIdByContactId(contactId)
   }
 
   /** Gets the direct message chat id with this contact and creates it if it doesn't exist yet */
   getDMChatId(contactId: number) {
-    const existingChatId = this._dc.getChatIdByContactId(contactId)
+    const existingChatId = this.selectedAccountContext.getChatIdByContactId(contactId)
     if (existingChatId !== 0) {
       return existingChatId
     }
-    const newChatId = this._dc.createChatByContactId(contactId)
+    const newChatId = this.selectedAccountContext.createChatByContactId(contactId)
     if (newChatId !== 0) {
       return newChatId
     } else {
@@ -83,7 +83,7 @@ export default class DCContacts extends SplitOut {
 
   getContactIds(listFlags: number, queryStr: string): number[] {
     return (
-      this._dc
+      this.selectedAccountContext
         .getContacts(listFlags, queryStr)
         // deduplicate the items here until its fixed in the core: see https://github.com/deltachat/deltachat-core-rust/issues/2552
         .filter(function (item: number, pos: number, ary: number[]) {
@@ -93,7 +93,7 @@ export default class DCContacts extends SplitOut {
   }
 
   _getDCContact(id: number) {
-    const contact = this._dc.getContact(id).toJson()
+    const contact = this.selectedAccountContext.getContact(id).toJson()
     return { ...contact }
   }
 
@@ -106,13 +106,13 @@ export default class DCContacts extends SplitOut {
   }
 
   getEncryptionInfo(contactId: number) {
-    return this._dc.getContactEncryptionInfo(contactId)
+    return this.selectedAccountContext.getContactEncryptionInfo(contactId)
   }
 
   lookupContactIdByAddr(email: string): number {
     if (!DeltaChat.maybeValidAddr(email)) {
       throw new Error(this._controller.translate('bad_email_address'))
     }
-    return this._dc.lookupContactIdByAddr(email)
+    return this.selectedAccountContext.lookupContactIdByAddr(email)
   }
 }
