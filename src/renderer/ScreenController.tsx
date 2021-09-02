3c3c3888f5cbb14f3c9d447e0bbd66926e5d373f
jikstra <jikstra@disroot.org>
2021-09-02 16:43:29 +0200

    Don't display network/login failed errors anymore

--- src/renderer/ScreenController.tsx
+++ src/renderer/ScreenController.tsx
@@ -97,8 +97,6 @@ export default class ScreenController extends Component {
   componentDidMount() {
     ipcRenderer.on('error', this.onError)
     ipcRenderer.on('DC_EVENT_ERROR', this.onError)
-    ipcRenderer.on('DC_EVENT_LOGIN_FAILED', this.onError)
-    ipcRenderer.on('DC_EVENT_ERROR_NETWORK', this.onError)
     ipcRenderer.on('success', this.onSuccess)
     ipcRenderer.on('showAboutDialog', this.onShowAbout)
     ipcRenderer.on('open-url', this.onOpenUrl)
@@ -111,8 +109,6 @@ export default class ScreenController extends Component {
     ipcRenderer.removeListener('showAboutDialog', this.onShowAbout)
     ipcRenderer.removeListener('error', this.onError)
     ipcRenderer.removeListener('DC_EVENT_ERROR', this.onError)
-    ipcRenderer.removeListener('DC_EVENT_LOGIN_FAILED', this.onError)
-    ipcRenderer.removeListener('DC_EVENT_ERROR_NETWORK', this.onError)
     ipcRenderer.removeListener('success', this.onSuccess)
     ipcRenderer.removeListener('open-url', this.onOpenUrl)
   }
