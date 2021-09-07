d27b8412b2239a0fa431396287886dc7fb739fa4
SimonLaux <mobile.info@simonlaux.de>
2021-09-07 15:39:26 +0200

    connectivity view sandboxing through iframe

--- src/renderer/components/dialogs/Settings-Connectivity.tsx
+++ src/renderer/components/dialogs/Settings-Connectivity.tsx
@@ -1,10 +1,14 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 import { Card, Elevation } from '@blueprintjs/core'
 import React from 'react'
 
 import { DeltaDialogBody, DeltaDialogCloseFooter } from './DeltaDialog'
 import { DeltaBackend } from '../../delta-remote'
 import { onDCEvent } from '../../ipc'
+
+const INHERIT_STYLES = ['line-height', 'background-color', 'color', 'font-size']
+const OverwrittenStyles =
+  'font-family: Arial, Helvetica, sans-serif;font-variant-ligatures: none;'
 
 export default function SettingsConnectivity({
   setShow,
@@ -14,21 +18,47 @@ export default function SettingsConnectivity({
   onClose: any
 }) {
   const [connectivityHTML, setConnectivityHTML] = useState('')
+  const styleSensor = useRef<HTMLDivElement | null>(null)
 
-  const updateConnectivity = async () => {
-    setConnectivityHTML(await DeltaBackend.call('context.getConnectivityHTML'))
-  }
+  const updateConnectivity = useMemo(
+    () => async () => {
+      let cHTML = await DeltaBackend.call('context.getConnectivityHTML')
+
+      if (styleSensor.current) {
+        const cstyle = window.getComputedStyle(styleSensor.current)
+        let resulting_style = ''
+        for (const property of INHERIT_STYLES) {
+          resulting_style += `${property}: ${cstyle.getPropertyValue(
+            property
+          )};`
+        }
+        cHTML = cHTML.replace(
+          '</style>',
+          `</style><style> html {${resulting_style}${OverwrittenStyles}}</style>`
+        )
+      }
+
+      setConnectivityHTML(cHTML)
+    },
+    [!styleSensor.current] // eslint-disable-line react-hooks/exhaustive-deps
+  )
 
   useEffect(() => {
     updateConnectivity()
     return onDCEvent('DC_EVENT_CONNECTIVITY_CHANGED', updateConnectivity)
-  }, [])
+  }, [updateConnectivity])
 
   return (
     <>
       <DeltaDialogBody noFooter>
         <Card elevation={Elevation.ONE} style={{ paddingTop: '0px' }}>
-          <div dangerouslySetInnerHTML={{ __html: connectivityHTML }} />
+          <div ref={styleSensor} style={{ height: '100%', width: '100%' }}>
+            <iframe
+              style={{ border: 0, height: '100%', width: '100%' }}
+              srcDoc={connectivityHTML}
+              sandbox={''}
+            />
+          </div>
         </Card>
       </DeltaDialogBody>
       <DeltaDialogCloseFooter onClose={() => setShow('main')} />
