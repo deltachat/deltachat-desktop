5228c1192948db230a81e13f7f555a018dac39c7
SimonLaux <mobile.info@simonlaux.de>
2022-03-03 21:16:41 +0100

    remove not existing function from typedef

--- src/main/types.d.ts
+++ src/main/types.d.ts
@@ -1,13 +1,7 @@
 import { App } from 'electron'
-import { DesktopSettingsType, RC_Config } from '../shared/shared-types'
+import { RC_Config } from '../shared/shared-types'
 
-export interface ExtendedAppMainProcess extends ExtendedApp {
-  saveState?: (arg?: { saved: DesktopSettingsType }) => void
-  // saveState is likely not shared to renderer so the type ExtendedApp can not be shared with renderer
-  // once: (event: 'ipcReady', listener: () => void) => this // how can we overload this function without overwriting it?
-}
-
-export interface ExtendedApp extends App {
+export interface ExtendedAppMainProcess extends App {
   rc: RC_Config
   isQuitting: boolean
   ipcReady: boolean
