226fd4f32c6ac45c33f75af0f666986242910be8
flipsimon <28535045+flipsimon@users.noreply.github.com>
2022-07-26 11:48:28 +0200

    started porting messages

--- src/renderer/components/Gallery.tsx
+++ src/renderer/components/Gallery.tsx
@@ -6,6 +6,10 @@ import { ScreenContext } from '../contexts'
 import MediaAttachment from './attachment/mediaAttachment'
 import { MessageType } from '../../shared/shared-types'
 import { getLogger } from '../../shared/logger'
+import { BackendRemote } from '../backend-com'
+import { selectedAccountId } from '../ScreenController'
+import { Viewtype, Message, WebxdcMessageInfo } from 'deltachat-node/deltachat-jsonrpc/typescript/generated/types'
+import { WebxdcInfo } from 'deltachat-node/node/dist/context'
 
 const log = getLogger('renderer/Gallery')
 
@@ -13,23 +17,23 @@ type MediaTabKey = 'images' | 'video' | 'audio' | 'documents' | 'webxdc'
 
 const MediaTabs: Readonly<
   {
-    [key in MediaTabKey]: { values: number[] }
+    [key in MediaTabKey]: { values: Type.Viewtype[] }
   }
 > = Object.freeze({
   images: {
-    values: [C.DC_MSG_GIF, C.DC_MSG_IMAGE],
+    values: ["Gif", "Image"],
   },
   video: {
-    values: [C.DC_MSG_VIDEO],
+    values: ["Video"],
   },
   audio: {
-    values: [C.DC_MSG_AUDIO, C.DC_MSG_VOICE],
+    values: ["Audio", "Voice"],
   },
   documents: {
-    values: [C.DC_MSG_FILE],
+    values: ["File"],
   },
   webxdc: {
-    values: [C.DC_MSG_WEBXDC],
+    values: ["Webxdc"],
   },
 })
 
@@ -37,7 +41,7 @@ type mediaProps = { chatId: number }
 
 export default class Gallery extends Component<
   mediaProps,
-  { id: MediaTabKey; msgTypes: number[]; medias: MessageType[] }
+  { id: MediaTabKey; msgTypes: Viewtype[]; medias: MessageType[] }
 > {
   constructor(props: mediaProps) {
     super(props)
@@ -58,27 +62,22 @@ export default class Gallery extends Component<
     }
   }
 
-  onSelect(id: MediaTabKey) {
+  async onSelect(id: MediaTabKey) {
     if (!this.props.chatId) {
       throw new Error('chat id missing')
     }
+
     const msgTypes = MediaTabs[id].values
-    DeltaBackend.call(
-      'chat.getChatMedia',
+    const mediaMessageIds = await BackendRemote.rpc.chatGetMedia(
+      selectedAccountId(),
       this.props.chatId,
       msgTypes[0],
-      msgTypes[1],
-      0
-    ).then(raw_medias => {
-      const medias = raw_medias.filter(m => !!m) as MessageType[]
-      if (medias.length !== raw_medias.length) {
-        log.error(
-          'some empty gallery items detected, maybe messages are missing?'
-        )
-      }
-      this.setState({ id, msgTypes, medias })
-      this.forceUpdate()
-    })
+      msgTypes[1] || null,
+      msgTypes[2] || null
+    )
+    const mediaMessages = await BackendRemote.rpc.messageGetMessages(selectedAccountId(), mediaMessageIds)
+    this.setState({ id, msgTypes, mediaMessages })
+    this.forceUpdate()
   }
 
   emptyTabMessage(id: MediaTabKey): string {
@@ -145,6 +144,83 @@ export default class Gallery extends Component<
         </div>
       </div>
     )
+  }
+}
+
+export function newViewtypeToOldViewtype(viewtype: Viewtype) {
+  switch (viewtype) {
+    case "Unknown":
+      return 0
+    case "Text":
+      return C.DC_MSG_TEXT
+    case "Image":
+      return C.DC_MSG_IMAGE
+    case "Gif":
+      return C.DC_MSG_GIF
+    case "Sticker":
+      return C.DC_MSG_STICKER
+    case "Audio":
+      return C.DC_MSG_AUDIO
+    case "Voice":
+      return C.DC_MSG_VOICE
+    case "Video":
+      return C.DC_MSG_VIDEO
+    case "File":
+      return C.DC_MSG_FILE
+    case "VideochatInvitation":
+      return C.DC_MSG_VIDEOCHAT_INVITATION
+    case "Webxdc":
+      return C.DC_MSG_WEBXDC
+  }
+}
+
+export function newWebxdcInfoToOldWebxdcInfo(newWebxdcInfo: WebxdcMessageInfo): WebxdcInfo {
+  return {
+    name: newWebxdcInfo.name,
+    icon: newWebxdcInfo.icon,
+    summary: newWebxdcInfo.summary || ""
+  }
+}
+
+export async function newMessageToOldMessage(message: Message): MessageType {
+  const accountId = selectedAccountId()
+  const oldViewType = newViewtypeToOldViewtype(message.viewType)
+  const webxdcInfo = message.viewType === 'Webxdc' ? newWebxdcInfoToOldWebxdcInfo(await BackendRemote.rpc.messageGetWebxdcInfo(accountId, message.id)) : null
+  const downloadState = message.downloadState
+  return {
+    chatId: message.chatId,
+    webxdcInfo,
+    downloadState,
+    duration: this.getDuration(),
+    file: this.getFile(),
+    fromId: this.getFromId(),
+    id: this.getId(),
+    quotedText: this.getQuotedText(),
+    quotedMessageId: quotedMessage ? quotedMessage.getId() : null,
+    receivedTimestamp: this.getReceivedTimestamp(),
+    sortTimestamp: this.getSortTimestamp(),
+    text: this.getText(),
+    timestamp: this.getTimestamp(),
+    hasLocation: this.hasLocation(),
+    hasHTML: this.hasHTML,
+    viewType,
+    state: binding.dcn_msg_get_state(this.dc_msg),
+    hasDeviatingTimestamp: this.hasDeviatingTimestamp(),
+    showPadlock: this.getShowpadlock(),
+    summary: this.getSummary().toJson(),
+    subject: this.subject,
+    isSetupmessage: this.isSetupmessage(),
+    isInfo: this.isInfo(),
+    isForwarded: this.isForwarded(),
+    dimensions: {
+      height: this.getHeight(),
+      width: this.getWidth(),
+    },
+    videochatType: this.getVideochatType(),
+    videochatUrl: this.getVideochatUrl(),
+    overrideSenderName: this.overrideSenderName,
+    parentId: this.parent?.getId(),
+
   }
 }
 
