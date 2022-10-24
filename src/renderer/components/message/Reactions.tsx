7693071e38b737fdcf05477b4c22a960d7ff4633
SimonLaux <mobile.info@simonlaux.de>
2022-10-24 23:59:18 +0200

    format document and add react key to fix react error

--- src/renderer/components/message/Reactions.tsx
+++ src/renderer/components/message/Reactions.tsx
@@ -1,37 +1,39 @@
-import React from 'react';
-import { C } from 'deltachat-node/node/dist/constants';
-import { BackendRemote } from '../../backend-com';
-import { T } from '@deltachat/jsonrpc-client';
-import { selectedAccountId } from '../../ScreenController';
+import React from 'react'
+import { C } from 'deltachat-node/node/dist/constants'
+import { BackendRemote } from '../../backend-com'
+import { T } from '@deltachat/jsonrpc-client'
+import { selectedAccountId } from '../../ScreenController'
 
 export function Reactions({
-  reactions, messageId,
+  reactions,
+  messageId,
 }: {
-  reactions: T.Reactions;
-  messageId: number;
+  reactions: T.Reactions
+  messageId: number
 }) {
-  const emojis = Object.keys(reactions.reactions);
-  const own_emojis = reactions.reactionsByContact[C.DC_CONTACT_ID_SELF] || [];
+  const emojis = Object.keys(reactions.reactions)
+  const own_emojis = reactions.reactionsByContact[C.DC_CONTACT_ID_SELF] || []
 
   // TODO: on hover show who reacted? maybe lazy loading onhover set title of show custom popover
   return (
     <div className='message-reactions'>
       {emojis.map(emoji => {
-        const count = reactions.reactions[emoji];
-        const is_own = own_emojis.includes(emoji);
+        const count = reactions.reactions[emoji]
+        const is_own = own_emojis.includes(emoji)
 
         return (
           <span
             className={`reaction ${is_own && 'own'}`}
             onClick={react.bind(null, messageId, own_emojis, emoji, is_own)}
+            key={emoji}
           >
             {emoji}
             {count > 1 ? ` ${count}` : ''}
           </span>
-        );
+        )
       })}
     </div>
-  );
+  )
 }
 
 async function react(
@@ -40,11 +42,11 @@ async function react(
   emoji: string,
   remove: boolean
 ) {
-  let emojis = [...own_emojis];
+  let emojis = [...own_emojis]
   if (remove) {
-    emojis = emojis.filter((e) => e !== emoji);
+    emojis = emojis.filter(e => e !== emoji)
   } else {
-    emojis.push(emoji);
+    emojis.push(emoji)
   }
-  await BackendRemote.rpc.sendReaction(selectedAccountId(), messageId, emojis);
+  await BackendRemote.rpc.sendReaction(selectedAccountId(), messageId, emojis)
 }
