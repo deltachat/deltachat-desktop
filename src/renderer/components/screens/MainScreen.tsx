bcf4d4fc7d04b5904bef1c14513e42100f43074a
jikstra <jikstra@disroot.org>
2022-11-03 15:29:36 +0100

    Fix formatting

--- src/renderer/components/screens/MainScreen.tsx
+++ src/renderer/components/screens/MainScreen.tsx
@@ -218,7 +218,7 @@ export default function MainScreen() {
                   cursor: 'pointer',
                   display: 'flex',
                   alignItems: 'center',
-                  width: '100%'
+                  width: '100%',
                 }}
                 onClick={onTitleClick}
               >
