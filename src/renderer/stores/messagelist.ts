66718a13e1707fd69dc7541929b64b1be9d21668
SimonLaux <mobile.info@simonlaux.de>
2022-11-07 16:07:49 +0100

    remove the message pages abstraction

--- src/renderer/stores/messagelist.ts
+++ src/renderer/stores/messagelist.ts
@@ -27,107 +27,22 @@ export interface MessagePage {
 interface MessageListState {
   // chat: Type.FullChat | null
   messageListItems: T.MessageListItem[]
-  messagePages: MessagePage[]
+  messageCache: { [msgId: number]: T.Message }
   newestFetchedMessageListItemIndex: number
   oldestFetchedMessageListItemIndex: number
   viewState: ChatViewState
   jumpToMessageStack: number[]
-  countFetchedMessages: number
 }
 
 const defaultState = () =>
   ({
     messageListItems: [],
-    messagePages: [],
+    messageCache: {},
     newestFetchedMessageListItemIndex: -1,
     oldestFetchedMessageListItemIndex: -1,
     viewState: defaultChatViewState(),
     jumpToMessageStack: [],
-    countFetchedMessages: 0,
   } as MessageListState)
-
-async function messagePageFromMessageIndexes(
-  accountId: number,
-  chatId: number,
-  indexStart: number,
-  indexEnd: number
-): Promise<MessagePage> {
-  const rawMessages = await getMessagesFromIndex(
-    accountId,
-    chatId,
-    indexStart,
-    indexEnd
-  )
-
-  if (rawMessages.length === 0) {
-    throw new Error(
-      'messagePageFromMessageIndexes: _messages.length equals zero. This should not happen'
-    )
-  }
-
-  // log.debug('messagePageFromMessageIndexes', rawMessages, indexEnd, indexStart)
-
-  if (rawMessages.length !== indexEnd - indexStart + 1) {
-    throw new Error(
-      "messagePageFromMessageIndexes: _messages.length doesn't equal indexEnd - indexStart + 1. This should not happen"
-    )
-  }
-
-  const messages = OrderedMap<
-    number | string,
-    Type.Message | { id: string; ts: number }
-  >().withMutations(messagePages => {
-    for (let i = 0; i < rawMessages.length; i++) {
-      const [messageId, message] = rawMessages[i]
-      messagePages.set(messageId, message)
-    }
-  })
-
-  // log.debug('messagePageFromMessageIndexes', messages)
-
-  const messagePage = {
-    pageKey: calculatePageKey(messages, indexStart, indexEnd),
-    messages,
-  }
-
-  return messagePage
-}
-
-async function messagePagesFromMessageIndexes(
-  accountId: number,
-  chatId: number,
-  indexStart: number,
-  indexEnd: number
-): Promise<MessagePage[]> {
-  const rawMessages = await getMessagesFromIndex(
-    accountId,
-    chatId,
-    indexStart,
-    indexEnd
-  )
-
-  const messagePages = []
-  while (rawMessages.length > 0) {
-    const pageMessages = rawMessages.splice(0, PAGE_SIZE)
-
-    const messages = OrderedMap<
-      number | string,
-      Type.Message | { id: string; ts: number }
-    >().withMutations(messagePages => {
-      for (let i = 0; i < pageMessages.length; i++) {
-        const [messageId, message] = pageMessages[i]
-        messagePages.set(messageId, message)
-      }
-    })
-
-    messagePages.push({
-      pageKey: calculatePageKey(messages, indexStart, indexEnd),
-      messages,
-    })
-  }
-
-  return messagePages
-}
 
 export function useMessageList(accountId: number, chatId: number) {
   let store = useMemo(() => {
@@ -140,10 +55,7 @@ export function useMessageList(accountId: number, chatId: number) {
     let cleanup = [
       onDCEvent(accountId, 'MsgDelivered', ({ chatId: eventChatId, msgId }) => {
         if (chatId === eventChatId) {
-          store.reducer.setMessageState({
-            messageId: msgId,
-            messageState: C.DC_STATE_OUT_DELIVERED,
-          })
+          store.reducer.setMessageState(msgId, C.DC_STATE_OUT_DELIVERED)
         }
       }),
       onDCEvent(accountId, 'IncomingMsg', ({ chatId: eventChatId }) => {
@@ -157,10 +69,7 @@ export function useMessageList(accountId: number, chatId: number) {
       }),
       onDCEvent(accountId, 'MsgRead', ({ chatId: eventChatId, msgId }) => {
         if (chatId === eventChatId) {
-          store.reducer.setMessageState({
-            messageId: msgId,
-            messageState: C.DC_STATE_OUT_MDN_RCVD,
-          })
+          store.reducer.setMessageState(msgId, C.DC_STATE_OUT_MDN_RCVD)
         }
       }),
       onDCEvent(accountId, 'MsgsChanged', ({ chatId: eventChatId, msgId }) => {
@@ -205,6 +114,10 @@ export function useMessageList(accountId: number, chatId: number) {
   return { store, state, fetchMoreTop, fetchMoreBottom }
 }
 
+function getView<T>(items: T[], start: number, end: number) {
+  return items.slice(start, end + 1)
+}
+
 class MessageListStore extends Store<MessageListState> {
   scheduler = new ChatStoreScheduler()
 
@@ -217,16 +130,15 @@ class MessageListStore extends Store<MessageListState> {
     super(defaultState())
   }
 
-  guardReducerTriesToAddDuplicatePageKey(pageKeyToAdd: string) {
-    const isDuplicatePageKey =
-      this.state.messagePages.findIndex(
-        messagePage => messagePage.pageKey === pageKeyToAdd
-      ) !== -1
-    if (isDuplicatePageKey) {
-      this.log.error('Duplicate page key')
-    }
-    return isDuplicatePageKey
+  get activeView() {
+    const items = [...this.state.messageListItems]
+    const start = this.state.oldestFetchedMessageListItemIndex
+    const end = this.state.newestFetchedMessageListItemIndex
+    const view = getView(items, start, end)
+    this.log.debug('get activeView', { end, start, view })
+    return view
   }
+
   reducer = {
     selectedChat: (payload: Partial<MessageListState>) => {
       this.setState(_ => {
@@ -240,7 +152,7 @@ class MessageListStore extends Store<MessageListState> {
     },
     refresh: (
       messageListItems: T.MessageListItem[],
-      messagePages: MessagePage[],
+      messageCache: MessageListState['messageCache'],
       newestFetchedMessageListItemIndex: number,
       oldestFetchedMessageListItemIndex: number
     ) => {
@@ -248,9 +160,8 @@ class MessageListStore extends Store<MessageListState> {
         const modifiedState: MessageListState = {
           ...state,
           messageListItems,
-          messagePages,
+          messageCache,
           viewState: ChatViewReducer.refresh(state.viewState),
-          countFetchedMessages: messageListItems.length,
           newestFetchedMessageListItemIndex,
           oldestFetchedMessageListItemIndex,
         }
@@ -268,76 +179,56 @@ class MessageListStore extends Store<MessageListState> {
     },
     appendMessagePageTop: (payload: {
       id: number
-      messagePage: MessagePage
-      countFetchedMessages: number
+      newMessageCacheItems: MessageListState['messageCache']
       oldestFetchedMessageListItemIndex: number
     }) => {
       this.setState(state => {
         const modifiedState: MessageListState = {
           ...state,
-          messagePages: [payload.messagePage, ...state.messagePages],
+          messageCache: {
+            ...state.messageCache,
+            ...payload.newMessageCacheItems,
+          },
           oldestFetchedMessageListItemIndex:
             payload.oldestFetchedMessageListItemIndex,
           viewState: ChatViewReducer.appendMessagePageTop(state.viewState),
-          countFetchedMessages: payload.countFetchedMessages,
-        }
-        if (
-          this.guardReducerTriesToAddDuplicatePageKey(
-            payload.messagePage.pageKey
-          )
-        ) {
-          return
         }
         return modifiedState
       }, 'appendMessagePageTop')
     },
     appendMessagePageBottom: (payload: {
-      messagePage: MessagePage
-      countFetchedMessages: number
+      newMessageCacheItems: MessageListState['messageCache']
       newestFetchedMessageIndex: number
     }) => {
       this.setState(state => {
         const modifiedState: MessageListState = {
           ...state,
-          messagePages: [...state.messagePages, payload.messagePage],
+          messageCache: {
+            ...state.messageCache,
+            ...payload.newMessageCacheItems,
+          },
           newestFetchedMessageListItemIndex: payload.newestFetchedMessageIndex,
           viewState: ChatViewReducer.appendMessagePageBottom(state.viewState),
-          countFetchedMessages: payload.countFetchedMessages,
         }
-        if (
-          this.guardReducerTriesToAddDuplicatePageKey(
-            payload.messagePage.pageKey
-          )
-        )
-          return
         return modifiedState
       }, 'appendMessagePageBottom')
     },
     fetchedIncomingMessages: (payload: {
       messageListItems: MessageListState['messageListItems']
       newestFetchedMessageIndex: number
-      messagePage: MessagePage
+      newMessageCacheItems: MessageListState['messageCache']
     }) => {
       this.setState(state => {
         const modifiedState: MessageListState = {
           ...state,
           messageListItems: payload.messageListItems,
-          messagePages: [...state.messagePages, payload.messagePage],
+          messageCache: {
+            ...state.messageCache,
+            ...payload.newMessageCacheItems,
+          },
           // newestFetchedMessageIndex: payload.newestFetchedMessageIndex,
           viewState: ChatViewReducer.fetchedIncomingMessages(state.viewState),
         }
-
-        if (
-          this.guardReducerTriesToAddDuplicatePageKey(
-            payload.messagePage.pageKey
-          )
-        ) {
-          throw new Error(
-            'We almost added the same page twice! We should prevent this in code duplicate pageKey: ' +
-              payload.messagePage.pageKey
-          )
-        }
-
         return modifiedState
       }, 'fetchedIncomingMessages')
     },
@@ -352,52 +243,26 @@ class MessageListStore extends Store<MessageListState> {
         return modifiedState
       }, 'unlockScroll')
     },
-    messageChanged: (payload: { messagesChanged: Type.Message[] }) => {
+    messageChanged: (message: Type.Message) => {
       this.setState(state => {
         const modifiedState: MessageListState = {
           ...state,
-          messagePages: state.messagePages.map(messagePage => {
-            let changed = false
-            const messages = messagePage.messages.withMutations(messages => {
-              for (const changedMessage of payload.messagesChanged) {
-                if (!messages.has(changedMessage.id)) continue
-                changed = true
-                messages.set(changedMessage.id, changedMessage)
-              }
-            })
-            if (changed === false) return messagePage
-            return {
-              ...messagePage,
-              messages,
-            }
-          }),
+          messageCache: { ...state.messageCache, [message.id]: message },
         }
         return modifiedState
       }, 'messageChanged')
     },
-    setMessageState: (payload: { messageId: number; messageState: number }) => {
-      const { messageId, messageState } = payload
+    setMessageState: (messageId: number, messageState: number) => {
       this.setState(state => {
         const modifiedState: MessageListState = {
           ...state,
-          messagePages: state.messagePages.map(messagePage => {
-            if (messagePage.messages.has(messageId)) {
-              const message = messagePage.messages.get(messageId)
-              if (message !== null && message !== undefined) {
-                const updatedMessages = messagePage.messages.set(messageId, {
-                  ...message,
-                  state: messageState,
-                })
-
-                return {
-                  ...messagePage,
-                  messages: updatedMessages,
-                }
-              }
-            }
-
-            return messagePage
-          }),
+          messageCache: {
+            ...state.messageCache,
+            [messageId]: {
+              ...state.messageCache[messageId],
+              state: messageState,
+            },
+          },
         }
         return modifiedState
       }, 'setMessageState')
@@ -455,7 +320,7 @@ class MessageListStore extends Store<MessageListState> {
 
         let oldestFetchedMessageListItemIndex = -1
         let newestFetchedMessageListItemIndex = -1
-        let messagePage: MessagePage | null = null
+        let messageCache: MessageListState['messageCache'] = {}
         if (messageListItems.length !== 0) {
           // mesageIds.length = 1767
           // oldestFetchedMessageListItemIndex = 1767 - 1 = 1766 - 10 = 1756
@@ -466,26 +331,21 @@ class MessageListStore extends Store<MessageListState> {
           )
           newestFetchedMessageListItemIndex = messageListItems.length - 1
 
-          messagePage = await messagePageFromMessageIndexes(
+          messageCache = await loadMessages(
             this.accountId,
-            this.chatId,
+            messageListItems,
             oldestFetchedMessageListItemIndex,
             newestFetchedMessageListItemIndex
           )
         }
 
         this.reducer.selectedChat({
-          messagePages: messagePage === null ? [] : [messagePage],
+          messageCache,
           messageListItems,
           oldestFetchedMessageListItemIndex,
           newestFetchedMessageListItemIndex,
           viewState: ChatViewReducer.selectChat(this.state.viewState),
         })
-        ActionEmitter.emitAction(
-          chat.archived
-            ? KeybindAction.ChatList_SwitchToArchiveView
-            : KeybindAction.ChatList_SwitchToNormalView
-        )
       },
       'selectChat'
     ),
@@ -599,7 +459,7 @@ class MessageListStore extends Store<MessageListState> {
           // calculate page indexes, so that jumpToMessageId is in the middle of the page
           let oldestFetchedMessageListItemIndex = -1
           let newestFetchedMessageListItemIndex = -1
-          let messagePage: MessagePage | null = null
+          let messageCache: MessageListState['messageCache'] = {}
           const half_page_size = Math.ceil(PAGE_SIZE / 2)
           if (messageListItems.length !== 0) {
             oldestFetchedMessageListItemIndex = Math.max(
@@ -629,22 +489,16 @@ class MessageListStore extends Store<MessageListState> {
               )
             }
 
-            messagePage = await messagePageFromMessageIndexes(
+            messageCache = await loadMessages(
               accountId,
-              chatId,
+              messageListItems,
               oldestFetchedMessageListItemIndex,
               newestFetchedMessageListItemIndex
             )
           }
 
-          if (messagePage === null) {
-            throw new Error(
-              'jumpToMessage: messagePage is null, this should not happen'
-            )
-          }
-
           this.reducer.selectedChat({
-            messagePages: [messagePage],
+            messageCache,
             messageListItems,
             oldestFetchedMessageListItemIndex,
             newestFetchedMessageListItemIndex: newestFetchedMessageListItemIndex,
@@ -690,18 +544,17 @@ class MessageListStore extends Store<MessageListState> {
             return false
           }
 
-          const messagePage: MessagePage = await messagePageFromMessageIndexes(
+          const newMessageCacheItems = await loadMessages(
             this.accountId,
-            id,
+            state.messageListItems,
             oldestFetchedMessageListItemIndex,
             lastMessageIndexOnLastPage - 1
           )
 
           this.reducer.appendMessagePageTop({
             id,
-            messagePage,
+            newMessageCacheItems,
             oldestFetchedMessageListItemIndex,
-            countFetchedMessages: fetchedMessageListItems.length,
           })
           return true
         },
@@ -747,17 +600,16 @@ class MessageListStore extends Store<MessageListState> {
             return false
           }
 
-          const messagePage: MessagePage = await messagePageFromMessageIndexes(
+          const newMessageCacheItems = await loadMessages(
             this.accountId,
-            this.chatId,
+            state.messageListItems,
             newestFetchedMessageListItemIndex,
             newNewestFetchedMessageListItemIndex
           )
 
           this.reducer.appendMessagePageBottom({
-            messagePage,
+            newMessageCacheItems,
             newestFetchedMessageIndex: newNewestFetchedMessageListItemIndex,
-            countFetchedMessages: fetchedMessageListItems.length,
           })
           return true
         },
@@ -789,16 +641,16 @@ class MessageListStore extends Store<MessageListState> {
             0
           )
 
-          const messagePages = await messagePagesFromMessageIndexes(
+          const messageCache = await loadMessages(
             this.accountId,
-            this.chatId,
+            messageListItems,
             oldestFetchedMessageListItemIndex,
             newestFetchedMessageListItemIndex
           )
 
           this.reducer.refresh(
             messageListItems,
-            messagePages,
+            messageCache,
             newestFetchedMessageListItemIndex,
             oldestFetchedMessageListItemIndex
           )
@@ -835,16 +687,17 @@ class MessageListStore extends Store<MessageListState> {
         this.reducer.setMessageListItems(messageListItems)
         return
       }
-      const messagePage = await messagePageFromMessageIndexes(
+
+      const newMessageCacheItems = await loadMessages(
         this.accountId,
-        this.chatId,
+        messageListItems,
         indexStart,
         indexEnd
       )
 
       this.reducer.fetchedIncomingMessages({
         messageListItems,
-        messagePage,
+        newMessageCacheItems,
         newestFetchedMessageIndex: indexEnd,
       })
     }, 'onEventIncomingMessage'),
@@ -864,9 +717,7 @@ class MessageListStore extends Store<MessageListState> {
               this.accountId,
               messageId
             )
-            this.reducer.messageChanged({
-              messagesChanged: [message],
-            })
+            this.reducer.messageChanged(message)
           } catch (error) {
             this.log.warn('failed to fetch message with id', messageId, error)
             // ignore not found and other errors
@@ -891,45 +742,26 @@ class MessageListStore extends Store<MessageListState> {
 
   stateToHumanReadable(state: MessageListState): any {
     return {
-      ...state,
-      messagePages: state.messagePages.map(messagePage => {
-        return {
-          ...messagePage,
-          messages: messagePage.messages.toArray().map(([msgId, message]) => {
-            return [
-              msgId,
-              typeof message.id !== 'string'
-                ? {
-                    messageId: message.id,
-                    messsage: (message as Type.Message).text,
-                  }
-                : {
-                    messageId: message.id,
-                    timestamp: (message as {
-                      id: string
-                      ts: number
-                    }).ts,
-                  },
-            ]
-          }),
-        }
-      }),
+      // ...state,
+      aV: this.activeView,
     }
   }
 }
 
-function calculatePageKey(
-  messages: MessagePage['messages'],
-  indexStart: number,
-  indexEnd: number
-): string {
-  const first = messages.first()?.id
-  const last = messages.last()?.id
-  if (!first && !last && indexStart === 0 && indexEnd === 0) {
-    throw new Error('calculatePageKey: non unique page key of 0')
-  } else {
-    return `page-${first}-${last}-${indexStart}-${indexEnd}`
-  }
+async function loadMessages(
+  accountId: number,
+  messageListItems: Type.MessageListItem[],
+  oldestFetchedMessageListItemIndex: number,
+  newestFetchedMessageListItemIndex: number
+) {
+  let view = getView(
+    messageListItems,
+    oldestFetchedMessageListItemIndex,
+    newestFetchedMessageListItemIndex
+  )
+    .map(m => (m.kind === 'message' ? m.msg_id : C.DC_MSG_ID_LAST_SPECIAL))
+    .filter(msgId => msgId !== C.DC_MSG_ID_LAST_SPECIAL)
+  return await BackendRemote.rpc.getMessages(accountId, view)
 }
 
 async function getMessagesFromIndex(
