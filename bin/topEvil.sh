ff1cafe0203cb76f2161d04e98c49e4fb15e1582
SimonLaux <mobile.info@simonlaux.de>
2023-02-15 19:48:43 +0100

    top evil summary

--- bin/topEvil.sh
+++ bin/topEvil.sh
@@ -5,11 +5,13 @@ echo "Top-Evil TypeScript"
 
 res=$( grep -r "^import.*blueprint" src --color=always )
 echo "🤔 Using Blueprint code: $( echo "$res" | wc -l )"
+code=$( echo "$res" | wc -l )
 echo "-----------------------------------------------------"
 echo "$res"
 
 res=$( grep -r "bp4-" src --color=always )
 echo "🤔 Using Blueprint css classes: $( echo "$res" | wc -l )"
+classes=$( echo "$res" | wc -l )
 echo "-----------------------------------------------------"
 echo "$res"
 
@@ -17,8 +19,12 @@ echo "Top-Evil SCSS"
 
 res=$( grep -r ".bp4-" scss --color=always )
 echo "🤔 Using Blueprint in css: $( echo "$res" | wc -l )"
+scss=$( echo "$res" | wc -l )
 echo "-----------------------------------------------------"
 echo "$res"
+
+echo "🤔Result🤔"
+echo "CODE: $code | CLASSES: $classes | SCSS: $scss"
 
 # res=$( grep -r "^import.*electron" src/main/deltachat --color=always )
 # echo "🤔 Using Electron inside of the deltachat controller: $( echo "$res" | wc -l )"
