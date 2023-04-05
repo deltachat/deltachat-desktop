e87781247c191d6b3a1abc3cbbbfd2887fa01cb7
Simon Laux <Simon-Laux@users.noreply.github.com>
2023-04-05 16:53:10 +0200

    device message for 1.36 (#3162)

--- src/renderer/deviceMessages.ts
+++ src/renderer/deviceMessages.ts
@@ -3,44 +3,22 @@ import { BackendRemote } from './backend-com'
 export function updateDeviceChats(accountId: number) {
   BackendRemote.rpc.addDeviceMessage(
     accountId,
-    'changelog-version-1.34.0-version0',
-    `What's new in 1.34.0?
+    'changelog-version-1.36.0-version0',
+    `What's new in 1.36.0?
 
-⚡️ Smooth rendering. Much snappier scrolling. The new version is easy on your patience and is the result of a refactoring that took more than a year (for our tech-savvy friends: we are now using JSON-RPC under the hood and threw away lots of old, annoying code :)  
-🧹 Clear chat history
-🔍 Search for messages in chat
-⏬ jump down button
-🤗 Friendlier contact lists: ordered by last seen and contacts seen within 10 minutes are marked by a dot 🟢
-🎛️ Tray icon is now enabled by default
-🔔 when receiving multiple notifications at once, they will be grouped into one single notification
-😀 right click to add sticker to the sticker selector
+💻📱 Use Delta Chat on all your devices easily - just follow three simple steps at \"Settings / Add Second Device\" (experimental)
 ✨ Many smaller bug fixes and general improvements
 
-Full Changelog: https://github.com/deltachat/deltachat-desktop/blob/master/CHANGELOG.md#1_34_0`
+Full Changelog: https://github.com/deltachat/deltachat-desktop/blob/master/CHANGELOG.md#1_36_0`
   )
 
+  const tx = window.static_translate
   BackendRemote.rpc.addDeviceMessage(
     accountId,
-    'changelog-version-1.34.2-version0',
-    `What's new in 1.34.2?
-
-We resolved two email (IMAP) connection issues.
-This release also includes some smaller improvements and bug fixes.
-
-Full Changelog: https://github.com/deltachat/deltachat-desktop/blob/master/CHANGELOG.md#1_34_2`
-  )
-
-  BackendRemote.rpc.addDeviceMessage(
-    accountId,
-    'changelog-version-1.34.4-version2',
-    `What's new in 1.34.4?
-
-More stability for sending Messages and support for the new unicode 13 and 14 emojis:
-13: 🥲😶‍🌫️😮‍💨😵‍💫🥸❤️‍🔥❤️‍🩹🤌🫀🫁🧔‍♂️🧔‍♀️🥷🤵‍♂️🤵‍♀️👰‍♂️👰‍♀️👩‍🍼👨‍🍼🧑‍🍼🧑‍🎄🫂🐈‍⬛🦬🦣🦫🐻‍❄️🦤🪶🦭🪲🪳🪰🪱🪴🫐🫒🫑🫓🫔🫕🫖🧋🪨🪵🛖🛻🛼🪄🪅🪆🪡🪢🩴🪖🪗🪘🪙🪃🪚🪛🪝🪜🛗🪞🪟🪠🪤🪣🪥🪦🪧⚧️🏳️‍⚧️
-14: 🫠🫢🫣🫡🫥🫤🥹🫱🫲🫳🫴🫰🫵🫶🫦🫅🫃🫄🧌🪸🪷🪹🪺🫘🫗🫙🛝🛞🛟🪩🪫🩼🩻🫧🪬🪪🟰
-
-The emoji picker now closes when you choose an emoji. Press shift to choose multiple emojis.
-
-Full Changelog: https://github.com/deltachat/deltachat-desktop/blob/master/CHANGELOG.md#1_34_4`
+    'changelog-version-1.36.0-audit',
+    tx(
+      'update_1_36_audit',
+      'https://delta.chat/en/2023-03-27-third-independent-security-audit'
+    )
   )
 }
