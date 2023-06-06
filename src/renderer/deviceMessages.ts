60649bda637042890988ac45a861c958192cb5c6
Simon Laux <Simon-Laux@users.noreply.github.com>
2023-06-06 13:41:43 +0200

    prepare 1.37.0 and update core (#3254)
    * prepare 1.37.0 and update core
    
    update deltachat-node and deltachat/jsonrpc-client to `v1.116.0`
    
    * add device message
    
    * fix typos

--- src/renderer/deviceMessages.ts
+++ src/renderer/deviceMessages.ts
@@ -25,11 +25,15 @@ Full Changelog: https://github.com/deltachat/deltachat-desktop/blob/master/CHANG
 
   BackendRemote.rpc.addDeviceMessage(
     accountId,
-    'changelog-version-1.36.4-version1',
-    `What's new in 1.36.4?
+    'changelog-version-1.37.0-version0',
+    `What's new in 1.37.0?
+    
+🖼️ Show thumbnail in chatlist of image, sticker and webxdc messages
+🔍 Improved design for message search results
+📎 Removed upper limit on attachment size
+☕ Wake up from standby now reconnects more reliably 
+✨ We fixed some bugs and improved stability
 
-✨ We fixed some bugs and improved the html email viewer.
-
-Full Changelog: https://github.com/deltachat/deltachat-desktop/blob/master/CHANGELOG.md#1_36_4`
+Full Changelog: https://github.com/deltachat/deltachat-desktop/blob/master/CHANGELOG.md#1_37_0`
   )
 }
