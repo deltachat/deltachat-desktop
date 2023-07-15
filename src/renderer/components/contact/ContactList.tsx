7211cb0b6e4af36316fb7eae965890e7949c23b3
Farooq Karimi Zadeh <fkz@riseup.net>
2023-07-15 14:19:09 +0330

    happy now, prettier?

--- src/renderer/components/contact/ContactList.tsx
+++ src/renderer/components/contact/ContactList.tsx
@@ -118,9 +118,12 @@ export function useContactsMap(listFlags: number, queryStr: string) {
   return [contacts, updateContacts] as [typeof contacts, typeof updateContacts]
 }
 
-
 // if trim is provided and is true, the queryStr is trimmed for contact search and for checking validity of the string as an Email address
-export function useContactsNew(listFlags: number, initialQueryStr: string, trim?: boolean) {
+export function useContactsNew(
+  listFlags: number,
+  initialQueryStr: string,
+  trim?: boolean
+) {
   const [state, setState] = useState<{
     contacts: Type.Contact[]
     queryStrIsValidEmail: boolean
