4e2ff7f3950a4942cad26dd6141cf81b1135c4bf
SimonLaux <mobile.info@simonlaux.de>
2023-09-08 00:30:13 +0200

    check schemes more explicitly

--- src/renderer/components/helpers/LinkConfirmation.ts
+++ src/renderer/components/helpers/LinkConfirmation.ts
@@ -4,7 +4,11 @@ import ConfirmationDialog from '../dialogs/ConfirmationDialog'
 /** opens http, https and mailto links, offers to copy all other links */
 export function openLinkSafely(url: string) {
   const tx = window.static_translate
-  if (url.startsWith('http') || url.startsWith('mailto')) {
+  if (
+    url.startsWith('http:') ||
+    url.startsWith('https:') ||
+    url.startsWith('mailto:')
+  ) {
     runtime.openLink(url)
   } else {
     window.__openDialog(ConfirmationDialog, {
