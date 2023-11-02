d497ef6b0430f2d2b8cb065e39ce4ac7dafdc172
adz <x12@adz.garden>
2023-11-02 11:53:56 +0100

    Initialise power monitor after electron signals readyness

--- src/main/index.ts
+++ src/main/index.ts
@@ -8,6 +8,7 @@ import type { EventEmitter } from 'events'
 import contextMenu from './electron-context-menu'
 import { findOutIfWeAreRunningAsAppx } from './isAppx'
 import { getHelpMenu } from './help_menu'
+import { initialisePowerMonitor } from './resume_from_sleep'
 
 // Hardening: prohibit all DNS queries, except for Mapbox
 // (see src/renderer/components/map/MapComponent.tsx)
@@ -181,6 +182,8 @@ async function onReady([_appReady, _loadedState, _appx, _webxdc_cleanup]: [
     log.error('Cleanup of old logfiles failed: ', err)
   )
   cleanupDraftTempDir()
+
+  initialisePowerMonitor()
 }
 
 ;(app as EventEmitter).once('ipcReady', () => {
@@ -313,5 +316,3 @@ ipcMain.handle('restart_app', async _ev => {
   app.relaunch()
   app.quit()
 })
-
-import './resume_from_sleep'
