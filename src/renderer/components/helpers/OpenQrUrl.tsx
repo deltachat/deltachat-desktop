2acfef2d1716c6caffc3a3b110b6e0c02dbdbb5b
adz <x12@adz.garden>
2023-11-03 12:40:28 +0100

    Move handling mailto links into own dedicated method

--- src/renderer/components/helpers/OpenQrUrl.tsx
+++ src/renderer/components/helpers/OpenQrUrl.tsx
@@ -72,6 +72,55 @@ async function setConfigFromQrCatchingErrorInAlert(qrContent: string) {
   }
 }
 
+export async function processMailtoUrl(url: string, callback: any = null) {
+  const tx = window.static_translate
+  log.debug('processing mailto url:', url)
+
+  try {
+    const accountId = selectedAccountId()
+    const mailto = parseMailto(url)
+    const messageText = mailto.subject
+      ? mailto.subject + (mailto.body ? '\n\n' + mailto.body : '')
+      : mailto.body
+
+    if (mailto.to) {
+      let contactId = await BackendRemote.rpc.lookupContactIdByAddr(
+        accountId,
+        mailto.to
+      )
+      if (contactId === null) {
+        contactId = await BackendRemote.rpc.createContact(
+          accountId,
+          mailto.to,
+          null
+        )
+      }
+      const chatId = await BackendRemote.rpc.createChatByContactId(
+        accountId,
+        contactId
+      )
+      if (messageText) {
+        await doMailtoAction(chatId, messageText)
+      } else {
+        selectChat(chatId)
+      }
+    } else {
+      if (messageText) {
+        window.__openDialog(MailtoDialog, { messageText })
+      }
+    }
+
+    callback && callback()
+  } catch (error) {
+    log.error('mailto decoding error', error)
+
+    window.__openDialog('AlertDialog', {
+      message: tx('mailto_link_could_not_be_decoded', url),
+      cb: callback,
+    })
+  }
+}
+
 export default async function processOpenQrUrl(
   url: string,
   callback: any = null,
@@ -80,48 +129,7 @@ export default async function processOpenQrUrl(
   const tx = window.static_translate
 
   if (url.toLowerCase().startsWith('mailto:')) {
-    log.debug('processing mailto url:', url)
-    try {
-      const accountId = selectedAccountId()
-      const mailto = parseMailto(url)
-      const messageText = mailto.subject
-        ? mailto.subject + (mailto.body ? '\n\n' + mailto.body : '')
-        : mailto.body
-
-      if (mailto.to) {
-        let contactId = await BackendRemote.rpc.lookupContactIdByAddr(
-          accountId,
-          mailto.to
-        )
-        if (contactId === null) {
-          contactId = await BackendRemote.rpc.createContact(
-            accountId,
-            mailto.to,
-            null
-          )
-        }
-        const chatId = await BackendRemote.rpc.createChatByContactId(
-          accountId,
-          contactId
-        )
-        if (messageText) {
-          await doMailtoAction(chatId, messageText)
-        } else {
-          selectChat(chatId)
-        }
-      } else {
-        if (messageText) {
-          window.__openDialog(MailtoDialog, { messageText })
-        }
-      }
-      callback && callback()
-    } catch (error) {
-      log.error('mailto decoding error', error)
-      window.__openDialog('AlertDialog', {
-        message: tx('mailto_link_could_not_be_decoded', url),
-        cb: callback,
-      })
-    }
+    processMailtoUrl(url, callback)
     return
   }
 
