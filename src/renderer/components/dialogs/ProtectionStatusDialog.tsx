06081cd79dc9ec99a416849c979d2241c95a2011
adz <x12@adz.garden>
2023-11-09 13:26:50 +0100

    Show dialogue when adding unverified member to protected group

--- src/renderer/components/dialogs/ProtectionStatusDialog.tsx
+++ src/renderer/components/dialogs/ProtectionStatusDialog.tsx
@@ -1,4 +1,5 @@
-import React, { CSSProperties, useContext } from 'react'
+import React, { useContext } from 'react'
+
 import { BackendRemote } from '../../backend-com'
 import { ScreenContext, useTranslationFunction } from '../../contexts'
 import { runtime } from '../../runtime'
@@ -8,13 +9,16 @@ import {
   DeltaDialogFooter,
   DeltaDialogFooterActions,
 } from './DeltaDialog'
-import type { DialogProps } from './DialogController'
 import QrCode from './QrCode'
 
-const WebLinks = {
-  Broken: 'https://staging.delta.chat/733/en/help#verificationbroken',
-  Enabled: 'https://staging.delta.chat/733/en/help#verifiedchats',
-}
+import type { DialogProps } from './DialogController'
+
+const VERIFICATION_BROKEN_URL =
+  'https://staging.delta.chat/733/en/help#verificationbroken'
+const VERIFICATION_ENABLED_URL =
+  'https://staging.delta.chat/733/en/help#verifiedchats'
+const VERIFICATION_REQUIRED_URL =
+  'https://staging.delta.chat/746/en/help#howtoe2ee'
 
 export function ProtectionBrokenDialog({
   name,
@@ -25,47 +29,88 @@ export function ProtectionBrokenDialog({
   const { openDialog } = useContext(ScreenContext)
   const accountId = selectedAccountId()
 
+  const onQRScan = async () => {
+    const [
+      qrCode,
+      qrCodeSVG,
+    ] = await BackendRemote.rpc.getChatSecurejoinQrCodeSvg(accountId, null)
+    onClose()
+    openDialog(QrCode, { selectScan: true, qrCode, qrCodeSVG })
+  }
+
+  const onLearnMore = () => {
+    runtime.openLink(VERIFICATION_BROKEN_URL)
+  }
+
   return (
     <SmallDialog isOpen={isOpen} onClose={onClose}>
       <div className='bp4-dialog-body-with-padding'>
-        <p
-          style={{
-            wordBreak: 'break-word',
-          }}
-        >
+        <p style={{ wordBreak: 'break-word' }}>
           {tx('chat_protection_broken_explanation', name)}
         </p>
       </div>
       <DeltaDialogFooter style={{ padding: '0px 20px 10px' }}>
-        <DeltaDialogFooterActions
-          style={{ justifyContent: 'space-between' } as CSSProperties}
-        >
+        <DeltaDialogFooterActions style={{ justifyContent: 'space-between' }}>
           <p
             className='delta-button bold primary'
-            onClick={() =>
-              runtime.openLink(WebLinks.Broken)
-            } /* TODO better link to inApp help at some point? */
+            onClick={onLearnMore}
             style={{ marginRight: '10px' }}
           >
             {tx('learn_more')}
           </p>
-          <p
-            className={`delta-button bold primary`}
-            onClick={async () => {
-              const [
-                qrCode,
-                qrCodeSVG,
-              ] = await BackendRemote.rpc.getChatSecurejoinQrCodeSvg(
-                accountId,
-                null
-              )
-              onClose()
-              openDialog(QrCode, { selectScan: true, qrCode, qrCodeSVG })
-            }}
-          >
+          <p className={`delta-button bold primary`} onClick={onQRScan}>
             {tx('qrscan_title')}
           </p>
-          <p className={`delta-button bold primary`} onClick={() => onClose()}>
+          <p className={`delta-button bold primary`} onClick={onClose}>
+            {tx('ok')}
+          </p>
+        </DeltaDialogFooterActions>
+      </DeltaDialogFooter>
+    </SmallDialog>
+  )
+}
+
+export function VerifiedContactsRequiredDialog({
+  isOpen,
+  onClose,
+}: DialogProps) {
+  const tx = useTranslationFunction()
+  const { openDialog } = useContext(ScreenContext)
+  const accountId = selectedAccountId()
+
+  const onQRScan = async () => {
+    const [
+      qrCode,
+      qrCodeSVG,
+    ] = await BackendRemote.rpc.getChatSecurejoinQrCodeSvg(accountId, null)
+    onClose()
+    openDialog(QrCode, { selectScan: true, qrCode, qrCodeSVG })
+  }
+
+  const onLearnMore = () => {
+    runtime.openLink(VERIFICATION_REQUIRED_URL)
+  }
+
+  return (
+    <SmallDialog isOpen={isOpen} onClose={onClose}>
+      <div className='bp4-dialog-body-with-padding'>
+        <p style={{ wordBreak: 'break-word' }}>
+          {tx('verified_contact_required_explain')}
+        </p>
+      </div>
+      <DeltaDialogFooter style={{ padding: '0px 20px 10px' }}>
+        <DeltaDialogFooterActions style={{ justifyContent: 'space-between' }}>
+          <p
+            className='delta-button bold primary'
+            onClick={onLearnMore}
+            style={{ marginRight: '10px' }}
+          >
+            {tx('learn_more')}
+          </p>
+          <p className={`delta-button bold primary`} onClick={onQRScan}>
+            {tx('qrscan_title')}
+          </p>
+          <p className={`delta-button bold primary`} onClick={onClose}>
             {tx('ok')}
           </p>
         </DeltaDialogFooterActions>
@@ -77,31 +122,27 @@ export function ProtectionBrokenDialog({
 export function ProtectionEnabledDialog({ isOpen, onClose }: DialogProps) {
   const tx = useTranslationFunction()
 
+  const onLearnMore = () => {
+    runtime.openLink(VERIFICATION_ENABLED_URL)
+  }
+
   return (
     <SmallDialog isOpen={isOpen} onClose={onClose}>
       <div className='bp4-dialog-body-with-padding'>
-        <p
-          style={{
-            wordBreak: 'break-word',
-          }}
-        >
+        <p style={{ wordBreak: 'break-word' }}>
           {tx('chat_protection_enabled_explanation')}
         </p>
       </div>
       <DeltaDialogFooter style={{ padding: '0px 20px 10px' }}>
-        <DeltaDialogFooterActions
-          style={{ justifyContent: 'space-between' } as CSSProperties}
-        >
+        <DeltaDialogFooterActions style={{ justifyContent: 'space-between' }}>
           <p
             className='delta-button bold primary'
-            onClick={() =>
-              runtime.openLink(WebLinks.Enabled)
-            } /* TODO better link to inApp help at some point? */
+            onClick={onLearnMore}
             style={{ marginRight: '10px' }}
           >
             {tx('learn_more')}
           </p>
-          <p className={`delta-button bold primary`} onClick={() => onClose()}>
+          <p className={`delta-button bold primary`} onClick={onClose}>
             {tx('ok')}
           </p>
         </DeltaDialogFooterActions>
