9088df563fca6ea591ebcf62c8f5060eeff88da6
SimonLaux <mobile.info@simonlaux.de>
2023-11-13 23:46:46 +0100

    prepare 1.41.0

--- src/renderer/deviceMessages.ts
+++ src/renderer/deviceMessages.ts
@@ -5,15 +5,19 @@ export async function updateDeviceChats(accountId: number) {
 
   await BackendRemote.rpc.addDeviceMessage(
     accountId,
-    'changelog-version-1.38.0-version0',
-    `What's new in 1.38.0?
+    'changelog-version-1.41.0-version0',
+    `What's new in 1.41.0?
     
-🖼️ Show thumbnail in chatlist of image, sticker and webxdc messages
-🔍 Improved design for message search results
-📎 Removed upper limit on attachment size
-☕ Wake up from standby now reconnects more reliably 
-✨ We fixed some bugs and improved stability
+💌 one-to-one chats guarantee end-to-end encryption for introduced contacts now
+😌 for everyone's simplicity, we mark these contacts and chats with green checkmarks
+👥 groups are created automatically with guaranteed end-to-end encryption if possible
+🔄 Accept/Blocked, Archived, Pinned, Mute is synced across all your devices
+🗜️ Images are now compressed (unless you send them as files)
+🖼️ Global Gallery with your pictures, documents, media across all chats
+✨ many more improvements and bugfixes
 
-Full Changelog: https://github.com/deltachat/deltachat-desktop/blob/master/CHANGELOG.md#1_38_0`
+Full Changelog: https://github.com/deltachat/deltachat-desktop/blob/master/CHANGELOG.md#1_41_0
+
+Thanks for testing this pre-release version, for more information and to five us feedback go to https://support.delta.chat/t/help-testing-the-upcoming-1-41-x-release/2793`
   )
 }
