c15919325157e80fc9df073296a92eb170aade6e
adz <x12@adz.garden>
2023-11-14 15:24:29 +0100

    Also expose closeDialog method in useDialog hook

--- src/renderer/hooks/useDialog.ts
+++ src/renderer/hooks/useDialog.ts
@@ -3,9 +3,10 @@ import { useContext } from 'react'
 import { DialogContext } from '../contexts/DialogContext'
 
 export function useDialog() {
-  const { openDialog } = useContext(DialogContext)
+  const { openDialog, closeDialog } = useContext(DialogContext)
 
   return {
     openDialog,
+    closeDialog,
   }
 }
