247202304643801c9f6c950d7723e100bb16c772
adz <x12@adz.garden>
2023-11-14 15:26:27 +0100

    Also expose hasOpenDialog in useDialog hook

--- src/renderer/hooks/useDialog.ts
+++ src/renderer/hooks/useDialog.ts
@@ -3,10 +3,11 @@ import { useContext } from 'react'
 import { DialogContext } from '../contexts/DialogContext'
 
 export function useDialog() {
-  const { openDialog, closeDialog } = useContext(DialogContext)
+  const { openDialog, closeDialog, hasOpenDialogs } = useContext(DialogContext)
 
   return {
     openDialog,
     closeDialog,
+    hasOpenDialogs,
   }
 }
