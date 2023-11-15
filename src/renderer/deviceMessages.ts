643db52781499fb9438b70e64d9436026af4a5e3
SimonLaux <mobile.info@simonlaux.de>
2023-11-15 14:52:40 +0100

    add device message for windows store release
    informing them that they got the testrelease early.

--- src/renderer/deviceMessages.ts
+++ src/renderer/deviceMessages.ts
@@ -1,4 +1,5 @@
 import { BackendRemote } from './backend-com'
+import { runtime } from './runtime'
 
 export async function updateDeviceChats(accountId: number) {
   // const tx = window.static_translate
@@ -20,4 +21,13 @@ Full Changelog: https://github.com/deltachat/deltachat-desktop/blob/master/CHANG
 
 Thanks for testing this pre-release version, for more information and to five us feedback go to https://support.delta.chat/t/help-testing-the-upcoming-1-41-x-release/2793`
   )
+
+  if (runtime.getRuntimeInfo().isAppx) {
+    BackendRemote.rpc.addDeviceMessage(
+      accountId,
+      'changelog-version-1.41.0-ms-store',
+      `If you wonder why you get a test version when you downloaded from Microsoft Store, we had a [bug that prevented installation](https://github.com/deltachat/deltachat-desktop/issues/3292) from there , we fixed it in the test version and decided to give you early access to it.
+If you encounter any bugs, please [notify us.](https://support.delta.chat/t/help-testing-the-upcoming-1-41-x-release/2793)`
+    )
+  }
 }
