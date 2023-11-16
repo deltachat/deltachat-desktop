402390db692ed606db7ff0c71b3005b23b861604
Andreas Dzialocha <adzialocha@users.noreply.github.com>
2023-11-16 17:45:33 +0100

    Update options for disappearing messages (#3530)
    * Move constants outside of component
    
    * And move back again because of the tx function
    
    * Give same options for disappearing messages as on mobile
    
    * Correct translation string key
    
    * Update values to reflect android settings
    
    * Add entry to CHANGELOG.md
    
    * Bring back missing value
    
    ---------
    
    Co-authored-by: Simon Laux <Simon-Laux@users.noreply.github.com>

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -10,25 +10,21 @@ export const appWindowTitle = appName
 export const enum Timespans {
   ZERO_SECONDS = 0,
   ONE_SECOND = 1,
-  THIRTY_SECONDS = 30,
   ONE_MINUTE_IN_SECONDS = 60,
   ONE_HOUR_IN_SECONDS = 60 * 60,
-  TWO_HOURS_IN_SECONDS = 60 * 60 * 2,
   ONE_DAY_IN_SECONDS = 60 * 60 * 24,
   ONE_WEEK_IN_SECONDS = 60 * 60 * 24 * 7,
-  FOUR_WEEKS_IN_SECONDS = 60 * 60 * 24 * 7 * 4,
   ONE_YEAR_IN_SECONDS = 60 * 60 * 24 * 365,
 }
 
 export const enum AutodeleteDuration {
   NEVER = Timespans.ZERO_SECONDS,
   AT_ONCE = Timespans.ONE_SECOND,
-  THIRTY_SECONDS = Timespans.THIRTY_SECONDS,
   ONE_MINUTE = Timespans.ONE_MINUTE_IN_SECONDS,
   ONE_HOUR = Timespans.ONE_HOUR_IN_SECONDS,
   ONE_DAY = Timespans.ONE_DAY_IN_SECONDS,
   ONE_WEEK = Timespans.ONE_WEEK_IN_SECONDS,
-  FOUR_WEEKS = Timespans.FOUR_WEEKS_IN_SECONDS,
+  FIVE_WEEKS = Timespans.ONE_WEEK_IN_SECONDS * 5,
   ONE_YEAR = Timespans.ONE_YEAR_IN_SECONDS,
 }
 
