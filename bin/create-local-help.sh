43a67cca171f2312b4fb5aaa61dd0143970fe795
B. Petersen <r10s@b44t.com>
2023-11-20 23:42:43 +0100

    add hint wrt needed CHANGELOG update

--- bin/create-local-help.sh
+++ bin/create-local-help.sh
@@ -1,2 +1,8 @@
 # update offline help; requires deltachat-pages to be checked out beside deltachat-desktop
+
 ../deltachat-pages/tools/create-local-help.py ../deltachat-pages/_site static/help
+
+echo
+echo "☝️ Compliance Warning: Add the following line to CHANGELOG.md:"
+echo "- Update local help ("`date "+%Y-%m-%d"`")"
+echo
