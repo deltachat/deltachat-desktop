7c40b2cf0c07394ca683fc42b2001510924c926c
bjoern <r10s@b44t.com>
2023-11-21 14:05:03 +0100

    Update bin/create-local-help.sh
    Co-authored-by: Simon Laux <Simon-Laux@users.noreply.github.com>

--- bin/create-local-help.sh
+++ bin/create-local-help.sh
@@ -1,4 +1,5 @@
 # update offline help; requires deltachat-pages to be checked out beside deltachat-desktop
+set -e
 
 ../deltachat-pages/tools/create-local-help.py ../deltachat-pages/_site static/help
 
