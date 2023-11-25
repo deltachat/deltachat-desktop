3c5f328814b566df81f091e165cf2e6cf7e86975
SimonLaux <mobile.info@simonlaux.de>
2023-11-25 22:54:44 +0100

    use stdio binary instead od dc node
    Todo:
    - [ ] account migration function in core
    - [ ] port into own npm module that has all the prebuilds somehow
    - [ ] ? fix open multiple instances
    - [ ] code review from "experts" -> fix linereader/performance

---
+++ src/main/deltachat/stdio_server.ts
@@ -0,0 +1,60 @@
+import { ChildProcessWithoutNullStreams, spawn } from 'child_process'
+import path from 'node:path'
+
+export class StdioServer {
+  cmd_path: string
+  server_process: ChildProcessWithoutNullStreams | null
+  constructor(
+    public on_data: (reponse: string) => void,
+    public accounts_path: string
+  ) {
+    this.cmd_path = path.join(
+      __dirname,
+      '../../../',
+      'deltachat-rpc-server-aarch64-macos'
+    )
+    this.server_process = null
+  }
+
+  start() {
+    this.server_process = spawn(this.cmd_path, {
+      env: { DC_ACCOUNTS_PATH: this.accounts_path },
+    })
+
+    let buffer = ''
+    this.server_process.stdout.on('data', data => {
+      console.log(`stdout: ${data}`)
+      buffer += data.toString()
+      while (true) {
+        const pos_end_line = buffer.indexOf('\n')
+        if (pos_end_line === 0) {
+          buffer = buffer.slice(1)
+        }
+        if (pos_end_line !== -1 && buffer[pos_end_line - 1] !== '\\') {
+          let message = buffer.slice(0, pos_end_line)
+          //   console.log(`${pos_end_line} Found message: ${message}`)
+          this.on_data(message)
+          buffer = buffer.slice(pos_end_line + 1)
+        } else {
+          break
+        }
+      }
+    })
+
+    this.server_process.stderr.on('data', data => {
+      console.log(`stderr: ${data}`)
+    })
+
+    this.server_process.on('close', code => {
+      console.log(`child process close all stdio with code ${code}`)
+    })
+
+    this.server_process.on('exit', code => {
+      console.log(`child process exited with code ${code}`)
+    })
+  }
+
+  send(message: string) {
+    this.server_process?.stdin.write(message + '\r\n')
+  }
+}
