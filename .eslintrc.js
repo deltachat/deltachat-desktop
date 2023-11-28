a0cc4fc49616de534646fc52648f9cecdea54690
adz <x12@adz.garden>
2023-11-28 13:52:36 +0100

    Update prettier plugin after update

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
     'plugin:react-hooks/recommended',
   ],
