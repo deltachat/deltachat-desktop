060669ed893bedfd7c72f5c9bef1da000e0ad9f9
adz <x12@adz.garden>
2023-12-19 20:57:48 +0100

    Move more icon styles into scss file, add default mask image property

--- src/renderer/components/Icon/index.tsx
+++ src/renderer/components/Icon/index.tsx
@@ -25,11 +25,14 @@ type Props = {
 import styles from './styles.module.scss'
 
 export default function Icon({ size = 20, icon, className }: Props) {
+  const maskImage = `url(../images/icons/${icon}.svg)`
+
   return (
     <span
       className={classNames(styles.icon, className)}
       style={{
-        WebkitMask: `url(../images/icons/${icon}.svg) no-repeat center`,
+        WebkitMaskImage: maskImage,
+        maskImage,
         height: `${size}px`,
         width: `${size}px`,
       }}
