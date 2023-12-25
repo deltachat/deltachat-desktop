b961a2033b6797ac22ea72588ffe1b65392c37a6
Farooq Karimi Zadeh <fkz@riseup.net>
2023-12-25 18:26:39 +0330

    Button component final

--- src/renderer/components/ui/Button.tsx
+++ src/renderer/components/ui/Button.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
+import classNames from 'classnames'
 
-type ButtonProps = {
+type ButtonProps = React.PropsWithChildren<{
   type: 'secondary' | 'primary' | 'danger'
   onClick: any
   round?: boolean
   id?: string
   style?: any
   'aria-label'?: string
-}
+  disabled?: boolean
+  className?: any
+}>
 
-export default function Button({ type, round, onClick, style, id, ...props}: ButtonProps) {
-
+export default function Button({ children, className, disabled, type, round, onClick, style, id, ...props}: ButtonProps) {
+  return (
+    <button
+      id={id}
+      disabled={!!disabled}
+      role='button'
+      className={classNames('delta-button' + (round ? '-round': ''), 'bold', type, className)}
+      onClick={onClick}
+      style={style}
+      aria-label={props['aria-label']}
+      >
+        {children}
+      </button>
+      )
 }
