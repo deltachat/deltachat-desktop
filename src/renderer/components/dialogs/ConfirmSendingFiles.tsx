ce973456aa30c006343018b4975cef257db8de46
adz <x12@adz.garden>
2024-01-01 01:54:27 +0100

    Use new Dialog in ConfirmSendingFiles.tsx

--- src/renderer/components/dialogs/ConfirmSendingFiles.tsx
+++ src/renderer/components/dialogs/ConfirmSendingFiles.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import useTranslationFunction from '../../hooks/useTranslationFunction'
-import {
-  DeltaDialogFooter,
-  DeltaDialogFooterActions,
-  SmallDialog,
-} from './DeltaDialog'
+import Dialog, {
+  DialogBody,
+  DialogContent,
+  DialogFooter,
+  FooterActionButton,
+  FooterActions,
+} from '../Dialog'
 
 import type { DialogProps } from '../../contexts/DialogContext'
 
@@ -37,33 +39,35 @@ export default function ConfirmSendingFiles({
   const fileCount = sanitizedFileList.length
 
   return (
-    <SmallDialog onClose={onClose}>
-      <div className='bp4-dialog-body-with-padding'>
-        <p style={{ wordBreak: 'break-word' }}>
-          {tx(
-            'ask_send_following_n_files_to',
-            fileCount > 1 ? [String(fileCount), chatName] : [chatName],
-            {
-              quantity: fileCount,
-            }
-          )}
-        </p>
-        <ul className='drop-file-dialog-file-list'>
-          {sanitizedFileList.map(({ name }) => (
-            <li key={name}>{' - ' + name}</li>
-          ))}
-        </ul>
-      </div>
-      <DeltaDialogFooter style={{ padding: '0px 20px 10px' }}>
-        <DeltaDialogFooterActions>
-          <p className='delta-button bold primary' onClick={handleCancel}>
+    <Dialog onClose={onClose}>
+      <DialogBody>
+        <DialogContent>
+          <p style={{ wordBreak: 'break-word' }}>
+            {tx(
+              'ask_send_following_n_files_to',
+              fileCount > 1 ? [String(fileCount), chatName] : [chatName],
+              {
+                quantity: fileCount,
+              }
+            )}
+          </p>
+          <ul className='drop-file-dialog-file-list'>
+            {sanitizedFileList.map(({ name }) => (
+              <li key={name}>{' - ' + name}</li>
+            ))}
+          </ul>
+        </DialogContent>
+      </DialogBody>
+      <DialogFooter>
+        <FooterActions>
+          <FooterActionButton onClick={handleCancel}>
             {tx('cancel')}
-          </p>
-          <p className={`delta-button bold primary`} onClick={handleConfirm}>
+          </FooterActionButton>
+          <FooterActionButton onClick={handleConfirm}>
             {tx('menu_send')}
-          </p>
-        </DeltaDialogFooterActions>
-      </DeltaDialogFooter>
-    </SmallDialog>
+          </FooterActionButton>
+        </FooterActions>
+      </DialogFooter>
+    </Dialog>
   )
 }
