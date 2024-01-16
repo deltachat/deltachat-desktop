5f1d7c812a5e83caa459d8d29ce58478437545e1
adz <x12@adz.garden>
2024-01-16 18:35:40 +0100

    Give user option to pick from all emojis

--- src/renderer/components/ReactionsBar/ReactionsBar.tsx
+++ src/renderer/components/ReactionsBar/ReactionsBar.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import Picker from '@emoji-mart/react'
+import React, { useState } from 'react'
 import classNames from 'classnames'
+import emojiData from '@emoji-mart/data'
 
 import { BackendRemote } from '../../backend-com'
 import { selectedAccountId } from '../../ScreenController'
 
 import styles from './styles.module.scss'
+
+import type { BaseEmoji } from 'emoji-mart/index'
 
 type Props = {
   messageId: number
@@ -14,6 +18,7 @@ type Props = {
 const DEFAULT_EMOJIS = ['👍', '👎', '❤️', '😂', '🙁']
 
 export default function ReactionsBar({ messageId, myReaction }: Props) {
+  const [showAllEmojis, setShowAllEmojis] = useState(false)
   const accountId = selectedAccountId()
 
   const toggleReaction = async (emoji: string) => {
@@ -24,21 +29,59 @@ export default function ReactionsBar({ messageId, myReaction }: Props) {
     }
   }
 
+  const handleShowAllEmojis = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.stopPropagation()
+    setShowAllEmojis(true)
+  }
+
+  const isMyReactionDefault = myReaction
+    ? DEFAULT_EMOJIS.includes(myReaction)
+    : false
+
   return (
     <div className={styles.reactionsBar}>
-      {DEFAULT_EMOJIS.map((emoji, index) => {
-        return (
-          <button
-            key={`emoji-${index}`}
-            onClick={() => toggleReaction(emoji)}
-            className={classNames(styles.reactionsBarEmoji, {
-              [styles.isFromSelf]: myReaction === emoji,
-            })}
-          >
-            {emoji}
-          </button>
-        )
-      })}
+      {!showAllEmojis && (
+        <>
+          {DEFAULT_EMOJIS.map((emoji, index) => {
+            return (
+              <button
+                key={`emoji-${index}`}
+                onClick={() => toggleReaction(emoji)}
+                className={classNames(styles.reactionsBarEmoji, {
+                  [styles.isFromSelf]: myReaction === emoji,
+                })}
+              >
+                {emoji}
+              </button>
+            )
+          })}
+          {(!myReaction || isMyReactionDefault) && (
+            <button onClick={handleShowAllEmojis}>...</button>
+          )}
+        </>
+      )}
+      {myReaction && !isMyReactionDefault && (
+        <button
+          onClick={() => toggleReaction(myReaction!)}
+          className={classNames(styles.reactionsBarEmoji, styles.isFromSelf)}
+        >
+          {myReaction}
+        </button>
+      )}
+      {showAllEmojis && (
+        <Picker
+          autoFocus={true}
+          data={emojiData}
+          native
+          navPosition={'bottom'}
+          onEmojiSelect={(emoji: BaseEmoji) => toggleReaction(emoji.native)}
+          previewPosition={'none'}
+          searchPosition={'sticky'}
+          skinTonePosition={'none'}
+        />
+      )}
     </div>
   )
 }
