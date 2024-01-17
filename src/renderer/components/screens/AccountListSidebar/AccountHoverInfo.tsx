91954c07dd6a50c8e56a91aa5053dd38fba94623
SimonLaux <mobile.info@simonlaux.de>
2024-01-17 05:01:27 +0100

    Add hover tooltip
    (id and size where taken from old account switcher screen, when hovering over the email address)

---
+++ src/renderer/components/screens/AccountListSidebar/AccountHoverInfo.tsx
@@ -0,0 +1,117 @@
+import { C, T } from '@deltachat/jsonrpc-client'
+import { filesize } from 'filesize'
+import React, { useEffect, useMemo, useState } from 'react'
+import styles from './styles.module.scss'
+import { BackendRemote, onDCEvent } from '../../../backend-com'
+import { getLogger } from '../../../../shared/logger'
+import { runtime } from '../../../runtime'
+import { debounceWithInit } from '../../chat/ChatListHelpers'
+
+const log = getLogger('AccountListSidebar/AccountHoverInfo')
+
+export function AccountHoverInfo({
+  account,
+  isSelected,
+}: {
+  account: T.Account
+  isSelected: boolean
+}) {
+  const [loadedAccount, setLoadedAccount] = useState(account)
+  const [accountSize, setSize] = useState<string>('?')
+  const [bgSyncDisabled, setBgSyncDisabled] = useState<boolean>(false)
+  useEffect(() => {
+    BackendRemote.rpc
+      .getAccountFileSize(account.id)
+      .catch(log.error)
+      .then(bytes => {
+        bytes && setSize(filesize(bytes))
+      })
+    runtime.getDesktopSettings().then(({ syncAllAccounts }) => {
+      setBgSyncDisabled(!syncAllAccounts)
+    })
+    BackendRemote.rpc
+      .getAccountInfo(account.id)
+      .then(setLoadedAccount)
+      .catch(log.error)
+  }, [account.id])
+
+  const showConnection = isSelected || !bgSyncDisabled
+
+  let content: JSX.Element
+
+  if (loadedAccount.kind === 'Unconfigured') {
+    content = (
+      <>
+        <b>Unconfigured Account</b>
+        <br />
+      </>
+    )
+  } else {
+    content = (
+      <>
+        <b>
+          {loadedAccount.displayName
+            ? loadedAccount.displayName
+            : loadedAccount.addr}
+        </b>
+        {showConnection && (
+          <div className={styles.HoverInfoProperty}>
+            <Connectivity accountId={account.id} />
+          </div>
+        )}
+        {bgSyncDisabled && (
+          <div className={styles.HoverInfoProperty}>
+            ⏻ {'Background Sync Disabled, Account is only synced when selected'}
+          </div>
+        )}
+      </>
+    )
+  }
+
+  return (
+    <div className={styles.AccountHoverInfo} role='tooltip'>
+      {content}
+      <div className={styles.HoverInfoFooter}>
+        <span>{accountSize}</span> - {account.id}
+      </div>
+    </div>
+  )
+}
+
+const Connectivity = ({ accountId }: { accountId: number }) => {
+  const [[color, label], setState] = useState<[string, string]>(['grey', '?'])
+
+  const onConnectivityChanged = useMemo(
+    () =>
+      debounceWithInit(async () => {
+        const tx = window.static_translate
+        const connectivity = await BackendRemote.rpc.getConnectivity(accountId)
+
+        if (connectivity >= C.DC_CONNECTIVITY_CONNECTED) {
+          setState(['green', tx('connectivity_connected')])
+        } else if (connectivity >= C.DC_CONNECTIVITY_WORKING) {
+          setState(['lightgreen', tx('connectivity_updating')])
+        } else if (connectivity >= C.DC_CONNECTIVITY_CONNECTING) {
+          setState(['orange', tx('connectivity_connecting')])
+        } else if (connectivity >= C.DC_CONNECTIVITY_NOT_CONNECTED) {
+          setState(['red', tx('connectivity_not_connected')])
+        }
+      }, 300),
+    [accountId]
+  )
+
+  useEffect(() => {
+    onConnectivityChanged()
+    return onDCEvent(accountId, 'ConnectivityChanged', onConnectivityChanged)
+  }, [onConnectivityChanged, accountId])
+
+  return (
+    <>
+      <div
+        className={styles.ConnectivityDot}
+        style={{ backgroundColor: color }}
+      ></div>{' '}
+      {label}
+    </>
+  )
+}
