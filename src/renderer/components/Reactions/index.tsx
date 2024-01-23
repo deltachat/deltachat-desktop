fba1e1f14f649ab40963598896fb38b564fd573b
SimonLaux <mobile.info@simonlaux.de>
2024-01-23 02:23:20 +0100

    draft for reaction tooltip
    if we want this we should probably implement it more efficiently, like caching contacts and maybe implementing getting the contact ids in the jsonrpc layer

--- src/renderer/components/Reactions/index.tsx
+++ src/renderer/components/Reactions/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import classNames from 'classnames'
 
 import useDialog from '../../hooks/useDialog'
@@ -7,6 +7,8 @@ import ReactionsDialog from '../dialogs/ReactionsDialog'
 import styles from './styles.module.scss'
 
 import type { T } from '@deltachat/jsonrpc-client'
+import { BackendRemote } from '../../backend-com'
+import { selectedAccountId } from '../../ScreenController'
 
 const SHOW_MAX_DIFFERENT_EMOJIS = 5
 
@@ -24,17 +26,54 @@ export default function Reactions(props: Props) {
     })
   }
 
+  // proably makes more sense to have some contacts cache to not make too many requests to core
+  const reactionContactIds = useMemo(
+    () => Object.keys(reactionsByContact).map(key => Number(key)),
+    [reactionsByContact]
+  )
+  const [reactionContacts, setReactionContacts] = useState<Record<
+    number,
+    T.Contact
+  > | null>(null)
+  useEffect(() => {
+    BackendRemote.rpc
+      .getContactsByIds(selectedAccountId(), reactionContactIds)
+      .then(setReactionContacts)
+  }, [reactionContactIds])
+
   return (
     <div className={styles.reactions} onClick={handleClick}>
       {reactions
         .slice(0, SHOW_MAX_DIFFERENT_EMOJIS)
         .map(({ emoji, isFromSelf, count }) => {
+          const contacts = []
+
+          if (reactionContacts) {
+            for (const contactId in reactionsByContact) {
+              if (
+                Object.prototype.hasOwnProperty.call(
+                  reactionsByContact,
+                  contactId
+                )
+              ) {
+                if (reactionsByContact[contactId].includes(emoji)) {
+                  const contact = reactionContacts[contactId]
+                  contact && contacts.push(contact.displayName)
+                }
+              }
+            }
+          }
+
+          // todo translation string
+          const title = `${emoji} reacted by ${contacts.join(', ')}`
+
           return (
             <span
               className={classNames(styles.emoji, {
                 [styles.isFromSelf]: isFromSelf,
               })}
               key={emoji}
+              title={title}
             >
               {emoji}
               {count > 1 && <span className={styles.emojiCount}>{count}</span>}
