ec1db0607125d781760cc3eee7fe20a183862d47
SimonLaux <mobile.info@simonlaux.de>
2024-02-10 13:15:01 +0100

    refactor connectivity of hover info to not produce a react unmounted error

--- src/renderer/components/AccountListSidebar/AccountHoverInfo.tsx
+++ src/renderer/components/AccountListSidebar/AccountHoverInfo.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import { C, T } from '@deltachat/jsonrpc-client'
 import { filesize } from 'filesize'
+import debounce from 'debounce'
 
 import { BackendRemote, onDCEvent } from '../../backend-com'
 import { getLogger } from '../../../shared/logger'
 import { runtime } from '../../runtime'
-import { debounceWithInit } from '../chat/ChatListHelpers'
 
 import styles from './styles.module.scss'
 
@@ -83,40 +83,65 @@ export default function AccountHoverInfo({
   )
 }
 
-const Connectivity = ({ accountId }: { accountId: number }) => {
-  const [[color, label], setState] = useState<[string, string]>(['grey', '?'])
+type ConnectivityState = { color: string; label: string }
+class Connectivity extends Component<{ accountId: number }, ConnectivityState> {
+  accountId = this.props.accountId // don't let react change the used account id
+  state = { color: '', label: '' }
+  wasDestroyed = false
+  eventUnregisterHandle?: () => void
 
-  const onConnectivityChanged = useMemo(
-    () =>
-      debounceWithInit(async () => {
-        const tx = window.static_translate
-        const connectivity = await BackendRemote.rpc.getConnectivity(accountId)
+  async onConnectivityChanged() {
+    if (this.wasDestroyed) {
+      return
+    }
+    const connectivity = await BackendRemote.rpc.getConnectivity(this.accountId)
+    if (!this.wasDestroyed) {
+      this.setState(Connectivity.convertConnectivityToState(connectivity))
+    }
+  }
 
-        if (connectivity >= C.DC_CONNECTIVITY_CONNECTED) {
-          setState(['green', tx('connectivity_connected')])
-        } else if (connectivity >= C.DC_CONNECTIVITY_WORKING) {
-          setState(['lightgreen', tx('connectivity_updating')])
-        } else if (connectivity >= C.DC_CONNECTIVITY_CONNECTING) {
-          setState(['orange', tx('connectivity_connecting')])
-        } else if (connectivity >= C.DC_CONNECTIVITY_NOT_CONNECTED) {
-          setState(['red', tx('connectivity_not_connected')])
-        }
-      }, 300),
-    [accountId]
-  )
+  static convertConnectivityToState(connectivity: number): ConnectivityState {
+    const tx = window.static_translate
+    if (connectivity >= C.DC_CONNECTIVITY_CONNECTED) {
+      return { color: 'green', label: tx('connectivity_connected') }
+    } else if (connectivity >= C.DC_CONNECTIVITY_WORKING) {
+      return { color: 'lightgreen', label: tx('connectivity_updating') }
+    } else if (connectivity >= C.DC_CONNECTIVITY_CONNECTING) {
+      return { color: 'orange', label: tx('connectivity_connecting') }
+    } else if (connectivity >= C.DC_CONNECTIVITY_NOT_CONNECTED) {
+      return { color: 'red', label: tx('connectivity_not_connected') }
+    } else {
+      log.error(
+        'Label for this Connectivity state number is unknown',
+        connectivity
+      )
+      return { color: 'purple', label: 'Error fetching Connection State' }
+    }
+  }
 
-  useEffect(() => {
-    onConnectivityChanged()
-    return onDCEvent(accountId, 'ConnectivityChanged', onConnectivityChanged)
-  }, [onConnectivityChanged, accountId])
+  componentDidMount(): void {
+    this.onConnectivityChanged()
+    this.eventUnregisterHandle = onDCEvent(
+      this.accountId,
+      'ConnectivityChanged',
+      debounce(this.onConnectivityChanged.bind(this), 300)
+    )
+  }
 
-  return (
-    <>
-      <div
-        className={styles.connectivityDot}
-        style={{ backgroundColor: color }}
-      ></div>{' '}
-      {label}
-    </>
-  )
+  componentWillUnmount(): void {
+    this.wasDestroyed = true
+    this.eventUnregisterHandle?.()
+  }
+
+  render(): React.ReactNode {
+    return (
+      <>
+        <div
+          className={styles.connectivityDot}
+          style={{ backgroundColor: this.state.color }}
+        ></div>{' '}
+        {this.state.label}
+      </>
+    )
+  }
 }
