43d5f5a878ce1a4b81b97a324bb5ad8ad0199277
SimonLaux <mobile.info@simonlaux.de>
2024-02-11 20:11:26 +0100

    show option to mute account in settings and separated selected account settings visually from global settings

--- src/renderer/components/Settings/Notifications.tsx
+++ src/renderer/components/Settings/Notifications.tsx
@@ -3,6 +3,13 @@ import React from 'react'
 import { DesktopSettingsType } from '../../../shared/shared-types'
 import useTranslationFunction from '../../hooks/useTranslationFunction'
 import DesktopSettingsSwitch from './DesktopSettingsSwitch'
+import SettingsSeparator from './SettingsSeparator'
+import AccountNotificationStoreInstance, {
+  useAccountNotificationStore,
+} from '../../stores/accountNotifications'
+import { selectedAccountId } from '../../ScreenController'
+import SettingsSwitch from './SettingsSwitch'
+import SettingsHeading from './SettingsHeading'
 
 type Props = {
   desktopSettings: DesktopSettingsType
@@ -13,6 +20,7 @@ export default function Notifications({ desktopSettings }: Props) {
 
   return (
     <>
+      <SettingsHeading>{tx('pref_for_all_accounts')}</SettingsHeading>
       <DesktopSettingsSwitch
         settingsKey='notifications'
         label={tx('pref_notifications_explain')}
@@ -22,6 +30,32 @@ export default function Notifications({ desktopSettings }: Props) {
         label={tx('pref_show_notification_content_explain')}
         disabled={!desktopSettings['notifications']}
       />
+      <SettingsSeparator></SettingsSeparator>
+      <SettingsHeading>{tx('pref_for_current_account')}</SettingsHeading>
+      <AccountNotificationMutedSwitch label={tx('menu_mute')} />
     </>
+  )
+}
+
+function AccountNotificationMutedSwitch({
+  label,
+  description,
+}: {
+  label: string
+  description?: string
+}) {
+  const accountId = selectedAccountId()
+  const { accounts } = useAccountNotificationStore()[0]!
+  const isMuted = accounts[accountId]?.muted || false
+
+  return (
+    <SettingsSwitch
+      label={label}
+      value={isMuted}
+      description={description}
+      onClick={() => {
+        AccountNotificationStoreInstance.effect.setMuted(accountId, !isMuted)
+      }}
+    />
   )
 }
