a9a06e7c83c6c7c6662c9f444eccb572f2dbbb5a
SimonLaux <mobile.info@simonlaux.de>
2024-02-12 21:54:37 +0100

    badge-counter: ignore muted accounts

--- src/renderer/system-integration/badge-counter.ts
+++ src/renderer/system-integration/badge-counter.ts
@@ -1,16 +1,17 @@
 import { debounce } from 'debounce'
 import { BackendRemote } from '../backend-com'
 import { runtime } from '../runtime'
+import AccountNotificationStoreInstance from '../stores/accountNotifications'
 
 async function updateBadgeCounter() {
   const accountIds = await BackendRemote.rpc.getAllAccountIds()
 
   let combined_count = (
     await Promise.all(
-      accountIds.map(
-        async accountId =>
-          // Later TODO when we can mute accounts -> don't include muted and not synced
-          (await BackendRemote.rpc.getFreshMsgs(accountId)).length
+      accountIds.map(async accountId =>
+        !AccountNotificationStoreInstance.isAccountMuted(accountId)
+          ? (await BackendRemote.rpc.getFreshMsgs(accountId)).length
+          : 0
       )
     )
   ).reduce((previous, current) => previous + current, 0)
