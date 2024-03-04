5ce12d48bcb14320a347b7cb5b6405f0350b2de9
adz <x12@adz.garden>
2024-03-04 11:57:40 +0100

    Reuse image selector for group images as well

--- src/renderer/components/dialogs/EditProfileDialog/ProfileImageSelector.tsx
+++ src/renderer/components/dialogs/EditProfileDialog/ProfileImageSelector.tsx
@@ -1,24 +1,15 @@
 import React from 'react'
-import { dirname } from 'path'
 
-import { runtime } from '../../../runtime'
+import ImageSelector from '../../ImageSelector'
+import { LastUsedSlot } from '../../../utils/lastUsedPaths'
 import { avatarInitial } from '../../Avatar'
-import useTranslationFunction from '../../../hooks/useTranslationFunction'
-import {
-  LastUsedSlot,
-  rememberLastUsedPath,
-} from '../../../utils/lastUsedPaths'
-import LargeProfileImage from '../../LargeProfileImage'
-import Icon from '../../Icon'
-
-import styles from './styles.module.scss'
 
 type Props = {
   addr: string
   color: string
   displayName: string
   hideDeleteButton?: boolean
-  profilePicture: string | null
+  profilePicture?: string
   setProfilePicture: (path: string) => void
 }
 
@@ -29,61 +20,15 @@ export default function ProfileImageSelector({
   profilePicture,
   setProfilePicture,
 }: Props) {
-  const tx = useTranslationFunction()
-
-  const handleSelect = async () => {
-    const { defaultPath, setLastPath } = rememberLastUsedPath(
-      LastUsedSlot.ProfileImage
-    )
-
-    const file = await runtime.showOpenFileDialog({
-      title: tx('select_your_new_profile_image'),
-      filters: [
-        {
-          name: tx('images'),
-          extensions: ['jpg', 'png', 'gif', 'jpeg'],
-        },
-      ],
-      properties: ['openFile'],
-      defaultPath,
-    })
-
-    if (file) {
-      setProfilePicture(file)
-      setLastPath(dirname(file))
-    }
-  }
-
-  const handleRemove = () => setProfilePicture('')
-
-  const imageUrl = profilePicture ? `file://${profilePicture}` : undefined
   const initials = avatarInitial(displayName, addr)
 
   return (
-    <div className={styles.profileImageSelector}>
-      <LargeProfileImage
-        color={color}
-        imageUrl={imageUrl}
-        initials={initials}
-      />
-      {!imageUrl && (
-        <button
-          title={tx('profile_image_select')}
-          className={styles.profileImageSelectorButton}
-          onClick={handleSelect}
-        >
-          <Icon className={styles.profileImageSelectorIcon} icon='image' />
-        </button>
-      )}
-      {imageUrl && (
-        <button
-          title={tx('profile_image_delete')}
-          className={styles.profileImageSelectorButton}
-          onClick={handleRemove}
-        >
-          <Icon className={styles.profileImageSelectorIcon} icon='cross' />
-        </button>
-      )}
-    </div>
+    <ImageSelector
+      color={color}
+      filePath={profilePicture}
+      initials={initials}
+      lastUsedSlot={LastUsedSlot.ProfileImage}
+      onChange={setProfilePicture}
+    />
   )
 }
