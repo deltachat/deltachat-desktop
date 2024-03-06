8652794ddfce28539e04c3a1ec6e7774e060b46b
adz <x12@adz.garden>
2024-03-06 11:07:19 +0100

    Add activeView, chatId and chat state

--- src/renderer/contexts/ChatContext.tsx
+++ src/renderer/contexts/ChatContext.tsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import type { PropsWithChildren } from 'react'
+import type { T } from '@deltachat/jsonrpc-client'
 
-export type ChatValue = {}
+export enum ChatView {
+  Map,
+  Media,
+  MessageList,
+}
+
+export type ChatValue = {
+  activeView: ChatView
+  chatId?: number
+  chat?: T.FullChat
+}
 
 export const ChatContext = React.createContext<ChatValue | null>(null)
 
 export const ChatProvider = ({ children }: PropsWithChildren<{}>) => {
-  const value: ChatValue = {}
+  const [activeView, setActiveView] = useState(ChatView.MessageList)
+  const [chatId, setChatId] = useState<number | undefined>()
+  const [chat, setChat] = useState<T.FullChat | undefined>()
+
+  const value: ChatValue = {
+    activeView,
+    chatId,
+    chat,
+  }
 
   return <ChatContext.Provider value={value}>{children}</ChatContext.Provider>
 }
