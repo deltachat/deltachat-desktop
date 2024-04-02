5ee3804319dfdeb7d0c8763230a39ba411ebceb7
adz <x12@adz.garden>
2024-04-02 14:02:10 +0200

    Use backend method, clean up a little

--- src/renderer/hooks/useCreateChatByContactId.ts
+++ src/renderer/hooks/useCreateChatByContactId.ts
@@ -1,25 +1,19 @@
 import { useCallback } from 'react'
 
-import { BackendRemote } from '../backend-com'
-import { getLogger } from '../../shared/logger'
 import useChat from './useChat'
-
-const log = getLogger('renderer/hooks/useCreateChatByContactId')
+import { BackendRemote } from '../backend-com'
+import { createChatByContactId } from '../backend/chat'
 
 export default function useCreateChatByContactId() {
   const { selectChat } = useChat()
 
   return useCallback(
     async (accountId: number, contactId: number) => {
-      const chatId = await BackendRemote.rpc.createChatByContactId(
-        accountId,
-        contactId
-      )
-
+      const chatId = await createChatByContactId(accountId, contactId)
       const chat = await BackendRemote.rpc.getFullChatById(accountId, chatId)
 
+      // Unarchive chat if it's been in archive and gets activated again
       if (chat.archived) {
-        log.debug('chat was archived, unarchiving it')
         await BackendRemote.rpc.setChatVisibility(accountId, chatId, 'Normal')
       }
 
