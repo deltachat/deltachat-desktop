65b8921033e372f0bac5af1df383047d7c9f8b72
adz <x12@adz.garden>
2024-04-15 12:00:39 +0200

    Separate alternative setup methods into external dialog

---
+++ src/renderer/components/screens/WelcomeScreen/AlternativeSetupsDialog.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { dirname } from 'path'
+
+import Button from '../../Button'
+import Dialog, { DialogBody, DialogContent, DialogHeader } from '../../Dialog'
+import ImportBackupProgressDialog from './ImportBackupProgressDialog'
+import ImportQrCode from '../../dialogs/ImportQrCode'
+import useDialog from '../../../hooks/useDialog'
+import useTranslationFunction from '../../../hooks/useTranslationFunction'
+import {
+  LastUsedSlot,
+  rememberLastUsedPath,
+} from '../../../utils/lastUsedPaths'
+import { Screens } from '../../../ScreenController'
+import { runtime } from '../../../runtime'
+
+// import styles from './styles.module.scss'
+
+import type { DialogProps } from '../../../contexts/DialogContext'
+
+export default function AlternativeSetupsDialog({ onClose }: DialogProps) {
+  const tx = useTranslationFunction()
+  const { openDialog } = useDialog()
+
+  const onClickSecondDevice = () =>
+    openDialog(ImportQrCode, {
+      subtitle: tx('multidevice_open_settings_on_other_device'),
+    })
+
+  async function onClickImportBackup() {
+    const { defaultPath, setLastPath } = rememberLastUsedPath(
+      LastUsedSlot.Backup
+    )
+
+    const file = await runtime.showOpenFileDialog({
+      title: tx('import_backup_title'),
+      properties: ['openFile'],
+      filters: [{ name: '.tar or .bak', extensions: ['tar', 'bak'] }],
+      defaultPath,
+    })
+
+    if (file) {
+      openDialog(ImportBackupProgressDialog, {
+        backupFile: file,
+      })
+      setLastPath(dirname(file))
+    }
+  }
+
+  const onClickLogin = () => window.__changeScreen(Screens.Login)
+
+  return (
+    <Dialog onClose={onClose}>
+      <DialogHeader onClose={onClose} />
+      <DialogBody>
+        <DialogContent>
+          <Button onClick={onClickSecondDevice}>
+            {tx('multidevice_receiver_title')}
+          </Button>
+          <Button onClick={onClickImportBackup}>
+            {tx('import_backup_title')}
+          </Button>
+          <Button onClick={onClickLogin}>{tx('login_header')}</Button>
+        </DialogContent>
+      </DialogBody>
+    </Dialog>
+  )
+}
--- src/renderer/components/screens/WelcomeScreen/index.tsx
+++ src/renderer/components/screens/WelcomeScreen/index.tsx
@@ -1,19 +1,17 @@
 import React, { useEffect, useState } from 'react'
-import classNames from 'classnames'
 
 import AlertDialog from '../../dialogs/AlertDialog'
+import AlternativeSetupsDialog from './AlternativeSetupsDialog'
 import Button from '../../Button'
 import Dialog, { DialogBody, DialogHeader } from '../../Dialog'
-import ImportButton from './ImportButton'
 import ImportQrCode from '../../dialogs/ImportQrCode'
 import useDialog from '../../../hooks/dialog/useDialog'
 import useProcessQr from '../../../hooks/useProcessQr'
 import useTranslationFunction from '../../../hooks/useTranslationFunction'
 import { BackendRemote, EffectfulBackendActions } from '../../../backend-com'
-import { Screens } from '../../../ScreenController'
 import { getLogger } from '../../../../shared/logger'
 
-import styles from './styles.module.scss'
+// import styles from './styles.module.scss'
 
 const log = getLogger('renderer/components/AccountsScreen')
 
@@ -29,19 +27,17 @@ export default function WelcomeScreen({
   onExitWelcomeScreen,
 }: Props) {
   const tx = useTranslationFunction()
-  const { openDialog, closeDialog } = useDialog()
+  const { openDialog } = useDialog()
   const processQr = useProcessQr()
   const [showBackButton, setShowBackButton] = useState(false)
 
-  const onClickLogin = () => window.__changeScreen(Screens.Login)
+  const onAlreadyHaveAccount = () => {
+    openDialog(AlternativeSetupsDialog)
+  }
 
-  const onClickSecondDevice = () =>
-    openDialog(ImportQrCode, {
-      subtitle: tx('multidevice_open_settings_on_other_device'),
-    })
-
-  const onClickScanQr = () =>
+  const onScanQRCode = () => {
     openDialog(ImportQrCode, { subtitle: tx('qrscan_hint') })
+  }
 
   const onCancel = async () => {
     try {
@@ -65,58 +61,43 @@ export default function WelcomeScreen({
   }
 
   useEffect(() => {
-    ;(async () => {
-      const allAccountIds = await BackendRemote.listAccounts()
-      if (allAccountIds && allAccountIds.length > 1) {
+    // Show back button when user has already created and configured accounts.
+    // On a fresh DC start we will not have any yet.
+    const checkAccounts = async () => {
+      const accounts = await BackendRemote.listAccounts()
+      if (accounts && accounts.length > 1) {
         setShowBackButton(true)
       }
-      if (window.__welcome_qr) {
-        // this is the "callback" when opening dclogin or dcaccount from an already existing account,
-        // the app needs to switch to the welcome screen first.
-        await processQr(selectedAccountId, window.__welcome_qr, undefined, true)
-        window.__welcome_qr = undefined
+    }
+
+    // "callback" when opening dclogin or dcaccount from an already existing
+    // account, the app needs to switch to the welcome screen first.
+    const checkQRCode = async () => {
+      if (!window.__welcome_qr) {
+        return
       }
-    })()
-  }, [openDialog, closeDialog, processQr, selectedAccountId])
+      await processQr(selectedAccountId, window.__welcome_qr, undefined, true)
+      window.__welcome_qr = undefined
+    }
+
+    checkAccounts()
+    checkQRCode()
+  }, [processQr, selectedAccountId])
 
   return (
-    <div className='login-screen'>
-      <div className='window'>
-        <Dialog canEscapeKeyClose fixed onClose={() => {}} width={400}>
-          <DialogHeader
-            onClickBack={showBackButton ? onCancel : undefined}
-            title={tx('add_account')}
-          />
-          <DialogBody>
-            <div className='welcome-deltachat'>
-              <img className='delta-icon' src='../images/intro1.png' />
-              <p className='f1'>{tx('welcome_chat_over_email')}</p>
-              <Button
-                className={classNames(styles.welcomeButton, styles.withGap)}
-                type='primary'
-                onClick={onClickLogin}
-              >
-                {tx('login_header')}
-              </Button>
-              <Button
-                className={styles.welcomeButton}
-                type='secondary'
-                onClick={onClickSecondDevice}
-              >
-                {tx('multidevice_receiver_title')}
-              </Button>
-              <Button
-                className={styles.welcomeButton}
-                type='secondary'
-                onClick={onClickScanQr}
-              >
-                {tx('scan_invitation_code')}
-              </Button>
-              <ImportButton />
-            </div>
-          </DialogBody>
-        </Dialog>
-      </div>
-    </div>
+    <Dialog canEscapeKeyClose fixed onClose={() => {}} width={400}>
+      <DialogHeader
+        onClickBack={showBackButton ? onCancel : undefined}
+        title={tx('add_account')}
+      />
+      <DialogBody>
+        <Button type='secondary' onClick={onScanQRCode}>
+          {tx('scan_invitation_code')}
+        </Button>
+        <Button type='secondary' onClick={onAlreadyHaveAccount}>
+          I already have an account
+        </Button>
+      </DialogBody>
+    </Dialog>
   )
 }
