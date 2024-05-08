f7b6c52725a3799d0d49bf7f1d9d6444a047e2b0
Max Philippov <max.philippos@gmail.com>
2024-05-08 19:58:29 +0500

    Fix markup, add className to DialogContent

--- src/renderer/components/Dialog/DialogContent.tsx
+++ src/renderer/components/Dialog/DialogContent.tsx
@@ -6,18 +6,20 @@ import type { PropsWithChildren } from 'react'
 import styles from './styles.module.scss'
 
 type Props = PropsWithChildren<{
+  className?: string
   paddingBottom?: boolean
   paddingTop?: boolean
 }>
 
 export default function DialogContent({
   children,
+  className,
   paddingBottom = false,
   paddingTop = false,
 }: Props) {
   return (
     <div
-      className={classNames(styles.dialogContent, {
+      className={classNames(styles.dialogContent, className, {
         [styles.paddingBottom]: paddingBottom,
         [styles.paddingTop]: paddingTop,
       })}
