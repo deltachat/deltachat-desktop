c96e41f6fb887883815b30cac02494648760aee3
SimonLaux <mobile.info@simonlaux.de>
2024-05-10 03:19:55 +0200

    reorganise some settings acording changes on android & ios
    closes #3809

--- src/renderer/components/Settings/Encryption.tsx
+++ src/renderer/components/Settings/Encryption.tsx
@@ -18,6 +18,7 @@ import useTranslationFunction from '../../hooks/useTranslationFunction'
 import useDialog from '../../hooks/dialog/useDialog'
 
 import type { DialogProps } from '../../contexts/DialogContext'
+import ManageKeys from './ManageKeys'
 
 export default function Encryption() {
   const { openDialog } = useDialog()
@@ -29,6 +30,7 @@ export default function Encryption() {
         settingsKey='e2ee_enabled'
         label={tx('autocrypt_prefer_e2ee')}
       />
+      <ManageKeys />
       <SettingsButton
         highlight
         onClick={() => openDialog(SendAutocryptSetupMessage)}
