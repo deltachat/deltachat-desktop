dd5061524eabec8ad51c49dc60c427f333b3fed3
SimonLaux <mobile.info@simonlaux.de>
2024-05-17 23:09:33 +0200

    exclude prebuilds that are not used

--- build/afterPackHook.cjs
+++ build/afterPackHook.cjs
@@ -1,7 +1,74 @@
 const { copyFileSync } = require('fs')
-const { readdir, writeFile } = require('fs/promises')
+const { readdir, writeFile, rm } = require('fs/promises')
 const { join } = require('path')
+
+const { Arch } = require('electron-builder')
+
+function convertArch(arch) {
+  switch (arch) {
+    case Arch.arm64:
+      return 'arm64'
+    case Arch.armv7l:
+      return 'arm'
+    case Arch.ia32:
+      return 'ia32' // electron seems to not exist anymore anyway
+    case Arch.x64:
+      return 'x64'
+    case Arch.universal:
+      return 'universal'
+
+    default:
+      throw new Error(`unhandled architecture: ${arch}`)
+  }
+}
+
 module.exports = async context => {
+  console.log({ context })
+
+  const prebuild_dir = join(
+    context.appOutDir,
+    `${
+      context.electronPlatformName === 'darwin'
+        ? 'DeltaChat.app/Contents/Resources'
+        : 'resources'
+    }/app.asar.unpacked/node_modules/@deltachat`
+  )
+
+  // delete not needed prebuilds
+  const prebuilds = await readdir(prebuild_dir)
+
+  const toDelete = prebuilds.filter(name => {
+    const architecture = name.split('-')[4]
+    if (architecture === convertArch(context.arch)) {
+      return false
+    } else if (
+      // convertArch(context.arch) === 'universal' && does not work for some reason
+      context.electronPlatformName === 'darwin' &&
+      (architecture === 'arm64' || architecture === 'x64')
+    ) {
+      return false
+    } else {
+      return true
+    }
+  })
+
+  console.log({ prebuilds, toDelete })
+
+  for (const targetOfDeletion of toDelete) {
+    await rm(join(prebuild_dir, targetOfDeletion), { recursive: true })
+  }
+
+  const prebuilds_after_cleanup = await readdir(prebuild_dir)
+  console.log({ prebuilds_after_cleanup })
+  if (
+    prebuilds_after_cleanup.length !== 1 &&
+    context.electronPlatformName !== 'darwin'
+  ) {
+    throw new Error(
+      "prebuilds were not cleared correctly or prebuild is missing, there should only be one (unless it's mac)"
+    )
+  }
+
   if (context.electronPlatformName === 'win32') {
     // package msvc redist
     const base = join(__dirname, 'vcredist/')
@@ -9,12 +76,12 @@ module.exports = async context => {
     dir.forEach(d => {
       copyFileSync(join(base, d), join(context.appOutDir, d))
     })
-    let windows_build_info = join(context.appOutDir, 'windows_build_info.json');
+    let windows_build_info = join(context.appOutDir, 'windows_build_info.json')
 
-    let isAPPX = false;
+    let isAPPX = false
 
-    if (context.targets.findIndex(({name})=>name == "appx") !=-1){
-      if(context.targets.length > 1) {
+    if (context.targets.findIndex(({ name }) => name == 'appx') != -1) {
+      if (context.targets.length > 1) {
         throw new Error("please don't build appx together with other formats")
       }
 
@@ -22,8 +89,11 @@ module.exports = async context => {
       isAPPX = true
     }
 
-    await writeFile(windows_build_info, JSON.stringify({
-      isAPPX
-    }))
+    await writeFile(
+      windows_build_info,
+      JSON.stringify({
+        isAPPX,
+      })
+    )
   }
 }
