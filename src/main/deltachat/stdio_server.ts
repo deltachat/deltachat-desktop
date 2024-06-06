a2063e74deb018df05b4b4b71de11fb7b2a7a8d6
Simon Laux <Simon-Laux@users.noreply.github.com>
2024-06-06 13:56:29 +0200

    Show error to user if core process exits unexpectedly (#3904)
    * Show error to user if core process exits unexpectedly
    
    * add pr number to changelog
    
    * fix typo
    
    * apply suggestion

--- src/main/deltachat/stdio_server.ts
+++ src/main/deltachat/stdio_server.ts
@@ -1,4 +1,5 @@
 import { ChildProcessWithoutNullStreams, spawn } from 'child_process'
+import { app, dialog } from 'electron/main'
 
 export class StdioServer {
   server_process: ChildProcessWithoutNullStreams | null
@@ -30,8 +31,12 @@ export class StdioServer {
       }
     })
 
+    // some kind of "buffer" that the text in the error dialog does not get too long
+    let errorLog = ''
+    const ERROR_LOG_LENGTH = 800
     this.server_process.stderr.on('data', data => {
       console.log(`stderr: ${data}`.trimEnd())
+      errorLog = (errorLog + data).slice(-ERROR_LOG_LENGTH)
     })
 
     this.server_process.on('close', (code, signal) => {
@@ -45,6 +50,14 @@ export class StdioServer {
     this.server_process.on('exit', (code, signal) => {
       if (code !== null) {
         console.log(`child process exited with code ${code}`)
+        if (code !== 0) {
+          console.error('Fatal: The Delta Chat Core exited unexpectedly', code)
+          dialog.showErrorBox(
+            'Fatal Error',
+            `The Delta Chat Core exited unexpectedly with code ${code}\n${errorLog}`
+          )
+          app.exit(1)
+        }
       } else {
         console.log(`child process exited with signal ${signal}`)
       }
