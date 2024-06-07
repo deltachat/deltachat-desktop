240e3931208a2bfcf69d26084445f09cc6f35cd8
SimonLaux <mobile.info@simonlaux.de>
2024-06-07 03:20:18 +0200

    fix: packaging: respect `NO_ASAR` env var in `afterPackHook`
    I need this for flatpak build

--- build/afterPackHook.cjs
+++ build/afterPackHook.cjs
@@ -3,6 +3,7 @@ const { readdir, writeFile, rm, copyFile, cp, mkdir } = require('fs/promises')
 const { join } = require('path')
 
 const { Arch } = require('electron-builder')
+const { env } = require('process')
 
 function convertArch(arch) {
   switch (arch) {
@@ -45,6 +46,66 @@ module.exports = async context => {
 
   // delete not needed prebuilds
   // ---------------------------------------------------------------------------------
+  if (!env['NO_ASAR']){
+    await deleteNotNeededPrebuildsFromUnpackedASAR(prebuild_dir, context, isMacBuild)
+  }
+
+  // package msvc redist
+  // ---------------------------------------------------------------------------------
+  if (context.electronPlatformName === 'win32') {
+    await packageMSVCRedist(context)
+  }
+
+  // copy map xdc
+  // ---------------------------------------------------------------------------------
+  if (!env['NO_ASAR']) {
+    await copyMapXdcToUnpackedASAR(resources_dir, source_dir)
+  }
+}
+
+async function packageMSVCRedist(context) {
+  const base = join(__dirname, 'vcredist/')
+  const dir = await readdir(base)
+  dir.forEach(d => {
+    copyFileSync(join(base, d), join(context.appOutDir, d))
+  })
+  let windows_build_info = join(context.appOutDir, 'windows_build_info.json')
+
+  let isAPPX = false
+
+  if (context.targets.findIndex(({ name }) => name == 'appx') != -1) {
+    if (context.targets.length > 1) {
+      throw new Error("please don't build appx together with other formats")
+    }
+
+    // Set a file to indicate that it is an appx to the running app.
+    isAPPX = true
+  }
+
+  await writeFile(
+    windows_build_info,
+    JSON.stringify({
+      isAPPX,
+    })
+  )
+}
+
+async function copyMapXdcToUnpackedASAR(resources_dir, source_dir) {
+  const destination = join(
+    resources_dir,
+    'app.asar.unpacked',
+    'html-dist',
+    'xdcs'
+  )
+  try {
+    await mkdir(destination, { recursive: true })
+  } catch (error) {
+    console.log('failed to create dir', destination, error)
+  }
+  await cp(join(source_dir, 'html-dist/xdcs'), destination, { recursive: true })
+}
+
+async function deleteNotNeededPrebuildsFromUnpackedASAR(prebuild_dir, context, isMacBuild) {
   const prebuilds = await readdir(prebuild_dir)
 
   const toDelete = prebuilds.filter(name => {
@@ -54,8 +115,7 @@ module.exports = async context => {
     } else if (
       // convertArch(context.arch) === 'universal' && does not work for some reason
       isMacBuild &&
-      (architecture === 'arm64' || architecture === 'x64')
-    ) {
+      (architecture === 'arm64' || architecture === 'x64')) {
       return false
     } else {
       return true
@@ -75,48 +135,5 @@ module.exports = async context => {
       "prebuilds were not cleared correctly or prebuild is missing, there should only be one (unless it's mac)"
     )
   }
-
-  // package msvc redist
-  // ---------------------------------------------------------------------------------
-  if (context.electronPlatformName === 'win32') {
-    const base = join(__dirname, 'vcredist/')
-    const dir = await readdir(base)
-    dir.forEach(d => {
-      copyFileSync(join(base, d), join(context.appOutDir, d))
-    })
-    let windows_build_info = join(context.appOutDir, 'windows_build_info.json')
-
-    let isAPPX = false
-
-    if (context.targets.findIndex(({ name }) => name == 'appx') != -1) {
-      if (context.targets.length > 1) {
-        throw new Error("please don't build appx together with other formats")
-      }
-
-      // Set a file to indicate that it is an appx to the running app.
-      isAPPX = true
-    }
-
-    await writeFile(
-      windows_build_info,
-      JSON.stringify({
-        isAPPX,
-      })
-    )
-  }
-
-  // copy map xdc
-  // ---------------------------------------------------------------------------------
-  const destination = join(
-    resources_dir,
-    'app.asar.unpacked',
-    'html-dist',
-    'xdcs'
-  )
-  try {
-    await mkdir(destination, { recursive: true })
-  } catch (error) {
-    console.log('failed to create dir', destination, error)
-  }
-  await cp(join(source_dir, 'html-dist/xdcs'), destination, { recursive: true })
 }
+
