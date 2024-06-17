899f196da874148f880577b3184e01d92fca21a9
Simon Laux <Simon-Laux@users.noreply.github.com>
2024-06-17 22:32:03 +0200

    simon/prepare 1.46.1 (#3953)
    * fix old device message, add new device message
    
    * change phrasing
    
    * prepare 1.46.1
    
    * rm minor device message

--- src/renderer/deviceMessages.ts
+++ src/renderer/deviceMessages.ts
@@ -27,15 +27,15 @@ export async function updateDeviceChats(
   await addDeviceMessage('changelog-version-1.46.0-version1', {
     text: `What's new in 1.46.0?
 
-    🐣 New Onboarding: ⚡️ Sign up to secure fast chatmail servers (https://delta.chat/chatmail) or use classic e-mail servers
-    
-    💻 Collapse chat list on small screens or window sizes
-    📋 Accept images from clipboard in QR reader
-    🖼️✂️ Image cropper for profile image selector
-    📌 Webxdc windows now remember their last position and size
-    🚀 Shorter startup times on newer Mac (through native ARM builds)
-    ✨ Many improvements and bug fixes
-    
-    [Full Changelog](https://github.com/deltachat/deltachat-desktop/blob/main/CHANGELOG.md#1_46_0)`,
+🐣 New Onboarding: ⚡️ Sign up to secure fast chatmail servers (https://delta.chat/chatmail) or use classic e-mail servers
+
+💻 Collapse chat list on small screens or window sizes
+📋 Accept images from clipboard in QR reader
+🖼️✂️ Image cropper for profile image selector
+📌 Webxdc windows now remember their last position and size
+🚀 Shorter startup times on newer Mac (through native ARM builds)
+✨ Many improvements and bug fixes
+
+[Full Changelog](https://github.com/deltachat/deltachat-desktop/blob/main/CHANGELOG.md#1_46_0)`,
   })
 }
