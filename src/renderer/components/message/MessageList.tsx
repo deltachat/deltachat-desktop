9cb8c0d76ca680ef9086e7384c41868e1e3f41f9
Farooq Karimi Zadeh <fkz@riseup.net>
2024-07-01 19:27:17 +0330

    gud format

--- src/renderer/components/message/MessageList.tsx
+++ src/renderer/components/message/MessageList.tsx
@@ -48,7 +48,7 @@ const onWindowFocus = (accountId: number) => {
       rect.top >= 0 &&
       rect.left >= 0 &&
       rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
+        (window.innerHeight || document.documentElement.clientHeight) &&
       rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     )
   })
@@ -330,9 +330,9 @@ export default function MessageList({ accountId, chat, refComposer }: Props) {
     } else if (scrollTo.type === 'scrollToLastKnownPosition') {
       log.debug(
         'scrollTo type: scrollToLastKnownPosition; lastKnownScrollHeight: ' +
-        scrollTo.lastKnownScrollHeight +
-        '; lastKnownScrollTop: ' +
-        scrollTo.lastKnownScrollTop
+          scrollTo.lastKnownScrollHeight +
+          '; lastKnownScrollTop: ' +
+          scrollTo.lastKnownScrollTop
       )
 
       if (scrollTo.appendedOn === 'top') {
