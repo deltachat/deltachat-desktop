0fb58eac1edd15bd5dbd1cf68890004cb6bdbc4f
Nico de Haen <mail@ndh-websolutions.de>
2024-07-06 11:33:29 +0200

    Add condition for Mac

--- src/main/notifications.ts
+++ src/main/notifications.ts
@@ -79,8 +79,13 @@ function showNotification(_event: IpcMainInvokeEvent, data: DcNotification) {
       notify.close()
     })
     notify.on('close', () => {
-      // this is triggered when the message is moved to notification center
-      // so we don't remove the instance from the list
+      // this can be triggered by system out when the message
+      // is moved to notification center (Window, Linux)
+      if (isMac) {
+        notifications[chatId] =
+          notifications[chatId]?.filter(n => n !== notify) || []
+      }
+      console.log('Notification close event triggered', notify)
     })
 
     if (notifications[chatId]) {
