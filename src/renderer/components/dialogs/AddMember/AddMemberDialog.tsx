9ce1e2821b02762af8ffffe35805b3180456f430
Nico de Haen <mail@ndh-websolutions.de>
2024-07-24 18:45:16 +0200

    Fix prettier error

--- src/renderer/components/dialogs/AddMember/AddMemberDialog.tsx
+++ src/renderer/components/dialogs/AddMember/AddMemberDialog.tsx
@@ -74,7 +74,8 @@ export const AddMemberChip = (props: {
         />
       </div>
       <div className={styles.DisplayName}>
-        <div>{contact.displayName}</div> {contact.isVerified && <InlineVerifiedIcon />}
+        <div>{contact.displayName}</div>
+        {contact.isVerified && <InlineVerifiedIcon />}
       </div>
       <div
         className={styles.removeMember}
