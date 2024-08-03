4a00aee4c4c483eeeac7de590c9f3e202d0c51a0
SimonLaux <mobile.info@simonlaux.de>
2024-08-04 01:38:25 +0200

    add scripts to change the core thats used

---
+++ bin/link_core/link_catalog.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+cd packages/target-electron
+pnpm add --save @deltachat/jsonrpc-client@catalog: @deltachat/stdio-rpc-server@catalog:
+
+cd ../frontend
+pnpm add @deltachat/jsonrpc-client@catalog:
