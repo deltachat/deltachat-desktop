947d4f91864be5b42996fc5fbd4e14e1cb2367c7
SimonLaux <mobile.info@simonlaux.de>
2024-08-04 19:43:10 +0200

    proper native sourcemap support during development for main process

--- packages/target-electron/bin/build.js
+++ packages/target-electron/bin/build.js
@@ -1,11 +1,15 @@
 import { build } from 'esbuild'
 
+const isProduction = process.env['NODE_ENV'] === 'production'
+const isMinify = process.argv.indexOf('-m') !== -1 
+
 await build({
   bundle: true,
   sourcemap: true,
   format: 'esm',
   platform: 'node',
   outdir: 'bundle_out',
+  minify: isMinify || isProduction,
   external: [
     // all dependencies not listed here can go to dev dependencies because they get bundled
     'electron',
