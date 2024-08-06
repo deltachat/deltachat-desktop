9e0bcd2e3d91296e4d3deb6eddd1c2df254b1106
SimonLaux <mobile.info@simonlaux.de>
2024-08-07 00:37:36 +0200

    update ./bin/topEvil.sh script

--- bin/topEvil.sh
+++ bin/topEvil.sh
@@ -3,13 +3,13 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
 
 echo "Top-Evil TypeScript"
 
-res=$( grep -r "^import.*blueprint" src --color=always )
+res=$( grep -r "^import.*blueprint" packages/frontend/src --color=always )
 echo "🤔 Using Blueprint code: $( echo "$res" | wc -l )"
 code=$( echo "$res" | wc -l )
 echo "-----------------------------------------------------"
 echo "$res"
 
-res=$( grep -r "bp4-" src --color=always )
+res=$( grep -r "bp4-" packages/frontend/src --color=always )
 echo "🤔 Using Blueprint css classes: $( echo "$res" | wc -l )"
 classes=$( echo "$res" | wc -l )
 echo "-----------------------------------------------------"
@@ -17,7 +17,7 @@ echo "$res"
 
 echo "Top-Evil SCSS"
 
-res=$( grep -r ".bp4-" scss --color=always )
+res=$( grep -r ".bp4-" packages/frontend/scss --color=always )
 echo "🤔 Using Blueprint in css: $( echo "$res" | wc -l )"
 scss=$( echo "$res" | wc -l )
 echo "-----------------------------------------------------"
@@ -26,12 +26,7 @@ echo "$res"
 echo "🤔Result🤔"
 echo "CODE: $code | CLASSES: $classes | SCSS: $scss"
 
-# res=$( grep -r "^import.*electron" src/main/deltachat --color=always )
+# res=$( grep -r "^import.*electron" packages/target-electron/src/deltachat --color=always )
 # echo "🤔 Using Electron inside of the deltachat controller: $( echo "$res" | wc -l )"
-# echo "-----------------------------------------------------"
-# echo "$res"
-
-# res=$( grep -r "require(" src/main --color=always )
-# echo "🤔 Requires in main Process: $( echo "$res" | wc -l )"
 # echo "-----------------------------------------------------"
 # echo "$res"
