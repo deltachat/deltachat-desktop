304d3b7958485f2c0ba2d4af8659f52ed5c728ea
WofWca <wofwca@protonmail.com>
2024-09-16 15:48:44 +0400

    improvement: a11y: add `aria-keyshortcuts`
    Not adding one for the "Settings" button because I'm lazy +
    it's already available in the "File" menu as an "accelerator".

--- packages/frontend/src/keybindings.ts
+++ packages/frontend/src/keybindings.ts
@@ -67,6 +67,8 @@ export function keyDownEvent2Action(
   if (window.__contextMenuActive) {
     return
   }
+  // When modifying this, don't forget to also update the corresponding
+  // `aria-keyshortcuts` properties, and the "Keybindings" help window.
   if (!ev.repeat) {
     // fire only on first press
     if (ev.altKey && ev.key === 'ArrowDown') {
