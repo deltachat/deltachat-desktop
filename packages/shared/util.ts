e6a9f7298fa0b472e8333d1e2a64ae01b05225bb
WofWca <wofwca@protonmail.com>
2024-09-28 16:48:54 +0400

    refactor: add a docstring for `isInviteLink`
    To clarify this is not just about contacts, but also groups.

--- packages/shared/util.ts
+++ packages/shared/util.ts
@@ -6,6 +6,10 @@ export function truncateText(text: string, max_len: number) {
   }
 }
 
+/**
+ * @returns Whether the `url` is a contact or a group invite link.
+ * (https://github.com/deltachat/invite/blob/1b5b2e2a9bfcaf87c4f44929fb7734819d1d9679/README.md?plain=1#L6-L7)
+ */
 export function isInviteLink(url: string) {
   return url.startsWith('https://i.delta.chat/') && url.includes('#')
 }
