deltachat/deltachat-desktop

commit 07cd2b2c5f9d779cb71bb8c034b31b503b348806

perf: improve `scrollToMessage` performance
`getElementById` should be faster than `querySelector`.
This also simplifies the code.

packages/frontend/src/components/message/MessageList.tsx
@@ -307,26 +307,19 @@ export default function MessageList({ accountId, chat, refComposer }: Props) {
     if (scrollTo.type === 'scrollToMessage') {
       log.debug('scrollTo: scrollToMessage: ' + scrollTo.msgId)
 
-      let domElement = document.querySelector(
-        `.message[id="${scrollTo.msgId.toString()}"]`
-      )
-      if (!domElement) {
-        domElement = document.querySelector(
-          `.info-message[id="${scrollTo.msgId.toString()}"]`
-        )
-      }
-      if (!domElement) {
-        domElement = document.querySelector(
-          `.videochat-invitation[id="${scrollTo.msgId.toString()}"]`
-        )
-      }
-
-      if (!domElement) {
+      const domElement = document.getElementById(scrollTo.msgId.toString())
+      if (
+        !domElement ||
+        (!domElement.classList.contains('message') &&
+          !domElement.classList.contains('info-message') &&
+          !domElement.classList.contains('videochat-invitation'))
+      ) {
         log.debug(
-          "scrollTo: scrollToMessage, couldn't find matching message in dom, returning"
+          `scrollTo: scrollToMessage, couldn't find matching message in dom: ${domElement}, returning`
         )
         return
       }
+
       domElement.scrollIntoView()
 
       if (scrollTo.highlight === true) {

commit 40e74e417b5d1cee41cef2839e5c255b2b05311c

fix: `jumpToMessage` not highlighting messages
The bug was introduced in 07cd2b2c5f9d779cb71bb8c034b31b503b348806.

packages/frontend/src/components/message/MessageList.tsx
@@ -337,15 +337,21 @@ export default function MessageList({ accountId, chat, refComposer }: Props) {
 
       if (scrollTo.highlight === true) {
         // Trigger highlight animation
-        const parentElement = domElement.parentElement
-        if (parentElement !== null) {
+
+        // As was menitioned above, `domElement` could either be an
+        // element inside of `<li>`, or the `<li>` itself.
+        // The `<li>` is what implements the animation, so let's ensure that
+        // we add the class to the `<li>` element.
+        const highlightableElement =
+          domElement.tagName === 'LI' ? domElement : domElement.parentElement
+        if (highlightableElement !== null) {
           setTimeout(() => {
             // Retrigger animation
-            parentElement.classList.add('highlight')
-            parentElement.style.animation = 'none'
-            parentElement.offsetHeight
+            highlightableElement.classList.add('highlight')
+            highlightableElement.style.animation = 'none'
+            highlightableElement.offsetHeight
             //@ts-ignore
-            parentElement.style.animation = null
+            highlightableElement.style.animation = null
           }, 0)
         }
       }