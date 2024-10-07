deltachat/deltachat-desktop

commit fecdbbe3676521dea0f7aee58b9beaef0c7fff4f

perf: improve `jumpToMessage` perf
Don't reload message cache unnecessarily.

E.g. if you quote a message with a shortcut,
or if you go to message that is not far away.

packages/frontend/src/stores/messagelist.ts
@@ -530,7 +530,7 @@ class MessageListStore extends Store<MessageListState> {
         // calculate page indexes, so that jumpToMessageId is in the middle of the page
         let oldestFetchedMessageListItemIndex = -1
         let newestFetchedMessageListItemIndex = -1
-        let messageCache: MessageListState['messageCache'] = {}
+        let newMessageCache: MessageListState['messageCache'] = {}
         const half_page_size = Math.ceil(PAGE_SIZE / 2)
         if (messageListItems.length !== 0) {
           oldestFetchedMessageListItemIndex = Math.max(
@@ -560,17 +560,48 @@ class MessageListStore extends Store<MessageListState> {
             )
           }
 
-          messageCache =
-            (await loadMessages(
-              accountId,
-              messageListItems,
-              oldestFetchedMessageListItemIndex,
-              newestFetchedMessageListItemIndex
-            ).catch(err => this.log.error('loadMessages failed', err))) || {}
+          const messagesAlreadyLoaded = getView(
+            messageListItems,
+            oldestFetchedMessageListItemIndex,
+            newestFetchedMessageListItemIndex
+          ).every(item => {
+            if (item.kind !== 'message') {
+              return true
+            }
+            return this.state.messageCache[item.msg_id] != undefined
+          })
+
+          this.log.debug(
+            'messagesAlreadyLoaded:',
+            messagesAlreadyLoaded,
+            messagesAlreadyLoaded
+              ? 'Using the existing cache'
+              : 'Resetting the messageCache'
+          )
+
+          if (messagesAlreadyLoaded) {
+            newMessageCache = this.state.messageCache
+            oldestFetchedMessageListItemIndex =
+              this.state.oldestFetchedMessageListItemIndex
+            newestFetchedMessageListItemIndex =
+              this.state.newestFetchedMessageListItemIndex
+          } else {
+            newMessageCache =
+              (await loadMessages(
+                accountId,
+                messageListItems,
+                oldestFetchedMessageListItemIndex,
+                newestFetchedMessageListItemIndex
+              ).catch(err => this.log.error('loadMessages failed', err))) || {}
+          }
         }
 
+        // TODO perf: it could so happen that nothing except `viewState` (which
+        // is only responsible for scrolling)
+        // has changed after this function has run.
+        // It woud be great to not re-render the message list in that case.
         this.reducer.selectedChat({
-          messageCache,
+          messageCache: newMessageCache,
           messageListItems,
           oldestFetchedMessageListItemIndex,
           newestFetchedMessageListItemIndex: newestFetchedMessageListItemIndex,

commit 1fc06f2fd58747af182c7538f1cdd3748ace1af0

fix: not scrolling to bottom on sending message
Closes https://github.com/deltachat/deltachat-desktop/issues/4186.
The bug was introduced in fecdbbe3676521dea0f7aee58b9beaef0c7fff4f,
https://github.com/deltachat/deltachat-desktop/pull/4145.

packages/frontend/src/stores/messagelist.ts
@@ -572,10 +572,37 @@ class MessageListStore extends Store<MessageListState> {
 
           if (messagesAlreadyLoaded) {
             newMessageCache = this.state.messageCache
-            oldestFetchedMessageListItemIndex =
-              this.state.oldestFetchedMessageListItemIndex
-            newestFetchedMessageListItemIndex =
-              this.state.newestFetchedMessageListItemIndex
+
+            // Why do we need `Math.min` / `Math.max` here, instead of simply
+            // keeping `this.state.oldestFetchedMessageListItemIndex`
+            // and `this.state.newestFetchedMessageListItemIndex` as they are?
+            // Because some other code might update the state in such a way
+            // that `messageCache` and these
+            // `(oldest|newest)FetchedMessageListItemIndex` are out of sync:
+            // `messageCache` actually has a message, but
+            // these integers say that the message is not yet fetched.
+            // Namely, this can happen inside of `messageChanged` when
+            // it gets invoked for a not yet fetched message, and it gets
+            // added `messageCache` instead of getting updated.
+            // This, in turn, can happen when you send a message.
+            //
+            // The result would be that we'd fail to jump to message inside of
+            // `MessageList.tsx`, because the message wouldn't be rendered,
+            // because we only render messages that are between
+            // `oldestFetchedMessageListItemIndex` and
+            // `newestFetchedMessageListItemIndex` (see `activeView`).
+            //
+            // TODO it would be ideal to get ensure that we don't corrupt
+            // the state in the first place, but let's make
+            // this workaround for now.
+            oldestFetchedMessageListItemIndex = Math.min(
+              this.state.oldestFetchedMessageListItemIndex,
+              oldestFetchedMessageListItemIndex
+            )
+            newestFetchedMessageListItemIndex = Math.max(
+              this.state.newestFetchedMessageListItemIndex,
+              newestFetchedMessageListItemIndex
+            )
           } else {
             newMessageCache =
               (await loadMessages(