c2facbfa17ee682ef0e0f5609795c6fa2d0d6911
Max Philippov <max.philippos@gmail.com>
2024-10-17 01:23:54 +0500

    Buttons accept HTMLButton props
    - make Button and some other components accept HTMLButton props
    - rename Button.props.type to styling so we don't conflict with
      <button type="">

--- packages/frontend/src/components/Button/index.tsx
+++ packages/frontend/src/components/Button/index.tsx
@@ -3,38 +3,27 @@ import classNames from 'classnames'
 
 import styles from './style.module.scss'
 
-type Props = React.PropsWithChildren<{
-  'aria-label'?: string
-  className?: string
-  disabled?: boolean
+export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   active?: boolean
-  id?: string
-  onClick: any
-  type?: 'primary' | 'secondary' | 'danger'
-}>
+  styling?: 'primary' | 'secondary' | 'danger'
+}
 
 export default function Button({
   children,
-  disabled = false,
   active = false,
-  id,
-  onClick,
   type,
+  className,
   ...props
-}: Props) {
+}: ButtonProps) {
   return (
     <button
-      id={id}
-      disabled={disabled}
-      role='button'
       className={classNames(
         styles.button,
         active && styles.active,
         type && styles[type],
-        props.className
+        className
       )}
-      onClick={onClick}
-      aria-label={props['aria-label']}
+      {...props}
     >
       {children}
     </button>
