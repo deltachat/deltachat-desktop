deltachat/deltachat-desktop

commit 0e0d0b80d598802ebb972c8d6c05b593bd896f24

blueprintjs removal (#4006)
* Remove blueprintJS

* Fix chat title + `subtile` typo

* Fix text color in inputs

* Add tabindex to Settings elements

- pressing Enter/Space doesn't work yet

* Fix FullscreenMedia Escape handling

* Update packages/frontend/src/components/Dialog/DialogBody.tsx

Co-authored-by: WofWca <wofwca@protonmail.com>

* Update packages/frontend/src/components/SmallSelectDialog.tsx

Co-authored-by: WofWca <wofwca@protonmail.com>

* Update packages/frontend/src/components/Settings/SettingsSelector.tsx

Co-authored-by: WofWca <wofwca@protonmail.com>

* Update packages/frontend/src/components/Settings/SettingsSwitch.tsx

Co-authored-by: WofWca <wofwca@protonmail.com>

* Drop type danger from showPassword

* Some feedback fixes

* Update packages/frontend/scss/misc/_context_menu.scss

Co-authored-by: WofWca <wofwca@protonmail.com>

* Button transparent changes

* Fix fullscreenTransparent and callout

* replace blueprint classes in gallery component

* ProgressBar intent, remove button

* Avatar button fixes

* Fix Collapse, remove button.minimal

* Update packages/frontend/scss/manifest.scss

Co-authored-by: WofWca <wofwca@protonmail.com>

* Update packages/frontend/src/components/Dialog/Dialog.tsx

Co-authored-by: WofWca <wofwca@protonmail.com>

* Fix Switch, delete unnecessary stuff

* Fix formatting

* Avoid hover on date & layout jump in gallery

* Use variable instead of hard coded color

* Use variable instead of hard coded color & flex gap

* Remove left over bp4 style

---------

Co-authored-by: WofWca <wofwca@protonmail.com>
Co-authored-by: Nico de Haen <ndh@deltachat.de>

packages/frontend/src/components/Login-Styles.tsx
@@ -1,12 +1,6 @@
 import React, { useState } from 'react'
-import {
-  Button,
-  InputGroup,
-  FormGroup,
-  Intent,
-  ProgressBar,
-  Switch,
-} from '@blueprintjs/core'
+import Button from './Button'
+import Icon from './Icon'
 import useTranslationFunction from '../hooks/useTranslationFunction'
 
 export const DeltaSelect = React.memo(
@@ -25,49 +19,21 @@ export const DeltaSelect = React.memo(
 
     return (
       <div className='delta-form-group delta-select'>
-        <FormGroup>
-          <div className={`label ${isFocused && 'focus'}`}>{props.label}</div>
-          <div className='bp4-select .modifier'>
-            <select
-              id={props.id}
-              value={props.value}
-              onChange={props.onChange}
-              onFocus={onFocus}
-              onBlur={onBlur}
-            >
-              {props.children}
-            </select>
-          </div>
-        </FormGroup>
-      </div>
-    )
-  }
-)
-
-export const DeltaSwitch = React.memo(
-  (
-    props: React.PropsWithChildren<{
-      label: string
-      id: any
-      value: any
-      disabled?: boolean
-      onChange: (isTrue: boolean) => void
-    }>
-  ) => {
-    return (
-      <div className='delta-form-group delta-switch'>
-        <FormGroup>
-          <Switch
-            label={props.label}
+        <label htmlFor={props.id} className={`${isFocused && 'focus'}`}>
+          {props.label}
+        </label>
+        <div className='delta-select-inner'>
+          <select
+            name={props.id}
             id={props.id}
-            disabled={props.disabled}
-            onChange={ev => {
-              props.onChange(ev.currentTarget.checked)
-            }}
-            alignIndicator='right'
-            checked={props.value === '1'}
-          />
-        </FormGroup>
+            value={props.value}
+            onChange={props.onChange}
+            onFocus={onFocus}
+            onBlur={onBlur}
+          >
+            {props.children}
+          </select>
+        </div>
       </div>
     )
   }
@@ -98,26 +64,24 @@ export const DeltaTextarea = React.memo(
 
     return (
       <div className='delta-form-group delta-textarea'>
-        <FormGroup>
-          <div
-            className={`label ${isFocused && 'focus'}`}
-            style={{ visibility: !showLabel ? 'hidden' : 'visible' }}
-          >
-            {props.label && props.label.length > 0
-              ? props.label
-              : props.placeholder}
-          </div>
-          <textarea
-            onChange={props.onChange}
-            value={props.value}
-            id={props.id}
-            disabled={props.disabled}
-            placeholder={!isFocused ? props.placeholder : ''}
-            onFocus={onFocus}
-            onBlur={onBlur}
-            rows={5}
-          />
-        </FormGroup>
+        <label
+          className={`${isFocused && 'focus'}`}
+          style={{ visibility: !showLabel ? 'hidden' : 'visible' }}
+        >
+          {props.label && props.label.length > 0
+            ? props.label
+            : props.placeholder}
+        </label>
+        <textarea
+          onChange={props.onChange}
+          value={props.value}
+          id={props.id}
+          disabled={props.disabled}
+          placeholder={!isFocused ? props.placeholder : ''}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          rows={5}
+        />
       </div>
     )
   }
@@ -158,29 +122,29 @@ export const DeltaInput = React.memo(
 
     return (
       <div className='delta-form-group delta-input'>
-        <FormGroup>
-          <div
-            className={`label ${isFocused && 'focus'}`}
-            style={{ visibility: !showLabel ? 'hidden' : 'visible' }}
-          >
-            {props.label && props.label.length > 0
-              ? props.label
-              : props.placeholder}
-          </div>
-          <InputGroup
-            id={props.id}
-            type={props.type}
-            value={props.value}
-            onChange={props.onChange}
-            placeholder={props.placeholder}
-            min={props.min}
-            max={props.max}
-            disabled={props.disabled}
-            onFocus={onFocus}
-            onBlur={onBlur}
-            rightElement={props.rightElement}
-          />
-        </FormGroup>
+        <label
+          className={`${isFocused && 'focus'}`}
+          style={{ visibility: !showLabel ? 'hidden' : 'visible' }}
+        >
+          {props.label && props.label.length > 0
+            ? props.label
+            : props.placeholder}
+        </label>
+        <input
+          id={props.id}
+          type={props.type}
+          value={props.value}
+          onChange={props.onChange}
+          placeholder={props.placeholder}
+          min={props.min}
+          max={props.max}
+          disabled={props.disabled}
+          onFocus={onFocus}
+          onBlur={onBlur}
+        />
+        {props.rightElement && (
+          <div className='right-element'>{props.rightElement}</div>
+        )}
       </div>
     )
   }
@@ -205,17 +169,18 @@ export const DeltaPasswordInput = React.memo(
 
     const password = props.password || ''
 
-    const lockButton = (
+    const rightElement = (
       <Button
-        icon={showPassword ? 'eye-open' : 'eye-off'}
-        title={showPassword ? tx('hide_password') : tx('show_password')}
-        intent={Intent.WARNING}
-        minimal
         onClick={() => setShowPassword(!showPassword)}
         aria-label={showPassword ? tx('hide_password') : tx('show_password')}
-      />
+      >
+        <Icon
+          coloring='contextMenu'
+          size={16}
+          icon={showPassword ? 'eye-open' : 'eye-off'}
+        />
+      </Button>
     )
-
     return (
       <DeltaInput
         id={props.id}
@@ -224,26 +189,29 @@ export const DeltaPasswordInput = React.memo(
         value={password}
         onChange={props.onChange}
         placeholder={props.placeholder}
-        rightElement={lockButton}
+        rightElement={rightElement}
       />
     )
   }
 )
 
-export const DeltaProgressBar = function (
-  props: React.PropsWithChildren<{
-    progress: number
-    intent?: Intent
-  }>
-) {
-  return (
-    <div style={{ marginTop: '20px', marginBottom: '10px' }}>
-      <ProgressBar
-        value={props.progress ? props.progress / 1000 : 0}
-        intent={Intent.PRIMARY}
-        stripes={false}
-        animate={false}
-      />
-    </div>
-  )
-}
+type DeltaIntent = 'primary' | 'success' | 'danger'
+
+type ProgressBarProps = React.PropsWithChildren<{
+  progress: number
+  intent?: DeltaIntent
+}>
+
+export const DeltaProgressBar = React.memo<ProgressBarProps>(
+  ({ progress, intent = 'primary' }) => {
+    return (
+      <div style={{ marginTop: '20px', marginBottom: '10px' }}>
+        <progress
+          className={`delta-progress-bar delta-intent-${intent}`}
+          value={progress ? progress / 1000 : 0}
+          max={100}
+        ></progress>
+      </div>
+    )
+  }
+)

commit d0d774fec3865d5fc1fe5d20defb14db2c8c6930

fix: progress bar not working
The bug was introduced in 0e0d0b80d598802ebb972c8d6c05b593bd896f24.
Blueprint uses a range of [0, 1] and not [0, 100]:
https://blueprintjs.com/docs/#core/components/progress-bar.

packages/frontend/src/components/Login-Styles.tsx
@@ -198,18 +198,26 @@ export const DeltaPasswordInput = React.memo(
 type DeltaIntent = 'primary' | 'success' | 'danger'
 
 type ProgressBarProps = React.PropsWithChildren<{
+  /**
+   * Min is 0, max is {@link ProgressBarProps.max}
+   */
   progress: number
+  /**
+   * @default 1000
+   * for reasons see <https://github.com/deltachat/deltachat-core-rust/blob/75fe4e106a740f8e3205d0bb56ce676fb2cd2411/deltachat-ffi/deltachat.h#L2928>
+   */
+  max?: number
   intent?: DeltaIntent
 }>
 
 export const DeltaProgressBar = React.memo<ProgressBarProps>(
-  ({ progress, intent = 'primary' }) => {
+  ({ progress, intent = 'primary', max = 1000 }) => {
     return (
       <div style={{ marginTop: '20px', marginBottom: '10px' }}>
         <progress
           className={`delta-progress-bar delta-intent-${intent}`}
-          value={progress ? progress / 1000 : 0}
-          max={100}
+          value={progress ? progress : 0}
+          max={max}
         ></progress>
       </div>
     )