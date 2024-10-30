e551b9e941a795b73132289205876a418dfb6ed7
SimonLaux <mobile.info@simonlaux.de>
2024-10-30 02:29:14 +0100

    make background image on "no account selected"- and welcome screen a drag area

--- packages/frontend/src/components/screens/NoAccountSelectedScreen/NoAccountSelectedScreen.tsx
+++ packages/frontend/src/components/screens/NoAccountSelectedScreen/NoAccountSelectedScreen.tsx
@@ -3,14 +3,15 @@ import React from 'react'
 import useTranslationFunction from '../../../hooks/useTranslationFunction'
 
 import styles from './styles.module.scss'
+import classNames from 'classnames'
 
 export function NoAccountSelectedScreen() {
   const tx = useTranslationFunction()
 
   return (
-    <div className={styles.noAccountSelectedScreen}>
+    <div className={classNames(styles.noAccountSelectedScreen, 'drag')}>
       <div className={styles.background}>
-        <div className={styles.infoBox}>{tx('no_account_selected')}</div>
+        <div className={classNames(styles.infoBox, 'no-drag')}>{tx('no_account_selected')}</div>
       </div>
     </div>
   )
