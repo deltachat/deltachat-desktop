deltachat/deltachat-desktop

commit 0e0d0b80d598802ebb972c8d6c05b593bd896f24

blueprintjs removal (#4006)
* Remove blueprintJS

* Fix chat title + `subtile` typo

* Fix text color in inputs

* Add tabindex to Settings elements

- pressing Enter/Space doesn't work yet

* Fix FullscreenMedia Escape handling

* Update packages/frontend/src/components/Dialog/DialogBody.tsx

Co-authored-by: WofWca <wofwca@protonmail.com>

* Update packages/frontend/src/components/SmallSelectDialog.tsx

Co-authored-by: WofWca <wofwca@protonmail.com>

* Update packages/frontend/src/components/Settings/SettingsSelector.tsx

Co-authored-by: WofWca <wofwca@protonmail.com>

* Update packages/frontend/src/components/Settings/SettingsSwitch.tsx

Co-authored-by: WofWca <wofwca@protonmail.com>

* Drop type danger from showPassword

* Some feedback fixes

* Update packages/frontend/scss/misc/_context_menu.scss

Co-authored-by: WofWca <wofwca@protonmail.com>

* Button transparent changes

* Fix fullscreenTransparent and callout

* replace blueprint classes in gallery component

* ProgressBar intent, remove button

* Avatar button fixes

* Fix Collapse, remove button.minimal

* Update packages/frontend/scss/manifest.scss

Co-authored-by: WofWca <wofwca@protonmail.com>

* Update packages/frontend/src/components/Dialog/Dialog.tsx

Co-authored-by: WofWca <wofwca@protonmail.com>

* Fix Switch, delete unnecessary stuff

* Fix formatting

* Avoid hover on date & layout jump in gallery

* Use variable instead of hard coded color

* Use variable instead of hard coded color & flex gap

* Remove left over bp4 style

---------

Co-authored-by: WofWca <wofwca@protonmail.com>
Co-authored-by: Nico de Haen <ndh@deltachat.de>

packages/frontend/scss/components/_radios.scss
@@ -10,12 +10,15 @@
     flex-direction: row;
     align-items: center;
     justify-content: flex-start;
+    & > input {
+      margin: 0 3px 0 0;
+    }
     & > img {
       height: 36px;
       width: 36px;
     }
     & > label {
-      padding-left: 4px;
+      padding-left: 7px;
       padding-right: 4px;
       height: 36px;
       display: inline-flex;

commit 3894e5ce143d057125813612e23b7b341aa33083

style: less vertical space between radio buttons
The "bug" was introduced in 0e0d0b80d598802ebb972c8d6c05b593bd896f24.
Now the `RadioGroup` items are spaced as they were
prior to the aforementioned commit.

This slightly changes the look of the "Video Chat"
section in settings.

packages/frontend/scss/components/_radios.scss
@@ -3,9 +3,8 @@
   display: flex;
   justify-content: space-around;
   flex-direction: column;
+  gap: 10px;
   & > .radiobutton {
-    padding-bottom: 2px;
-    height: 42px;
     display: flex;
     flex-direction: row;
     align-items: center;
@@ -20,7 +19,6 @@
     & > label {
       padding-left: 7px;
       padding-right: 4px;
-      height: 36px;
       display: inline-flex;
       justify-content: space-between;
       flex-direction: column;
@@ -32,9 +30,12 @@
         // the subtitle
         color: var(--bp4InputPlaceholder);
       }
-    }
-    & > label.no-subtitle > span:nth-child(1) {
-      padding-top: 9px !important;
+      &.subtitle-height {
+        height: 36px;
+      }
+      &.no-subtitle {
+        justify-content: center;
+      }
     }
   }
 }

packages/frontend/src/components/Radio.tsx
@@ -8,6 +8,11 @@ type RadioProps = {
   value: string
   className?: string
   name?: string
+  /**
+   * If not omitted, and if it's an empty string, the height of the radio
+   * will be the same as if the subtitle was present.
+   * Otherwise only the {@link RadioProps.label} height will define the height.
+   */
   subtitle?: string
 }
 
@@ -31,7 +36,13 @@ export default function Radio({
         value={value}
         defaultChecked={Boolean(selected)}
       />
-      <label htmlFor={id} className={classNames(!subtitle && 'no-subtitle')}>
+      <label
+        htmlFor={id}
+        className={classNames({
+          'subtitle-height': subtitle != undefined,
+          'no-subtitle': subtitle == undefined || subtitle === '',
+        })}
+      >
         <span>{label}</span>
         {subtitle && <span>{subtitle}</span>}
       </label>

packages/frontend/src/components/dialogs/EditVideochatInstanceDialog.tsx
@@ -85,7 +85,12 @@ export default function EditVideochatInstanceDialog({
             selectedValue={radioValue}
             name='videochat-instance'
           >
-            <Radio key='select-none' label={tx('off')} value='disabled' />
+            <Radio
+              key='select-none'
+              label={tx('off')}
+              value='disabled'
+              subtitle=''
+            />
             <Radio
               key='select-systemli'
               label='Systemli'
@@ -102,6 +107,7 @@ export default function EditVideochatInstanceDialog({
               key='select-custom'
               label={tx('custom')}
               value='custom'
+              subtitle=''
               className={'test-videochat-custom'}
             />
           </RadioGroup>