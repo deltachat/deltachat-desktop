341998978250623d959f9afb4547881d36f1e8c3
SimonLaux <mobile.info@simonlaux.de>
2024-11-01 01:25:41 +0100

    better overlay that also works with dialogs

--- packages/frontend/src/experimental.ts
+++ packages/frontend/src/experimental.ts
@@ -78,8 +78,71 @@ only for debugging:
     return runtime
   }
 
+  debugDragAreaActive: boolean = false
+  debugDragAreaUpdateInterval: ReturnType<typeof setInterval> | null
   showDragAreas() {
-    document.body.classList.toggle("show-drag-areas")
+    let element: HTMLCanvasElement | undefined = document.getElementById(
+      'drag-area-visualisation'
+    ) as HTMLCanvasElement
+    if (!element) {
+      element = document.createElement('canvas')
+      element.height = window.visualViewport.height
+      element.width = window.visualViewport.width
+      element.id = 'drag-area-visualisation'
+      element.popover = 'manual'
+      document.body.append(element)
+    }
+
+    if (this.debugDragAreaActive) {
+      element.hidePopover()
+      clearInterval(this.debugDragAreaUpdateInterval)
+      this.debugDragAreaUpdateInterval = null
+      this.debugDragAreaActive = false
+    } else {
+      element.showPopover()
+      const drawing = element.getContext('2d')
+      drawing.translate(0.5, 0.5)
+      this.debugDragAreaUpdateInterval = setInterval(() => {
+        // hack to make it show on top of dialogs
+        element.hidePopover()
+        element.showPopover()
+
+        let { height, width } = element.getBoundingClientRect()
+        drawing.clearRect(0, 0, width, height)
+
+        function drawElement(rect: DOMRect, color: string) {
+          drawing.fillStyle = color
+          drawing.fillRect(rect.x, rect.y, rect.width, rect.height)
+        }
+
+        function traverseDOMRecursively(element: HTMLElement) {
+          // element.computedStyleMap
+          const styles = window.getComputedStyle(element)
+
+          //@ts-ignore
+          const role = styles.webkitAppRegion
+
+          if (role !== 'none') {
+            const boundingRect = element.getClientRects()[0]
+            if (role === 'no-drag') {
+              drawElement(boundingRect, 'green')
+            } else if (role === 'drag') {
+              drawElement(boundingRect, 'red')
+            }
+            drawing.moveTo(0, 0)
+          }
+
+          for (let child of element.children) {
+            if (child instanceof HTMLElement) {
+              traverseDOMRecursively(child)
+            }
+          }
+        }
+
+        traverseDOMRecursively(document.documentElement)
+      }, 50)
+      this.debugDragAreaActive = true
+    }
   }
 }
 
