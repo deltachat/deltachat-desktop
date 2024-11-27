deltachat/deltachat-desktop

commit 78fef8b6b45329250c369dc23bd1c4bcd02adca5

improvement: make quotes' content non-focusabe

packages/frontend/src/components/message/MessageMarkdown.tsx
@@ -98,7 +98,7 @@ function renderElement(elm: ParsedElement, key?: number): JSX.Element {
   }
 }
 
-/** render in preview mode for ChatListItem summary,
+/** render in preview mode for ChatListItem summary and for quoted messages,
  *  not interactive (links can not be clicked) just looks more similar to the message in the chatview/message-list */
 function renderElementPreview(elm: ParsedElement, key?: number): JSX.Element {
   switch (elm.t) {

packages/frontend/src/components/message/Message.tsx
@@ -805,6 +805,7 @@ export const Quote = ({
                 quote.text.slice(0, 3000 /* limit quoted message size */) || ''
               }
               disableJumbomoji
+              nonInteractiveContent
             />
           </div>
         </div>

packages/frontend/src/components/message/MessageBody.tsx
@@ -9,9 +9,15 @@ const UPPER_LIMIT_FOR_PARSED_MESSAGES = 20_000
 function MessageBody({
   text,
   disableJumbomoji,
+  nonInteractiveContent = false,
 }: {
   text: string
   disableJumbomoji?: boolean
+  /**
+   * Ensure that links and other things are non-interactive,
+   * display them as regular text.
+   */
+  nonInteractiveContent?: boolean
 }): JSX.Element {
   if (text.length >= UPPER_LIMIT_FOR_PARSED_MESSAGES) {
     return <>{text}</>
@@ -28,7 +34,7 @@ function MessageBody({
       )
     }
   }
-  return message2React(emojifiedText, false)
+  return message2React(emojifiedText, nonInteractiveContent)
 }
 const trimRegex = /^[\s\uFEFF\xA0\n\t]+|[\s\uFEFF\xA0\n\t]+$/g
 

commit f70f1b0fddfcb56c8c0efa5082e45ef148008705

style: keep line breaks in quoted messages
Closes https://github.com/deltachat/deltachat-desktop/issues/4354.
Supersedes https://github.com/deltachat/deltachat-desktop/pull/4356.

The bug was introduced in 78fef8b6b45329250c369dc23bd1c4bcd02adca5.

Co-authored-by: Nico de Haen <ndh@deltachat.de>

packages/frontend/src/components/message/MessageMarkdown.tsx
@@ -126,7 +126,10 @@ function renderElementPreview(elm: ParsedElement, key?: number): JSX.Element {
       return <span key={key}>{elm.c.label.map(renderElementPreview)} </span>
 
     case 'Linebreak':
-      return <span key={key}>{''}</span>
+      // In ChatListItem this will be collapsed by default.
+      // We need line breaks to be displayed for quoted messages
+      // and in the composer.
+      return <span key={key}>{'\n'}</span>
 
     case 'Tag':
     case 'EmailAddress':