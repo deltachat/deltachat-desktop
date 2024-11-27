f744922fc3aa91aeab5d269ad45b110f4c23316c
Nico de Haen <ndh@deltachat.de>
2024-11-27 08:43:31 +0100

    WIP: Extend create-local-help with pagefind

--- bin/create-local-help.sh
+++ bin/create-local-help.sh
@@ -3,12 +3,12 @@
 # update offline help; requires deltachat-pages to be checked out beside deltachat-desktop
 set -e
 
-# ../deltachat-pages/tools/create-local-help.py ../deltachat-pages/_site static/help
+../deltachat-pages/tools/create-local-help.py ../deltachat-pages/_site static/help
 
-SEARCH="<link rel=\"stylesheet\" href=\"..\/help.css\" \/>"
-REPLACE="<title>Results:<\/title>\n<link rel=\"stylesheet\" href=\"..\/help.css\" \/>\n<link href=\".\/pagefind\/pagefind-ui.css\" rel=\"stylesheet\"><script src=\".\/pagefind\/pagefind-ui.js\"><\/script><div id=\"search\"><\/div>\n<script>window.addEventListener('DOMContentLoaded', (event) => {new PagefindUI({ element: \"#search\", showSubResults: true, excerptLength: 30 });});<\/script>"
+SEARCH="<link rel=\"stylesheet\" href=\"..\/help.css\" \/><\/head>"
+REPLACE="<title>Results:<\/title>\n<link rel=\"stylesheet\" href=\"..\/help.css\" \/>\n<link href=\".\/pagefind\/pagefind-ui.css\" rel=\"stylesheet\"><script src=\".\/pagefind\/pagefind-ui.js\"><\/script><\/head><div id=\"search\"><\/div>\n<script>window.addEventListener('DOMContentLoaded', (event) => {new PagefindUI({ element: \"#search\", showSubResults: true, excerptLength: 30 });});<\/script>"
 
-dirs=( $( ls ./static/help/  -1p | grep / | sed 's/^\(.*\)/\1/') )
+dirs=( $( ls ./static/help/  -1p | grep / | sed 's/\/$//' | sed 's/^\(.*\)/\1/') )
 
 # Define an array of languages
 
@@ -16,12 +16,12 @@ dirs=( $( ls ./static/help/  -1p | grep / | sed 's/^\(.*\)/\1/') )
 for LANGUAGE in "${dirs[@]}"; do
   FILE="./static/help/$LANGUAGE/help.html"
   echo ./static/help/$LANGUAGE
-  npx pagefind --site ./static/help/$LANGUAGE
   # Überprüfen, ob die Datei existiert
   if [ -f "$FILE" ]; then
     # Ersetzen des Textes in der Datei
     sed -i "s/$SEARCH/$REPLACE/g" "$FILE"
     echo "Ersetzung erfolgreich in $FILE"
+    npx pagefind --site ./static/help/$LANGUAGE
   else
     echo "Datei $FILE nicht gefunden"
   fi
