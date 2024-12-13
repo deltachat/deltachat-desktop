411700295f7652ee997e4feee958d9b1a7bdb62e
Simon Laux <Simon-Laux@users.noreply.github.com>
2024-12-13 22:54:48 +0100

    macOS: make area under traffic lights dragable and slightly larger (#4408)
    * macOS: make area under traffic lights dragable and slightly larger
    
    raiden suggested this change to me in a private message.
    
    * add pr number to changelog
    
    * fix fmt
    
    * fix the bug that size of that area changed based on profile acount and window height

--- packages/frontend/src/components/AccountListSidebar/AccountListSidebar.tsx
+++ packages/frontend/src/components/AccountListSidebar/AccountListSidebar.tsx
@@ -139,7 +139,9 @@ export default function AccountListSidebar({
   return (
     <div className={styles.accountListSidebar}>
       {runtime.getRuntimeInfo().isMac && !smallScreenMode && (
-        <div className={styles.macOSTrafficLightBackground} />
+        <div
+          className={classNames(styles.macOSTrafficLightBackground, 'drag')}
+        />
       )}
       <div
         ref={accountsListRef}
