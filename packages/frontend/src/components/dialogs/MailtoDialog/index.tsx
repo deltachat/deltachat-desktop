2d33d5b5e6aa47dbe5c0e48d264875f2568be9a5
WofWca <wofwca@protonmail.com>
2024-12-24 20:08:46 +0400

    refactor: unify "Select Chat": use one component
    For:
    - webxdc `sendToChat()`
    - mailto dialog
    - "Forward Message" dialog
    
    What this commit comes down to:
    - Take the ForwardTo dialog and use it to create
      a more generic, reusable SelectChat dialog.
    - Make those similar dialogs use the new SelectChat component,
      instead of defining their markup and logic manually.
    
    The dialogs have practically the same markup and the same
    search logic, with very little differences.
    They apparently were copy-pasted from `ForwardMessage` dialog,
    which is the oldest of them all.
    
    It's getting pretty annoying to maintain them, with all the
    `RovingTabindex` and `InfiniteLoader`, so, it's about time
    to unify them.
    
    This is mainly a refactor, but causes a few small visual changes.
    For example, this fixes a small visual bug
    in the `WebxdcSendToChat` dialog
    where the search field takes full height
    when there are no search results.

--- packages/frontend/src/components/dialogs/MailtoDialog/index.tsx
+++ packages/frontend/src/components/dialogs/MailtoDialog/index.tsx
@@ -1,21 +1,12 @@
-import React, { useRef, useState } from 'react'
-import AutoSizer from 'react-virtualized-auto-sizer'
+import React from 'react'
 import { C } from '@deltachat/jsonrpc-client'
 
-import ChatListItem from '../../chat/ChatListItem'
-import { PseudoListItemNoSearchResults } from '../../helpers/PseudoListItem'
-import { ChatListPart, useLogicVirtualChatList } from '../../chat/ChatList'
-import { useChatList } from '../../chat/ChatListHelpers'
-import { useThemeCssVar } from '../../../ThemeManager'
 import { selectedAccountId } from '../../../ScreenController'
-import Dialog, { DialogBody, DialogHeader } from '../../Dialog'
 import useCreateDraftMessage from '../../../hooks/chat/useCreateDraftMesssage'
 import useTranslationFunction from '../../../hooks/useTranslationFunction'
 
-import styles from './styles.module.scss'
-
 import type { DialogProps } from '../../../contexts/DialogContext'
-import { RovingTabindexProvider } from '../../../contexts/RovingTabindex'
+import SelectChat from '../SelectChat'
 
 type Props = {
   messageText: string
@@ -23,87 +14,22 @@ type Props = {
 
 export default function MailtoDialog(props: Props & DialogProps) {
   const { onClose, messageText } = props
-  const listFlags = C.DC_GCL_FOR_FORWARDING | C.DC_GCL_NO_SPECIALS
 
   const tx = useTranslationFunction()
   const createDraftMessage = useCreateDraftMessage()
   const accountId = selectedAccountId()
-  const [queryStr, setQueryStr] = useState('')
-  const { chatListIds } = useChatList(listFlags, queryStr)
-  const { isChatLoaded, loadChats, chatCache } =
-    useLogicVirtualChatList(chatListIds)
-
-  const resultsRef = useRef<HTMLDivElement>(null)
 
   const onChatClick = async (chatId: number) => {
     createDraftMessage(accountId, chatId, messageText)
     onClose()
   }
 
-  const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setQueryStr(e.target.value)
-
-  const CHATLISTITEM_CHAT_HEIGHT =
-    Number(useThemeCssVar('--SPECIAL-chatlist-item-chat-height')) || 64
-
-  const noResults = chatListIds.length === 0 && queryStr !== ''
-
   return (
-    <Dialog onClose={onClose} fixed className={styles.MailtoDialog}>
-      <DialogHeader
-        onClose={onClose}
-        title={tx('mailto_dialog_header_select_chat')}
-      />
-      <DialogBody className={styles.MailtoDialogBody}>
-        <div className='mailto-dialog'>
-          <div className='select-chat-chat-list'>
-            <input
-              className='search-input no-drag'
-              onChange={onSearchChange}
-              value={queryStr}
-              placeholder={tx('search')}
-              autoFocus
-              spellCheck={false}
-            />
-            {noResults && queryStr && (
-              <PseudoListItemNoSearchResults queryStr={queryStr} />
-            )}
-            <div
-              ref={resultsRef}
-              style={noResults ? { height: '0px' } : {}}
-              className='results'
-            >
-              <RovingTabindexProvider wrapperElementRef={resultsRef}>
-                <AutoSizer>
-                  {({ width, height }) => (
-                    <ChatListPart
-                      isRowLoaded={isChatLoaded}
-                      loadMoreRows={loadChats}
-                      rowCount={chatListIds.length}
-                      width={width}
-                      height={height}
-                      itemKey={index => 'key' + chatListIds[index]}
-                      itemHeight={CHATLISTITEM_CHAT_HEIGHT}
-                    >
-                      {({ index, style }) => {
-                        const chatId = chatListIds[index]
-                        return (
-                          <div style={style}>
-                            <ChatListItem
-                              chatListItem={chatCache[chatId] || undefined}
-                              onClick={onChatClick.bind(null, chatId)}
-                            />
-                          </div>
-                        )
-                      }}
-                    </ChatListPart>
-                  )}
-                </AutoSizer>
-              </RovingTabindexProvider>
-            </div>
-          </div>
-        </div>
-      </DialogBody>
-    </Dialog>
+    <SelectChat
+      headerTitle={tx('mailto_dialog_header_select_chat')}
+      onChatClick={onChatClick}
+      onClose={onClose}
+      listFlags={C.DC_GCL_FOR_FORWARDING | C.DC_GCL_NO_SPECIALS}
+    />
   )
 }
--- packages/frontend/src/components/dialogs/WebxdcSendToChat/index.tsx
+++ packages/frontend/src/components/dialogs/WebxdcSendToChat/index.tsx
@@ -1,39 +1,22 @@
-import React, { useCallback, useRef, useState } from 'react'
+import React, { useCallback } from 'react'
 import { C } from '@deltachat/jsonrpc-client'
-import AutoSizer from 'react-virtualized-auto-sizer'
-import classNames from 'classnames'
 
-import ChatListItem from '../../chat/ChatListItem'
-import { PseudoListItemNoSearchResults } from '../../helpers/PseudoListItem'
-import { ChatListPart, useLogicVirtualChatList } from '../../chat/ChatList'
-import { useChatList } from '../../chat/ChatListHelpers'
-import { useThemeCssVar } from '../../../ThemeManager'
 import { BackendRemote } from '../../../backend-com'
 import { selectedAccountId } from '../../../ScreenController'
 import { runtime } from '@deltachat-desktop/runtime-interface'
-import Dialog, {
-  DialogBody,
-  DialogFooter,
-  DialogHeader,
-  FooterActionButton,
-  FooterActions,
-} from '../../Dialog'
+import { DialogFooter, FooterActionButton, FooterActions } from '../../Dialog'
 import ConfirmationDialog from '../ConfirmationDialog'
 import useChat from '../../../hooks/chat/useChat'
 import useDialog from '../../../hooks/dialog/useDialog'
 import useTranslationFunction from '../../../hooks/useTranslationFunction'
 
-import styles from './styles.module.scss'
-
 import type { DialogProps } from '../../../contexts/DialogContext'
-import { RovingTabindexProvider } from '../../../contexts/RovingTabindex'
+import SelectChat from '../SelectChat'
 
 type Props = {
   messageText: string | null
   file: { file_name: string; file_content: string } | null
 } & DialogProps
-
-const LIST_FLAGS = C.DC_GCL_FOR_FORWARDING | C.DC_GCL_NO_SPECIALS
 
 export default function WebxdcSaveToChatDialog(props: Props) {
   const { onClose, messageText, file } = props
@@ -41,12 +24,6 @@ export default function WebxdcSaveToChatDialog(props: Props) {
   const tx = useTranslationFunction()
   const accountId = selectedAccountId()
   const sendToChatAction = useSendToChatAction()
-  const [queryStr, setQueryStr] = useState('')
-  const { chatListIds } = useChatList(LIST_FLAGS, queryStr)
-  const { isChatLoaded, loadChats, chatCache } =
-    useLogicVirtualChatList(chatListIds)
-
-  const resultsRef = useRef<HTMLDivElement>(null)
 
   const onChatClick = async (chatId: number) => {
     let path = null
@@ -75,81 +52,28 @@ export default function WebxdcSaveToChatDialog(props: Props) {
     }
   }
 
-  const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setQueryStr(e.target.value)
-
-  const CHATLISTITEM_CHAT_HEIGHT =
-    Number(useThemeCssVar('--SPECIAL-chatlist-item-chat-height')) || 64
-
   const title = file
     ? tx('send_file_to', file.file_name)
     : tx('send_message_to')
 
-  const noResults = chatListIds.length === 0 && queryStr !== ''
   return (
-    <Dialog className={styles.sendToChatDialog} onClose={onClose} fixed>
-      <DialogHeader onClose={onClose} title={title} />
-      <DialogBody
-        className={classNames(
-          'webxdc-send-to-chat-dialog',
-          styles.sendToChatDialogBody
-        )}
-      >
-        <input
-          className='search-input no-drag'
-          onChange={onSearchChange}
-          value={queryStr}
-          placeholder={tx('search')}
-          autoFocus
-          spellCheck={false}
-        />
-        {noResults && queryStr && (
-          <PseudoListItemNoSearchResults queryStr={queryStr} />
-        )}
-        <div
-          ref={resultsRef}
-          className='results'
-          style={{ height: noResults ? '0px' : '100%' }}
-        >
-          <RovingTabindexProvider wrapperElementRef={resultsRef}>
-            <AutoSizer>
-              {({ width, height }) => (
-                <ChatListPart
-                  isRowLoaded={isChatLoaded}
-                  loadMoreRows={loadChats}
-                  rowCount={chatListIds.length}
-                  width={width}
-                  height={height}
-                  itemKey={index => 'key' + chatListIds[index]}
-                  itemHeight={CHATLISTITEM_CHAT_HEIGHT}
-                >
-                  {({ index, style }) => {
-                    const chatId = chatListIds[index]
-                    return (
-                      <div style={style}>
-                        <ChatListItem
-                          chatListItem={chatCache[chatId] || undefined}
-                          onClick={onChatClick.bind(null, chatId)}
-                        />
-                      </div>
-                    )
-                  }}
-                </ChatListPart>
-              )}
-            </AutoSizer>
-          </RovingTabindexProvider>
-        </div>
-      </DialogBody>
-      <DialogFooter>
-        <FooterActions align='start'>
-          {file && (
-            <FooterActionButton onClick={onSaveClick}>
-              {tx('save_as')}
-            </FooterActionButton>
-          )}
-        </FooterActions>
-      </DialogFooter>
-    </Dialog>
+    <SelectChat
+      headerTitle={title}
+      onChatClick={onChatClick}
+      onClose={onClose}
+      listFlags={C.DC_GCL_FOR_FORWARDING | C.DC_GCL_NO_SPECIALS}
+      footer={
+        <DialogFooter>
+          <FooterActions align='start'>
+            {file && (
+              <FooterActionButton onClick={onSaveClick}>
+                {tx('save_as')}
+              </FooterActionButton>
+            )}
+          </FooterActions>
+        </DialogFooter>
+      }
+    />
   )
 }
 
