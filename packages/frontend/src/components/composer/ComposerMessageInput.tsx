deltachat/deltachat-desktop

commit d1fbc7ac3b16b607504a333d36f6cf51180cdae3

Add throttle impl and make saveDraft use throttle (#4144)
- use throttle in saveDraft for messages
- fixes #3733

packages/frontend/src/components/composer/ComposerMessageInput.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import debounce from 'debounce'
+import { throttle } from '@deltachat-desktop/shared/util.js'
 
 import { ActionEmitter, KeybindAction } from '../../keybindings'
 import { getLogger } from '../../../../shared/logger'
@@ -29,7 +29,7 @@ export default class ComposerMessageInput extends React.Component<
   composerSize: number
   setCursorPosition: number | false
   textareaRef: React.RefObject<HTMLTextAreaElement>
-  saveDraft: () => void
+  saveDraft: ReturnType<typeof throttle>
   constructor(props: ComposerMessageInputProps) {
     super(props)
     this.state = {
@@ -46,10 +46,11 @@ export default class ComposerMessageInput extends React.Component<
     this.insertStringAtCursorPosition =
       this.insertStringAtCursorPosition.bind(this)
 
-    this.saveDraft = debounce(() => {
-      const { text, chatId } = this.state
-      this.props.updateDraftText(text.trim() === '' ? '' : text, chatId)
-    }, 1000)
+    this.saveDraft = throttle((text, chatId) => {
+      if (this.state.chatId === chatId) {
+        this.props.updateDraftText(text.trim() === '' ? '' : text, chatId)
+      }
+    }, 400)
 
     this.textareaRef = React.createRef()
     this.focus = this.focus.bind(this)
@@ -122,19 +123,13 @@ export default class ComposerMessageInput extends React.Component<
     }
     if (prevState.chatId === this.state.chatId) {
       this.resizeTextareaAndComposer()
-      if (prevState.text !== this.state.text) {
-        if (!this.state.loadingDraft) {
-          this.saveDraft()
-        }
-      }
     }
   }
 
   onChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
-    this.setState({ text: e.target.value /*error: false*/ })
-    if (!this.state.loadingDraft) {
-      this.saveDraft()
-    }
+    const text = e.target.value
+    this.setState({ text /*error: false*/ })
+    this.saveDraft(text, this.state.chatId)
   }
 
   keyEventToAction(e: React.KeyboardEvent<HTMLTextAreaElement>) {

packages/shared/util.ts
@@ -14,3 +14,28 @@ export function truncateText(text: string, max_len: number) {
 export function isInviteLink(url: string) {
   return url.startsWith('https://i.delta.chat/') && url.includes('#')
 }
+
+export function throttle<R, A extends any[]>(
+  fn: (...args: A) => R,
+  wait: number
+) {
+  let inThrottle: boolean,
+    timeout: ReturnType<typeof setTimeout>,
+    lastTime: number
+  return (...args: A) => {
+    if (!inThrottle) {
+      fn(...args)
+      lastTime = performance.now()
+      inThrottle = true
+    } else {
+      clearTimeout(timeout)
+      timeout = setTimeout(
+        () => {
+          fn(...args)
+          lastTime = performance.now()
+        },
+        Math.max(wait - (performance.now() - lastTime), 0)
+      )
+    }
+  }
+}

commit cabea32784ef46f2de49e9d4d59a569f893b3e3a

fix: draft not saving after inserting emoji
The bug was introduced in https://github.com/deltachat/deltachat-desktop/pull/4144.
See https://github.com/deltachat/deltachat-desktop/pull/4144#discussion_r1861963395.

packages/frontend/src/components/composer/ComposerMessageInput.tsx
@@ -217,6 +217,7 @@ export default class ComposerMessageInput extends React.Component<
     this.setCursorPosition = textareaElem.selectionStart + str.length
 
     this.setState({ text: updatedText })
+    this.throttledSaveDraft(updatedText, this.state.chatId)
   }
 
   render() {