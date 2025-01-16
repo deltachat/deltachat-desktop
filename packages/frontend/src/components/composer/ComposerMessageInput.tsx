deltachat/deltachat-desktop

commit d1fbc7ac3b16b607504a333d36f6cf51180cdae3

Add throttle impl and make saveDraft use throttle (#4144)
- use throttle in saveDraft for messages
- fixes #3733

packages/frontend/src/components/composer/ComposerMessageInput.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import debounce from 'debounce'
+import { throttle } from '@deltachat-desktop/shared/util.js'
 
 import { ActionEmitter, KeybindAction } from '../../keybindings'
 import { getLogger } from '../../../../shared/logger'
@@ -29,7 +29,7 @@ export default class ComposerMessageInput extends React.Component<
   composerSize: number
   setCursorPosition: number | false
   textareaRef: React.RefObject<HTMLTextAreaElement>
-  saveDraft: () => void
+  saveDraft: ReturnType<typeof throttle>
   constructor(props: ComposerMessageInputProps) {
     super(props)
     this.state = {
@@ -46,10 +46,11 @@ export default class ComposerMessageInput extends React.Component<
     this.insertStringAtCursorPosition =
       this.insertStringAtCursorPosition.bind(this)
 
-    this.saveDraft = debounce(() => {
-      const { text, chatId } = this.state
-      this.props.updateDraftText(text.trim() === '' ? '' : text, chatId)
-    }, 1000)
+    this.saveDraft = throttle((text, chatId) => {
+      if (this.state.chatId === chatId) {
+        this.props.updateDraftText(text.trim() === '' ? '' : text, chatId)
+      }
+    }, 400)
 
     this.textareaRef = React.createRef()
     this.focus = this.focus.bind(this)
@@ -122,19 +123,13 @@ export default class ComposerMessageInput extends React.Component<
     }
     if (prevState.chatId === this.state.chatId) {
       this.resizeTextareaAndComposer()
-      if (prevState.text !== this.state.text) {
-        if (!this.state.loadingDraft) {
-          this.saveDraft()
-        }
-      }
     }
   }
 
   onChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
-    this.setState({ text: e.target.value /*error: false*/ })
-    if (!this.state.loadingDraft) {
-      this.saveDraft()
-    }
+    const text = e.target.value
+    this.setState({ text /*error: false*/ })
+    this.saveDraft(text, this.state.chatId)
   }
 
   keyEventToAction(e: React.KeyboardEvent<HTMLTextAreaElement>) {

packages/shared/util.ts
@@ -14,3 +14,28 @@ export function truncateText(text: string, max_len: number) {
 export function isInviteLink(url: string) {
   return url.startsWith('https://i.delta.chat/') && url.includes('#')
 }
+
+export function throttle<R, A extends any[]>(
+  fn: (...args: A) => R,
+  wait: number
+) {
+  let inThrottle: boolean,
+    timeout: ReturnType<typeof setTimeout>,
+    lastTime: number
+  return (...args: A) => {
+    if (!inThrottle) {
+      fn(...args)
+      lastTime = performance.now()
+      inThrottle = true
+    } else {
+      clearTimeout(timeout)
+      timeout = setTimeout(
+        () => {
+          fn(...args)
+          lastTime = performance.now()
+        },
+        Math.max(wait - (performance.now() - lastTime), 0)
+      )
+    }
+  }
+}

commit 8062d2ba354187012efb972199191d9d6fa26ac6

fix: draft not clearing after sending message
Closes https://github.com/deltachat/deltachat-desktop/issues/4430.

The bug was introduced in https://github.com/deltachat/deltachat-desktop/pull/4144.
There we made `throttle` accept the `text` argument
instead of reading `this.state.text`, and `this.state.text`
would get set to `''` after the message has been sent,
but then we'd still invoke the throttled function,
with the text from the last `onChange` event.

This probably doesn't address _all_ the "draft not cleared" issues,
e.g. https://github.com/deltachat/deltachat-desktop/issues/3586
could be a different issue.

packages/frontend/src/components/composer/ComposerMessageInput.tsx
@@ -49,6 +49,11 @@ export default class ComposerMessageInput extends React.Component<
     this.insertStringAtCursorPosition =
       this.insertStringAtCursorPosition.bind(this)
 
+    // Remember that the draft might be updated from the outside
+    // of this component (with the `setText` method,
+    // e.g. when the draft gets cleared after sending a message).
+    // This can happen _after_ an `onChange` event but _before_
+    // the unrelying throttled function invokation.
     this.throttledSaveDraft = throttle((text, chatId) => {
       if (this.state.chatId === chatId) {
         this.props.updateDraftText(text.trim() === '' ? '' : text, chatId)
@@ -77,8 +82,16 @@ export default class ComposerMessageInput extends React.Component<
     return null
   }
 
+  /**
+   * Sets the text area value, and ensures that `updateDraftText`
+   * does not get invoked until the next change to the draft text.
+   *
+   * Useful for setting / clearing draft text afer loading it from core,
+   * e.g. after sending the message or opening a chat with a.
+   */
   setText(text: string | null) {
     this.setState({ text: text || '', loadingDraft: false })
+    this.throttledSaveDraft.cancel()
   }
 
   setComposerSize(size: number) {

packages/shared/util.ts
@@ -22,7 +22,7 @@ export function throttle<R, A extends any[]>(
   let inThrottle: boolean,
     timeout: ReturnType<typeof setTimeout>,
     lastTime: number
-  return (...args: A) => {
+  const ret = (...args: A) => {
     if (!inThrottle) {
       fn(...args)
       lastTime = performance.now()
@@ -38,4 +38,8 @@ export function throttle<R, A extends any[]>(
       )
     }
   }
+  ret.cancel = () => {
+    clearTimeout(timeout)
+  }
+  return ret
 }