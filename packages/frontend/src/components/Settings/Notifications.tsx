ec212d3043d47dd5fcf8409e23c738168c5e51b5
Nico de Haen <ndh@deltachat.de>
2025-01-23 10:24:15 +0100

    fix: adapt notification settings
    - disable dependendant settings
    - switch order All profiles => Current profile
    - mark "disabled" switches with opacity: 0.4

--- packages/frontend/src/components/Settings/Notifications.tsx
+++ packages/frontend/src/components/Settings/Notifications.tsx
@@ -17,18 +17,32 @@ type Props = {
 
 export default function Notifications({ desktopSettings }: Props) {
   const tx = useTranslationFunction()
+  const accountId = selectedAccountId()
+  const { accounts } = useAccountNotificationStore()[0]!
+  const isMuted = accounts[accountId]?.muted || false
+
+  function AccountNotificationMutedSwitch({
+    label,
+    description,
+  }: {
+    label: string
+    description?: string
+  }) {
+    return (
+      <SettingsSwitch
+        label={label}
+        value={isMuted}
+        description={description}
+        disabled={!desktopSettings['notifications']}
+        onChange={() => {
+          AccountNotificationStoreInstance.effect.setMuted(accountId, !isMuted)
+        }}
+      />
+    )
+  }
 
   return (
     <>
-      <SettingsHeading>{tx('pref_current_account')}</SettingsHeading>
-      <AccountNotificationMutedSwitch label={tx('menu_mute')} />
-      <DesktopSettingsSwitch
-        settingsKey='isMentionsEnabled'
-        label={tx('pref_mention_notifications')}
-        description={tx('pref_mention_notifications_explain')}
-        disabled={!desktopSettings['notifications']}
-      />
-      <SettingsSeparator></SettingsSeparator>
       <SettingsHeading>{tx('pref_all_accounts')}</SettingsHeading>
       <DesktopSettingsSwitch
         settingsKey='notifications'
@@ -39,29 +53,15 @@ export default function Notifications({ desktopSettings }: Props) {
         label={tx('pref_show_notification_content_explain')}
         disabled={!desktopSettings['notifications']}
       />
+      <SettingsSeparator></SettingsSeparator>
+      <SettingsHeading>{tx('pref_current_account')}</SettingsHeading>
+      <AccountNotificationMutedSwitch label={tx('menu_mute')} />
+      <DesktopSettingsSwitch
+        settingsKey='isMentionsEnabled'
+        label={tx('pref_mention_notifications')}
+        description={tx('pref_mention_notifications_explain')}
+        disabled={isMuted || !desktopSettings['notifications']}
+      />
     </>
-  )
-}
-
-function AccountNotificationMutedSwitch({
-  label,
-  description,
-}: {
-  label: string
-  description?: string
-}) {
-  const accountId = selectedAccountId()
-  const { accounts } = useAccountNotificationStore()[0]!
-  const isMuted = accounts[accountId]?.muted || false
-
-  return (
-    <SettingsSwitch
-      label={label}
-      value={isMuted}
-      description={description}
-      onChange={() => {
-        AccountNotificationStoreInstance.effect.setMuted(accountId, !isMuted)
-      }}
-    />
   )
 }
