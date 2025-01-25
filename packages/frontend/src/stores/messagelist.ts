deltachat/deltachat-desktop

commit 7a968b68cd0c6d586e4554f47dbf0f628555595a

fix: chat scrolling up on quoting sometimes
Closes https://github.com/deltachat/deltachat-desktop/issues/3763
(again).
See https://github.com/deltachat/deltachat-desktop/issues/3763#issuecomment-2602630507.
Previous supposed fix: https://github.com/deltachat/deltachat-desktop/pull/4119.

Apparently the bug came back after the recent React upgrade.

packages/frontend/src/stores/messagelist.ts
@@ -938,6 +938,24 @@ class MessageListStore extends Store<MessageListState> {
             return
           }
         } else {
+          // The draft message does not affect the return value of
+          // `getMessageListItems()`.
+          // The main purpose of this check is not just reduced resource usage,
+          // but to fix the messages list "scrolling up"
+          // when you quote a message. See
+          // https://github.com/deltachat/deltachat-desktop/issues/3763#issuecomment-2602630507
+          //
+          // A more correct solution would perhaps be to reduce the delay
+          // between `getLastKnownScrollPosition()` and the actual scroll,
+          // perhaps by moving `getLastKnownScrollPosition()`
+          // to the render function of `MessageList`.
+          if (
+            (await BackendRemote.rpc.getMessage(this.accountId, messageId))
+              .state === C.DC_STATE_OUT_DRAFT
+          ) {
+            return
+          }
+
           this.log.debug(
             'DC_EVENT_MSGS_CHANGED',
             'changed message seems to be a new message, refetching messageIds'

commit 5b2901ac18354c1f149b64928c1afbd97e7b1679

fix: `onEventMessagesChanged`: handle `msgId == 0`
This sometimes causes the message list to be empty
when the chat gets opened, and not update on some events.

Reproduction steps are roughly as follows:
1. Create two accounts
2. On Alice, send ~20 messages to Bob.
3. Switch to Bob's account and open the chat with Alice.
  Do not scroll to bottom.
4. Switch to Alice account and send another message to Bob.
5. Switch to Bob's account and open the chat with Alice.

You will see the `Failed to load message Msg#0 for account` error
in the console and the chat will be empty.

The bug was introduced in 7a968b68cd0c6d586e4554f47dbf0f628555595a
(https://github.com/deltachat/deltachat-desktop/pull/4529).

The added `messageId > C.DC_MSG_ID_LAST_SPECIAL` in the first `if`
is purely for consistency and performance.
It's the second one that matters.

packages/frontend/src/stores/messagelist.ts
@@ -922,6 +922,7 @@ class MessageListStore extends Store<MessageListState> {
     onEventMessagesChanged: this.scheduler.queuedEffect(
       async (messageId: number) => {
         if (
+          messageId > C.DC_MSG_ID_LAST_SPECIAL &&
           this.state.messageListItems.findIndex(
             m => m.kind === 'message' && m.msg_id === messageId
           ) !== -1
@@ -954,6 +955,7 @@ class MessageListStore extends Store<MessageListState> {
           // perhaps by moving `getLastKnownScrollPosition()`
           // to the render function of `MessageList`.
           if (
+            messageId > C.DC_MSG_ID_LAST_SPECIAL &&
             (await BackendRemote.rpc.getMessage(this.accountId, messageId))
               .state === C.DC_STATE_OUT_DRAFT
           ) {