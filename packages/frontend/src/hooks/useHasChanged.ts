cf8bab7faa6f1d78f207d0c10a92cb17a46075a0
WofWca <wofwca@protonmail.com>
2025-01-25 18:00:57 +0400

    perf: add useHasChanged2 (no `useEffect`)
    Reduce the amount of `MessageListInner` re-renders,
    as a fist step of using this function.
    
    `useHasChanged2` is faster thanks to not using `useEffect`,
    i.e. it doesn't cause an extra re-render. See
    https://react.dev/learn/you-might-not-need-an-effect#adjusting-some-state-when-a-prop-changes
    
    Supersedes https://github.com/deltachat/deltachat-desktop/pull/4164.
    
    In case you're wondering, yes, `usePrevious2` is currently unused.

--- packages/frontend/src/hooks/useHasChanged.ts
+++ packages/frontend/src/hooks/useHasChanged.ts
@@ -1,4 +1,5 @@
-import usePrevious from './usePrevious'
+import { useRef } from 'react'
+import { usePrevious } from './usePrevious'
 
 /**
  * Helper hook returning true if an observed prop- or state value
@@ -11,7 +12,30 @@ import usePrevious from './usePrevious'
  * - https://legacy.reactjs.org/docs/hooks-faq.html#how-to-get-the-previous-props-or-state
  * - https://stackoverflow.com/questions/53446020/how-to-compare-oldvalues-and-newvalues-on-react-hooks-useeffect
  */
-export default function useHasChanged(val: any) {
+export function useHasChanged(val: any) {
   const prevVal = usePrevious(val)
   return prevVal !== val
+}
+
+/**
+ * Like {@link useHasChanged}, but the returned value
+ * can only be `true` once per render function execution
+ * (i.e. one hook execution) per one change to `val`.
+ * It does not remain being `true` until the actual render of the commponent,
+ * like it is for `useHasChanged`.
+ * Use this if you need to adjust some state based on some other state,
+ * without causing a re-render that you'd get when using `useEffect`.
+ *
+ * It is this pattern:
+ * https://react.dev/learn/you-might-not-need-an-effect#adjusting-some-state-when-a-prop-changes
+ */
+export function useHasChanged2(val: unknown, trueOnFirstRun = true): boolean {
+  // `Symbol()` ensures that `val` is never equal to `prev.current`
+  // on first run.
+  const prev = useRef(trueOnFirstRun ? Symbol() : val)
+  if (prev.current !== val) {
+    prev.current = val
+    return true
+  }
+  return false
 }
