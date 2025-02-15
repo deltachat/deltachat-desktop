deltachat/deltachat-desktop

commit 1a752fa56918857c8f27d2ea9d52000b995f0b8f

perf: fewer `MessageList` re-renders
Don't re-render it on unread count change.
Namely, when scrolling through unread messages.

packages/frontend/src/components/message/MessageList.tsx
@@ -147,8 +147,6 @@ export default function MessageList({ accountId, chat, refComposer }: Props) {
   } = useMessageList(accountId, chat.id)
   const { hideReactionsBar, isReactionsBarShown } = useReactionsBar()
 
-  const countUnreadMessages = useUnreadCount(accountId, chat)
-
   const messageListRef = useRef<HTMLDivElement | null>(null)
   const [showJumpDownButton, setShowJumpDownButton] = useState(false)
 
@@ -674,7 +672,8 @@ export default function MessageList({ accountId, chat, refComposer }: Props) {
       />
       {showJumpDownButton && (
         <JumpDownButton
-          countUnreadMessages={countUnreadMessages}
+          accountId={accountId}
+          chat={chat}
           jumpToMessage={jumpToMessage}
           jumpToMessageStack={jumpToMessageStack}
         />
@@ -952,11 +951,13 @@ function MessageLoading({
 }
 
 function JumpDownButton({
-  countUnreadMessages,
+  accountId,
+  chat,
   jumpToMessage,
   jumpToMessageStack,
 }: {
-  countUnreadMessages: number
+  accountId: number
+  chat: Parameters<typeof useUnreadCount>[1]
   jumpToMessage: (params: {
     msgId: number | undefined
     highlight?: boolean
@@ -966,6 +967,8 @@ function JumpDownButton({
   }) => Promise<void>
   jumpToMessageStack: number[]
 }) {
+  const countUnreadMessages = useUnreadCount(accountId, chat)
+
   let countToShow: string = countUnreadMessages.toString()
   if (countUnreadMessages > 99) {
     countToShow = '99+'

commit f1873b57e64e40ad39d3cbc7677ea30f28b8ed0b

fix: "jump down" unread counter not clearing
Reproduction steps:
1. Open a chat.
2. Scroll it up.
3. Receive a message or a few in this chat.
4. Scroll down to read all the new messages.
  The counter will be updating as you read the messages, as expected.
5. Scroll up a bit.

The "jump down" button will appear again,
but with an incorrect count.

The bug was introduced in 1a752fa56918857c8f27d2ea9d52000b995f0b8f
(https://github.com/deltachat/deltachat-desktop/pull/4594).

This commit somewhat reverts it, i.e. ensures that
`useUnreadCount` does not lose its state when we start / stop
displaying the "jump down" button.

packages/frontend/src/components/message/MessageList.tsx
@@ -692,14 +692,18 @@ export default function MessageList({ accountId, chat, refComposer }: Props) {
         }
         loadMissingMessages={loadMissingMessages}
       />
-      {showJumpDownButton && (
-        <JumpDownButton
-          accountId={accountId}
-          chat={chat}
-          jumpToMessage={jumpToMessage}
-          jumpToMessageStack={jumpToMessageStack}
-        />
-      )}
+      <JumpDownButton
+        // We're using `hidden` prop instead of simply not rendering
+        // the component because it's stateful,
+        // namely, its `useUnreadCount()` is stateful
+        // (it keeps track of `chat` changes).
+        // It could show incorrect unread count otherwise.
+        hidden={!showJumpDownButton}
+        accountId={accountId}
+        chat={chat}
+        jumpToMessage={jumpToMessage}
+        jumpToMessageStack={jumpToMessageStack}
+      />
     </MessagesDisplayContext.Provider>
   )
 }
@@ -973,11 +977,13 @@ function MessageLoading({
 }
 
 function JumpDownButton({
+  hidden,
   accountId,
   chat,
   jumpToMessage,
   jumpToMessageStack,
 }: {
+  hidden: boolean
   accountId: number
   chat: Parameters<typeof useUnreadCount>[1]
   jumpToMessage: ReturnType<
@@ -986,6 +992,10 @@ function JumpDownButton({
   jumpToMessageStack: number[]
 }) {
   const countUnreadMessages = useUnreadCount(accountId, chat)
+
+  if (hidden) {
+    return null
+  }
 
   let countToShow: string = countUnreadMessages.toString()
   if (countUnreadMessages > 99) {