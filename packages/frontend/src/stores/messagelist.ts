deltachat/deltachat-desktop

commit 637497c8da528af403c33050e19f5d091b6ca479

fix: jumpToMessage flashing the bottom of new chat
That is, when you jump to a message in a different chat
(e.g. by clicking on a notification, or from message search,
or from a "private reply" quote),
we would momentarily show the new chat scrolled to bottom
prior to jumping to the desired message.

Why this is bad:

- We mark the visible messages as read.
  In this case that would be the last messages in the chat.
  But they weren't actually read, the user did not intend
  to scroll to them.
- Extra layout shifting: not great visually.
- Bad for performance. Let's just load the part of the chat
  that we need right away.

Related:
- https://github.com/deltachat/deltachat-desktop/issues/4508
- https://github.com/deltachat/deltachat-desktop/pull/4510
- https://github.com/deltachat/deltachat-desktop/pull/4554

packages/frontend/src/stores/messagelist.ts
@@ -322,6 +322,23 @@ class MessageListStore extends Store<MessageListState> {
       async () => {
         const startTime = performance.now()
 
+        // FYI there is similar code in `MessageList.tsx`.
+        if (
+          window.__internal_jump_to_message_asap?.accountId ===
+            this.accountId &&
+          window.__internal_jump_to_message_asap.chatId === this.chatId
+        ) {
+          const jumpArgs =
+            window.__internal_jump_to_message_asap.jumpToMessageArgs
+          // Not entirely sure if `queueMicrotask` (or `setTimeout`),
+          // same as below
+          queueMicrotask(() => {
+            this.effect.jumpToMessage(...jumpArgs)
+          })
+          window.__internal_jump_to_message_asap = undefined
+          return false
+        }
+
         const firstUnreadMsgIdP = BackendRemote.rpc.getFirstUnreadMessageOfChat(
           this.accountId,
           this.chatId
@@ -337,7 +354,8 @@ class MessageListStore extends Store<MessageListState> {
         if (firstUnreadMsgId !== null) {
           // Not entirely sure if `queueMicrotask` (or `setTimeout`)
           // is requred. It is to return from this `effect`
-          // prior to entering another one (`jumpToMessage()`)
+          // prior to entering another one (`jumpToMessage()`).
+          // Same as above.
           queueMicrotask(() => {
             this.effect.jumpToMessage({
               msgId: firstUnreadMsgId,

packages/frontend/src/components/message/MessageList.tsx
@@ -223,6 +223,7 @@ export default function MessageList({ accountId, chat, refComposer }: Props) {
   }, [])
 
   const maybeJumpToMessageHack = () => {
+    // FYI there is similar code in `messagelist.ts`.
     if (
       window.__internal_jump_to_message_asap?.accountId === accountId &&
       window.__internal_jump_to_message_asap.chatId === chat.id

commit 859b268af0411d3e01bcaadd90dc264680503429

fix: message list being empty on chat double click
To reproduce, simply double click on a chat in the chat list
really fast.
Or:

```javascript
item = document.querySelector(
  '.chat-list-item:not(.archive-link-item):not(.selected)'
);
chatListItem.click();
// Let React re-render, then click again.
setTimeout(() => chatListItem.click());
```

The problem is, in `selectChat` we have code that is supposed to
jump to the last message when the currently selected chat
gets clicked again.
However, if the chat has not loaded yet, and thus a new instance
of `MessageListStore` has not been created yet, and thus its
`loadChat()` has not been called yet,
then, when `loadChat()` does get called eventually,
it will see that `__internal_jump_to_message_asap` is set,
and so it will call `__jumpToMessage`, with `msgId: undefined`.
But, it turns out, `__jumpToMessage` cannot handle this case.
It would try to look up the last message in
`this.state.messageListItems`, but it is empty
(see the `items.length <= 0` line), so `__jumpToMessage()`
would simply return and do nothing, resulting in an empty chat.
But `messageListItems` is empty not because
there are really no messages in the chat,
but because `MessageListStore` has not loaded anything yet,
because, as you remember, `loadChat()` delegated its responsibility
to `__jumpToMessage()`.

This commit ensures that `__jumpToMessage()` handles the case where
`MessageListStore` has not loaded anything yet,
and where `msgId === undefined`.

It appears that the reproduction steps above is the only way
this bug can occur, because it's pretty much the only way
to have `msgId: undefined` (except for the "jump down" button).
So this commit probably doesn't fix any other current bugs.

The issue that this commit fixes was likely introduced in
https://github.com/deltachat/deltachat-desktop/pull/4562
(637497c8da528af403c33050e19f5d091b6ca479).

packages/frontend/src/stores/messagelist.ts
@@ -780,7 +780,9 @@ class MessageListStore extends Store<MessageListState> {
 
   /**
    * Loads and shows the message in the messages list.
-   * It can handle loading the message if it is missing
+   * It can handle initializing MessageListStore
+   * (loading `messageListItems` and `messageCache`, etc),
+   * loading the message if it is missing
    * from `this.state.messageCache`,
    * reloading `messageListItems` if the message is missing from there,
    * and showing the message in a chat other than `this.chatId`.
@@ -853,22 +855,6 @@ class MessageListStore extends Store<MessageListState> {
           (await BackendRemote.rpc.getMessage(accountId, jumpToMessageId))
             .chatId
       } else {
-        const items = this.state.messageListItems
-          .map(m =>
-            m.kind === 'message' ? m.msg_id : C.DC_MSG_ID_LAST_SPECIAL
-          )
-          .filter(msgId => msgId !== C.DC_MSG_ID_LAST_SPECIAL)
-
-        if (items.length <= 0) {
-          // This can happen when clicking the chat in the chat list twice,
-          // which is supposed to jump to the last message.
-
-          // Since we haven't changed `viewState`, `MessageList` won't
-          // call `unlockScroll()`, so let's unlock it now.
-          return false
-        }
-
-        jumpToMessageId = items[items.length - 1]
         // Since `jumpToMessageId` is coming from
         // `this.state.messageListItems`, it's guaranteed to belong
         // to the current chat. No need to
@@ -876,6 +862,7 @@ class MessageListStore extends Store<MessageListState> {
         chatId = chatIdPreset ?? this.chatId
         jumpToMessageStack = []
         highlight = false
+        // We will determine `jumpToMessageId` below
       }
     } else {
       const fromCache = this.state.messageCache[jumpToMessageId]
@@ -921,6 +908,16 @@ class MessageListStore extends Store<MessageListState> {
 
     let messageListItems = this.state.messageListItems
     const findMessageIndex = (): number | undefined => {
+      if (jumpToMessageId == undefined) {
+        return messageListItems.length > 0
+          ? // The last `messageListItems` item is guaranteed to be _not_
+            // a daymarker, so we can safely return it without checking
+            // `m.kind === 'message'`.
+            messageListItems.length - 1
+          : undefined
+        // Maybe it would make sense to also set `jumpToMessageId` here.
+      }
+
       const ind = messageListItems.findIndex(
         m => m.kind === 'message' && m.msg_id === jumpToMessageId
       )
@@ -956,7 +953,18 @@ class MessageListStore extends Store<MessageListState> {
     let oldestFetchedMessageListItemIndex = -1
     let newestFetchedMessageListItemIndex = -1
     let newMessageCache: MessageListState['messageCache'] = {}
-    if (messageListItems.length !== 0) {
+    let newViewState: ChatViewState
+    if (messageListItems.length === 0) {
+      if (jumpToMessageId != undefined) {
+        this.log.error(
+          `Tried to jumpToMessage ${jumpToMessageId}, but messageListItems ` +
+            `is empty. Anyways, proceeding.`
+        )
+      }
+
+      // Same as in `loadChat()`
+      newViewState = ChatViewReducer.selectChat(this.state.viewState)
+    } else {
       if (jumpToMessageIndex == undefined) {
         // To be fair, it's expected that we could jump to a message
         // that is now deleted, e.g. if it got deleted just recently
@@ -1063,6 +1071,30 @@ class MessageListStore extends Store<MessageListState> {
             newestFetchedMessageListItemIndex
           ).catch(err => this.log.error('loadMessages failed', err))) || {}
       }
+
+      if (jumpToMessageId == undefined) {
+        const item = messageListItems[jumpToMessageIndex]
+        if (item.kind !== 'message') {
+          // This should never happen, but let's write it to make
+          // TypeScript happy, and juuuuuust in case.
+          // Maybe we could refactor things, so that types guarantee this.
+          this.log.error(
+            'messageListItems[jumpToMessageIndex] is not of type "message"??',
+            item,
+            messageListItems,
+            jumpToMessageIndex
+          )
+          throw new Error()
+        }
+        jumpToMessageId = item.msg_id
+      }
+      newViewState = ChatViewReducer.jumpToMessage(
+        this.state.viewState,
+        jumpToMessageId,
+        highlight,
+        focus,
+        scrollIntoViewArg
+      )
     }
 
     this.log.debug('jumpToMessage took', performance.now() - startTime)
@@ -1075,13 +1107,7 @@ class MessageListStore extends Store<MessageListState> {
       messageListItems,
       oldestFetchedMessageListItemIndex,
       newestFetchedMessageListItemIndex: newestFetchedMessageListItemIndex,
-      viewState: ChatViewReducer.jumpToMessage(
-        this.state.viewState,
-        jumpToMessageId,
-        highlight,
-        focus,
-        scrollIntoViewArg
-      ),
+      viewState: newViewState,
       jumpToMessageStack,
     })
   }