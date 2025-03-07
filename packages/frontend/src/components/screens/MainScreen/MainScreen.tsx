deltachat/deltachat-desktop

commit 66c96d7df19f591a8c8610842952cb9f7b9ed540

fix: a11y: improve tab order (navbar)
This ensures that the list of chats follows the search bar,
and that the messages list follows the chat header.
Closes https://github.com/deltachat/deltachat-desktop/issues/4225.

This mostly moves code around, and simplifies some things
(such as replacing `width: 30%` duplication) along the way.

But it also changes the relative sizes of the "chat list" and the
"chat" (main view) sections, to a ratio of 3:8
(before the "chat list" section was 30% of the window size).

Otherwise, besides, of course, the tab order fix,
this is not supposed to affect the behavior and the look of the app.

packages/frontend/src/components/screens/MainScreen/MainScreen.tsx
@@ -1,3 +1,4 @@
+import styles from './styles.module.scss'
 import React, {
   useState,
   useRef,
@@ -11,7 +12,6 @@ import Gallery from '../../Gallery'
 import { useThreeDotMenu } from '../../ThreeDotMenu'
 import ChatList from '../../chat/ChatList'
 import { Avatar } from '../../Avatar'
-import { Navbar, NavbarGroupLeft, NavbarGroupRight } from '../../Navbar'
 import ConnectivityToast from '../../ConnectivityToast'
 import MailingListProfile from '../../dialogs/MessageListProfile'
 import SettingsStoreInstance, {
@@ -185,90 +185,34 @@ export default function MainScreen({ accountId }: Props) {
         !messageSectionShouldBeHidden ? 'chat-view-open' : ''
       }`}
     >
-      <div className='navbar-wrapper' data-tauri-drag-region>
-        <Navbar>
-          {!chatListShouldBeHidden && (
-            <NavbarGroupLeft>
-              {showArchivedChats && (
-                <>
-                  <span data-no-drag-region>
-                    <Button
-                      aria-label={tx('back')}
-                      onClick={() => setArchivedChatsSelected(false)}
-                      className='backButton'
-                    >
-                      <Icon icon='arrow-left' className='backButtonIcon'></Icon>
-                    </Button>
-                  </span>
-                  <div className='archived-chats-title' data-no-drag-region>
-                    {tx('chat_archived_chats_title')}
-                  </div>
-                </>
-              )}
-              {!showArchivedChats && (
-                <SearchInput
-                  id='chat-list-search'
-                  inputRef={searchRef}
-                  onChange={handleSearchChange}
-                  onClear={queryChatId ? () => handleSearchClear() : undefined}
-                  value={queryStr}
-                />
-              )}
-            </NavbarGroupLeft>
-          )}
-          {!messageSectionShouldBeHidden && (
-            <NavbarGroupRight>
-              {smallScreenMode && (
-                <span data-no-drag-region>
-                  <Button
-                    aria-label={tx('back')}
-                    onClick={onBackButton}
-                    className='backButton'
-                  >
-                    <Icon icon='arrow-left' className='backButtonIcon'></Icon>
-                  </Button>
-                </span>
-              )}
-              {alternativeView === 'global-gallery' && (
-                <>
-                  <div className='navbar-heading'>{tx('menu_all_media')}</div>
-                  <span className='views' />
-                </>
-              )}
-              {chatWithLinger && <ChatHeading chat={chatWithLinger} />}
-              {chatWithLinger && <ChatNavButtons />}
-              {(chatWithLinger || alternativeView === 'global-gallery') && (
-                <span
-                  style={{
-                    marginLeft: 0,
-                    marginRight: '3px',
-                    ...(threeDotMenuHidden
-                      ? { opacity: 0, pointerEvents: 'none' }
-                      : {}),
-                  }}
-                  data-no-drag-region
-                  aria-disabled={threeDotMenuHidden}
+      <section className={styles.chatListAndNavbar}>
+        <nav className={styles.chatListNavbar} data-tauri-drag-region>
+          {showArchivedChats && (
+            <>
+              <span data-no-drag-region>
+                <Button
+                  aria-label={tx('back')}
+                  onClick={() => setArchivedChatsSelected(false)}
+                  className='backButton'
                 >
-                  <Button
-                    id='three-dot-menu-button'
-                    className='navbar-button'
-                    aria-label={tx('main_menu')}
-                    onClick={onClickThreeDotMenu}
-                  >
-                    <Icon
-                      coloring='navbar'
-                      icon='more'
-                      rotation={90}
-                      size={24}
-                    />
-                  </Button>
-                </span>
-              )}
-            </NavbarGroupRight>
+                  <Icon icon='arrow-left' className='backButtonIcon'></Icon>
+                </Button>
+              </span>
+              <div className='archived-chats-title' data-no-drag-region>
+                {tx('chat_archived_chats_title')}
+              </div>
+            </>
           )}
-        </Navbar>
-      </div>
-      <div className='main-app-content'>
+          {!showArchivedChats && (
+            <SearchInput
+              id='chat-list-search'
+              inputRef={searchRef}
+              onChange={handleSearchChange}
+              onClear={queryChatId ? () => handleSearchClear() : undefined}
+              value={queryStr}
+            />
+          )}
+        </nav>
         <ChatList
           queryStr={queryStr}
           showArchivedChats={showArchivedChats}
@@ -280,13 +224,60 @@ export default function MainScreen({ accountId }: Props) {
             setQueryChatId(null)
           }}
         />
+      </section>
+      <section className={styles.chatAndNavbar}>
+        <nav className={styles.chatNavbar} data-tauri-drag-region>
+          {smallScreenMode && (
+            <span data-no-drag-region>
+              <Button
+                aria-label={tx('back')}
+                onClick={onBackButton}
+                className='backButton'
+              >
+                <Icon icon='arrow-left' className='backButtonIcon'></Icon>
+              </Button>
+            </span>
+          )}
+          <div className={styles.chatNavbarHeadingWrapper}>
+            {alternativeView === 'global-gallery' && (
+              <>
+                <div className='navbar-heading'>{tx('menu_all_media')}</div>
+                <span className='views' />
+              </>
+            )}
+            {chatWithLinger && <ChatHeading chat={chatWithLinger} />}
+          </div>
+          {chatWithLinger && <ChatNavButtons />}
+          {(chatWithLinger || alternativeView === 'global-gallery') && (
+            <span
+              style={{
+                marginLeft: 0,
+                marginRight: '3px',
+                ...(threeDotMenuHidden
+                  ? { opacity: 0, pointerEvents: 'none' }
+                  : {}),
+              }}
+              data-no-drag-region
+              aria-disabled={threeDotMenuHidden}
+            >
+              <Button
+                id='three-dot-menu-button'
+                className='navbar-button'
+                aria-label={tx('main_menu')}
+                onClick={onClickThreeDotMenu}
+              >
+                <Icon coloring='navbar' icon='more' rotation={90} size={24} />
+              </Button>
+            </span>
+          )}
+        </nav>
         <MessageListView
           accountId={accountId}
           alternativeView={alternativeView}
           galleryRef={galleryRef}
           onUpdateGalleryView={updatethreeDotMenuHidden}
         />
-      </div>
+      </section>
       {!chatListShouldBeHidden && <ConnectivityToast />}
     </div>
   )

packages/frontend/src/components/screens/MainScreen/styles.module.scss
@@ -1,0 +1,115 @@
+@import '../../../../scss/mixins';
+
+// FYI there is also `_main_screen.scss`.
+
+:global(.main-screen.small-screen.chat-view-open) {
+  .chatListAndNavbar {
+    display: none;
+  }
+}
+:global(.main-screen.small-screen:not(.chat-view-open)) {
+  .chatAndNavbar {
+    display: none;
+  }
+}
+
+@mixin navbar {
+  height: var(--navBarHeight);
+  background-color: var(--navBarBackground);
+  color: var(--navBarText);
+  border-bottom: var(--outlineProperties);
+  // Just in case the "main" section below the navbar decides
+  // to do something like `height: 100%` instead of `flex-grow: 1`.
+  flex-shrink: 0;
+}
+
+.chatListAndNavbar {
+  display: flex;
+  flex-direction: column;
+
+  // See below, `.chatAndNavbar`'s flex-grow.
+  width: 0;
+  flex-grow: 3;
+
+  min-width: 295px;
+
+  .chatListNavbar {
+    @include navbar;
+    display: flex;
+    align-items: center;
+    padding: 0px 10px;
+
+    .archived-chats-title {
+      flex: 1 1 auto;
+      overflow-x: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      font-size: 18px;
+    }
+  }
+}
+.chatAndNavbar {
+  // Otherwise e.g. the contents of the gallery view
+  // (tabs, sepcifically) will refuse to shrink,
+  // and this element (`.chatAndNavbar`) will overflow the parent.
+  // Also this ensures that `.chatListAndNavbar` and `.chatAndNavbar`
+  // take respectively 3 and 8 parts of the width of the parent
+  // on wide screens.
+  width: 0;
+  flex-grow: 8;
+
+  display: flex;
+  flex-direction: column;
+
+  .chatNavbar {
+    @include navbar;
+    display: flex;
+    align-items: center;
+    padding: 0px;
+
+    .chatNavbarHeadingWrapper {
+      // Otherwise it will refuse to truncate the chat name
+      // and will make the parent overflow instead.
+      width: 0;
+      flex-grow: 1;
+    }
+    :global(.navbar-heading--button) {
+      @include button-reset;
+    }
+    :global(.navbar-heading) {
+      // See `.chatNavbarHeadingWrapper`.
+      max-width: 100%;
+
+      display: flex;
+      align-items: center;
+      padding-left: 5px;
+      margin-right: 0px;
+      overflow-x: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      font-size: 18px;
+      font-weight: bold;
+      overflow: hidden;
+    }
+
+    :global(.views) {
+      margin-right: 15px;
+      display: flex;
+      height: 100%;
+    }
+
+    :global(.navbar-button) {
+      padding: 0;
+      width: 40px;
+      border-radius: 0;
+      &:hover {
+        background-color: var(--navBarButtonHover);
+      }
+    }
+
+    :global(#three-dot-menu-button) {
+      height: 40px;
+      border-radius: 100%;
+    }
+  }
+}

packages/frontend/src/components/Navbar/styles.module.scss
@@ -1,35 +1,0 @@
-.navbar {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  background-color: var(--navBarBackground);
-  color: var(--navBarText);
-  border-bottom: var(--outlineProperties);
-}
-
-.navbarGroupLeft {
-  width: 30%;
-  height: var(--navBarHeight);
-  display: flex;
-  align-items: center;
-  padding: 0px 10px;
-  min-width: 295px; /* don't forget to update .chat-list */
-}
-
-.navbarGroupRight {
-  // Otherwise it will refuse to truncate the chat name
-  // and will overflow instead.
-  width: 0;
-  flex-grow: 1;
-
-  height: var(--navBarHeight);
-  display: flex;
-  align-items: center;
-  padding: 0px;
-}
-
-:global(.small-screen) {
-  .navbarGroupLeft {
-    flex-grow: 1;
-  }
-}

packages/frontend/scss/chat/_chat-list.scss
@@ -1,8 +1,6 @@
 .chat-list {
   z-index: $z-index-new-local-scope; // needed for .floating-action-button
-  width: 30%;
-  min-width: 295px; /* don't forget to update navbar, .navbarGroupLeft */
-  height: 100%;
+  flex-grow: 1;
   overflow-y: auto;
   box-shadow:
     0 0 4px 1px rgba(16, 22, 26, 0.1),

packages/frontend/scss/gallery/_media.scss
@@ -1,12 +1,11 @@
 $tab-height: 46px;
 
 .media-view {
-  width: calc(100% - var(--chat-list-width, 30%));
   background-color: var(--galleryBg);
   padding-left: 10px;
   overflow: hidden;
   min-width: 200px;
-  height: 100%;
+  flex-grow: 1;
   display: flex;
   flex-direction: column;
 

packages/frontend/scss/main_screen/_main_screen.scss
@@ -1,7 +1,8 @@
+// FYI there is also `MainScreen/styles.module.scss`.
+
 .main-screen {
   flex-grow: 1;
   display: flex;
-  flex-direction: column;
   position: relative;
   overflow: hidden;
 
@@ -14,32 +15,6 @@
     &:hover {
       background-color: rgba(143, 153, 168, 0.15);
     }
-  }
-
-  &.small-screen {
-    .message-list-and-composer,
-    .chat-list,
-    .media-view {
-      width: unset;
-      flex-grow: 1;
-    }
-
-    &.chat-view-open {
-      .chat-list {
-        display: none;
-      }
-    }
-
-    &:not(.chat-view-open) {
-      .message-list-and-composer {
-        display: none;
-      }
-    }
-  }
-
-  .main-app-content {
-    display: flex;
-    flex-grow: 1;
   }
 
   .navbar-chat-name {

packages/frontend/scss/main_screen/_navbar_wrapper.scss
@@ -1,48 +1,0 @@
-.navbar-wrapper {
-  position: relative;
-
-  .navbar-heading--button {
-    @include button-reset;
-  }
-  .navbar-heading {
-    display: flex;
-    align-items: center;
-    padding-left: 5px;
-    margin-right: 0px;
-    overflow-x: hidden;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    font-size: 18px;
-    font-weight: bold;
-    overflow: hidden;
-  }
-
-  span.views {
-    margin-left: auto;
-    margin-right: 15px;
-    display: flex;
-  }
-
-  .navbar-button {
-    padding: 0;
-    width: 40px;
-    height: 50px;
-    border-radius: 0;
-    &:hover {
-      background-color: var(--navBarButtonHover);
-    }
-  }
-
-  .archived-chats-title {
-    flex: 1 1 auto;
-    overflow-x: hidden;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    font-size: 18px;
-  }
-
-  #three-dot-menu-button {
-    height: 40px;
-    border-radius: 100%;
-  }
-}

packages/frontend/scss/manifest.scss
@@ -43,7 +43,6 @@
 
 // MainScreen
 @import 'main_screen/main_screen';
-@import 'main_screen/navbar_wrapper';
 
 // Message
 @import 'message/_message-attachment';

commit 1038a3d6c82a542e8d105691ee9780848b9c0434

style: fix "Archived Chats" title ugliness
This is a regression introduced in
66c96d7df19f591a8c8610842952cb9f7b9ed540
(https://github.com/deltachat/deltachat-desktop/pull/4672).

packages/frontend/src/components/screens/MainScreen/MainScreen.tsx
@@ -198,7 +198,7 @@ export default function MainScreen({ accountId }: Props) {
                   <Icon icon='arrow-left' className='backButtonIcon'></Icon>
                 </Button>
               </span>
-              <div className='archived-chats-title' data-no-drag-region>
+              <div className={styles.archivedChatsTitle} data-no-drag-region>
                 {tx('chat_archived_chats_title')}
               </div>
             </>

packages/frontend/src/components/screens/MainScreen/styles.module.scss
@@ -39,7 +39,7 @@
     align-items: center;
     padding: 0px 10px;
 
-    .archived-chats-title {
+    .archivedChatsTitle {
       flex: 1 1 auto;
       overflow-x: hidden;
       white-space: nowrap;