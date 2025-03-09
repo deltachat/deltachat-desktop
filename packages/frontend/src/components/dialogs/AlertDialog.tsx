06d9b80da9ecea7a92706d6e34458803abab42b8
Nico de Haen <ndh@deltachat.de>
2025-03-09 19:59:41 +0100

    Alert ok button as secondary action

--- packages/frontend/src/components/dialogs/AlertDialog.tsx
+++ packages/frontend/src/components/dialogs/AlertDialog.tsx
@@ -39,7 +39,7 @@ export default function AlertDialog({
       </DialogBody>
       <DialogFooter>
         <FooterActions>
-          <FooterActionButton styling='primary' onClick={onClick}>
+          <FooterActionButton styling='secondary' onClick={onClick}>
             {okBtnLabel || tx('ok')}
           </FooterActionButton>
         </FooterActions>
