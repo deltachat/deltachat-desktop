e6ecb662b48d3185d528d545e9c596e128980c16
SimonLaux <mobile.info@simonlaux.de>
2025-03-13 16:31:57 +0100

    tauri: fix open logfile from settings, fix opening stickerfolder, fix opening weblinks

---
+++ packages/target-tauri/src-tauri/src/runtime_capabilities.rs
@@ -0,0 +1,45 @@
+use std::{path::PathBuf, str::FromStr};
+
+use anyhow::Context;
+use serde::Serialize;
+use tauri::{ipc::CapabilityBuilder, AppHandle, Manager};
+
+use crate::{AppState, DeltaChatAppState};
+
+#[derive(Serialize)]
+struct OpenerPluginPathEntry {
+    path: String,
+}
+
+pub(crate) fn add_runtime_capabilies(app: &AppHandle) -> anyhow::Result<()> {
+    let current_log_file_path = app.state::<AppState>().current_log_file_path.clone();
+    let log_file = PathBuf::from_str(&current_log_file_path)?;
+    let log_folder = log_file.parent().expect("parent folder of logfile exists");
+
+    let accounts_dir = app.state::<DeltaChatAppState>().accounts_dir.clone();
+
+    let sticker_folders = format!("{accounts_dir}/*/stickers");
+
+    app.add_capability(
+        CapabilityBuilder::new("open-paths")
+            .window("main")
+            .permission_scoped(
+                "opener:allow-open-path",
+                vec![
+                    OpenerPluginPathEntry {
+                        path: log_file.to_str().context("string conversion")?.to_owned(),
+                    },
+                    OpenerPluginPathEntry {
+                        path: log_folder.to_str().context("string conversion")?.to_owned(),
+                    },
+                    // TODO: tmp folder
+                    OpenerPluginPathEntry {
+                        path: sticker_folders,
+                    },
+                ],
+                vec![],
+            ),
+    )?;
+
+    Ok(())
+}
