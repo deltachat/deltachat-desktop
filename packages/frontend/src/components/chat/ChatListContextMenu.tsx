2ab09969e865631cbc64e8ba57e80324e8018c7f
B. Petersen <r10s@b44t.com>
2025-03-17 17:57:10 +0100

    'mute for 2 hours' is deprecated, see recent discussions

--- packages/frontend/src/components/chat/ChatListContextMenu.tsx
+++ packages/frontend/src/components/chat/ChatListContextMenu.tsx
@@ -171,19 +171,6 @@ export function useChatListContextMenu(): {
                       },
                     },
                     {
-                      label: tx('mute_for_two_hours'),
-                      action: () => {
-                        BackendRemote.rpc.setChatMuteDuration(
-                          accountId,
-                          chatListItem.id,
-                          {
-                            kind: 'Until',
-                            duration: Timespans.ONE_HOUR_IN_SECONDS * 2,
-                          }
-                        )
-                      },
-                    },
-                    {
                       label: tx('mute_for_eight_hours'),
                       action: () => {
                         BackendRemote.rpc.setChatMuteDuration(
