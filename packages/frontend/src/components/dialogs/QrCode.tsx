ba590b85cf1279dc4f70e6172030ac88313e669d
Nico de Haen <ndh@deltachat.de>
2025-03-18 09:58:29 +0100

    chore: fix false eslint error

--- packages/frontend/src/components/dialogs/QrCode.tsx
+++ packages/frontend/src/components/dialogs/QrCode.tsx
@@ -20,6 +20,10 @@ import { qrCodeFromClipboard } from '../QrReader/helper'
 
 import { BackendRemote } from '../../backend-com'
 import { getLogger } from '../../../../shared/logger'
+
+// the following line is needed to avoid a false positive from eslint
+// not capable of detecting that this variable is used later on
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { runtime } from '@deltachat-desktop/runtime-interface'
 import { ScreenContext } from '../../contexts/ScreenContext'
 import useContextMenu from '../../hooks/useContextMenu'
