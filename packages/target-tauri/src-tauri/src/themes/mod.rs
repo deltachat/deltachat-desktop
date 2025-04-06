cb7fd25a1c2ba72d12b8b50e6f8692286b6b7cb0
SimonLaux <mobile.info@simonlaux.de>
2025-04-06 17:00:11 +0200

    apply theme changes live in html window

--- packages/target-tauri/src-tauri/src/themes/mod.rs
+++ packages/target-tauri/src-tauri/src/themes/mod.rs
@@ -10,3 +10,17 @@ mod themes;
 pub(crate) fn custom_theme_dir(app: &AppHandle) -> Result<PathBuf, Error> {
     Ok(app.path().app_data_dir()?.join("custom-themes"))
 }
+
+/// Triggers a (re)load of the current theme in all the other windows that support theming
+pub(crate) fn update_theme_in_other_windows(app: &AppHandle) -> Result<(), Error> {
+    // HTML windows
+    for (_label, html_email_header_view) in app
+        .webviews()
+        .iter()
+        .filter(|(label, _webview)| label.starts_with("html-window:") && label.ends_with("-header"))
+    {
+        html_email_header_view.eval("window.updateTheme()")?;
+    }
+
+    Ok(())
+}
