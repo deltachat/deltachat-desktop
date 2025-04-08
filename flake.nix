69f49ddc4eca6488f3efd369a35b54d260f0d3b4
Sebastian Klähn <scoreplayer2000@gmail.com>
2025-04-08 09:28:34 +0200

    cleanup

--- flake.nix
+++ flake.nix
@@ -61,25 +61,20 @@
           pango
           webkitgtk_4_1
           openssl
-        ] ++ nativeBuildInputs;
-        packages = with pkgs;
-          [
-            rustToolchain
-            rust-analyzer
-            typescript-language-server
-            vscode-extensions.esbenp.prettier-vscode
-          ]
-          ++ nativeBuildInputs
-          ++ buildInputs;
+          libayatana-appindicator
+
+          # coding
+          rustToolchain
+          rust-analyzer
+          typescript-language-server
+          vscode-extensions.esbenp.prettier-vscode
+        ];
         env = {
           # Required by rust-analyzer
           RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH";
+           WEBKIT_DISABLE_COMPOSITING_MODE = 1;
         };
-            shellHook = ''
-              export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH
-              export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
-              export WEBKIT_DISABLE_COMPOSITING_MODE=1 
-            '';
       };
     });
   };
