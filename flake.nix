d2def47fdaa761dbfc986ef93a7c2215aac81948
Sebastian Klähn <scoreplayer2000@gmail.com>
2025-04-08 09:44:36 +0200

    xdg data dirs

--- flake.nix
+++ flake.nix
@@ -73,7 +73,8 @@
           # Required by rust-analyzer
           RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
           LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH";
-           WEBKIT_DISABLE_COMPOSITING_MODE = 1;
+          XDG_DATA_DIRS="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS";
+          WEBKIT_DISABLE_COMPOSITING_MODE = 1;
         };
       };
     });
