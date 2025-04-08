472122330c2e72d42a4049ca2f7cc428dcad3c4c
WofWca <wofwca@protonmail.com>
2025-04-08 09:02:41 +0000

    Add comment
    Co-authored-by: Simon Laux <Simon-Laux@users.noreply.github.com>

--- packages/target-tauri/src-tauri/src/lib.rs
+++ packages/target-tauri/src-tauri/src/lib.rs
@@ -117,7 +117,7 @@ pub fn run() {
                 tauri_plugin_window_state::Builder::new()
                     // Disabled for webxdc, because we generate a new UUID label
                     // every time we create a webxdc window,
-                    // so it doesn't make sense to store it. TODO.
+                    // so it doesn't make sense to store it. TODO (https://github.com/deltachat/deltachat-desktop/issues/4468)
                     .with_filter(|label| !label.starts_with("webxdc:"))
                     .build(),
             )
