f0e49cb8f3db94b48eb2b05a1dbd5a29ecb85a31
WofWca <wofwca@protonmail.com>
2025-04-10 15:13:09 +0400

    tauri: refactor: rewrite a helper fn with trait
    This should make it easier to use in future code.

--- packages/target-tauri/src-tauri/src/util/sanitization.rs
+++ packages/target-tauri/src-tauri/src/util/sanitization.rs
@@ -1,5 +1,10 @@
-pub fn is_alphanumeric_with_dashes_and_underscores(string: &str) -> bool {
-    string
-        .chars()
-        .all(|c| c.is_ascii_alphanumeric() || c == '-' || c == '_')
+pub trait StrExtension {
+    fn is_ascii_alphanumeric_with_dashes_and_underscores(&self) -> bool;
+}
+
+impl StrExtension for str {
+    fn is_ascii_alphanumeric_with_dashes_and_underscores(&self) -> bool {
+        self.chars()
+            .all(|c| c.is_ascii_alphanumeric() || c == '-' || c == '_')
+    }
 }
