551abd6be83e098d0ee362a32d60e10aede3281a
Nico de Haen <ndh@deltachat.de>
2025-04-13 10:55:40 +0200

    Refactoring: move ConfigureProgressDialog in own file (#4961)

--- packages/frontend/src/hooks/useInstantOnboarding.ts
+++ packages/frontend/src/hooks/useInstantOnboarding.ts
@@ -3,10 +3,7 @@ import { useCallback, useContext } from 'react'
 import useDialog from './dialog/useDialog'
 import useSecureJoin from './useSecureJoin'
 import { BackendRemote } from '../backend-com'
-import {
-  ConfigureProgressDialog,
-  defaultCredentials,
-} from '../components/LoginForm'
+import { ConfigureProgressDialog } from '../components/dialogs/ConfigureProgressDialog'
 import { DEFAULT_CHATMAIL_QR_URL } from '../components/screens/WelcomeScreen/chatmailInstances'
 import { InstantOnboardingContext } from '../contexts/InstantOnboardingContext'
 
@@ -19,6 +16,7 @@ import type {
   VerifyGroupQr,
 } from '../backend/qr'
 import AlertDialog from '../components/dialogs/AlertDialog'
+import { defaultCredentials } from '../components/LoginForm'
 
 type InstantOnboarding = {
   createInstantAccount: (accountId: number) => Promise<T.FullChat['id'] | null>
