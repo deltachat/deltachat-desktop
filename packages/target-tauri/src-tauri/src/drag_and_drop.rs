ddd8a47cad2e5f7366731bdf41eae7a2b3b53acd
Simon <git@simonlaux.de>
2025-05-14 07:54:58 +0200

    looks like explicit portal usage is not needed for drag out to work in flatpak

--- packages/target-tauri/src-tauri/src/drag_and_drop.rs
+++ packages/target-tauri/src-tauri/src/drag_and_drop.rs
@@ -11,29 +11,6 @@ pub(crate) async fn drag_file_out(
 ) -> Result<(), String> {
     let file: PathBuf = PathBuf::from(file_name.as_ref());
 
-    // #[cfg(feature = "flatpak")]
-    // {
-    //     use std::os::fd::AsFd;
-    //     let file = std::fs::File::open(file).map_err(|err| format!("can't open file {err:?}"))?;
-
-    //     let transfer = ashpd::documents::FileTransfer::new()
-    //         .await
-    //         .map_err(|err| format!("fasiled to create transfer: {err:?}"))?;
-    //     let key = transfer
-    //         .start_transfer(false, true)
-    //         .await
-    //         .map_err(|err| format!("fasiled to start transfer: {err:?}"))?;
-    //     transfer
-    //         .add_files(&key, &[file])
-    //         .await
-    //         .map_err(|err| format!("failed add file to transfer: {err:?}"))?;
-
-    //     // TODO: somehow needs to replace the file with a file transfer file
-    //     // that contains the transfer key and is of mimetype "application/vnd.portal.filetransfer
-    //     //
-    //     // seems to not be supported yet, I suggest we add another DragItem type f
-    // }
-
     use drag::DragItem;
     let preview_icon =
         drag::Image::Raw(include_bytes!("../../../../images/electron-file-drag-out.png").to_vec());
