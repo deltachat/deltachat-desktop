deltachat/deltachat-desktop

commit cc130b47ab7d9241eb980033909c36bcb63ff83a

feat: play sound on incoming msg in current chat
This is mostly to address an accessibility issue
where screen reader users are not notified of incoming messages.

Applying `aria-live` to the MessageList and thus announcing
every message's text is something that requires
a lot of consideration.
So let's settle on this simple solution for now instead.
When you hear the sound, you know that it's time to focus
the message list to go through the messages.

See
- https://github.com/deltachat/deltachat-desktop/issues/4743.
- https://github.com/deltachat/deltachat-desktop/pull/4692.

The new setting to control the volume of the sound is also present
on Delta Chat Android, except that it's only an on-off toggle.

packages/target-tauri/runtime-tauri/runtime.ts
@@ -193,6 +193,7 @@ class TauriRuntime implements Runtime {
       enableRelatedChats: false,
       galleryImageKeepAspectRatio: false,
       isMentionsEnabled: false,
+      inChatSoundsVolume: 50,
       useSystemUIFont: false,
     } satisfies Partial<DesktopSettingsType>
 

packages/frontend/src/components/Settings/Notifications.tsx
@@ -10,6 +10,10 @@ import AccountNotificationStoreInstance, {
 import { selectedAccountId } from '../../ScreenController'
 import SettingsSwitch from './SettingsSwitch'
 import SettingsHeading from './SettingsHeading'
+import SettingsSelector from './SettingsSelector'
+import SmallSelectDialog from '../SmallSelectDialog'
+import SettingsStoreInstance from '../../stores/settings'
+import useDialog from '../../hooks/dialog/useDialog'
 
 type Props = {
   desktopSettings: DesktopSettingsType
@@ -20,6 +24,44 @@ export default function Notifications({ desktopSettings }: Props) {
   const accountId = selectedAccountId()
   const { accounts } = useAccountNotificationStore()[0]!
   const isMuted = accounts[accountId]?.muted || false
+
+  const inChatSoundsVolumeOptions: Parameters<
+    typeof SmallSelectDialog
+  >[0]['values'] = [
+    ['0', tx('muted')],
+    ['0.1', '10%'],
+    ['0.2', '20%'],
+    ['0.3', '30%'],
+    ['0.4', '40%'],
+    ['0.5', '50%'],
+    ['0.6', '60%'],
+    ['0.7', '70%'],
+    ['0.8', '80%'],
+    ['0.9', '90%'],
+    ['1', '100%'],
+  ]
+  const volumeNumberToString = (volume: number) => {
+    if (volume === 0) {
+      return tx('muted')
+    }
+    return volume * 100 + '%'
+  }
+
+  const { openDialog } = useDialog()
+  const onOpenInChatSoundsVolumeDialog = async () => {
+    openDialog(SmallSelectDialog, {
+      values: inChatSoundsVolumeOptions,
+      initialSelectedValue: desktopSettings.inChatSoundsVolume.toString(10),
+      title: tx('pref_in_chat_sounds'),
+      onSave: async (volume_: string) => {
+        const volume = Number(volume_)
+        SettingsStoreInstance.effect.setDesktopSetting(
+          'inChatSoundsVolume',
+          volume
+        )
+      },
+    })
+  }
 
   return (
     <>
@@ -33,6 +75,12 @@ export default function Notifications({ desktopSettings }: Props) {
         label={tx('pref_show_notification_content_explain')}
         disabled={!desktopSettings['notifications']}
       />
+      <SettingsSelector
+        onClick={onOpenInChatSoundsVolumeDialog.bind(null)}
+        currentValue={volumeNumberToString(desktopSettings.inChatSoundsVolume)}
+      >
+        {tx('pref_in_chat_sounds')}
+      </SettingsSelector>
       <SettingsSeparator></SettingsSeparator>
       <SettingsHeading>{tx('pref_current_account')}</SettingsHeading>
       <SettingsSwitch

packages/frontend/src/stores/messagelist.ts
@@ -10,10 +10,11 @@ import {
   defaultChatViewState,
 } from './chat/chat_view_reducer'
 import { ChatStoreScheduler } from './chat/chat_scheduler'
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 import { useDebouncedCallback } from 'use-debounce'
 import { debounce } from 'debounce'
 import { getLogger } from '@deltachat-desktop/shared/logger'
+import { useSettingsStore } from './settings'
 
 const log = getLogger('messagelist')
 
@@ -58,6 +59,26 @@ export function useMessageList(accountId: number, chatId: number) {
     return store
   }, [accountId, chatId])
 
+  // perf: It's a shame that we have to re-render on settings changes
+  // even though we only depend on `volume`,
+  // but let's hope the React compiler will take care of this
+  // when it's released.
+  const settingsStore = useSettingsStore()[0]
+
+  const incomingMessageAudioElement_ = useRef<HTMLAudioElement>(null)
+  if (incomingMessageAudioElement_.current == null) {
+    incomingMessageAudioElement_.current = document.createElement('audio')
+    incomingMessageAudioElement_.current.src = './audio/sound_in.wav'
+  }
+  const incomingMessageAudioElement = incomingMessageAudioElement_.current
+  {
+    const volume = settingsStore?.desktopSettings.inChatSoundsVolume
+    if (volume != null) {
+      // Note that `volume` could be 0.
+      incomingMessageAudioElement.volume = volume
+    }
+  }
+
   useEffect(() => {
     const cleanup = [
       onDCEvent(accountId, 'MsgDelivered', ({ chatId: eventChatId, msgId }) => {
@@ -68,6 +89,12 @@ export function useMessageList(accountId: number, chatId: number) {
       onDCEvent(accountId, 'IncomingMsg', ({ chatId: eventChatId }) => {
         if (chatId === eventChatId) {
           store.effect.onEventIncomingMessage()
+
+          // Note that the element might already be playing,
+          // if we received two or more messages rapidly.
+          // In that case it could be nice to play multiple sounds in parallel.
+          incomingMessageAudioElement.currentTime = 0
+          incomingMessageAudioElement.play()
         } else {
           store.log.debug(
             `chatId of IncomingMsg event (${chatId}) doesn't match id of selected chat (${eventChatId}). Skipping.`
@@ -104,7 +131,7 @@ export function useMessageList(accountId: number, chatId: number) {
       }),
     ]
     return () => cleanup.forEach(off => off())
-  }, [accountId, chatId, store])
+  }, [accountId, chatId, incomingMessageAudioElement, store])
 
   const [state, setState] = useState(store.getState())
 

packages/shared/shared-types.d.ts
@@ -29,6 +29,16 @@ export interface DesktopSettingsType {
   notifications: boolean
   showNotificationContent: boolean
   isMentionsEnabled: boolean
+  /**
+   * Controls the volume of the sound accompanying incoming
+   * (and possibly outgoing, in the future) messages
+   * of the currently open chat.
+   * A number between 0 and 1.
+   *
+   * This is important for accessibility. We do not otherwise announce
+   * incoming messages in the current chat.
+   */
+  inChatSoundsVolume: number
   /** @deprecated isn't used anymore since the move to jsonrpc */
   lastChats: { [accountId: number]: number }
   zoomFactor: number

packages/shared/state.ts
@@ -33,6 +33,7 @@ export function getDefaultState(): DesktopSettingsType {
     useSystemUIFont: false,
     contentProtectionEnabled: false,
     isMentionsEnabled: true,
+    inChatSoundsVolume: 0.5,
     autostart: true,
   }
 }

commit c39535e31fac8a16523b7f4def32f05cca7219b5

tauri: fix: messagelist crashing due to `<audio>`
The bug was introduced in cc130b47ab7d9241eb980033909c36bcb63ff83a
(https://github.com/deltachat/deltachat-desktop/pull/5143).
It does not affect Electron.

The workaround is to go to Settings -> Notifications
and set "In-Chat Sounds" to any other value.

packages/target-tauri/runtime-tauri/runtime.ts
@@ -193,7 +193,7 @@ class TauriRuntime implements Runtime {
       enableRelatedChats: false,
       galleryImageKeepAspectRatio: false,
       isMentionsEnabled: false,
-      inChatSoundsVolume: 50,
+      inChatSoundsVolume: 0.5,
       useSystemUIFont: false,
     } satisfies Partial<DesktopSettingsType>
 