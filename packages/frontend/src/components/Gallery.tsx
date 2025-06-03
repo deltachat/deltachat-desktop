deltachat/deltachat-desktop

commit 3bc21041bfbafea2da9c297adcdffa28528e8480

Open media view (gallery) in dialog (#5149)

packages/frontend/src/components/Gallery.tsx
@@ -63,7 +63,7 @@ const MediaTabs: Readonly<{
   },
 }
 
-type Props = { chatId: number | 'all'; onUpdateView?: () => void }
+type Props = { chatId: number | 'all' }
 
 const getCurrentDocumentVerticalScrollbarWidth = () => {
   const outer = document.createElement('div')
@@ -225,7 +225,6 @@ export default class Gallery extends Component<
           loading: false,
         })
         this.forceUpdate()
-        this.props.onUpdateView?.()
       })
       .catch(log.error.bind(log))
   }

packages/frontend/src/components/screens/MainScreen/MainScreen.tsx
@@ -8,7 +8,6 @@ import React, {
 } from 'react'
 import { C } from '@deltachat/jsonrpc-client'
 
-import Gallery from '../../Gallery'
 import { useThreeDotMenu } from '../../ThreeDotMenu'
 import ChatList from '../../chat/ChatList'
 import { Avatar } from '../../Avatar'
@@ -17,7 +16,7 @@ import MailingListProfile from '../../dialogs/MessageListProfile'
 import SettingsStoreInstance, {
   useSettingsStore,
 } from '../../../stores/settings'
-import { Type } from '../../../backend-com'
+import { BackendRemote, Type } from '../../../backend-com'
 import { InlineVerifiedIcon } from '../../VerifiedIcon'
 import Button from '../../Button'
 import Icon from '../../Icon'
@@ -33,11 +32,13 @@ import useTranslationFunction from '../../../hooks/useTranslationFunction'
 import { KeybindAction } from '../../../keybindings'
 import { selectedAccountId } from '../../../ScreenController'
 import { openMapWebxdc } from '../../../system-integration/webxdc'
-import { ChatView } from '../../../contexts/ChatContext'
 import { ScreenContext } from '../../../contexts/ScreenContext'
+import MediaView from '../../dialogs/MediaView'
+import { openWebxdc } from '../../message/messageFunctions'
 
 import type { T } from '@deltachat/jsonrpc-client'
 import CreateChat from '../../dialogs/CreateChat'
+import { runtime } from '@deltachat-desktop/runtime-interface'
 
 type Props = {
   accountId?: number
@@ -52,15 +53,9 @@ export default function MainScreen({ accountId }: Props) {
   const [queryStr, setQueryStr] = useState('')
   const [queryChatId, setQueryChatId] = useState<null | number>(null)
   const [archivedChatsSelected, setArchivedChatsSelected] = useState(false)
-  const {
-    activeView,
-    chatId,
-    chatWithLinger,
-    alternativeView,
-    selectChat,
-    unselectChat,
-  } = useChat()
+  const { chatId, chatWithLinger, selectChat, unselectChat } = useChat()
   const { smallScreenMode } = useContext(ScreenContext)
+  const [lastWebxdcApps, setLastWebxdcApps] = useState<T.Message[]>([])
 
   // Small hack/misuse of keyBindingAction to setArchivedChatsSelected from
   // other components (especially ViewProfile when selecting a shared chat/group)
@@ -71,10 +66,8 @@ export default function MainScreen({ accountId }: Props) {
     setArchivedChatsSelected(false)
   )
 
-  const chatListShouldBeHidden =
-    smallScreenMode && (chatId !== undefined || alternativeView !== null)
-  const messageSectionShouldBeHidden =
-    smallScreenMode && chatId === undefined && alternativeView === null
+  const chatListShouldBeHidden = smallScreenMode && chatId !== undefined
+  const messageSectionShouldBeHidden = smallScreenMode && chatId === undefined
 
   const onBackButton = () => {
     unselectChat()
@@ -148,38 +141,46 @@ export default function MainScreen({ accountId }: Props) {
     openDialog(CreateChat)
   })
 
+  useKeyBindingAction(KeybindAction.GlobalGallery_Open, () => {
+    openDialog(MediaView, {
+      chatId: 'all',
+    })
+  })
+
+  useEffect(() => {
+    const fetchMedia = async () => {
+      const maxIcons = smallScreenMode ? 1 : 3
+      if (!accountId || !chatId) {
+        return
+      }
+      const mediaIds = await BackendRemote.rpc.getChatMedia(
+        accountId,
+        chatId,
+        'Webxdc',
+        null,
+        null
+      )
+      mediaIds.reverse() // newest first
+      const mediaLoadResult = await BackendRemote.rpc.getMessages(
+        accountId,
+        mediaIds.slice(0, maxIcons)
+      )
+      const lastMessages = Object.values(mediaLoadResult).filter(
+        result => result.kind === 'message'
+      )
+      setLastWebxdcApps(lastMessages.reverse()) // show newest first
+    }
+    if (accountId && chatId) {
+      fetchMedia()
+    }
+  }, [accountId, chatId, smallScreenMode])
+
   useEffect(() => {
     // Make sure it uses new version of settings store instance
     SettingsStoreInstance.effect.load()
   }, [])
 
-  const onClickThreeDotMenu = useThreeDotMenu(
-    chatWithLinger,
-    alternativeView === 'global-gallery' || activeView === ChatView.Media
-      ? 'gallery'
-      : 'chat'
-  )
-  const galleryRef = useRef<Gallery | null>(null)
-
-  // @TODO: This could be refactored into a context which knows about the
-  // gallery tab state
-  const [threeDotMenuHidden, setthreeDotMenuHidden] = useState(false)
-  const updatethreeDotMenuHidden = useCallback(() => {
-    setthreeDotMenuHidden(
-      (alternativeView === 'global-gallery' || activeView === ChatView.Media) &&
-        // galleryRef.current?.state.currentTab holds the
-        // previous tab when this is called (won't fix since will be
-        // obsolete after gallery updates step 2)
-        !['images', 'video'].includes(
-          galleryRef.current?.state.currentTab || ''
-        )
-    )
-  }, [activeView, alternativeView])
-
-  useEffect(() => {
-    updatethreeDotMenuHidden()
-  }, [alternativeView, galleryRef, updatethreeDotMenuHidden])
-
+  const onClickThreeDotMenu = useThreeDotMenu(chatWithLinger)
   const isSearchActive = queryStr.length > 0 || queryChatId !== null
   const showArchivedChats = !isSearchActive && archivedChatsSelected
 
@@ -248,28 +249,19 @@ export default function MainScreen({ accountId }: Props) {
             className={styles.chatNavbarHeadingWrapper}
             data-tauri-drag-region
           >
-            {alternativeView === 'global-gallery' && (
-              <>
-                <div className='navbar-heading' data-tauri-drag-region>
-                  {tx('menu_all_media')}
-                </div>
-                <span className='views' data-tauri-drag-region />
-              </>
-            )}
             {chatWithLinger && <ChatHeading chat={chatWithLinger} />}
           </div>
-          {chatWithLinger && <ChatNavButtons />}
-          {(chatWithLinger || alternativeView === 'global-gallery') && (
+          {lastWebxdcApps.length > 0 && (
+            <AppIcons accountId={accountId} apps={lastWebxdcApps} />
+          )}
+          {chatWithLinger && <ChatNavButtons chat={chatWithLinger} />}
+          {chatWithLinger && (
             <span
               style={{
                 marginLeft: 0,
                 marginRight: '3px',
-                ...(threeDotMenuHidden
-                  ? { opacity: 0, pointerEvents: 'none' }
-                  : {}),
               }}
               data-no-drag-region
-              aria-disabled={threeDotMenuHidden}
             >
               <Button
                 id='three-dot-menu-button'
@@ -283,12 +275,7 @@ export default function MainScreen({ accountId }: Props) {
             </span>
           )}
         </nav>
-        <MessageListView
-          accountId={accountId}
-          alternativeView={alternativeView}
-          galleryRef={galleryRef}
-          onUpdateGalleryView={updatethreeDotMenuHidden}
-        />
+        <MessageListView accountId={accountId} />
       </section>
       {!chatListShouldBeHidden && <ConnectivityToast />}
     </div>
@@ -400,10 +387,17 @@ function ChatHeading({ chat }: { chat: T.FullChat }) {
   )
 }
 
-function ChatNavButtons() {
+function ChatNavButtons({ chat }: { chat: T.FullChat }) {
   const tx = useTranslationFunction()
-  const { activeView, setChatView, chatId } = useChat()
+  const { chatId } = useChat()
   const settingsStore = useSettingsStore()[0]
+  const { openDialog } = useDialog()
+
+  const openMediaViewDialog = useCallback(() => {
+    openDialog(MediaView, {
+      chatId: chat.id,
+    })
+  }, [openDialog, chat])
 
   return (
     <>
@@ -415,29 +409,11 @@ function ChatNavButtons() {
       >
         <Button
           role='tab'
-          id='tab-message-list-view'
-          onClick={() => setChatView(ChatView.MessageList)}
-          active={activeView === ChatView.MessageList}
-          aria-selected={activeView === ChatView.MessageList}
-          // FYI there are 2 components that use this ID,
-          // but they're not supposed to be rendered at the same time.
-          aria-controls='message-list-and-composer'
-          aria-label={tx('chat')}
-          title={tx('chat')}
-          className='navbar-button'
-          styling='borderless'
-        >
-          <Icon coloring='navbar' icon='chats' size={18} />
-        </Button>
-        <Button
-          role='tab'
           id='tab-media-view'
-          onClick={() => setChatView(ChatView.Media)}
-          active={activeView === ChatView.Media}
-          aria-selected={activeView === ChatView.Media}
+          onClick={openMediaViewDialog}
           aria-controls='media-view'
-          aria-label={`${tx('webxdc_apps')} & ${tx('media')}`}
-          title={`${tx('webxdc_apps')} & ${tx('media')}`}
+          aria-label={tx('apps_and_media')}
+          title={tx('apps_and_media')}
           className='navbar-button'
           styling='borderless'
         >
@@ -458,5 +434,35 @@ function ChatNavButtons() {
         )}
       </span>
     </>
+  )
+}
+
+function AppIcons({
+  accountId,
+  apps,
+}: {
+  accountId: number | undefined
+  apps: T.Message[]
+}) {
+  if (!accountId || !apps || apps.length === 0) {
+    return null
+  }
+  return (
+    <div className={styles.webxdcIcons} data-no-drag-region='true'>
+      {apps.map(app => (
+        <img
+          key={app.id}
+          className={styles.webxdcIcon}
+          src={runtime.getWebxdcIconURL(accountId, app.id)}
+          alt={app.webxdcInfo?.name}
+          aria-hidden='true'
+          onClick={() => {
+            openWebxdc(app)
+          }}
+          title={app.webxdcInfo?.name}
+          aria-label={app.webxdcInfo?.name}
+        />
+      ))}
+    </div>
   )
 }

_locales/ar.xml
@@ -187,8 +187,6 @@
     <string name="pref_language">اللغة</string>
     <string name="pref_use_system_emoji">استخدام الرموز التعبيرية بالنظام</string>
     <string name="pref_use_system_emoji_explain"> Delta Chat تعطيل الرموز التعبيرية المدمجة في </string>
-    <!-- deprecated -->
-    <string name="pref_app_access">دخول البرنامج</string>
     <string name="pref_chats">المحادثات</string>
     <string name="pref_in_chat_sounds">أصوات خلال الدردش</string>
     <string name="pref_other">أخرى</string>

_locales/az.xml
@@ -317,8 +317,6 @@
     <string name="pref_manage_keys">Açar edarəetməsi</string>
     <string name="pref_use_system_emoji">Sistem emojisini istifadə edin</string>
     <string name="pref_use_system_emoji_explain">Emoji Delta chat üçün daxili dəstəyi bağlayın. </string>
-    <!-- deprecated -->
-    <string name="pref_app_access">Daxil oldu</string>
     <string name="pref_chats">Çatlar</string>
     <string name="pref_in_chat_sounds">Çatda olan səslər</string>
     <string name="pref_view_log">Log-a bax</string>
@@ -361,11 +359,6 @@
     <string name="autocrypt_send_asm_button">Autocrypt Setup Message-ı göndər</string>
     <string name="autocrypt_send_asm_explain_after">Sizin açarınız sizə göndərildi. Başqa bir cihaza keçin və mesajın ayarını açın. Siz quraşdırma kodu daxil etməlisiniz. Orada aşağıdakı rəqəmləri daxil edin. Bitirən zaman, başqa bir cihazda Autocrypt istifadə üçün hazır olacaq.</string>
     <string name="autocrypt_prefer_e2ee">Ucdan-uca şifrələmə üstünlük təşkil edir</string>
-    <!-- deprecated -->
-    <string name="autocrypt_asm_subject">Autocrypt mesaj ayarı</string>
-    <!-- deprecated -->
-    <string name="autocrypt_asm_general_body">Bu, müvəqqəti quraşdırma üçün müştərilər arasında köçürmək üçün istifadə edilən Autocrypt Setup Mesajıdır. \n\nSenfikanızı çözmək və istifadə etmək üçün autocrypt uyğun müştəri içərisində mesajı açın və yaradan cihazda göstərilən quraşdırma kodunu daxil edin.</string>
-
 
     <!-- %1$s will be replaced by the old group name, %2$s will be replaced by the new group name -->
     <string name="group_name_changed_by_you">Qrupun adı \"%1$s\" - dən \"%2$s\" - yə deyişildi məndən. </string>
@@ -421,6 +414,7 @@
     <string name="perm_required_title">İzin tələb olunur</string>
     <string name="perm_continue">Davam edin</string>
     <string name="perm_explain_access_to_camera_denied">Delta Chat şəkillər və ya video çəkmək üçün kamera icazə tələb edir, lakin bu, əbədi imtina edilmişdir. Proqramın sistem ayarlarına gedib \"İcazələr\"-i seçin və \"kamera\" izni verin.</string>
+    <!-- give the user an idea where to find the "Microphone" option. the hint is only shown if access was initially denied by the user. pick up wordings really used on the systems, but do not be overly precise: things shift around often and there are too many system we support to track every detail and click path -->
     <string name="perm_explain_access_to_mic_denied">Delta Chat yer əlavə etmək üçün yer icazə tələb edir, lakin daimi olaraq rədd edilmişdir. \"İcazələr\"-i seçin və\"Yerləşmə\" izni verin. </string>
     <string name="perm_explain_access_to_storage_denied">Delta Chat, fotoşəkilləri, videoları və ya səsləri əlavə etmək və ya ixrac etmək üçün saxlama icazəsini tələb edir, lakin bu daimi olaraq rədd edildi. Xahiş edirik tətbiq ayarları menyusuna davam edin, \"İzinlər\" seçin və \"Yaddaş\" funksiyasını aktivləşdirin.</string>
     <string name="perm_explain_access_to_location_denied">Delta Chat yer əlavə etmək üçün yer icazə tələb edir, lakin daimi olaraq rədd edilmişdir. \"İcazələr\"-i seçin və\"Yerləşmə\" izni verin. </string>

_locales/bg.xml
@@ -656,8 +656,6 @@
     <string name="pref_use_system_emoji_explain">Изключване на вградената поддръжка за Emoji на Delta Chat</string>
     <string name="pref_show_system_contacts">Прочитане на системната адресна книга</string>
     <string name="pref_show_system_contacts_explain">Да се предлага създаване на чатове с контакти от адресната книга. За някои доставчици е необходимо първо да се настрои криптиране от край до край.</string>
-    <!-- deprecated -->
-    <string name="pref_app_access">Достъп до приложението</string>
     <string name="pref_chats">Чатове</string>
     <string name="pref_in_chat_sounds">Звуци в чата</string>
     <string name="pref_view_log">Разглеждане на дневника</string>
@@ -778,11 +776,6 @@
     <string name="autocrypt_send_asm_button">Изпращане на съобщението за настройка на Autocrypt</string>
     <string name="autocrypt_send_asm_explain_after">Изпратихте Вашата настройка до себе си. Отворете съобщението за настройка в другото устройство. Би трябвало да Ви бъде поискан код за настройка. Въведете следните цифри:</string>
     <string name="autocrypt_prefer_e2ee">Да се предпочита криптиране \"от край до край\"</string>
-    <!-- deprecated -->
-    <string name="autocrypt_asm_subject">Съобщение за настройка на Autocrypt</string>
-    <!-- deprecated -->
-    <string name="autocrypt_asm_general_body">Това е съобщението за настройка на Autocrypt, чрез което се прехвърля Вашата настройка за криптиране \"от край до край\" между различни клиенти.\n\nЗа да декриптирате и използвате Вашата настройка, отворете съобщението в клиент, съвместим с Autocrypt, и въведете кода за настройка, представен на генериращото устройство.</string>
-
 
     <!-- system messages -->
     <string name="systemmsg_cannot_decrypt">Това съобщение не може да бъде декриптирано.\n\n• Бихте могли просто да му отговорите и да помолите подателя да го изпрати отново.\n\n• Ако току-що сте преинсталирали Delta Chat, е най-добре да настроите приложението отново сега и да изберете \"Добавяне като второ устройство\" или да импортирате от архив.</string>
@@ -925,8 +918,6 @@
     <!-- translators: "setup" is the "encryption setup" here, as in "Autocrypt Setup Message" -->
     <string name="contact_setup_changed">Настройката за %1$s е сменена.</string>
     <string name="verified_contact_required_explain">За да се гарантира криптирането от край до край, можете да добавяте в тази група само контакти, отбелязани със зелена отметка.\n\nБихте могли да се срещнете с лицата, с които желаете да контактувате, лично и да сканирате техните QR кодове, за да ги въведете.</string>
-    <!-- deprecated -->
-    <string name="copy_qr_data_success">URL адресът от QR кода е копиран в клипборда</string>
     <string name="mailto_dialog_header_select_chat">Изберете чат, към който да бъде пратено съобщението</string>
     <!-- first placeholder is the name of the chat -->
     <string name="confirm_replace_draft">%1$s вече има съобщение чернова, искате ли да го замените?</string>
@@ -952,6 +943,7 @@
     <string name="perm_required_title">Изисква се позволение</string>
     <string name="perm_continue">Продължаване</string>
     <string name="perm_explain_access_to_camera_denied">За да правите снимки или заснемате видеоклипове, отидете в настройките на приложението, изберете \"Позволения\" и разрешете \"Камера\".</string>
+    <!-- give the user an idea where to find the "Microphone" option. the hint is only shown if access was initially denied by the user. pick up wordings really used on the systems, but do not be overly precise: things shift around often and there are too many system we support to track every detail and click path -->
     <string name="perm_explain_access_to_mic_denied">За да изпращате аудиосъобщения, отидете в настройките на приложението, изберете \"Позволения\" и разрешете \"Микрофон\".</string>
     <string name="perm_explain_access_to_storage_denied">За да получавате или изпращате файлове, отидете в настройките на приложението, изберете \"Позволения\" и разрешете \"Съхранение\".</string>
     <string name="perm_explain_access_to_location_denied">За да прикрепите информация за местоположението, отидете в настройките на приложението, изберете \"Позволения\" и разрешете \"Местоположение\".</string>

_locales/ca.xml
@@ -741,8 +741,6 @@
     <string name="pref_use_system_emoji_explain">Desactiva el suport de sèrie per emojis a Delta Chat</string>
     <string name="pref_show_system_contacts">Llegeix la llibreta d\'adreces del sistema</string>
     <string name="pref_show_system_contacts_explain">Ofereix de crear xats amb els contactes de la llibreta d\'adreces. Alguns proveïdors necessiten de configurar abans el xifratge d\'extrem a extrem.</string>
-    <!-- deprecated -->
-    <string name="pref_app_access">Accés a l\'aplicació</string>
     <string name="pref_chats">Xats</string>
     <string name="pref_in_chat_sounds">Sons d\'entrada</string>
     <string name="pref_view_log">Mostra el registre</string>
@@ -832,7 +830,7 @@
     <!-- %1$s will be replaced by an emoji. %2$s will be replaced by message summary (the summary is often long and whole string gets truncated; words after %2$s will often not being visible). Eg. 'You reacted 👍 to "Found my suitcase"'. Use less than 20 characters, otherwise the string will be truncated too soon and too few information are shown. Do not try to translate "reacted to" too strict. Depending on the language, "added 👍 to" or "gave 👍 to" or just "👍 to" may be shorter and/or clearer and work as well. -->
     <string name="reaction_by_you">Heu reaccionat amb %1$s a «%2$s»</string>
     <!-- %1$s will be replaced a name. %2$s will be replaced by an emoji. %3$s will be replaced by message summary (the summary is often long and whole string gets truncated; words after %3$s will often not being visible). Eg. 'Alice reacted 👍 to "Nice photos"'. Use less than 20 characters, otherwise the string will be truncated too soon and too few information are shown. Do not try to translate "reacted to" too strict. Depending on the language, "added 👍 to" or "gave 👍 to" or just "👍 to" may be shorter and/or clearer work as well. -->
-    <string name="reaction_by_other">%1$sha reaccionat %2$s a «%3$s»</string>
+    <string name="reaction_by_other">%1$s ha reaccionat %2$s a «%3$s»</string>
 
     <!-- automatically delete message -->
     <string name="delete_old_messages">Esborra els missatges antics</string>
@@ -865,11 +863,6 @@
     <string name="autocrypt_send_asm_button">Envia el missatge de configuració Autocrypt</string>
     <string name="autocrypt_send_asm_explain_after">Se us ha enviat la vostra configuració. Canvieu a l\'altre dispositiu i obriu el missatge de configuració. Se us demanarà un codi de configuració. Introduïu els dígits següents a l\'indicador.\n\nUn cop fet, l\'altre dispositiu estarà llest per utilitzar Autocrypt.</string>
     <string name="autocrypt_prefer_e2ee">Prefereix l\'encriptació d\'extrem a extrem</string>
-    <!-- deprecated -->
-    <string name="autocrypt_asm_subject">Missatge de configuració Autocrypt</string>
-    <!-- deprecated -->
-    <string name="autocrypt_asm_general_body">Aquest és el missatge de configuració Autocrypt que s’usa per a transferir la vostra configuració d\'extrem a extrem entre dispositius.\n\nPer a desxifrar i usar aquesta configuració, obriu el missatge en un dispositiu compatible amb Autocript i introduïu el codi de configuració que apareix al dispositiu primari.</string>
-
 
     <!-- system messages -->
     <string name="systemmsg_cannot_decrypt">Aquest missatge no es pot desxifrar.\n\n• Potser pot ajudar de respondre el missatge i demanar a l\'emissor que torni a enviar-lo,\n\n• Si acabeu de reinstal·lar Delta Chat, aleshores és millor de reconfigurar Delta Chat ara i triar «Afegeix un segon dispositiu» o importar una còpia de seguretat.</string>
@@ -1012,8 +1005,6 @@
     <!-- translators: "setup" is the "encryption setup" here, as in "Autocrypt Setup Message" -->
     <string name="contact_setup_changed">Configuració canviada per %1$s</string>
     <string name="verified_contact_required_explain">Per a garantir el xifrat extrem a extrem, només podeu afegir a aquest grup contactes que tinguin la marca verda.\n\nPodeu trobar-vos en persona amb els contactes per a escanejar els seus codis QR i poder-los afegir. </string>
-    <!-- deprecated -->
-    <string name="copy_qr_data_success">S\'ha copiat l\'URL del codi QR al porta-retalls</string>
     <string name="mailto_dialog_header_select_chat">Trieu el xat on enviar el missatge</string>
     <!-- first placeholder is the name of the chat -->
     <string name="confirm_replace_draft">%1$s ja té un missatge d\'esborrany, voleu substituir-lo?</string>
@@ -1039,6 +1030,7 @@
     <string name="perm_required_title">Es requereix permís</string>
     <string name="perm_continue">Continua</string>
     <string name="perm_explain_access_to_camera_denied">Delta Chat demana accés a la càmera per poder fer fotos o vídeos, però li ha estat denegat. Sisplau, vés al menú de configuració de l\'app, busca-hi els \"Permisos\" i habilita\'n la \"Càmera\".</string>
+    <!-- give the user an idea where to find the "Microphone" option. the hint is only shown if access was initially denied by the user. pick up wordings really used on the systems, but do not be overly precise: things shift around often and there are too many system we support to track every detail and click path -->
     <string name="perm_explain_access_to_mic_denied">Delta Chat demana accés al micròfon per poder enviar missatges d\'àudio, però li ha estat denegat. Sisplau, vés al menú de configuració de l\'app, busca-hi els \"Permisos\" i habilita\'n el \"Micròfon\".</string>
     <string name="perm_explain_access_to_storage_denied">Per a rebre o enviar fitxers, aneu a la configuració de l\'aplicació, seleccioneu «Permisos» i activeu «Emmagatzematge».</string>
     <string name="perm_explain_access_to_location_denied">Per a adjuntar la ubicació, aneu a la configuració de l\'aplicació, trieu «Permisos» i activeu la «Ubicació».</string>

commit a27307baf8eee0cb7722283c462a40c1486843bd

fix: a11y: fix incorrect gallery "tab" role
The issue has been introduced in
3bc21041bfbafea2da9c297adcdffa28528e8480
(https://github.com/deltachat/deltachat-desktop/pull/5149).
That MR was unfinished in this aspect.
This MR finished it, and closes
https://github.com/deltachat/deltachat-desktop/issues/5074.

This reverts 00c7e6939675156e842f45ca9948dfa9b4cce507
(https://github.com/deltachat/deltachat-desktop/pull/4675).
Also see 4073e2a400fc98b22a18d3b760a8f441e4fe42f8
(https://github.com/deltachat/deltachat-desktop/pull/5076).

packages/frontend/src/components/Gallery.tsx
@@ -302,18 +302,8 @@ export default class Gallery extends Component<
     const showDateHeader =
       currentTab !== 'files' && currentTab !== 'webxdc_apps'
 
-    // FYI the role="tablist" element is not rendered when
-    // `props.chatId === 'all'`.
-    // Would be nice to DRY this somehow.
-    const isTabpanel = this.props.chatId !== 'all'
-
     return (
-      <div
-        role={isTabpanel ? 'tabpanel' : undefined}
-        aria-labelledby={isTabpanel ? 'tab-media-view' : undefined}
-        id='media-view'
-        className='media-view'
-      >
+      <div className='media-view'>
         <ul ref={this.tabListRef} className='tab-list .modifier' role='tablist'>
           <RovingTabindexProvider
             wrapperElementRef={this.tabListRef}

packages/frontend/src/components/NoChatSelected.tsx
@@ -13,16 +13,7 @@ export default function NoChatSelected() {
     : {}
 
   return (
-    <div
-      role='tabpanel'
-      // See MessageListAndComposer as to why this is commented out.
-      // aria-labelledby='tab-message-list-view'
-
-      // The main MessageListAndComposer also has this ID and class.
-      id='message-list-and-composer'
-      className='message-list-and-composer'
-      style={style}
-    >
+    <div className='message-list-and-composer' style={style}>
       <div
         className='message-list-and-composer__message-list'
         style={{ display: 'flex' }}

packages/frontend/src/components/message/MessageListAndComposer.tsx
@@ -292,18 +292,6 @@ export default function MessageListAndComposer({ accountId, chat }: Props) {
 
   return (
     <div
-      role='tabpanel'
-      // Techically we must apply `aria-labelledby` to `tabpanel`,
-      // but it's a little annoying that screen readers (NVDA)
-      // announce "'Chat' property page" every time
-      // the focus enters this tabpanel, because it's the "default" one,
-      // it's not often that another tab (Gallery) is selected.
-      // So, let's comment this out for now, until we resolve
-      // https://github.com/deltachat/deltachat-desktop/issues/5074.
-      // aria-labelledby='tab-message-list-view'
-
-      // NoChatSelected also has this ID and class.
-      id='message-list-and-composer'
       className='message-list-and-composer'
       style={style}
       ref={conversationRef}

packages/frontend/src/components/screens/MainScreen/MainScreen.tsx
@@ -401,17 +401,9 @@ function ChatNavButtons({ chat }: { chat: T.FullChat }) {
 
   return (
     <>
-      <span
-        role='tablist'
-        aria-orientation='horizontal'
-        className='views'
-        data-no-drag-region
-      >
+      <span className='views' data-no-drag-region>
         <Button
-          role='tab'
-          id='tab-media-view'
           onClick={openMediaViewDialog}
-          aria-controls='media-view'
           aria-label={tx('apps_and_media')}
           title={tx('apps_and_media')}
           className='navbar-button'
@@ -420,8 +412,6 @@ function ChatNavButtons({ chat }: { chat: T.FullChat }) {
           <Icon coloring='navbar' icon='apps' size={18} />
         </Button>
         {settingsStore?.desktopSettings.enableOnDemandLocationStreaming && (
-          // Yes, this is not marked as `role='tab'`.
-          // I'm not sure if this is alright.
           <Button
             onClick={() => openMapWebxdc(selectedAccountId(), chatId)}
             aria-label={tx('tab_map')}