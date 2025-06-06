deltachat/deltachat-desktop

commit 928d73a70ec991266e8b0ff274ce8194c9b18b03

improvement: notify on msg in all non-curr chats
Instead of only showing notifications for messages
of other accounts, when the window is focused.

This is mainly for accessibility, but is also in line with
other messengers.

packages/frontend/src/system-integration/notifications.ts
@@ -103,10 +103,25 @@ function incomingMessageHandler(
     return
   }
 
-  if (document.hasFocus() && accountId === window.__selectedAccountId) {
-    // window has focus don't send notification for the selected account
+  if (chatId === window.__selectedChatId && document.hasFocus()) {
+    // window has focus don't send notification for the selected chat
+    //
+    // It is important for accessibility to notify the user of all new messages,
+    // no matter what chat or account they belong to.
+    // For the current chat we might utilize `aria-live` on the messages list,
+    // or a simple sound effect
+    // (https://github.com/deltachat/deltachat-desktop/pull/5143).
+    // For other chats, let's use OS notifications for this.
+    // See https://github.com/deltachat/deltachat-desktop/issues/4743.
+    //
+    // Additionally, one has to remember that we have the "narrow screen mode",
+    // where the list of chats is not shown,
+    // thus there is no indication of a new message,
+    // besides the unread badge counter increasing.
+    //
+    // This is also in line with other messengers, such as Telegram.
     log.debug(
-      'notification ignored: window has focus and account of the notification is selected'
+      'notification ignored: window has focus and chat of the notification is selected'
     )
     return
   }

packages/frontend/src/contexts/ChatContext.tsx
@@ -73,6 +73,7 @@ export const ChatProvider = ({
   const cancelPendingSetChat = useRef<(() => void) | undefined>(undefined)
 
   const [chatId, setChatId] = useState<number | undefined>()
+  window.__selectedChatId = chatId
 
   const setChatView = useCallback<SetView>((nextView: ChatView) => {
     setActiveView(nextView)

packages/frontend/src/global.d.ts
@@ -11,6 +11,7 @@ declare global {
     __changeScreen: (screen: Screens) => void
     __selectAccount: (accountId: number) => Promise<void>
     readonly __selectedAccountId: number | undefined
+    __selectedChatId: number | undefined
     __screen: Screens
     readonly __contextMenuActive: boolean
     __setContextMenuActive: (newVal: boolean) => void

commit 3c81927d857b66a156b9162507f0b989e9a56743

fix: notifications not working sometimes
To be precise, when the chat ID of the currently selected chat
mathches the chat ID of the chat on another account
in which a message gets received.

The bug has been introduced in
928d73a70ec991266e8b0ff274ce8194c9b18b03
(https://github.com/deltachat/deltachat-desktop/pull/5146).

packages/frontend/src/system-integration/notifications.ts
@@ -103,7 +103,11 @@ function incomingMessageHandler(
     return
   }
 
-  if (chatId === window.__selectedChatId && document.hasFocus()) {
+  if (
+    accountId === window.__selectedAccountId &&
+    chatId === window.__selectedChatId &&
+    document.hasFocus()
+  ) {
     // window has focus don't send notification for the selected chat
     //
     // It is important for accessibility to notify the user of all new messages,