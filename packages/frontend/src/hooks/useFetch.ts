a6355638d4957dd48639fa78557db25f803be644
Nico de Haen <ndh@deltachat.de>
2025-06-27 22:44:24 +0200

    Extend some comments (#5233)
    * Extend some comments
    
    * Update comments as suggested
    
    Co-authored-by: WofWca <wofwca@protonmail.com>
    
    ---------
    
    Co-authored-by: WofWca <wofwca@protonmail.com>

--- packages/frontend/src/hooks/useFetch.ts
+++ packages/frontend/src/hooks/useFetch.ts
@@ -1,7 +1,16 @@
 import { useCallback, useEffect, useMemo, useState } from 'react'
 import { BackendRemote } from '../backend-com'
 
+/**
+ * Possible results of an async operation - either success with a value
+ * or failure with an error
+ */
 type Result<T> = { ok: true; value: T } | { ok: false; err: unknown }
+
+/**
+ * Generic return type for fetch hooks with conditional loading state.
+ * Uses TypeScript's conditional types to ensure type safety based on loading state.
+ */
 type RetGeneric<T extends (...args: any) => any, Loading extends boolean> = {
   readonly loading: Loading
   /**
@@ -19,27 +28,42 @@ type RetGeneric<T extends (...args: any) => any, Loading extends boolean> = {
     : Result<Awaited<ReturnType<T>>>
   readonly refresh: () => void
 }
+
+/**
+ * Union type representing the two possible states of a fetch hook
+ */
 type Ret<T extends (...args: any) => any> =
   | RetGeneric<T, true>
   | RetGeneric<T, false>
 
 type AsyncFNoArgs = () => Promise<any>
 
+/**
+ * This hook provides a generic way to fetch data from an async function.
+ * Similar to various `useFetch` implementations. See
+ * - https://react.dev/learn/reusing-logic-with-custom-hooks#when-to-use-custom-hooks
+ * - https://nuxt.com/docs/api/composables/use-fetch
+ * - https://vueuse.org/core/useFetch
+ *
+ * Key features:
+ * - Automatic refetching when dependencies change
+ * - Prevents race conditions with fetch ID tracking
+ * - Maintains previous results during loading (lingeringResult)
+ * - Manual refresh capability
+ */
+
 export function useFetchGeneric(fn: null): null
 export function useFetchGeneric<F extends AsyncFNoArgs>(fn: F): Ret<F>
 export function useFetchGeneric<F extends AsyncFNoArgs>(
   fn: null | F
 ): null | Ret<F>
+
 /**
  * Invokes `fn` every time it changes.
  *
  * Most of the time, using {@link useFetch} or {@link useRpcFetch}
  * would be more convenient.
  *
- * This is similar to various `useFetch`. See
- * - https://react.dev/learn/reusing-logic-with-custom-hooks#when-to-use-custom-hooks
- * - https://nuxt.com/docs/api/composables/use-fetch
- * - https://vueuse.org/core/useFetch
  */
 export function useFetchGeneric<F extends AsyncFNoArgs>(
   /**
@@ -64,6 +88,10 @@ export function useFetchGeneric<F extends AsyncFNoArgs>(
     fetchId: typeof fetchId
   }>(null)
 
+  /**
+   * Dummy state used to trigger manual refreshes.
+   * Incrementing this value forces a new fetch.
+   */
   const [refreshDummyValue, _setRefreshDummyValue] = useState(0)
   const refresh = useCallback(() => _setRefreshDummyValue(old => old + 1), [])
 
@@ -74,12 +102,19 @@ export function useFetchGeneric<F extends AsyncFNoArgs>(
    * This value changes when and only when the dependencies
    * for the fetch change, i.e. when the `fn` itself changes.
    * That is, when we need to (and will) perform a new fetch.
+   *
+   * Using Symbol() ensures each fetch has a unique identifier.
    */
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const fetchId = useMemo(() => Symbol(), [
     fn,
     refreshDummyValue,
   ] as Dependencies)
+
+  /**
+   * Determines if we're currently loading by comparing the current fetchId
+   * with the fetchId of the most recent completed fetch.
+   */
   const loading = resultAndFetchId?.fetchId !== fetchId
   type DependenciesWithFetchId = [...Dependencies, typeof fetchId]
 
@@ -138,6 +173,9 @@ export function useFetchGeneric<F extends AsyncFNoArgs>(
   }
 }
 
+/**
+ * Type for async functions that take arguments
+ */
 type AsyncF<A extends any[]> = (...args: A) => Promise<any>
 type SomeTuple =
   | []
@@ -164,11 +202,10 @@ export function useFetch<Args extends SomeTuple, F extends AsyncF<Args>>(
   fn: F,
   args: null | Args
 ): null | Ret<F>
+
 /**
- * This is similar to various `useFetch`. See
- * - https://react.dev/learn/reusing-logic-with-custom-hooks#when-to-use-custom-hooks
- * - https://nuxt.com/docs/api/composables/use-fetch
- * - https://vueuse.org/core/useFetch
+ * A specific implementation of {@link useFetchGeneric} that handles
+ * functions with arguments.
  */
 export function useFetch<Args extends SomeTuple, F extends AsyncF<Args>>(
   fn: F,
@@ -177,6 +214,10 @@ export function useFetch<Args extends SomeTuple, F extends AsyncF<Args>>(
    */
   args: null | Args
 ): null | Ret<F> {
+  /**
+   * Destructure arguments to individual variables.
+   * This allows us to use them as dependencies in useCallback.
+   */
   const [arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9] =
     args ?? []
   if (args) {
@@ -186,6 +227,10 @@ export function useFetch<Args extends SomeTuple, F extends AsyncF<Args>>(
 
   type AnyArgs = (...args: SomeTuple) => ReturnType<typeof fn>
 
+  /**
+   * Create a function with no arguments that calls the original function
+   * with the provided arguments. This allows us to use useFetchGeneric.
+   */
   const fnNoArgs = useCallback(
     () =>
       (fn as unknown as AnyArgs)(
@@ -205,10 +250,22 @@ export function useFetch<Args extends SomeTuple, F extends AsyncF<Args>>(
   return useFetchGeneric(args ? fnNoArgs : null)
 }
 
+/**
+ * Utility type that represents only method properties from an object type.
+ */
 type MethodsOnly<T> = {
   [P in keyof T]: T[P] extends (...args: any) => any ? T[P] : never
 }
+
+/**
+ * Union type of all method types from an object.
+ * This gives us a union of all possible method signatures.
+ */
 type MethodOf<T> = MethodsOnly<T>[keyof MethodsOnly<T>]
+
+/**
+ * Type representing all available RPC methods from BackendRemote.rpc (Type RawClient)
+ */
 type MethodOfRpc = MethodOf<typeof BackendRemote.rpc>
 
 export function useRpcFetch<Method extends MethodOfRpc>(
@@ -223,9 +280,16 @@ export function useRpcFetch<
   Method extends MethodOfRpc,
   Args extends null | Parameters<Method>,
 >(method: Method, args: Args): null | Ret<Method>
+
 /**
  * A (possibly) more convenient version of {@link useFetch},
  * when dealing with `BackendRemote.rpc` calls.
+ *
+ * Namely, it allows you to pass `BackendRemote.rpc` methods directly
+ * as the `method` arg, without having to worry about the `this` value.
+ *
+ * @example
+ * useRpcFetch(BackendRemote.rpc.getMessage, [accId, msgId])
  */
 export function useRpcFetch<
   Method extends MethodOfRpc,
@@ -237,6 +301,10 @@ export function useRpcFetch<
    */
   args: Args
 ): null | Ret<Method> {
+  /**
+   * Bind the method to BackendRemote.rpc to ensure it's called
+   * with the correct 'this' context.
+   */
   const methodBound = useMemo(() => method.bind(BackendRemote.rpc), [method])
   return useFetch(methodBound as (...args: SomeTuple) => Promise<any>, args)
 }
