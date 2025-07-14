fe12e5b32206a32e19d0af1438ef5526cfe3dc45
WofWca <wofwca@protonmail.com>
2025-07-14 17:37:33 +0400

    refactor: rename `selectedChat` to `activeChat`
    This is for the upcoming multiselect feature, to avoid confusion.

--- packages/frontend/src/components/dialogs/ViewProfile/index.tsx
+++ packages/frontend/src/components/dialogs/ViewProfile/index.tsx
@@ -354,7 +354,7 @@ export function ViewProfileInner({
                       chatListIds,
                       onChatClick,
 
-                      selectedChatId: null,
+                      activeChatId: null,
                       activeContextMenuChatId: null,
                       openContextMenu: async () => {},
                     }}
