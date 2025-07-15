deltachat/deltachat-desktop

commit e594d379eb8de8366ec3d0dd37d0560d7bad71a8

fix copying of links into richtext editors (#5286)

packages/frontend/src/components/message/MessageMarkdown.tsx
@@ -224,7 +224,7 @@ function EmailLink({
 
   return (
     <a
-      href={'#'}
+      href={`mailto:${email}`}
       x-not-a-link='email'
       x-target-email={email}
       onClick={handleClick}

packages/frontend/src/components/message/Link.tsx
@@ -86,7 +86,7 @@ export const LabeledLink = ({
   }
   return (
     <a
-      href={'#' + target}
+      href={target}
       x-target-url={target}
       title={realUrl}
       onClick={onClick}
@@ -210,7 +210,7 @@ export const Link = ({
 
   return (
     <a
-      href='#'
+      href={asciiUrl}
       x-target-url={asciiUrl}
       title={asciiUrl}
       onClick={onClick}

commit 856bd47caa3f93d6eecdb3a4e69053559715099c

prevent navigation when clicking on email addresses
(affects mainly browser and tauri edition)

fixes regression introduced by #5286

for more info see
https://github.com/deltachat/deltachat-desktop/pull/5286/files/d3262d2a74d4a7d14759b89f43ea6100521f4825#r2207270975

packages/frontend/src/components/message/MessageMarkdown.tsx
@@ -215,7 +215,9 @@ function EmailLink({
   const createChatByEmail = useCreateChatByEmail()
   const { selectChat } = useChat()
 
-  const handleClick = async () => {
+  const handleClick: React.MouseEventHandler<HTMLAnchorElement> = async ev => {
+    ev.preventDefault()
+    ev.stopPropagation()
     const chatId = await createChatByEmail(accountId, email)
     if (chatId) {
       selectChat(accountId, chatId)