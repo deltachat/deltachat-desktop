ce7bbe5cd7fee1395a3cd7170628dd0f0e2bb72c
Nico de Haen <ndh@deltachat.de>
2025-07-17 18:44:21 +0200

    Reduce mailing list header

--- packages/frontend/src/components/dialogs/MessageListProfile.tsx
+++ packages/frontend/src/components/dialogs/MessageListProfile.tsx
@@ -1,18 +1,17 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-import { useGroupImage, ChatSettingsSetNameAndProfileImage } from './CreateChat'
+import { useGroupImage } from './CreateChat'
 import Dialog, {
+  CloseFooterAction,
   DialogBody,
   DialogContent,
   DialogHeader,
-  OkCancelFooterAction,
 } from '../Dialog'
 import useTranslationFunction from '../../hooks/useTranslationFunction'
-import { selectedAccountId } from '../../ScreenController'
-import { modifyGroup } from '../../backend/group'
 
 import type { T } from '@deltachat/jsonrpc-client'
-import type { DialogProps } from '../../contexts/DialogContext'
+import { DialogProps } from '../../contexts/DialogContext'
+import ProfileInfoHeader from '../ProfileInfoHeader'
 
 export default function MailingListProfile(
   props: {
@@ -22,62 +21,26 @@ export default function MailingListProfile(
   const { onClose, chat } = props
 
   const tx = useTranslationFunction()
-  const accountId = selectedAccountId()
-  const [groupName, setGroupName] = useState(chat.name)
-  const [errorMissingGroupName, setErrorMissingGroupName] = useState(false)
-  const [groupImage, onSetGroupImage, onUnsetGroupImage] = useGroupImage(
+  const [groupImage] = useGroupImage(
     chat.profileImage
   )
-  const onUpdateGroup = useEdit(
-    accountId,
-    groupName,
-    groupImage,
-    chat.id,
-    onClose
-  )
+
+
 
   return (
     <Dialog onClose={onClose} fixed>
       <DialogHeader title={tx('mailing_list')} />
       <DialogBody>
         <DialogContent>
-          <ChatSettingsSetNameAndProfileImage
-            groupImage={groupImage}
-            onSetGroupImage={onSetGroupImage}
-            onUnsetGroupImage={onUnsetGroupImage}
-            chatName={groupName}
-            setChatName={setGroupName}
-            errorMissingChatName={errorMissingGroupName}
-            setErrorMissingChatName={setErrorMissingGroupName}
-            color={chat.color}
-            type='group'
-          />
-          <div style={{ padding: '15px 0px' }}>
-            {tx('mailing_list_profile_info')}
-          </div>
+        <ProfileInfoHeader
+          avatarPath={groupImage ? groupImage : undefined}
+          color={chat.color}
+          displayName={chat.name}
+          isVerified={chat.isProtected}
+        />
         </DialogContent>
       </DialogBody>
-      <OkCancelFooterAction onCancel={onClose} onOk={onUpdateGroup} />
+      <CloseFooterAction onClose={onClose} />
     </Dialog>
   )
-}
-
-const useEdit = (
-  accountId: number,
-  groupName: string,
-  groupImage: string | null,
-  groupId: number,
-  onClose: DialogProps['onClose']
-) => {
-  const updateGroup = async () => {
-    await modifyGroup(accountId, groupId, groupName, groupImage)
-  }
-
-  const onUpdateGroup = async () => {
-    if (groupName === '') return
-    await updateGroup()
-    onClose()
-  }
-
-  return onUpdateGroup
 }
