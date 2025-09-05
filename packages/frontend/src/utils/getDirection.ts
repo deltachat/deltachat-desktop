b9d352f3643917a3e63d40376c7685d9a791160f
Farooq Karimi Zadeh <fkz@riseup.net>
2025-09-05 07:56:45 +0330

    write docstring for getDirection

--- packages/frontend/src/utils/getDirection.ts
+++ packages/frontend/src/utils/getDirection.ts
@@ -1,6 +1,12 @@
 import { C } from '@deltachat/jsonrpc-client'
 
 // TODO: move to frontend
-export function getDirection({ fromId }: { fromId: number }) {
+/*
+ * For a given contact ID, is the message to this account or to another account?
+ * In other words, for a given contact ID, is it id of self or another contact?
+ *
+ * @return "outgoing" | "incoming"
+  */
+export function getDirection({ fromId }: { fromId: number }): "outgoing" | "incoming" {
   return fromId === C.DC_CONTACT_ID_SELF ? 'outgoing' : 'incoming'
 }
