ad188298e39008493d03a027608d678170c6b237
Farooq Karimi Zadeh <fkz@riseup.net>
2025-09-05 07:56:45 +0330

    write docstring for getDirection
    Co-authored-by: WofWca <wofwca@protonmail.com>

--- packages/frontend/src/utils/getDirection.ts
+++ packages/frontend/src/utils/getDirection.ts
@@ -1,6 +1,13 @@
 import { C } from '@deltachat/jsonrpc-client'
 
 // TODO: move to frontend
-export function getDirection({ fromId }: { fromId: number }) {
+/*
+ * Is this a message that we sent out, or someone sent to us?
+ */
+export function getDirection({
+  fromId,
+}: {
+  fromId: number
+}): 'outgoing' | 'incoming' {
   return fromId === C.DC_CONTACT_ID_SELF ? 'outgoing' : 'incoming'
 }
