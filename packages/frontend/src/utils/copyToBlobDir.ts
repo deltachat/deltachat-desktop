869960322f985ca17498f1005e8c1fe22e681525
Farooq Karimi Zadeh <fkz@riseup.net>
2025-09-06 08:53:13 +0330

    pretty code for copyToBlobDir.ts in frontend

--- packages/frontend/src/utils/copyToBlobDir.ts
+++ packages/frontend/src/utils/copyToBlobDir.ts
@@ -1,12 +1,12 @@
 import { BackendRemote } from '../backend-com'
 
 /*
-* Copy the file from `filepath` to the blob directory and create a blob.
-* Throws an error if no account is selected.
-*
-* @param {string} Path to the file
-* @return {Promise<string>} A promise which resolves to the blob path
-*/
+ * Copy the file from `filepath` to the blob directory and create a blob.
+ * Throws an error if no account is selected.
+ *
+ * @param {string} Path to the file
+ * @return {Promise<string>} A promise which resolves to the blob path
+ */
 export async function copyToBlobDir(filepath: string): Promise<string> {
   const acc = await BackendRemote.rpc.getSelectedAccountId()
   if (acc === null) {
