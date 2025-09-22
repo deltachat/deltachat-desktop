4dab9670ed832cb8a69fcb940486a75b378089a3
Simon <git@simonlaux.de>
2025-09-22 16:32:47 +0200

    move context menu to extra file

---
+++ packages/frontend/src/components/dialogs/ChatAuditLogDialog/contextMenu.tsx
@@ -0,0 +1,92 @@
+import { C, T } from '@deltachat/jsonrpc-client'
+
+import { openMessageInfo, setQuoteInDraft } from '../../message/messageFunctions'
+
+import type { DialogProps, OpenDialog } from '../../../contexts/DialogContext'
+import type { JumpToMessage } from '../../../hooks/chat/useMessage'
+import type { PrivateReply } from '../../../hooks/chat/usePrivateReply'
+
+export function buildContextMenu(
+  jumpToMessage: JumpToMessage,
+  openDialog: OpenDialog,
+  privateReply: PrivateReply,
+  accountId: number,
+  message: T.Message,
+  isGroup: boolean,
+  closeDialogCallback: DialogProps['onClose']
+) {
+  const tx = window.static_translate // don't use the i18n context here for now as this component is inefficient (rendered one menu for every message)
+  const isInfoOrCallInvitation =
+    message.isInfo || message.viewType === 'VideochatInvitation'
+  return [
+    // Show in Chat
+    {
+      label: tx('show_in_chat'),
+      action: () => {
+        closeDialogCallback()
+        setTimeout(() =>
+          jumpToMessage({
+            accountId,
+            msgId: message.id,
+            msgChatId: message.chatId,
+            highlight: true,
+            focus: true,
+            scrollIntoViewArg: { block: 'center' },
+          })
+        )
+      },
+    },
+    // Show Webxdc in Chat
+    message.systemMessageType == 'WebxdcInfoMessage' && {
+      label: tx('show_app_in_chat'),
+      action: () => {
+        if (message.parentId) {
+          closeDialogCallback()
+          jumpToMessage({
+            accountId,
+            msgId: message.parentId,
+            // Currently the info message is always in the same chat
+            // as the message with `message.parentId`,
+            // but let's not pass `chatId` here, for future-proofing.
+            msgChatId: undefined,
+            highlight: true,
+            focus: true,
+            scrollIntoViewArg: { block: 'center' },
+          })
+        }
+      },
+    },
+    // Reply
+    // TODO also check `chat.canSend`, as with `showReply` in `Message.tsx`,
+    // and double-check other items.
+    !isInfoOrCallInvitation && {
+      label: tx('reply_noun'),
+      action: () => {
+        setQuoteInDraft(message.id)
+        closeDialogCallback()
+      },
+    },
+    // Reply privately -> only show in groups, don't show on info messages or outgoing messages
+    isGroup &&
+      !isInfoOrCallInvitation &&
+      message.fromId > C.DC_CONTACT_ID_LAST_SPECIAL && {
+        label: tx('reply_privately'),
+        action: () => {
+          privateReply(accountId, message)
+          closeDialogCallback()
+        },
+      },
+    // Copy to clipboard
+    {
+      label: tx('global_menu_edit_copy_desktop'),
+      action: () => {
+        navigator.clipboard.writeText(message.text || '')
+      },
+    },
+    // Message Info
+    {
+      label: tx('info'),
+      action: openMessageInfo.bind(null, openDialog, message),
+    },
+  ]
+}
--- packages/frontend/src/components/dialogs/ChatAuditLogDialog/index.tsx
+++ packages/frontend/src/components/dialogs/ChatAuditLogDialog/index.tsx
@@ -18,101 +18,14 @@ import { ContextMenuContext } from '../../../contexts/ContextMenuContext'
 import { getDirection } from '../../../utils/getDirection'
 import { getLogger } from '@deltachat-desktop/shared/logger'
 import { mapCoreMsgStatus2String } from '../../helpers/MapMsgStatus'
-import { openMessageInfo, setQuoteInDraft } from '../../message/messageFunctions'
 import { runtime } from '@deltachat-desktop/runtime-interface'
 import { selectedAccountId } from '../../../ScreenController'
 
-import type { DialogProps, OpenDialog } from '../../../contexts/DialogContext'
-import type { JumpToMessage } from '../../../hooks/chat/useMessage'
-import type { PrivateReply } from '../../../hooks/chat/usePrivateReply'
+import type { DialogProps } from '../../../contexts/DialogContext'
 import { mouseEventToPosition } from '../../../utils/mouseEventToPosition'
+import { buildContextMenu } from './contextMenu'
 
 const log = getLogger('render/ChatAuditLog')
-
-function buildContextMenu(
-  jumpToMessage: JumpToMessage,
-  openDialog: OpenDialog,
-  privateReply: PrivateReply,
-  accountId: number,
-  message: Type.Message,
-  isGroup: boolean,
-  closeDialogCallback: DialogProps['onClose']
-) {
-  const tx = window.static_translate // don't use the i18n context here for now as this component is inefficient (rendered one menu for every message)
-  const isInfoOrCallInvitation =
-    message.isInfo || message.viewType === 'VideochatInvitation'
-  return [
-    // Show in Chat
-    {
-      label: tx('show_in_chat'),
-      action: () => {
-        closeDialogCallback()
-        setTimeout(() =>
-          jumpToMessage({
-            accountId,
-            msgId: message.id,
-            msgChatId: message.chatId,
-            highlight: true,
-            focus: true,
-            scrollIntoViewArg: { block: 'center' },
-          })
-        )
-      },
-    },
-    // Show Webxdc in Chat
-    message.systemMessageType == 'WebxdcInfoMessage' && {
-      label: tx('show_app_in_chat'),
-      action: () => {
-        if (message.parentId) {
-          closeDialogCallback()
-          jumpToMessage({
-            accountId,
-            msgId: message.parentId,
-            // Currently the info message is always in the same chat
-            // as the message with `message.parentId`,
-            // but let's not pass `chatId` here, for future-proofing.
-            msgChatId: undefined,
-            highlight: true,
-            focus: true,
-            scrollIntoViewArg: { block: 'center' },
-          })
-        }
-      },
-    },
-    // Reply
-    // TODO also check `chat.canSend`, as with `showReply` in `Message.tsx`,
-    // and double-check other items.
-    !isInfoOrCallInvitation && {
-      label: tx('reply_noun'),
-      action: () => {
-        setQuoteInDraft(message.id)
-        closeDialogCallback()
-      },
-    },
-    // Reply privately -> only show in groups, don't show on info messages or outgoing messages
-    isGroup &&
-      !isInfoOrCallInvitation &&
-      message.fromId > C.DC_CONTACT_ID_LAST_SPECIAL && {
-        label: tx('reply_privately'),
-        action: () => {
-          privateReply(accountId, message)
-          closeDialogCallback()
-        },
-      },
-    // Copy to clipboard
-    {
-      label: tx('global_menu_edit_copy_desktop'),
-      action: () => {
-        navigator.clipboard.writeText(message.text || '')
-      },
-    },
-    // Message Info
-    {
-      label: tx('info'),
-      action: openMessageInfo.bind(null, openDialog, message),
-    },
-  ]
-}
 
 export default function ChatAuditLogDialog(
   props: {
