4c6130322c82e10df86342a8b1e69e6d2ab6f8a6
Nico de Haen <ndh@deltachat.de>
2025-10-10 12:13:52 +0200

    Add info dialogs for experimental settings

--- packages/frontend/src/components/Settings/ExperimentalFeatures.tsx
+++ packages/frontend/src/components/Settings/ExperimentalFeatures.tsx
@@ -1,31 +1,79 @@
 import React from 'react'
 
-import SettingsStoreInstance, { useSettingsStore } from '../../stores/settings'
+import SettingsStoreInstance, {
+  useSettingsStore,
+  type SettingsStoreState,
+} from '../../stores/settings'
 import DesktopSettingsSwitch from './DesktopSettingsSwitch'
 import useTranslationFunction from '../../hooks/useTranslationFunction'
 import SettingsSwitch from './SettingsSwitch'
 import { runtime } from '@deltachat-desktop/runtime-interface'
+import useDialog from '../../hooks/dialog/useDialog'
+import ConfirmationDialog from '../dialogs/ConfirmationDialog'
 
 export function ExperimentalFeatures() {
   const tx = useTranslationFunction()
+  const { openDialog } = useDialog()
+
+  const showExperimentalInfoDialog = async (
+    settingsKey: keyof SettingsStoreState['desktopSettings'],
+    updatedValue: boolean
+  ) => {
+    if (!updatedValue) {
+      return
+    }
+    let header = ''
+    let message = ''
+    if (settingsKey === 'enableAVCallsV2') {
+      header = 'Thanks for helping to debug "Calls"!'
+      message =
+        '• You can now debug calls using the "phone" icon in one-to-one-chats' +
+        "\n\n• The experiment is about making decentralised calls work and reliable at all, not about options or UI. We're happy about focused feedback at support.delta.chat"
+    } else if (settingsKey === 'enableBroadcastLists') {
+      header = 'Thanks for trying out "Channels"'
+      message = '• You can now create "Channels" from the "New Chat" dialog'
+    } else if (settingsKey === 'enableOnDemandLocationStreaming') {
+      header = 'Thanks for trying out "On-Demand Location Streaming"'
+      message =
+        '• If enabled you will find a map icon above the message list, which opens a map with shared locations of your contacts' +
+        '\n\n• Sharing your own location is only available in mobile clients'
+    }
+    message +=
+      '\n\n• If you want to quit the experimental feature, you can disable it at "Settings / Advanced"'
+    openDialog(ConfirmationDialog, {
+      header,
+      message,
+      confirmLabel: tx('ok'),
+      cb: () => {},
+    })
+  }
 
   return (
     <>
       {runtime.getRuntimeInfo().target === 'electron' && (
         <DesktopSettingsSwitch
           settingsKey='enableAVCallsV2'
-          label={tx('calls')}
+          label={'Debug Calls'}
           description='Work in progress…'
+          callback={value =>
+            showExperimentalInfoDialog('enableAVCallsV2', value)
+          }
         />
       )}
       <DesktopSettingsSwitch
         settingsKey='enableBroadcastLists'
         label={tx('channels')}
         description={tx('chat_new_channel_hint')}
+        callback={value =>
+          showExperimentalInfoDialog('enableBroadcastLists', value)
+        }
       />
       <DesktopSettingsSwitch
         settingsKey='enableOnDemandLocationStreaming'
         label={tx('pref_on_demand_location_streaming')}
+        callback={value =>
+          showExperimentalInfoDialog('enableOnDemandLocationStreaming', value)
+        }
       />
       <DesktopSettingsSwitch
         settingsKey='enableChatAuditLog'
