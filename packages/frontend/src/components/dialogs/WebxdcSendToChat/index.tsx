61faab3cecf6eacfbd23bb0797cc886cd6ac0efb
WofWca <wofwca@protonmail.com>
2025-10-24 14:13:17 +0400

    refactor: deduplicate fn, `useCreateDraftMessage`

--- packages/frontend/src/components/dialogs/WebxdcSendToChat/index.tsx
+++ packages/frontend/src/components/dialogs/WebxdcSendToChat/index.tsx
@@ -1,18 +1,14 @@
-import React, { useCallback } from 'react'
+import React from 'react'
 import { C } from '@deltachat/jsonrpc-client'
 
-import { BackendRemote } from '../../../backend-com'
 import { selectedAccountId } from '../../../ScreenController'
 import { runtime } from '@deltachat-desktop/runtime-interface'
 import { DialogFooter, FooterActionButton, FooterActions } from '../../Dialog'
-import ConfirmationDialog from '../ConfirmationDialog'
-import useChat from '../../../hooks/chat/useChat'
-import useDialog from '../../../hooks/dialog/useDialog'
 import useTranslationFunction from '../../../hooks/useTranslationFunction'
 
 import type { DialogProps } from '../../../contexts/DialogContext'
 import SelectChat from '../SelectChat'
-import { basename } from 'path'
+import useCreateDraftMessage from '../../../hooks/chat/useCreateDraftMesssage'
 
 type Props = {
   messageText: string | null
@@ -24,19 +20,21 @@ export default function WebxdcSaveToChatDialog(props: Props) {
 
   const tx = useTranslationFunction()
   const accountId = selectedAccountId()
-  const sendToChatAction = useSendToChatAction()
+  const createDraftMessage = useCreateDraftMessage()
 
   const onChatClick = async (chatId: number) => {
-    let path = null
-    if (file) {
-      path = await runtime.writeTempFileFromBase64(
-        file.file_name,
-        file.file_content
-      )
-    }
-    await sendToChatAction(accountId, chatId, messageText, path)
-    if (path) {
-      await runtime.removeTempFile(path)
+    const file2 = file
+      ? {
+          path: await runtime.writeTempFileFromBase64(
+            file.file_name,
+            file.file_content
+          ),
+          name: file.file_name,
+        }
+      : undefined
+    await createDraftMessage(accountId, chatId, messageText ?? '', file2)
+    if (file2) {
+      await runtime.removeTempFile(file2.path)
     }
     onClose()
   }
@@ -75,53 +73,5 @@ export default function WebxdcSaveToChatDialog(props: Props) {
         </DialogFooter>
       }
     />
-  )
-}
-
-function useSendToChatAction() {
-  const { openDialog } = useDialog()
-  const { selectChat } = useChat()
-  const tx = useTranslationFunction()
-
-  return useCallback(
-    async (
-      accountId: number,
-      chatId: number,
-      messageText: string | null,
-      file: string | null
-    ) => {
-      const chatP = BackendRemote.rpc.getBasicChatInfo(accountId, chatId)
-      const draft = await BackendRemote.rpc.getDraft(accountId, chatId)
-
-      selectChat(accountId, chatId)
-
-      if (draft) {
-        // ask if the draft should be replaced
-        const chat = await chatP
-        const continueProcess = await new Promise((resolve, _reject) => {
-          openDialog(ConfirmationDialog, {
-            message: tx('confirm_replace_draft', chat.name),
-            confirmLabel: tx('replace_draft'),
-            cb: resolve,
-          })
-        })
-        if (!continueProcess) {
-          return
-        }
-      }
-      const fileName = file ? basename(file) : null
-      await BackendRemote.rpc.miscSetDraft(
-        accountId,
-        chatId,
-        messageText,
-        file,
-        fileName,
-        null,
-        file ? 'File' : 'Text'
-      )
-
-      window.__reloadDraft && window.__reloadDraft()
-    },
-    [tx, openDialog, selectChat]
   )
 }
