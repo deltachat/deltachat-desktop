deltachat/deltachat-desktop

commit ce82703ab4d43770cdcfa10f3fb3f87551a8e3f2

Test: Create non chatmail account (#5342)
* Create plain mail chat

packages/e2e-tests/playwright-helper.ts
@@ -1,6 +1,13 @@
 import { expect, Page } from '@playwright/test'
 
-export const chatmailServer = 'https://ci-chatmail.testrun.org'
+const { config } = await import('dotenv')
+config()
+
+export const chatmailServerUrl = process.env.DC_CHATMAIL_SERVER
+
+export const mailServerUrl = process.env.DC_MAIL_SERVER
+
+export const mailServerToken = process.env.DC_MAIL_SERVER_TOKEN
 
 export const userNames = ['Alice', 'Bob', 'Chris', 'Denis', 'Eve']
 
@@ -60,9 +67,15 @@ export async function createUser(
   userName: string,
   page: Page,
   existingProfiles: User[],
-  isFirstOnboarding: boolean
+  isFirstOnboarding: boolean,
+  useChatmail: boolean = true
 ): Promise<User> {
-  const user = await createNewProfile(page, userName, isFirstOnboarding)
+  const user = await createNewProfile(
+    page,
+    userName,
+    isFirstOnboarding,
+    useChatmail
+  )
 
   expect(user.id).toBeDefined()
 
@@ -91,7 +104,8 @@ export const getUser = (index: number, existingProfiles: User[]) => {
 export async function createNewProfile(
   page: Page,
   name: string,
-  isFirstOnboarding: boolean
+  isFirstOnboarding: boolean,
+  useChatmail: boolean
 ): Promise<User> {
   await page.waitForSelector('.styles_module_account')
   const accountList = page.locator('.styles_module_account')
@@ -103,9 +117,10 @@ export async function createNewProfile(
   // create a new account
   await page.getByTestId('create-account-button').click()
 
-  await page.evaluate(
-    `navigator.clipboard.writeText('dcaccount:${chatmailServer}/new')`
-  )
+  const dcAccountLink = useChatmail
+    ? `dcaccount:${chatmailServerUrl}/new`
+    : `dcaccount:${mailServerUrl}//new_email?t=${mailServerToken}&n=ci_github`
+  await page.evaluate(`navigator.clipboard.writeText('${dcAccountLink}')`)
   await clickThroughTestIds(page, [
     'other-login-button',
     'scan-qr-login',
@@ -197,7 +212,8 @@ export async function createProfiles(
   number: number,
   existingProfiles: User[],
   page: Page,
-  browserName: string
+  browserName: string,
+  useChatmail: boolean = true
 ): Promise<void> {
   const hasProfileWithName = (name: string): boolean => {
     let hasProfile = false
@@ -215,7 +231,13 @@ export async function createProfiles(
   }
   for (let n = 0; n < number; n++) {
     if (!hasProfileWithName(userNames[n])) {
-      await createUser(userNames[n], page, existingProfiles, n === 0)
+      await createUser(
+        userNames[n],
+        page,
+        existingProfiles,
+        n === 0,
+        useChatmail
+      )
     } else {
       console.log('User already exists')
     }

.github/workflows/e2e.yml
@@ -83,6 +83,9 @@ jobs:
       NODE_ENV: test
       VERSION_INFO_GIT_REF: ${{ github.ref }}
       DC_ACCOUNTS_DIR: ../../e2e-tests/data/accounts
+      DC_CHATMAIL_SERVER: ${{ vars.CI_CHATMAIL_SERVER }}
+      DC_MAIL_SERVER: ${{ vars.CI_MAIL_SERVER }}
+      DC_MAIL_SERVER_TOKEN: ${{ secrets.CI_MAIL_SERVER_TOKEN }}
     timeout-minutes: 60
     runs-on: ${{ matrix.os }}
     strategy:

packages/e2e-tests/_env
@@ -1,2 +1,8 @@
 DC_ACCOUNTS_DIR=../../e2e-tests/data/accounts
 NODE_ENV=test
+
+# URL of chatmail user
+DC_CHATMAIL_SERVER=
+
+# URL of non chatmail server (including token)
+DC_MAIL_SERVER_URL=

packages/e2e-tests/tests/plain-email-tests.spec.ts
@@ -1,0 +1,90 @@
+import { test, expect, type Page } from '@playwright/test'
+
+import {
+  createProfiles,
+  User,
+  loadExistingProfiles,
+  deleteAllProfiles,
+  switchToProfile,
+  reloadPage,
+} from '../playwright-helper'
+
+test.describe.configure({ mode: 'serial' })
+
+let existingProfiles: User[] = []
+
+const numberOfProfiles = 4
+
+// https://playwright.dev/docs/next/test-retries#reuse-single-page-between-tests
+let page: Page
+
+test.beforeAll(async ({ browser }) => {
+  const contextForProfileCreation = await browser.newContext()
+  const pageForProfileCreation = await contextForProfileCreation.newPage()
+  const pageForTestsP = browser.newPage()
+
+  await reloadPage(pageForProfileCreation)
+
+  existingProfiles =
+    (await loadExistingProfiles(pageForProfileCreation)) ?? existingProfiles
+
+  await createProfiles(
+    numberOfProfiles,
+    existingProfiles,
+    pageForProfileCreation,
+    browser.browserType().name(),
+    false // useChatmail = false
+  )
+
+  await contextForProfileCreation.close()
+  page = await pageForTestsP
+  await reloadPage(page)
+})
+
+test.afterAll(async ({ browser }) => {
+  await page.close()
+
+  const context = await browser.newContext()
+  const pageForProfileDeletion = await context.newPage()
+  await reloadPage(pageForProfileDeletion)
+  await deleteAllProfiles(pageForProfileDeletion, existingProfiles)
+  await context.close()
+})
+
+test('check "New E-Mail" option is shown and a chat can be created', async () => {
+  const userA = existingProfiles[0]
+  const userB = existingProfiles[1]
+  const subject = 'Test Chat Subject'
+  const emailUserB = userB.address
+  // prepare last open chat for receiving user
+  await switchToProfile(page, userA.id)
+  await page.locator('#new-chat-button').click()
+
+  await expect(page.getByRole('button', { name: 'New Group' })).toBeVisible()
+
+  // Since we're on a non-Chatmail server, this button is supposed to be shown.
+  const newEmailButton = page.getByRole('button', { name: 'New E-Mail' })
+  await expect(newEmailButton).toBeVisible()
+
+  await newEmailButton.click()
+
+  await page.getByTestId('group-name-input').fill(subject)
+  await page.locator('#addmember').click()
+
+  await page.getByTestId('add-member-search').fill(emailUserB)
+
+  const contact = page
+    .locator('.styles_module_addMemberContactList li button')
+    .filter({ hasText: emailUserB })
+    .first()
+  await contact.click()
+
+  await page.getByTestId('ok').click()
+
+  await page.getByTestId('group-create-button').click()
+
+  const chatListItem = page
+    .locator('.chat-list .chat-list-item')
+    .filter({ hasText: subject })
+  await expect(chatListItem).toBeVisible()
+})

packages/frontend/src/components/dialogs/CreateChat/index.tsx
@@ -961,6 +961,7 @@ export const ChatSettingsSetNameAndProfileImage = ({
         <div className='group-name-input-wrapper'>
           <input
             className='group-name-input'
+            data-testid='group-name-input'
             placeholder={inputLabel}
             value={chatName}
             onChange={onChange}

commit 1689d80370a3db6334f779c7432bd280aeb15014

test: fix Playwright VSCode extension not working
The issue has apparently been introduced in
ce82703ab4d43770cdcfa10f3fb3f87551a8e3f2.

packages/e2e-tests/playwright-helper.ts
@@ -1,8 +1,13 @@
 /* eslint-disable no-console */
+import path from 'node:path'
 import { expect, test as base, Page } from '@playwright/test'
 
 const { config } = await import('dotenv')
-config()
+config({
+  // Need to specify path in order to make the Playwright VSCode extension work.
+  // Otherwise it complains that `DC_CHATMAIL_SERVER` is not defined.
+  path: path.join(import.meta.dirname, '.env'),
+})
 
 export const chatmailServerUrl = process.env.DC_CHATMAIL_SERVER
 