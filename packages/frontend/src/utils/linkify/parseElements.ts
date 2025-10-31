2f8d873cad4c50ed7e1194bc13a7cf3261fffdb9
WofWca <wofwca@protonmail.com>
2025-10-31 17:03:05 +0400

    refactor: remove error handling in `parseElements`
    Errors are handled in `parseAndRenderMessage` anyway,
    no need to do it twice.
    The nicest thing about this commit is that it removes a type cast.

--- packages/frontend/src/utils/linkify/parseElements.ts
+++ packages/frontend/src/utils/linkify/parseElements.ts
@@ -5,26 +5,21 @@ import * as linkify from 'linkifyjs'
  *
  * @param message - The message text to parse
  * @returns Array of parsed elements or null if parsing fails
+ * @throws maybe, if parsing failed. We don't expect it,
+ * but must handle it gracefully.
  */
 export function parseElements(message: string): linkify.MultiToken[] {
-  try {
-    const elements = linkify.tokenize(message).map(element => {
-      if (element.t === 'botcommand') {
-        const start = element.startIndex()
-        // do not render botcommands if they
-        // not start at index 0 or after a space
-        if (start > 0 && !/\s/.test(message[start - 1])) {
-          element.t = 'text'
-        }
+  return linkify.tokenize(message).map(element => {
+    if (element.t === 'botcommand') {
+      const start = element.startIndex()
+      // do not render botcommands if they
+      // not start at index 0 or after a space
+      if (start > 0 && !/\s/.test(message[start - 1])) {
+        element.t = 'text'
       }
-      // here we could also disable email links in fediverse mentions
-      // or hashtags that have no preceeding space
-      return element
-    })
-    return elements
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error('parseElements failed:', { input: message, error })
-    return [{ t: 'text', v: message } as linkify.MultiToken]
-  }
+    }
+    // here we could also disable email links in fediverse mentions
+    // or hashtags that have no preceeding space
+    return element
+  })
 }
