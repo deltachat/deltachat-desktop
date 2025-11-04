6954ed685fcb9c90485f84b00e696acd52530ba3
WofWca <wofwca@protonmail.com>
2025-11-04 19:43:14 +0400

    fix: some bugs when switching chats
    While working on `useDraft`, I found myself trying to implement
    complex logic to manage outdated async functions,
    deciding when and when not to set state.
    
    However, `key` is just much simpler and better.
    
    We do not rely on any kind of cross-chat state,
    so having `key={chat.id}` makes sense.
    
    The performance for switching chats appears to be about the same.

--- packages/frontend/src/components/MessageListView.tsx
+++ packages/frontend/src/components/MessageListView.tsx
@@ -15,7 +15,21 @@ export default function MessageListView({
   if (chatWithLinger && accountId) {
     return (
       <RecoverableCrashScreen reset_on_change_key={chatWithLinger.id}>
-        <MessageListAndComposer accountId={accountId} chat={chatWithLinger} />
+        <MessageListAndComposer
+          // Note that `key` has not always been here.
+          // Some downstream components still try to support variable `chatId`.
+          // To name a few:
+          // - `useDraft`'s `abortController`.
+          // - `hasChatChanged` in `MessageList`.
+          // - `useHasChanged2(chatId)` in `Composer`.
+          //
+          // However, most of that code is about resetting some state,
+          // so it probably can be removed.
+          // We do not and should actually rely on any kind of cross-chat state.
+          key={`${accountId}_${chatWithLinger.id}`}
+          accountId={accountId}
+          chat={chatWithLinger}
+        />
       </RecoverableCrashScreen>
     )
   }
