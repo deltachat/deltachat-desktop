deltachat/deltachat-desktop

commit f3630f587d4b109e0144d9917fee5c7235687cc9

fix: remove some key codes from keyhandler (#5667)

packages/frontend/src/keybindings.ts
@@ -78,6 +78,8 @@ export function keyDownEvent2Action(
   // When modifying this, don't forget to also update the corresponding
   // `aria-keyshortcuts` properties, and the "Keybindings" help window.
   if (!ev.repeat) {
+    // check if key is latin, if not we have to use ev.code instead of ev.key
+    const isLatin = ev.key && /^\p{Script=Latin}$/u.test(ev.key)
     // fire only on first press
     if (ev.altKey && ev.code === 'ArrowDown') {
       return KeybindAction.ChatList_SelectNextChat
@@ -97,8 +99,7 @@ export function keyDownEvent2Action(
       // }
     } else if (
       (ev.metaKey || ev.ctrlKey) &&
-      // fallback to KeyF code for keyboard layouts without f key
-      (ev.key === 'f' || ev.code === 'KeyF')
+      (ev.key === 'f' || (!isLatin && ev.code === 'KeyF'))
     ) {
       // https://github.com/deltachat/deltachat-desktop/issues/4579
       if (ev.shiftKey) {
@@ -107,10 +108,13 @@ export function keyDownEvent2Action(
       return KeybindAction.ChatList_FocusSearchInput
     } else if (
       (ev.metaKey || ev.ctrlKey) &&
-      (ev.key === 'n' || ev.code === 'KeyN')
+      (ev.key === 'n' || (!isLatin && ev.code === 'KeyN'))
     ) {
       return KeybindAction.NewChat_Open
-    } else if (ev.ctrlKey && (ev.key === 'm' || ev.code === 'KeyM')) {
+    } else if (
+      ev.ctrlKey &&
+      (ev.key === 'm' || (!isLatin && ev.code === 'KeyM'))
+    ) {
       return KeybindAction.Composer_Focus
     } else if (
       // Also consider adding this to `ev.repeat` when it stops being so sluggish
@@ -129,7 +133,7 @@ export function keyDownEvent2Action(
       return KeybindAction.Composer_SelectReplyToDown
     } else if (
       (ev.metaKey || ev.ctrlKey) &&
-      (ev.key === ',' || ev.code === 'Comma')
+      (ev.key === ',' || (!isLatin && ev.code === 'Comma'))
     ) {
       return KeybindAction.Settings_Open
     } else if (ev.code === 'Escape') {
@@ -154,7 +158,10 @@ export function keyDownEvent2Action(
       if ((ev.target as HTMLElement)?.id === 'composer-textarea') {
         return KeybindAction.MessageList_PageDown
       }
-    } else if ((ev.metaKey || ev.ctrlKey) && ev.key === '/') {
+    } else if (
+      ((ev.metaKey || ev.ctrlKey) && ev.key === '/') ||
+      (!isLatin && ev.code === 'Slash')
+    ) {
       return KeybindAction.KeybindingCheatSheet_Open
     }
   } else {

commit 2e106482d7500d911568279bac147c366a3ea8c9

fix: not being able to type question mark
The bug was introduced in
f3630f587d4b109e0144d9917fee5c7235687cc9
(https://github.com/deltachat/deltachat-desktop/pull/5667).

packages/frontend/src/keybindings.ts
@@ -159,8 +159,8 @@ export function keyDownEvent2Action(
         return KeybindAction.MessageList_PageDown
       }
     } else if (
-      ((ev.metaKey || ev.ctrlKey) && ev.key === '/') ||
-      (!isLatin && ev.code === 'Slash')
+      (ev.metaKey || ev.ctrlKey) &&
+      (ev.key === '/' || (!isLatin && ev.code === 'Slash'))
     ) {
       return KeybindAction.KeybindingCheatSheet_Open
     }