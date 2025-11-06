a1638a942dff7b1754b7ee6d4e382fb223bc48d7
Nico de Haen <ndh@deltachat.de>
2025-11-06 11:10:11 +0100

    test: use name instead addr

--- packages/e2e-tests/tests/group-tests.spec.ts
+++ packages/e2e-tests/tests/group-tests.spec.ts
@@ -191,7 +191,7 @@ test('Invite existing user to group', async ({ browserName }) => {
   await page.getByTestId('confirm-join-group').getByTestId('confirm').click()
   // userA invited you to group message
   await expect(page.locator('#message-list li').nth(1)).toContainText(
-    userA.address
+    userA.name
   )
   const composer = page.locator('textarea#composer-textarea')
   await expect(composer).not.toBeVisible({ timeout: 1 })
