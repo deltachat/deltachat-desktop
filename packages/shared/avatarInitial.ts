deltachat/deltachat-desktop

commit 18c4250ad55f52f91073c84724549dfa13c5322d

refactor: dedup `avatarInitial` fn
And this allows to reduce the complexity
of the `extractFirstEmojisFromString` function
(renamed to `countEmojisIfOnlyContainsEmoji`),
which now only does one thing.

`tsconfig`s need to get updated in order for it to not complain
about `Property 'Segmenter' does not exist on type 'typeof Intl'`.

packages/shared/avatarInitial.ts
@@ -1,0 +1,15 @@
+const userPerceivedCharacterSegmenter = new Intl.Segmenter(undefined, {
+  granularity: 'grapheme',
+})
+/**
+ * Works both in Node.js and in the browser.
+ *
+ * This could return an empty string, e.g. if the inputs are also empty strings.
+ */
+export function avatarInitial(name: string, addr?: string): string {
+  const str = name || addr || ''
+  return (
+    userPerceivedCharacterSegmenter.segment(str)[Symbol.iterator]().next().value
+      ?.segment ?? ''
+  )
+}

packages/frontend/src/components/AccountListSidebar/AccountItem.tsx
@@ -8,7 +8,7 @@ import {
   EffectfulBackendActions,
 } from '../../backend-com'
 import { runtime } from '@deltachat-desktop/runtime-interface'
-import { avatarInitial } from '../Avatar'
+import { avatarInitial } from '@deltachat-desktop/shared/avatarInitial'
 import { getLogger } from '../../../../shared/logger'
 import useTranslationFunction from '../../hooks/useTranslationFunction'
 import { useContextMenuWithActiveState } from '../ContextMenu'

packages/frontend/src/components/Avatar/index.tsx
@@ -9,7 +9,7 @@ import { runtime } from '@deltachat-desktop/runtime-interface'
 
 import styles from './styles.module.scss'
 import type { T } from '@deltachat/jsonrpc-client'
-import { extractFirstEmojisFromString } from '../message/MessageParser'
+import { avatarInitial } from '@deltachat-desktop/shared/avatarInitial'
 
 export function QRAvatar() {
   return (
@@ -19,17 +19,6 @@ export function QRAvatar() {
       </div>
     </div>
   )
-}
-
-export function avatarInitial(name: string, addr?: string) {
-  const emojis = extractFirstEmojisFromString(name)
-  if (emojis) {
-    return emojis[0]
-  } else {
-    const nameOrAddr = name || addr
-    const codepoint = nameOrAddr && nameOrAddr.codePointAt(0)
-    return codepoint ? String.fromCodePoint(codepoint).toUpperCase() : ''
-  }
 }
 
 type htmlDivProps = React.HTMLAttributes<HTMLDivElement>

packages/frontend/src/components/Settings/Profile.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { avatarInitial } from '../Avatar'
+import { avatarInitial } from '@deltachat-desktop/shared/avatarInitial'
 import LargeProfileImage from '../LargeProfileImage'
 
 import type { SettingsStoreState } from '../../stores/settings'

packages/frontend/src/components/conversations/emoji.ts
@@ -2,7 +2,7 @@
 // We only really need the emoji data of this module
 import EmojiConvertor from 'emoji-js-clean'
 import { getLogger } from '../../../../shared/logger'
-import { extractFirstEmojisFromString } from '../message/MessageParser'
+import { countEmojisIfOnlyContainsEmoji } from '../message/MessageParser'
 
 const log = getLogger('renderer/emoji')
 
@@ -58,15 +58,15 @@ export function getSizeClass(str: string) {
   if (str.length > MAX_STRING_LENGTH_FOR_BIG_EMOJI) {
     return undefined
   } else {
-    const emojis = extractFirstEmojisFromString(str, true)
+    const emojiCount = countEmojisIfOnlyContainsEmoji(str)
 
-    if (emojis == null || emojis.length > 8) {
+    if (emojiCount == null || emojiCount > 8) {
       return undefined
-    } else if (emojis.length > 6) {
+    } else if (emojiCount > 6) {
       return 'small'
-    } else if (emojis.length > 4) {
+    } else if (emojiCount > 4) {
       return 'medium'
-    } else if (emojis.length > 2) {
+    } else if (emojiCount > 2) {
       return 'large'
     } else {
       return 'jumbo'

packages/frontend/src/components/dialogs/EditProfileDialog/ProfileImageSelector.tsx
@@ -4,7 +4,7 @@ import ImageSelector from '../../ImageSelector'
 import ImageCropper from '../../ImageCropper'
 
 import { LastUsedSlot } from '../../../utils/lastUsedPaths'
-import { avatarInitial } from '../../Avatar'
+import { avatarInitial } from '@deltachat-desktop/shared/avatarInitial'
 import useDialog from '../../../hooks/dialog/useDialog'
 import { copyToBlobDir } from '../../../utils/copyToBlobDir'
 

packages/frontend/src/components/dialogs/ViewGroup.tsx
@@ -11,10 +11,8 @@ import {
   PseudoListItemAddMember,
 } from '../helpers/PseudoListItem'
 import ViewProfile from './ViewProfile'
-import {
-  avatarInitial,
-  shouldDisableClickForFullscreen as shouldDisableFullscreenAvatar,
-} from '../Avatar'
+import { avatarInitial } from '@deltachat-desktop/shared/avatarInitial'
+import { shouldDisableClickForFullscreen as shouldDisableFullscreenAvatar } from '../Avatar'
 import { DeltaInput } from '../Login-Styles'
 import { BackendRemote, onDCEvent } from '../../backend-com'
 import { selectedAccountId } from '../../ScreenController'

packages/frontend/src/components/message/Message.tsx
@@ -56,7 +56,7 @@ import type { PrivateReply } from '../../hooks/chat/usePrivateReply'
 import type { JumpToMessage } from '../../hooks/chat/useMessage'
 import { mouseEventToPosition } from '../../utils/mouseEventToPosition'
 import { useRovingTabindex } from '../../contexts/RovingTabindex'
-import { avatarInitial } from '../Avatar'
+import { avatarInitial } from '@deltachat-desktop/shared/avatarInitial'
 import { getLogger } from '@deltachat-desktop/shared/logger'
 
 const log = getLogger('Message')

commit 4e410c4c4eb902bcf285ade53f870a9d63ba43cd

fix: avatar initials not being always uppercase
The issue has been introduced in
18c4250ad55f52f91073c84724549dfa13c5322d
(https://github.com/deltachat/deltachat-desktop/pull/5652).

packages/shared/avatarInitial.ts
@@ -8,8 +8,6 @@ const userPerceivedCharacterSegmenter = new Intl.Segmenter(undefined, {
  */
 export function avatarInitial(name: string, addr?: string): string {
   const str = name || addr || ''
-  return (
-    userPerceivedCharacterSegmenter.segment(str)[Symbol.iterator]().next().value
-      ?.segment ?? ''
-  )
+  const segments = userPerceivedCharacterSegmenter.segment(str)
+  return segments[Symbol.iterator]().next().value?.segment.toUpperCase() ?? ''
 }