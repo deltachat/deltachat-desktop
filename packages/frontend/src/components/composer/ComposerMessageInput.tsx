1b9065b08f2d94d559c61f26fbc8511a91cf609b
Nico de Haen <ndh@deltachat.de>
2025-11-28 11:53:33 +0100

    fix: skip key arrow up if meta key is pressed (#5780)
    * fix: skip key arrow up if meta key is pressed
    
    * Apply suggestion from review
    
    ---------
    
    Co-authored-by: WofWca <wofwca@protonmail.com>

--- packages/frontend/src/components/composer/ComposerMessageInput.tsx
+++ packages/frontend/src/components/composer/ComposerMessageInput.tsx
@@ -165,6 +165,10 @@ export default class ComposerMessageInput extends React.Component<
       e.stopPropagation()
     } else if (
       e.key === 'ArrowUp' &&
+      !e.ctrlKey &&
+      !e.metaKey &&
+      !e.altKey &&
+      !e.shiftKey &&
       this.props.text.trim() === '' &&
       this.props.onArrowUpWhenEmpty
     ) {
