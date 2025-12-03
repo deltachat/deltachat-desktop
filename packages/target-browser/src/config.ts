75ffc86620cbc968b09c40df5438315f9b003c5c
Nico de Haen <ndh@deltachat.de>
2025-12-03 17:02:57 +0100

    disable console warning
    Co-authored-by: WofWca <wofwca@protonmail.com>

--- packages/target-browser/src/config.ts
+++ packages/target-browser/src/config.ts
@@ -11,6 +11,7 @@ const envPath = join(__dirname, '../.env')
 try {
   process.loadEnvFile?.(envPath)
 } catch (error) {
+  // eslint-disable-next-line no-console
   console.error(`Failed to load ${envPath}`, error)
 }
 
