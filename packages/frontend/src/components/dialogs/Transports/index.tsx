92665ef3487d9a03f6b4a6e168f2308ab2f4ae09
Nico de Haen <ndh@deltachat.de>
2025-12-06 17:23:31 +0100

    change: update multi device warning (#5820)

--- packages/frontend/src/components/dialogs/Transports/index.tsx
+++ packages/frontend/src/components/dialogs/Transports/index.tsx
@@ -77,11 +77,10 @@ export default function TransportsDialog(
       'bcc_self'
     )
     if (multiDeviceMode === '1') {
-      openAlertDialog({
+      await openAlertDialog({
         message:
-          'It is not possible to add transports when multi-device mode is enabled.',
+          'Note if using multi-device:\nbefore changing or adding transports make sure all other devices have at least version 2.33.0 installed. Otherwise they will run out of sync.',
       })
-      return
     }
     openDialog(BasicQrScanner, {
       onSuccess: async (result: string) => {
