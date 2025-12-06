c43eb30e3c440193a6325c55812e413f836448e4
Nico de Haen <ndh@deltachat.de>
2025-12-06 13:34:06 +0100

    revert wrong move of lines

--- packages/frontend/src/contexts/RovingTabindex.tsx
+++ packages/frontend/src/contexts/RovingTabindex.tsx
@@ -43,9 +43,9 @@ import React, {
 export function useRovingTabindex(elementRef: RefObject<HTMLElement | null>) {
   const context = useContext(RovingTabindexContext)
 
-  // If the active element has not been chosen yet,
-  // let' keep the default behavior (tabindex="0")
   const tabIndex: 0 | -1 =
+    // If the active element has not been chosen yet,
+    // let' keep the default behavior (tabindex="0")
     context.activeElement == null ||
     // eslint-disable-next-line react-hooks/refs
     context.activeElement === elementRef.current
