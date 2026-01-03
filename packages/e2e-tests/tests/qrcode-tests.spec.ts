a05e57d5e3c1bb1d2a7013df344f1c82d3ec83b2
Nico de Haen <ndh@deltachat.de>
2026-01-03 20:56:43 +0100

    build: update dependencies (#5838)
    * build: update dependencies
    
    * build: fix file exists error

--- packages/e2e-tests/tests/qrcode-tests.spec.ts
+++ packages/e2e-tests/tests/qrcode-tests.spec.ts
@@ -310,14 +310,8 @@ test('wrong qr code for onboarding shows error message', async ({
 // maybe move this to group tests?
 test.fixme('instant onboarding with group invite link', async () => {})
 
-test.fixme(
-  'instant onboarding fails with withdrawn group invite link',
-  async () => {}
-)
+test.fixme('instant onboarding fails with withdrawn group invite link', async () => {})
 
 test.fixme('instant onboarding works with DCLOGIN qr code', async () => {})
 
-test.fixme(
-  'instant onboarding with DCACCOUNT link from loaded image',
-  async () => {}
-)
+test.fixme('instant onboarding with DCACCOUNT link from loaded image', async () => {})
