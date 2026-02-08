0ac7097f2890511e2032f1771bdf1f015c527883
Nico de Haen <ndh@deltachat.de>
2026-02-08 22:04:49 +0100

    fix: useRpcFetch to avoid useEffect

--- packages/frontend/src/components/Settings/ConnectivityButton.tsx
+++ packages/frontend/src/components/Settings/ConnectivityButton.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useEffect, useMemo } from 'react'
 import { C } from '@deltachat/jsonrpc-client'
 
 import SettingsIconButton from './SettingsIconButton'
@@ -7,37 +7,39 @@ import { selectedAccountId } from '../../ScreenController'
 import ConnectivityDialog from '../dialogs/ConnectivityDialog'
 import useTranslationFunction from '../../hooks/useTranslationFunction'
 import useDialog from '../../hooks/dialog/useDialog'
+import { useRpcFetch } from '../../hooks/useFetch'
 
 export default function ConnectivityButton() {
   const { openDialog } = useDialog()
-  const [connectivityString, setConnectivityString] = useState('')
   const accountId = selectedAccountId()
   const tx = useTranslationFunction()
 
-  const updateConnectivity = useCallback(async () => {
-    const connectivity = await BackendRemote.rpc.getConnectivity(accountId)
-
-    let connectivityString = ''
-    if (connectivity >= C.DC_CONNECTIVITY_CONNECTED) {
-      connectivityString = window.static_translate('connectivity_connected')
-    } else if (connectivity >= C.DC_CONNECTIVITY_WORKING) {
-      connectivityString = window
-        .static_translate('connectivity_updating')
-        .replace('…', '')
-    } else if (connectivity >= C.DC_CONNECTIVITY_CONNECTING) {
-      connectivityString = window
-        .static_translate('connectivity_connecting')
-        .replace('…', '')
-    } else if (connectivity >= C.DC_CONNECTIVITY_NOT_CONNECTED) {
-      connectivityString = window.static_translate('connectivity_not_connected')
-    }
-    setConnectivityString(`(${connectivityString})`)
-  }, [accountId])
+  const { lingeringResult, refresh } = useRpcFetch(
+    BackendRemote.rpc.getConnectivity,
+    [accountId]
+  )
 
   useEffect(() => {
-    updateConnectivity()
-    return onDCEvent(accountId, 'ConnectivityChanged', updateConnectivity)
-  }, [updateConnectivity, accountId])
+    return onDCEvent(accountId, 'ConnectivityChanged', refresh)
+  }, [refresh, accountId])
+
+  const connectivityString = useMemo(() => {
+    if (!lingeringResult?.ok) {
+      return ''
+    }
+    const connectivity = lingeringResult.value
+    let str = ''
+    if (connectivity >= C.DC_CONNECTIVITY_CONNECTED) {
+      str = window.static_translate('connectivity_connected')
+    } else if (connectivity >= C.DC_CONNECTIVITY_WORKING) {
+      str = window.static_translate('connectivity_updating').replace('…', '')
+    } else if (connectivity >= C.DC_CONNECTIVITY_CONNECTING) {
+      str = window.static_translate('connectivity_connecting').replace('…', '')
+    } else if (connectivity >= C.DC_CONNECTIVITY_NOT_CONNECTED) {
+      str = window.static_translate('connectivity_not_connected')
+    }
+    return `(${str})`
+  }, [lingeringResult])
 
   return (
     <SettingsIconButton
