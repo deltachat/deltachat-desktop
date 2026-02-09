2b48a18ee43b66dbc1e35baa5942055390d15ec8
Nico de Haen <ndh@deltachat.de>
2026-02-09 14:25:54 +0100

    Add exception for qr scan dialog

--- packages/frontend/src/components/Dialog/DialogContent.tsx
+++ packages/frontend/src/components/Dialog/DialogContent.tsx
@@ -7,11 +7,22 @@ import styles from './styles.module.scss'
 
 type Props = PropsWithChildren<{
   className?: string
+  allowTopPadding?: boolean
 }>
 
-export default function DialogContent({ children, className }: Props) {
+export default function DialogContent({
+  children,
+  className,
+  allowTopPadding,
+}: Props) {
   return (
-    <div className={classNames(styles.dialogContent, className)}>
+    <div
+      className={classNames(
+        styles.dialogContent,
+        allowTopPadding && styles.allowTopPadding,
+        className
+      )}
+    >
       {children}
     </div>
   )
