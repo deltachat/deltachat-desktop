cc867f257db6d542c3a9e8c0cb1d75073054444b
WofWca <wofwca@protonmail.com>
2026-03-20 15:24:35 +0400

    refactor: move `openExternalHttpOrPromptToCopy`
    Follow-up to b21e6addf5c5d571dc3385f91e2322eb0b926c58
    (part of https://github.com/deltachat/deltachat-desktop/pull/5852).

--- packages/target-electron/src/deltachat/webxdc.ts
+++ packages/target-electron/src/deltachat/webxdc.ts
@@ -10,10 +10,8 @@ import Mime from 'mime-types'
 import {
   Menu,
   nativeImage,
-  shell,
   MenuItemConstructorOptions,
   dialog,
-  clipboard,
   IpcMainInvokeEvent,
 } from 'electron'
 import { join } from 'path'
@@ -41,6 +39,7 @@ import { setContentProtection } from '../content-protection.js'
 import { Server } from 'net'
 import { openHelpWindow } from '../windows/help.js'
 import { getCurrentLocaleDate } from '../load-translations.js'
+import { openExternalHttpOrPromptToCopy } from './link-clicks.js'
 
 const log = getLogger('main/deltachat/webxdc')
 
@@ -1318,41 +1317,3 @@ ipcMain.handle('delete_webxdc_account_data', async (_ev, accountId: number) => {
     }
   }
 })
-
-/**
- * Opens the link externally (in the browser) if it's an HTTP(S) link,
- * or opens a dialog suggesting to copy the link if it's not an HTTP(S) link.
- *
- * This should only be called if the user confirmed their intent
- * to open the link. Preferably the user also got a chance to look
- * at the link text, in its ASCII (i.e. Punycode) form.
- * If that's the case, this can be used for untrusted links.
- *
- * @param win the window on which the "copy?" dialog will be opened.
- *
- * @see `useOpenLinkSafely`
- * @see `openLink` in `html_email.ts`
- * @see `open_url`
- */
-export async function openExternalHttpOrPromptToCopy(
-  win: BrowserWindow,
-  url: string
-): Promise<void> {
-  if (
-    url.toLowerCase().startsWith('https:') ||
-    url.toLowerCase().startsWith('http:')
-  ) {
-    shell.openExternal(url)
-  } else if (url) {
-    await dialog
-      .showMessageBox(win, {
-        buttons: [tx('no'), tx('menu_copy_link_to_clipboard')],
-        message: tx('ask_copy_unopenable_link_to_clipboard', url),
-      })
-      .then(({ response }) => {
-        if (response == 1) {
-          clipboard.writeText(url)
-        }
-      })
-  }
-}
