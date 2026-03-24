deltachat/deltachat-desktop

commit ac5e5979e7f84a38ca18a74106278e67d62f4d6b

fix: do not cover video controls (#6096)

packages/frontend/src/components/attachment/messageAttachment.tsx
@@ -172,12 +172,7 @@ export default function Attachment({
     }
     // the native fullscreen option is better right now so we don't need to open our own one
     return (
-      <div
-        className={classNames(
-          'message-attachment-media',
-          withCaption ? 'content-below' : null
-        )}
-      >
+      <div className={classNames('message-attachment-media')}>
         <video
           className='attachment-content video-content'
           src={runtime.transformBlobURL(message.file)}

packages/frontend/scss/message/_message.scss
@@ -345,6 +345,18 @@ $info-text-max-width: 400px;
   }
 }
 
+.message.outgoing.video-only,
+.message.incoming.video-only {
+  .msg-container {
+    /* make the whole container black, as videos always have a black background */
+    background-color: #000;
+    .message-attachment-media {
+      /* nneeded to avoid metadata video controls overlap */
+      padding-bottom: 12px;
+    }
+  }
+}
+
 .message.type-sticker {
   .msg-container {
     background-color: transparent !important;

packages/frontend/src/components/message/Message.tsx
@@ -28,7 +28,7 @@ import {
   enterEditMessageMode,
 } from './messageFunctions'
 import Attachment from '../attachment/messageAttachment'
-import { isGenericAttachment, isImage } from '../attachment/Attachment'
+import { isGenericAttachment, isImage, isVideo } from '../attachment/Attachment'
 import { runtime } from '@deltachat-desktop/runtime-interface'
 import { ConversationType } from './MessageList'
 import { getDirection } from '../../utils/getDirection'
@@ -869,6 +869,7 @@ export default function Message(props: {
         direction,
         styles.message,
         rovingTabindex.className,
+        isWithoutText && isVideo(fileMime) ? 'video-only' : '',
         {
           [styles.withReactions]: message.reactions,
           'type-sticker': viewType === 'Sticker',

commit 627b36bbc8ddb4c04c3638cc6e1f80a9e78899a9

fix: video overlapping with below text
This partially reverts
ac5e5979e7f84a38ca18a74106278e67d62f4d6b
(https://github.com/deltachat/deltachat-desktop/pull/6096),
which introduced this issue.

What I meant in
[my suggestion](https://github.com/deltachat/deltachat-desktop/pull/6096#discussion_r2919693993)
on that MR is that we don't need to _change_ the condition
when `content-below` is applied, and not to completely remove it.

I have tested that the metadata still doesn't overlap
with video controls if there is no text,
and that the video doesn't overlap with text if there _is_ text.

packages/frontend/src/components/attachment/messageAttachment.tsx
@@ -174,7 +174,12 @@ export default function Attachment({
     }
     // the native fullscreen option is better right now so we don't need to open our own one
     return (
-      <div className={classNames('message-attachment-media')}>
+      <div
+        className={classNames(
+          'message-attachment-media',
+          withCaption ? 'content-below' : null
+        )}
+      >
         <video
           className='attachment-content video-content'
           src={runtime.transformBlobURL(message.file)}