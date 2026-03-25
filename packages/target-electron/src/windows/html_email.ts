a80f8c9d0a4f3e1300082ad383714dbd16d1822b
WofWca <wofwca@protonmail.com>
2026-03-25 17:53:01 +0400

    fix: WebXDC, HTML viewer: handle all invite links
    The `openpgp4fpr:` and `https://i.delta.chat` links
    are pretty much the same.
    I see no reason why we need to handle one kind only in WebXDC
    and the other only in the HTML email viewer.
    The handling in these two places has been introduced
    in separate MRs several months apart,
    and thus it's probably just an oversight.
    See
    - 14f3f45119abacddcebed059b4d069b7e246b906
      (https://github.com/deltachat/deltachat-desktop/pull/3683).
    - 6d7be7d260c7dab918509de6d5569e08f53c37fd
      (https://github.com/deltachat/deltachat-desktop/pull/3457).

--- packages/target-electron/src/windows/html_email.ts
+++ packages/target-electron/src/windows/html_email.ts
@@ -509,7 +509,11 @@ function makeBrowserView(
     }`)
 
   const openLink = (url: string) => {
-    if (url.startsWith('mailto:') || isInviteLink(url)) {
+    if (
+      url.startsWith('mailto:') ||
+      url.startsWith('openpgp4fpr:') ||
+      isInviteLink(url)
+    ) {
       open_url(url)
       mainWindow.window?.show()
     } else {
