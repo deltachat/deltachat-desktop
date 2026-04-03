b5b69038a636515cacb171776dab7f7001dbec00
WofWca <wofwca@protonmail.com>
2026-04-03 12:46:40 +0400

    feat: "Mark as Unread" menu item (and "as Read")
    Closes https://github.com/deltachat/deltachat-desktop/issues/6181.

--- packages/frontend/src/components/chat/ChatContextMenu.tsx
+++ packages/frontend/src/components/chat/ChatContextMenu.tsx
@@ -59,6 +59,7 @@ type ChatListItem = Pick<
   | 'isPinned'
   | 'isArchived'
   | 'isMuted'
+  | 'freshMessageCounter'
   | 'isDeviceTalk'
   | 'isSelfTalk'
   | 'isSelfInGroup'
@@ -79,6 +80,7 @@ export function fullChatToChatListItem(chat: T.FullChat): ChatListItem {
     isPinned: chat.pinned,
     isArchived: chat.archived,
     isMuted: chat.isMuted,
+    freshMessageCounter: chat.freshMessageCounter,
     isDeviceTalk: chat.isDeviceChat,
     isSelfTalk: chat.isSelfTalk,
     isSelfInGroup: chat.selfInGroup,
@@ -509,6 +511,29 @@ export function useChatContextMenu(): {
         action: onSearchInChat,
       },
       !isMainView && pin,
+      !isMainView &&
+        (chatListItems.some(c => c.freshMessageCounter > 0)
+          ? {
+              label: tx('mark_as_read'),
+              action: () =>
+                Promise.all(
+                  chatListItems.map(c =>
+                    BackendRemote.rpc.marknoticedChat(accountId, c.id)
+                  )
+                ),
+            }
+          : {
+              label: tx('mark_as_unread_short'),
+              action: () =>
+                Promise.all(
+                  chatListItems.map(c => {
+                    if (c.id === activeChatId) {
+                      unselectChat()
+                    }
+                    return BackendRemote.rpc.markfreshChat(accountId, c.id)
+                  })
+                ),
+            }),
       ephemeralMessagesMenuItem,
       muteMenuItem,
       archive,
