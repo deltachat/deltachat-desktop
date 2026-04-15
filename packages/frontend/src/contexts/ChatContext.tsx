deltachat/deltachat-desktop

commit 0bddb68a127f8626d81e8215c899a0c7bd94e8ce

refactor: move "switch to archive on selectChat"

packages/frontend/src/contexts/ChatContext.tsx
@@ -8,6 +8,7 @@ import type { RefObject, PropsWithChildren } from 'react'
 import type { T } from '@deltachat/jsonrpc-client'
 import { useRpcFetch } from '../hooks/useFetch'
 import { getLogger } from '@deltachat-desktop/shared/logger'
+import { useHasChanged2 } from '../hooks/useHasChanged'
 
 const log = getLogger('ChatContext')
 
@@ -147,19 +148,6 @@ export const ChatProvider = ({
         resolvePendingSetChatPromise.current = r
       })
 
-      setChatPromise.then(nextChat => {
-        if (nextChat == null) {
-          return
-        }
-        // Switch to "archived" view if selected chat is there
-        // @TODO: We probably want this to be part of the UI logic instead
-        ActionEmitter.emitAction(
-          nextChat.archived
-            ? KeybindAction.ChatList_SwitchToArchiveView
-            : KeybindAction.ChatList_SwitchToNormalView
-        )
-      })
-
       return setChatPromise.then(nextChat => nextChat != null)
     },
     [accountId, chatId]
@@ -170,6 +158,16 @@ export const ChatProvider = ({
   }, [])
 
   unselectChatRef.current = unselectChat
+
+  if (useHasChanged2(chatNoLinger?.id) && chatNoLinger != undefined) {
+    // Switch to "archived" view if selected chat is there
+    // @TODO: We probably want this to be part of the UI logic instead
+    ActionEmitter.emitAction(
+      chatNoLinger.archived
+        ? KeybindAction.ChatList_SwitchToArchiveView
+        : KeybindAction.ChatList_SwitchToNormalView
+    )
+  }
 
   const refreshChat = chatFetch?.refresh
   // Subscribe to events coming from the core

commit 562e787b877250d4c21cc8e37199ff56dd81d52d

fix: chat list switching between archive by itself
Closes https://github.com/deltachat/deltachat-desktop/issues/6253.

This bug has been introduced in
0bddb68a127f8626d81e8215c899a0c7bd94e8ce
(https://github.com/deltachat/deltachat-desktop/pull/5824).
The error is that `chatNoLinger` is `undefined` while
the chat is getting reloaded (`chatFetch.refresh`),
so `useHasChanged2()` returns `true` when the chat finishes refreshing,
whereas we only want to perform the "is archived?" check
on the initial load of the chat.

packages/frontend/src/contexts/ChatContext.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 
 import { ActionEmitter, KeybindAction } from '../keybindings'
 import { markChatAsSeen, saveLastChatId } from '../backend/chat'
@@ -166,7 +166,17 @@ export const ChatProvider = ({
   // eslint-disable-next-line react-hooks/refs
   unselectChatRef.current = unselectChat
 
-  if (useHasChanged2(chatNoLinger?.id) && chatNoLinger != undefined) {
+  const isArchivedChecked = useRef(false)
+  if (useHasChanged2(chatId) && chatId != undefined) {
+    // eslint-disable-next-line react-hooks/refs
+    isArchivedChecked.current = false
+  }
+  if (
+    // eslint-disable-next-line react-hooks/refs
+    !isArchivedChecked.current &&
+    chatNoLinger != undefined
+  ) {
+    isArchivedChecked.current = true
     // Switch to "archived" view if selected chat is there
     // @TODO: We probably want this to be part of the UI logic instead
     ActionEmitter.emitAction(