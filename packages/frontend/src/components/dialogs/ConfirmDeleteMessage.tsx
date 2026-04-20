7fcfb41f1a1cf99780b5145416074fd8df4b305e
WofWca <wofwca@protonmail.com>
2026-04-20 21:31:10 +0400

    feat: message multiselect: support deletion

--- packages/frontend/src/components/dialogs/ConfirmDeleteMessage.tsx
+++ packages/frontend/src/components/dialogs/ConfirmDeleteMessage.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useCallback, useContext } from 'react'
 
 import Dialog, {
   DialogBody,
@@ -13,10 +13,17 @@ import type { DialogProps } from '../../contexts/DialogContext'
 import { BackendRemote, Type } from '../../backend-com'
 import { C } from '@deltachat/jsonrpc-client'
 import { ScreenContext } from '../../contexts/ScreenContext'
+import { useFetch } from '../../hooks/useFetch'
+import { unknownErrorToString } from '@deltachat-desktop/shared/unknownErrorToString'
 
 export type Props = {
   accountId: number
-  msg: Type.Message
+  messageIds: Array<Type.Message['id']>
+  /**
+   * If some messages from {@linkcode messageIds} are already loaded,
+   * provide them here for better performance.
+   */
+  loadedMessages: { [id: Type.Message['id']]: Type.Message | undefined }
   chat: Type.FullChat
 } & DialogProps
 
@@ -24,19 +31,65 @@ export default function ConfirmDeleteMessageDialog(props: Props) {
   const tx = useTranslationFunction()
   const { userFeedback } = useContext(ScreenContext)
 
-  const { accountId, msg, chat, onClose } = props
+  const { accountId, chat, onClose, messageIds, loadedMessages } = props
 
-  const deleteForAllPossible =
-    msg.sender.id === C.DC_CONTACT_ID_SELF &&
-    !msg.isInfo &&
-    chat.canSend &&
-    !chat.isSelfTalk &&
-    chat.isEncrypted
+  const deleteForAllPossibleFetch = useFetch(
+    useCallback(async () => {
+      if (!chat.canSend || chat.isSelfTalk || !chat.isEncrypted) {
+        return false
+      }
+
+      // Check all the loaded messages
+      function msgDeleteForAllPossible(msg: Type.Message): boolean {
+        return msg.sender.id === C.DC_CONTACT_ID_SELF && !msg.isInfo
+      }
+      if (
+        messageIds
+          .map(id => loadedMessages[id])
+          .filter(m => m != undefined)
+          .some(m => !msgDeleteForAllPossible(m))
+      ) {
+        return false
+      }
+      const missingMessageIds = messageIds.filter(
+        id => loadedMessages[id] == undefined
+      )
+      if (missingMessageIds.length === 0) {
+        return true
+      }
+
+      // Still not decided. Load missing messages and check them as well.
+      if (
+        Object.values(
+          await BackendRemote.rpc.getMessages(accountId, missingMessageIds)
+        ).some((m, ind) => {
+          if (m.kind === 'loadingError') {
+            throw new Error(
+              `Could not load message with ID ${missingMessageIds[ind]}`
+            )
+          }
+
+          return !msgDeleteForAllPossible(m)
+        })
+      ) {
+        return false
+      }
+      return true
+    }, [
+      accountId,
+      chat.canSend,
+      chat.isEncrypted,
+      chat.isSelfTalk,
+      loadedMessages,
+      messageIds,
+    ]),
+    []
+  )
 
   const deleteMessage = (deleteForEveryone: boolean) => {
     if (deleteForEveryone) {
       BackendRemote.rpc
-        .deleteMessagesForAll(accountId, [msg.id])
+        .deleteMessagesForAll(accountId, messageIds)
         .catch((err: Error) => {
           userFeedback({
             type: 'error',
@@ -45,7 +98,7 @@ export default function ConfirmDeleteMessageDialog(props: Props) {
         })
     } else {
       BackendRemote.rpc
-        .deleteMessages(accountId, [msg.id])
+        .deleteMessages(accountId, messageIds)
         .catch((err: Error) => {
           userFeedback({
             type: 'error',
@@ -60,29 +113,66 @@ export default function ConfirmDeleteMessageDialog(props: Props) {
     <Dialog onClose={onClose}>
       <DialogBody>
         <DialogContent>
-          <p className='whitespace'>{tx('ask_delete_message')}</p>
+          <p className='whitespace'>
+            {messageIds.length > 1
+              ? tx('ask_delete_messages', messageIds.length.toString(), {
+                  quantity: messageIds.length,
+                })
+              : tx('ask_delete_message')}
+          </p>
         </DialogContent>
+        {deleteForAllPossibleFetch.result?.ok === false && (
+          <p>
+            {tx(
+              'error_x',
+              'Failed to check if we can delete the messages for everyone:\n' +
+                unknownErrorToString(deleteForAllPossibleFetch.result.err)
+            )}
+          </p>
+        )}
       </DialogBody>
       <DialogFooter>
         <FooterActions>
-          <FooterActionButton onClick={() => onClose()} data-testid='cancel'>
-            {tx('cancel')}
-          </FooterActionButton>
+          {/* Don't show any buttons while loading
+          to ensure that the user doesn't click the wrong one
+          due to layout shifting when we finish loading.
+          But display the actual (disabled) button element
+          to ensure that the height of the dialog remains the same. */}
           <FooterActionButton
-            styling={'danger'}
-            onClick={() => deleteMessage(false)}
-            data-testid='delete_for_me'
+            onClick={() => onClose()}
+            data-testid='cancel'
+            disabled={deleteForAllPossibleFetch.loading}
           >
-            {chat.isSelfTalk ? tx('delete') : tx('delete_for_me')}
+            {deleteForAllPossibleFetch.loading ? tx('loading') : tx('cancel')}
           </FooterActionButton>
-          {deleteForAllPossible && (
-            <FooterActionButton
-              styling={'danger'}
-              onClick={() => deleteMessage(true)}
-              data-testid='delete_for_everyone'
-            >
-              {tx('delete_for_everyone')}
-            </FooterActionButton>
+          {deleteForAllPossibleFetch.loading ? (
+            <p>{tx('loading')}</p>
+          ) : (
+            <>
+              <FooterActionButton
+                styling={'danger'}
+                onClick={() => deleteMessage(false)}
+                data-testid='delete_for_me'
+              >
+                {chat.isSelfTalk ? tx('delete') : tx('delete_for_me')}
+              </FooterActionButton>
+              {/* The error is handled above.
+              One might think that it's better to show the button by default
+              if we failed to check if deleting for everyone is possible,
+              and then simply showing an error if Core returns an error.
+              But we shouldn't do it until the behavior
+              of `deleteMessagesForAll()` is clearly defined and documented. */}
+              {deleteForAllPossibleFetch.result.ok &&
+                deleteForAllPossibleFetch.result.value && (
+                  <FooterActionButton
+                    styling={'danger'}
+                    onClick={() => deleteMessage(true)}
+                    data-testid='delete_for_everyone'
+                  >
+                    {tx('delete_for_everyone')}
+                  </FooterActionButton>
+                )}
+            </>
           )}
         </FooterActions>
       </DialogFooter>
