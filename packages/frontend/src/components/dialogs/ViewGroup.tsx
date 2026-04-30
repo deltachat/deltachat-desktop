deltachat/deltachat-desktop

commit 15ff5058bf017e96e23d12f78167d1b6e804f3b7

chore: remove strings from untranslated that are available in upstream (#6289)

packages/frontend/src/components/dialogs/ViewGroup.tsx
@@ -614,9 +614,7 @@ export function EditGroupNameDialog({
                 marginBottom: '-18px',
               }}
             >
-              {!isBroadcast
-                ? tx('group_please_enter_group_name')
-                : tx('please_enter_channel_name')}
+              {!tx('please_enter_chat_name')}
             </p>
           )}
           <DeltaTextarea

packages/frontend/src/components/AudioRecorder/AudioRecorder.tsx
@@ -182,7 +182,7 @@ export const AudioRecorder = ({
     return (
       <button
         type='button'
-        aria-label={tx('voice_send')}
+        aria-label={tx('voice_message')}
         className={styles.microphoneButton}
         onClick={() => onRecordingStart()}
       >

packages/frontend/src/components/chat/ChatContextMenu.tsx
@@ -276,7 +276,7 @@ function buildViewEditMenuItems(
     },
     // Edit Channel
     relatedChat.chatType === 'OutBroadcast' && {
-      label: tx('edit_channel'),
+      label: tx('menu_view_profile'),
       action: onViewGroup,
     },
   ]

packages/frontend/src/components/dialogs/CreateChat/index.tsx
@@ -850,21 +850,20 @@ export const ChatSettingsSetNameAndProfileImage = ({
   switch (groupType) {
     case GroupType.REGULAR_GROUP:
       inputLabel = tx('group_name')
-      missingNameErrorText = tx('group_please_enter_group_name')
+      missingNameErrorText = tx('please_enter_chat_name')
       break
     case GroupType.PLAIN_EMAIL:
       inputLabel = tx('subject')
-      // TODO do we need another string?
-      missingNameErrorText = tx('group_please_enter_group_name')
+      missingNameErrorText = tx('please_enter_chat_name')
       break
     case GroupType.BROADCAST_LIST:
       inputLabel = tx('name_desktop')
-      missingNameErrorText = tx('please_enter_channel_name')
+      missingNameErrorText = tx('please_enter_chat_name')
       break
     default: {
       const _assert: never = groupType
       inputLabel = tx('group_name')
-      missingNameErrorText = tx('group_please_enter_group_name')
+      missingNameErrorText = tx('please_enter_chat_name')
       break
     }
   }

packages/frontend/src/components/dialogs/ViewProfile/index.tsx
@@ -295,7 +295,7 @@ export function ViewProfileInner({
             {contact.isBlocked && (
               <div>
                 <i className='material-svg-icon material-icon-blocked' />
-                {tx('contact_is_blocked')}
+                {tx('contact_blocked')}
               </div>
             )}
           </div>

packages/frontend/src/hooks/useProcessQr.ts
@@ -183,7 +183,7 @@ export default function useProcessQR() {
         }
       } else if (qr.kind === 'askJoinBroadcast') {
         const userConfirmed = await openConfirmationDialog({
-          message: tx('instant_onboarding_confirm_channel', qr.name),
+          message: tx('instant_onboarding_confirm_group', qr.name),
           confirmLabel: tx('ok'),
           dataTestid: 'ask-create-profile-and-join-channel',
         })

commit a02a627e64a1f8269f900861ff0e02ed24d33ca8

fix: invisible error msg when editing group name
The issue has been introduced in
15ff5058bf017e96e23d12f78167d1b6e804f3b7
(https://github.com/deltachat/deltachat-desktop/pull/6289).

packages/frontend/src/components/dialogs/ViewGroup.tsx
@@ -616,7 +616,7 @@ export function EditGroupNameDialog({
                   marginBottom: '-18px',
                 }}
               >
-                {!tx('please_enter_chat_name')}
+                {tx('please_enter_chat_name')}
               </p>
             )}
             <DeltaTextarea