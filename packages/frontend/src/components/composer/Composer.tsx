2c162e12ed557e2f1b1f97b4f570c95f160dd429
1009 <enrico@privacyrequired.com>
2026-05-02 17:08:51 +0200

    test: add test for focus after send button clicked/touched

--- packages/frontend/src/components/composer/Composer.tsx
+++ packages/frontend/src/components/composer/Composer.tsx
@@ -773,6 +773,7 @@ const Composer = forwardRef<
               disabled={sendButtonAction == null}
               onClick={sendButtonAction ?? (() => {})}
               aria-label={tx('menu_send')}
+              data-testid='send-button'
               aria-keyshortcuts={ariaSendShortcut}
             >
               <div className='paper-plane'></div>
