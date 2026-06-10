jhayashi/deltachat-desktop-1

feat: `Ctrl + Alt + PageDown` to switch accounts
Why not `Ctrl + Shift + PageDown`? Because it feels like
an "alternative action" on the message list
where we have `Ctrl + PageDown` to switch between chats.
Whereas "Alt" is a little more awkward,
so it feels further from the action of switching between chats.

packages/frontend/src/components/KeyboardShortcutHint.tsx
@@ -165,6 +165,13 @@ export function getKeybindings(
         keyBindings: [[ctrl, 'Shift', 'F']],
       },
       {
+        title: tx('switch_between_accounts'),
+        keyBindings: [
+          ['Control', 'Alt', 'PageUp'],
+          ['Control', 'Alt', 'PageDown'],
+        ],
+      },
+      {
         title: tx('switch_between_chats'),
         keyBindings: [
           ['Alt', 'ArrowUp'],

deltachat/deltachat-desktop

packages/frontend/src/components/KeyboardShortcutHint.tsx
@@ -176,6 +176,13 @@ export function getKeybindings(
         ],
       },
       {
+        title: tx('switch_account'),
+        keyBindings: [
+          ['Control', 'Alt', 'PageUp'],
+          ['Control', 'Alt', 'PageDown'],
+        ],
+      },
+      {
         title: tx('scroll_messages'),
         keyBindings: [['PageUp'], ['PageDown']],
       },