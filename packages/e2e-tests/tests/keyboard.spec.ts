373f5e263c49f0a3ec829a108936bdc65a8d0bdd
WofWca <wofwca@protonmail.com>
2026-06-10 16:04:07 +0400

    test: migrate keyboard tests to dummy backup acc
    So that these tests can be run offline.

--- packages/e2e-tests/tests/keyboard.spec.ts
+++ packages/e2e-tests/tests/keyboard.spec.ts
@@ -1,12 +1,10 @@
 import { expect, type Page } from '@playwright/test'
 
 import {
-  createProfiles,
-  User,
-  loadExistingProfiles,
-  deleteAllProfiles,
   reloadPage,
   test,
+  importDummyProfileFromBackup,
+  deleteSelectedProfile,
 } from '../playwright-helper'
 
 test.describe.configure({
@@ -15,33 +13,13 @@ test.describe.configure({
 
 expect.configure({ timeout: 5_000 })
 
-let existingProfiles: User[] = []
-
-const numberOfProfiles = 1
-
 // https://playwright.dev/docs/next/test-retries#reuse-single-page-between-tests
 let page: Page
 
-test.beforeAll(async ({ browser, isChatmail }) => {
-  const contextForProfileCreation = await browser.newContext()
-  const pageForProfileCreation = await contextForProfileCreation.newPage()
-
-  await reloadPage(pageForProfileCreation)
-
-  existingProfiles =
-    (await loadExistingProfiles(pageForProfileCreation)) ?? existingProfiles
-
-  await createProfiles(
-    numberOfProfiles,
-    existingProfiles,
-    pageForProfileCreation,
-    browser.browserType().name(),
-    isChatmail
-  )
-
-  await contextForProfileCreation.close()
+test.beforeAll(async ({ browser }) => {
   page = await browser.newPage()
   await reloadPage(page)
+  await importDummyProfileFromBackup(page)
 
   // Shortcuts don't seem to work otherwise. Maybe a Playwright issue.
   await page.locator('body').click()
@@ -61,7 +39,7 @@ test.afterAll(async ({ browser }) => {
   const context = await browser.newContext()
   const pageForProfileDeletion = await context.newPage()
   await reloadPage(pageForProfileDeletion)
-  await deleteAllProfiles(pageForProfileDeletion, existingProfiles)
+  await deleteSelectedProfile(pageForProfileDeletion)
   await context.close()
 })
 
