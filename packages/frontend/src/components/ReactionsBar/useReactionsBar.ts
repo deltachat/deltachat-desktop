6d09e8bbf556c3926b0e9b511dc439920bc63f63
Nico de Haen <ndh@deltachat.de>
2026-06-24 20:47:46 +0200

    hide reaction button for saved messages

--- packages/frontend/src/components/ReactionsBar/useReactionsBar.ts
+++ packages/frontend/src/components/ReactionsBar/useReactionsBar.ts
@@ -46,7 +46,7 @@ export default function useReactionsBar(): UseReactionsBar {
 
 /** Returns true if user should be able to send reactions to a message */
 export function showReactionsUi(message: T.Message, chat: T.FullChat): boolean {
-  return chat.canSend && !message.isInfo
+  return chat.canSend && !message.isInfo && message.originalMsgId === null
 }
 
 function getMyReaction(reactions: T.Message['reactions']): string | undefined {
