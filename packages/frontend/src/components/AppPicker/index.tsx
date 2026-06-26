deltachat/deltachat-desktop

commit 50de20982d2620640e175ec6d593f0ce04fce4d9

improvement: better app picker fetch error msg
Especially now that it's possible to set a custom app store URL,
let's make it clearer what's going on.

packages/frontend/src/components/AppPicker/index.tsx
@@ -93,12 +93,13 @@ export function AppPicker({ onAppSelected }: Props) {
     // Ensure that it ends with a slash.
     return res.endsWith('/') ? (res as `${string}/`) : (`${res}/` as const)
   }, [settingsStore])
+  const appListUrl = appStoreUrl + 'xdcget-lock.json'
 
-  const fetchApps = useCallback(async (appStoreUrl: string) => {
+  const fetchApps = useCallback(async (appListUrl: string) => {
     // This may throw, e.g. on network error.
     const response = await BackendRemote.rpc.getHttpResponse(
       selectedAccountId(),
-      appStoreUrl + 'xdcget-lock.json'
+      appListUrl
     )
     const apps = getJsonFromBase64(response.blob) as AppInfo[]
     if (apps == null) {
@@ -118,7 +119,7 @@ export function AppPicker({ onAppSelected }: Props) {
     }
     return apps
   }, [])
-  const appsFetch = useFetch(fetchApps, [appStoreUrl])
+  const appsFetch = useFetch(fetchApps, [appListUrl])
   const apps = appsFetch.result?.ok ? appsFetch.result.value : null
 
   const appsFetchFailed = appsFetch.result?.ok === false
@@ -353,7 +354,8 @@ export function AppPicker({ onAppSelected }: Props) {
                 ? tx('offline')
                 : tx(
                     'error_x',
-                    'Failed to fetch apps:\n' +
+                    'Failed to fetch apps list:\n' +
+                      `(at ${appListUrl})\n` +
                       unknownErrorToString(appsFetch.result.err)
                   )}
             </div>

commit 7b31678a16f598f907182c4e0fbea26ad6f884cd

refactor: fix linter warning for `appListUrl`
The issue was introduced in 50de20982d2620640e175ec6d593f0ce04fce4d9
(https://github.com/deltachat/deltachat-desktop/pull/6217).

packages/frontend/src/components/AppPicker/index.tsx
@@ -103,7 +103,7 @@ export function AppPicker({ onAppSelected }: Props) {
     )
     const apps = getJsonFromBase64(response.blob) as AppInfo[]
     if (apps == null) {
-      throw new Error(`Received \`null\` response from ${appStoreUrl}`)
+      throw new Error(`Received \`null\` response from ${appListUrl}`)
     }
     apps.sort((a: AppInfo, b: AppInfo) => {
       const dateA = new Date(a.date)