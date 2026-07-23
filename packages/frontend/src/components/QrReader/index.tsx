ed7c5a2c33d28783b91532aba621ecb2c993d743
WofWca <wofwca@protonmail.com>
2026-07-23 14:13:14 +0400

    improvement: QR scanner: prefer environment facing

--- packages/frontend/src/components/QrReader/index.tsx
+++ packages/frontend/src/components/QrReader/index.tsx
@@ -371,6 +371,7 @@ export const QrReader = forwardRef<QrCodeScanRef, Props>(
                     ? { exact: deviceId }
                     : { ideal: deviceId }
                   : undefined,
+                facingMode: { ideal: 'environment' },
               },
               audio: false,
             })
