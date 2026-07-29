364e7c6c0d28a2fed7768d3a26fb6b7ea606f067
Nico de Haen <ndh@deltachat.de>
2026-07-29 09:59:15 +0200

    fix: remove Shift+click handler
    Now we have some blinking text on multi select messages back

--- packages/frontend/src/components/message/Message.tsx
+++ packages/frontend/src/components/message/Message.tsx
@@ -619,7 +619,6 @@ export default function Message(props: {
   const commonAttrs = {
     ref,
     tabIndex: focusAndMultiselect.tabIndex,
-    onMouseDown: focusAndMultiselect.onMouseDown,
     onKeyDown: (e: React.KeyboardEvent) => {
       const messageIds =
         focusAndMultiselect.selectedItems.size === 0
