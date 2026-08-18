edf3e2a954616cae815025c32dcb2010fbc9281f
Nico de Haen <ndh@deltachat.de>
2026-08-18 15:54:07 +0200

    Update flake.nix
    Co-authored-by: Simon Laux <Simon-Laux@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -45,9 +45,6 @@
           openssl
           libayatana-appindicator
           # Video/Audio data playback
-          gst_all_1.gst-plugins-base
-          gst_all_1.gst-plugins-good
-          gst_all_1.gst-plugins-bad
           gst_all_1.gst-libav
           gst_all_1.gst-vaapi
 
