95bb9fbc60ddbf319ac8930ea4beb8912139859b
WofWca <wofwca@protonmail.com>
2026-08-24 17:21:35 +0400

    feat: adding transport: progress bar and "Cancel"
    Closes https://github.com/deltachat/deltachat-desktop/issues/5874.

--- packages/frontend/src/hooks/dialog/useAddTransportDialog.ts
+++ packages/frontend/src/hooks/dialog/useAddTransportDialog.ts
@@ -1,9 +1,10 @@
 import { useCallback } from 'react'
 
-import { BackendRemote } from '../../backend-com'
 import useAlertDialog from './useAlertDialog'
 import useConfirmationDialog from './useConfirmationDialog'
 import useTranslationFunction from '../useTranslationFunction'
+import useDialog from './useDialog'
+import { ConfigureProgressDialog } from '../../components/dialogs/ConfigureProgressDialog'
 
 type AddTransportDialogFn = (
   accountId: number,
@@ -15,12 +16,13 @@ type AddTransportDialogFn = (
 
 export default function useAddTransportDialog(): AddTransportDialogFn {
   const tx = useTranslationFunction()
+  const { openDialog } = useDialog()
   const openAlertDialog = useAlertDialog()
   const openConfirmationDialog = useConfirmationDialog()
 
   return useCallback(
     async (
-      accountId,
+      _accountId,
       transportString,
       domainOrAddress,
       multiDeviceMode,
@@ -37,16 +39,29 @@ export default function useAddTransportDialog(): AddTransportDialogFn {
       if (!confirmed) {
         return false
       }
-      try {
-        await BackendRemote.rpc.addTransportFromQr(accountId, transportString)
-      } catch (e) {
-        openAlertDialog({
-          message: 'Relay could not be added. ' + (e as Error).message,
+      const relayAdded = await new Promise<boolean>(r => {
+        openDialog(ConfigureProgressDialog, {
+          qrCode: transportString,
+          credentials: null,
+          onSuccess() {
+            r(true)
+          },
+          onFail(error) {
+            r(false)
+            openAlertDialog({
+              message: 'Relay could not be added. ' + (error satisfies string),
+            })
+          },
+          onClose() {
+            // The dialog may call `onClose` before `onSuccess` or `onFail`,
+            // so let's ensure to "prefer" those callbacks,
+            // by adding `setTimeout`.
+            setTimeout(() => r(false))
+          },
         })
-        return false
-      }
-      return true
+      })
+      return relayAdded
     },
-    [openAlertDialog, openConfirmationDialog, tx]
+    [openAlertDialog, openConfirmationDialog, openDialog, tx]
   )
 }
