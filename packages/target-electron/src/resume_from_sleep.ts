b04e3778bfd5e4ecbda8a0be1db468ce0e4c3490
WofWca <wofwca@protonmail.com>
2026-08-29 12:25:42 +0400

    fix: check for resume from sleep with setInterval
    Closes https://github.com/deltachat/deltachat-desktop/issues/5915
    and other similar issues.
    This is a method independent of permissions.
    
    I had to add the package directly from GitHub and not NPM
    because the NPM version has a bug where it ignores
    the `threshold` parameter.

--- packages/target-electron/src/resume_from_sleep.ts
+++ packages/target-electron/src/resume_from_sleep.ts
@@ -1,4 +1,5 @@
 import { powerMonitor } from 'electron'
+import SleepTime from 'sleeptime'
 
 import { send } from './windows/main.js'
 
@@ -10,4 +11,18 @@ export function initialisePowerMonitor() {
   powerMonitor.on('resume', onResumeFromSleep)
   powerMonitor.on('unlock-screen', onResumeFromSleep)
   powerMonitor.on('user-did-become-active', onResumeFromSleep)
+
+  // Looks like `powerMonitor.on('resume'` doesn't work
+  // if certain permissions are not given to the app,
+  // so let's have this dumb `setInterval` check.
+  //
+  // It might fire false-positives, but we're generally fine with this,
+  // as `onResumeFromSleep` is only used for `rpc.maybeNetwork()`
+  // and updating timestamps, so it's only about performance basically.
+  new SleepTime(
+    onResumeFromSleep,
+    // `setInterval` period. `onResumeFromSleep` might be delayed
+    // by up to this amount of milliseconds.
+    10 * 1000
+  )
 }
