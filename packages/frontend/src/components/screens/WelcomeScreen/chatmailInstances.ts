6d50a231b13c4c8e303721b37ea96e61e0021125
Nico de Haen <ndh@deltachat.de>
2026-09-16 15:05:35 +0200

    feat: generic policy isntead of domain specific (#6746)

--- packages/frontend/src/components/screens/WelcomeScreen/chatmailInstances.ts
+++ packages/frontend/src/components/screens/WelcomeScreen/chatmailInstances.ts
@@ -6,9 +6,6 @@ export const CHATMAIL_INSTANCES_LIST_URL = 'https://chatmail.at/relays'
 // Hostname of the default chatmail instance
 export const DEFAULT_CHATMAIL_HOSTNAME = 'nine.testrun.org'
 
-// URL to privacy policy of default DeltaChat Chatmail instance
-export const DEFAULT_INSTANCE_PRIVACY_POLICY_URL = `https://${DEFAULT_CHATMAIL_HOSTNAME}/privacy.html`
-
 export function isDefaultInstance(value: string): boolean {
   return value.includes(DEFAULT_CHATMAIL_HOSTNAME)
 }
